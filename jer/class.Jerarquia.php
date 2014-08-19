e98ab7efdd62d652ba229b284670c7014c3bf470
Alex <renderpci@users.noreply.github.com>
2014-08-19 20:37:56 +0200

    Dedalo beta 1

--- jer/class.Jerarquia.php
+++ jer/class.Jerarquia.php
@@ -1,4 +1,28 @@
 <?php
+/************************************************************************
+	
+    Dédalo : Cultural Heritage & Oral History Management Platform
+	
+	Copyright (C) 1998 - 2014  Authors: Juan Francisco Onielfa, Alejandro Peña
+
+    This program is free software: you can redistribute it and/or modify
+    it under the terms of the GNU Affero General Public License as
+    published by the Free Software Foundation, either version 3 of the
+    License, or (at your option) any later version.
+
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU Affero General Public License for more details.
+
+    You should have received a copy of the GNU Affero General Public License
+    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+	
+	http://www.fmomo.org
+	dedalo@fmomo.org
+	
+************************************************************************/
+
 require_once(DEDALO_ROOT . '/jer/class.RecordObj_jer.php');
 require_once(DEDALO_LIB_BASE_PATH . '/db/class.RecordObj_ts.php');
 require_once(DEDALO_LIB_BASE_PATH . '/db/class.RecordObj_descriptors.php');
