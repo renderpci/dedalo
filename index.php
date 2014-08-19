86e1175e85215a74bb6d3dc40078fd58d839ba72
https://github.com/renderpci/dedalo-4.git <paco@Mac-mini-Arriba.local>
2014-08-19 18:23:52 +0200

    Dedalo 4 beta 1
    First pubic beta 1 of New Dedalo 4 version

---
+++ index.php
@@ -0,0 +1,27 @@
+<?php  
+/************************************************************************
+	
+    Dédalo : Intangible Cultural Heritage Management Platform
+	
+	Copyright (C) 2011  Authors: Juan Francisco Onielfa, Alejandro Peña
+
+    This program is free software: you can redistribute it and/or modify
+    it under the terms of the GNU Affero General Public License as
+    published by the Free Software Foundation, either version 3 of the
+    License, or (at your option) any later version.
+
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU Affero General Public License for more details.
+
+    You should have received a copy of the GNU Affero General Public License
+    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+	
+	http://www.fmomo.org
+	dedalo@fmomo.org
+	
+************************************************************************/
+header("HTTP/1.1 301 Moved Permanently"); 
+header("Location: lib/dedalo/main/"); exit();
+?>
