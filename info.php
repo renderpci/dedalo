7f15313cbfab9d97e3901b568755110a60d182d9
Alex <renderpci@users.noreply.github.com>
2014-08-19 21:11:06 +0200

    Beta 1
    First pubic beta 1 of New Dedalo 4 version

--- info.php
+++ info.php
@@ -2,7 +2,7 @@
 
 /************************************************************************
 	
-    Dédalo : Cultural Heritage & Oral history Management Platform
+    Dédalo : Cultural Heritage & Oral History Management Platform
 	
 	Copyright (C) 1998 - 2014  Authors: Juan Francisco Onielfa, Alejandro Peña
 
