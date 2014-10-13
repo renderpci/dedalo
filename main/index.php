edb961ea918da67accac0c7e67305171edb0e568
renderpci <renderpci@users.noreply.github.com>
2014-10-13 16:36:07 +0200

    change to beta 2

--- main/index.php
+++ main/index.php
@@ -1,4 +1,28 @@
 <?php
+/************************************************************************
+	
+    Dédalo : Cultural Heritage & Oral History Management Platform
+	Version: Beta 1-2
+	Copyright (C) 1998 - 2014  
+    Authors: Juan Francisco Onielfa, Alejandro Peña Carbonell
+
+    This program is free software: you can redistribute it and/or modify
+    it under the terms of the GNU Affero General Public License as
+    published by the Free Software Foundation, either version 3 of the
+    License, or (at your option) any later version.
+
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU Affero General Public License for more details.
+
+    You should have received a copy of the GNU Affero General Public License
+    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+	
+	http://www.fmomo.org
+	dedalo@fmomo.org
+	
+************************************************************************/
 # REDIRECT TO DEDALO4/MAIN 
 header("HTTP/1.1 301 Moved Permanently"); 
 header("Location: ../lib/dedalo/main/"); exit();
