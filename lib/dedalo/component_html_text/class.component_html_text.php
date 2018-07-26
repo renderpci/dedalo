cf4f6f1aa01eb4b575f21a8d9540d3a42cf1796c
renderpci <alex@killer-tomatoes.local>
2018-07-26 20:04:25 +0200

    Add support to JSON_QUERY_OBJECT

--- lib/dedalo/component_html_text/class.component_html_text.php
+++ lib/dedalo/component_html_text/class.component_html_text.php
@@ -13,6 +13,8 @@ class component_html_text extends component_common {
 		return (string)$dato;
 	}
 
+
+
 	# SET_DATO
 	public function set_dato($dato) {			
 		if ($dato==='""') {
@@ -21,6 +23,8 @@ class component_html_text extends component_common {
 
 		parent::set_dato( (string)$dato );
 	}
+
+
 
 	/**
 	* SAVE OVERRIDE
@@ -42,6 +46,8 @@ class component_html_text extends component_common {
 		# A partir de aquí, salvamos de forma estándar
 		return parent::Save();
 	}
+
+
 
 	# OVERRIDE COMPONENT_COMMON METHOD
 	public function get_ar_tools_obj() {		
@@ -88,6 +94,7 @@ class component_html_text extends component_common {
 	}//end get_valor
 
 
+
 	/**
 	* CLEAN_TEXT
 	* Anclaje para futuros preprocesados del texto. De momento sólo haremos un trim
@@ -100,6 +107,153 @@ class component_html_text extends component_common {
 
 		return trim($string);
 	}//end clean_text
+
+
+
+	/**
+	* RESOLVE_QUERY_OBJECT_SQL
+	* @return object $query_object
+	*/
+	public static function resolve_query_object_sql($query_object) {
+		
+    	# Always set fixed values
+		$query_object->type = 'string';
+
+		$q = $query_object->q;
+		$q = pg_escape_string(stripslashes($q));
+
+        switch (true) {
+        	# IS NULL
+			case ($q==='!*'):
+				$operator = 'IS NULL';
+				$q_clean  = '';
+				$query_object->operator = $operator;
+    			$query_object->q_parsed	= $q_clean;
+    			$query_object->unaccent = false;
+
+				$clone = clone($query_object);
+	    			$clone->operator = '~*';
+	    			$clone->q_parsed = '\'.*""\'';
+
+				$logical_operator = '$or';
+    			$new_query_json = new stdClass;    			
+	    			$new_query_json->$logical_operator = [$query_object, $clone];
+    			# override
+    			$query_object = $new_query_json ;
+
+				break;
+			# IS NOT NULL
+			case ($q==='*'):
+				$operator = 'IS NOT NULL';
+				$q_clean  = '';
+				$query_object->operator = $operator;
+    			$query_object->q_parsed	= $q_clean;
+    			$query_object->unaccent = false;
+
+				$clone = clone($query_object);
+	    			//$clone->operator = '!=';
+	    			$clone->operator = '!~';
+	    			$clone->q_parsed = '\'.*""\'';
+
+
+				$logical_operator ='$and';
+    			$new_query_json = new stdClass;    			
+    				$new_query_json->$logical_operator = [$query_object, $clone];    
+
+				# override
+    			$query_object = $new_query_json ;
+				break;
+			# IS DIFFERENT			
+			case (strpos($q, '!=')===0):
+				$operator = '!=';
+				$q_clean  = str_replace($operator, '', $q);
+				$query_object->operator = '!~';
+    			$query_object->q_parsed	= '\'.*"'.$q_clean.'".*\'';
+    			$query_object->unaccent = false;
+				break;
+			# IS SIMILAR
+			case (strpos($q, '=')===0):
+				$operator = '=';
+				$q_clean  = str_replace($operator, '', $q);
+				$query_object->operator = '~';
+    			$query_object->q_parsed	= '\'.*"'.$q_clean.'".*\'';
+    			$query_object->unaccent = true;
+				break;
+			# NOT CONTAIN
+			case (strpos($q, '-')===0):
+				$operator = '!~*';
+				$q_clean  = str_replace('-', '', $q);
+				$query_object->operator = $operator;
+    			$query_object->q_parsed	= '\'.*'.$q_clean.'.*\'';
+    			$query_object->unaccent = true;
+				break;	
+			# CONTAIN				
+			case (substr($q, 0, 1)==='*' && substr($q, -1)==='*'):
+				$operator = '~*';
+				$q_clean  = str_replace('*', '', $q);
+				$query_object->operator = $operator;
+    			$query_object->q_parsed	= '\'.*".*'.$q_clean.'.*\'';
+    			$query_object->unaccent = true;
+				break;
+			# ENDS WITH
+			case (substr($q, 0, 1)==='*'):
+				$operator = '~*';
+				$q_clean  = str_replace('*', '', $q);
+				$query_object->operator = $operator;
+    			$query_object->q_parsed	= '\'.*".*'.$q_clean.'".*\'';
+    			$query_object->unaccent = true;
+				break;
+			# BEGINS WITH
+			case (substr($q, -1)==='*'):
+				$operator = '~*';
+				$q_clean  = str_replace('*', '', $q);
+				$query_object->operator = $operator;
+    			$query_object->q_parsed	= '\'.*"'.$q_clean.'.*\'';
+    			$query_object->unaccent = true;
+				break;
+			# LITERAL
+			case (substr($q, 0, 1)==='\"' && substr($q, -1)==='\"'):
+				$operator = '~';
+				$q_clean  = str_replace('\"', '', $q);
+				$query_object->operator = $operator;
+				$query_object->q_parsed	= '\'.*"'.$q_clean.'".*\'';
+				$query_object->unaccent = false;
+				break;
+			# CONTAIN
+			default:
+				$operator = '~*';
+				$q_clean  = $q;
+				$query_object->operator = $operator;
+    			$query_object->q_parsed	= '\'.*".*'.$q_clean.'.*\'';
+    			$query_object->unaccent = true;
+				break;			
+		}//end switch (true) {		
+       
+
+        return $query_object;
+	}//end resolve_query_object_sql
+
+	/**
+	* SEARCH_OPERATORS_INFO
+	* Return valid operators for search in current component
+	* @return array $ar_operators
+	*/
+	public function search_operators_info() {
+		
+		$ar_operators = [
+			'*' 	 => 'no_vacio', // not null
+			'!*' 	 => 'campo_vacio', // null	
+			'=' 	 => 'similar_a',
+			'!=' 	 => 'distinto_de',
+			'-' 	 => 'no_contiene',
+			'*text*' => 'contiene',
+			'text*'  => 'empieza_con',
+			'*text'  => 'acaba_con',
+			'"text"' => 'literal',
+		];
+
+		return $ar_operators;
+	}//end search_operators_info
 
 
 	
