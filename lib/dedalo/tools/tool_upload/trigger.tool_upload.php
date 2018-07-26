23e4e36c22c52f3981f192fb41f96b12d49879d1
renderpci <alex@killer-tomatoes.local>
2018-07-26 19:50:42 +0200

    Security update; enforce XSS

--- lib/dedalo/tools/tool_upload/trigger.tool_upload.php
+++ lib/dedalo/tools/tool_upload/trigger.tool_upload.php
@@ -11,6 +11,8 @@ if(login::is_logged()!==true) {
 	die();
 }
 
+#ini_set('max_input_time', 0); // Unlimited time 
+#ini_set('max_execution_time', 60 * 5); // Unlimited time 
 set_time_limit(0);
 
 
@@ -29,9 +31,8 @@ $vars = array('mode','SID','quality','tipo','parent','section_tipo');
 
 
 # UPLOAD FILE GENÉRICO
-if( $mode=='upload_file' ) {
+if( $mode==='upload_file' ) {
 	
-	#dump($_REQUEST, '$_REQUEST');die();
 	#trigger_error("Necesita parent!!! desde la llamada js");
 
 	if(!$SID) 			exit('Error SID not defined (2)');
@@ -43,12 +44,12 @@ if( $mode=='upload_file' ) {
 	# Write session to unlock session file
 	session_write_close();
 
-	debug_log(__METHOD__." Uploading file ($SID). Session is writeed and closed ".to_string(), logger::DEBUG);
+	debug_log(__METHOD__." Uploading file ($SID) - Session is writeed and closed ".to_string(), logger::DEBUG);
 
 	$component_name = RecordObj_dd::get_modelo_name_by_tipo($tipo, true);
 	$component_obj 	= component_common::get_instance($component_name, $tipo, $parent, 'edit', DEDALO_DATA_LANG, $section_tipo);
 	$tool_upload 	= new tool_upload($component_obj);
-		#dump($tool_upload,'$tool_upload');
+	
 
 	$response = $tool_upload->upload_file( $quality );
 		#dump($response, ' response ++ '.to_string());
