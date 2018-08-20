df5e5f97b01da3259176384e1f813288f51addf7
Juan Francisco Onielfa Veneros <paco@render.es>
2018-08-20 23:02:22 +0200

    Add safe_dato to manage possible empty locators

--- lib/dedalo/component_relation_common/class.component_relation_common.php
+++ lib/dedalo/component_relation_common/class.component_relation_common.php
@@ -135,6 +135,7 @@ class component_relation_common extends component_common {
 	}//end get_dato_with_references
 
 
+
 	/**
 	* SET_DATO
 	* Set raw dato overwrite existing dato.
@@ -142,6 +143,8 @@ class component_relation_common extends component_common {
 	* insert complete array of locators at once. Use this method in this cases
 	*/
 	public function set_dato($dato) {
+
+		$safe_dato = [];
 
 		if (!empty($dato)) {
 
@@ -158,6 +161,7 @@ class component_relation_common extends component_common {
 			# Verify all locators are well formed
 			$relation_type 		 = $this->relation_type;
 			$from_component_tipo = $this->tipo;
+			
 			foreach ((array)$dato as $key => $current_locator) {
 
 				if (!is_object($current_locator)) {
@@ -166,11 +170,18 @@ class component_relation_common extends component_common {
 					debug_log( __METHOD__ . $msg, logger::ERROR);
 					throw new Exception("Error Processing Request. Look server log for details", 1);			
 				}
+
+				// section_id
+				if (!isset($current_locator->section_id) || !isset($current_locator->section_tipo)) {
+					debug_log(__METHOD__." IGNORED bad formed locator (empty section_id or section_tipo) [$this->section_tipo, $this->parent, $this->tipo] ". get_called_class().' - $current_locator: '.to_string($current_locator), logger::ERROR);
+					#throw new Exception("Error Processing Request. Look server log for details", 1);
+					continue;
+				}
 				
 				// type
 				if (!isset($current_locator->type)) {
+					debug_log(__METHOD__." Fixing bad formed locator (empty type) [$this->section_tipo, $this->parent, $this->tipo] ". get_called_class().' - $current_locator: '.to_string($current_locator), logger::WARNING);
 					$current_locator->type = $relation_type;
-					debug_log(__METHOD__." Fixed bad formed locator (empty type) [$this->section_tipo, $this->parent, $this->tipo] ". get_called_class().' '.to_string(), logger::WARNING);
 				//}else if ($current_locator->type!==$relation_type) {
 					//debug_log(__METHOD__." Fixed bad formed locator (bad type $current_locator->type to $relation_type) [$this->section_tipo, $this->parent, $this->tipo] ".to_string(), logger::WARNING);
 					//$current_locator->type = $relation_type;					
@@ -183,10 +194,13 @@ class component_relation_common extends component_common {
 					debug_log(__METHOD__." Fixed bad formed locator (bad from_component_tipo $current_locator->from_component_tipo) [$this->section_tipo, $this->parent, $from_component_tipo] ".get_called_class().' '.to_string(), logger::WARNING);
 					$current_locator->from_component_tipo = $from_component_tipo;
 				}
+
+				# Add
+				$safe_dato[] = $current_locator;
 			}
 		}
 
-		parent::set_dato( (array)$dato );	
+		parent::set_dato( (array)$safe_dato );
 	}//end set_dato
 
 
