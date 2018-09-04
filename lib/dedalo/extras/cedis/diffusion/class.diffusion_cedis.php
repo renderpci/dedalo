15cad10389c53697feb0237477cafec8b243232b
Juan Francisco Onielfa Veneros <paco@render.es>
2018-09-04 12:29:11 +0200

    Added calculate_date_tape function to pass tape Date of capture to interview Date of capture

--- lib/dedalo/extras/cedis/diffusion/class.diffusion_cedis.php
+++ lib/dedalo/extras/cedis/diffusion/class.diffusion_cedis.php
@@ -189,6 +189,59 @@ class diffusion_cedis {
 
 
 	/**
+	* CALCULATE_DATE_TAPE
+	* Returns last date of audiovisual rsc44 (Date of capture) inside portal (interview)
+	* @return 
+	*/
+	public static function calculate_date_tape( $options, $dato, $format='d.m.Y' ) {
+
+		$date = null;
+
+		$source_tipo = reset($options->propiedades->data_source); // Like 'rsc44'
+		$modelo_name = RecordObj_dd::get_modelo_name_by_tipo($source_tipo,true);
+		$lang 		 = $options->lang;
+
+		$ar_dates = array();
+		foreach ((array)$dato as $key => $locator) {
+			#$section = section::get_instance($locator->section_id, $locator->section_tipo);
+			#$date 	 = $section->get_modified_date();
+
+			$parent 		= $locator->section_id;
+			$section_tipo 	= $locator->section_tipo;
+
+			$component 		= component_common::get_instance($modelo_name,
+															 $source_tipo,
+															 $parent,
+															 'list',
+															 $lang,
+															 $section_tipo);
+			$dato = (array)$component->get_dato();
+			if (empty($dato)) {
+				continue;
+			}
+
+			$dd_date = new dd_date($dato[0]->start);
+			$date 	 = $dd_date->get_dd_timestamp("Y-m-d H:i:s");
+
+			$date = dd_date::get_date_with_format($date, $format="Y-m-d H:i:s");
+			$ar_dates[] = $date;
+		}
+
+		if (!empty($ar_dates)) {
+			$max  = max(array_map('strtotime', $ar_dates));
+			$last = date('Y-m-d H:i:s', $max); // Like 2012-06-11 08:30:49
+
+			// Format: string dd.mm.yyyy
+			$date = date($format, strtotime($last));
+		}
+		
+
+		return $date;
+	}//end calculate_date_tape
+
+
+
+	/**
 	* CALCULATE_IMAGE_NAME
 	* Returns first image indentify file name
 	* @return 
