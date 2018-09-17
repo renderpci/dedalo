d018f3fd305c46f9ece50e35f7ac3a4c2f35ba0e
Juan Francisco Onielfa Veneros <paco@render.es>
2018-09-17 11:15:29 +0200

    Fixed issue when service_autocomplete search contains parenthesis or commas

--- lib/dedalo/search/class.search_development2.php
+++ lib/dedalo/search/class.search_development2.php
@@ -1497,12 +1497,15 @@ class search_development2 {
 				}
 
 				# q
-				$sql_where .= $search_object->q_parsed;
+				// Escape parenthesis
+				$q_parsed_clean = str_replace(['(',')'], ['\(','\)'], $search_object->q_parsed);
+				$sql_where .= $q_parsed_clean;
 				#$sql_where .= pg_escape_string(stripslashes($search_object->q_parsed));
 
 				if($search_object_unaccent===true) {
 					$sql_where .= ')';
 				}
+				debug_log(__METHOD__." sql_where ".to_string($sql_where), logger::DEBUG);
 				break;
 
 			case 'array_elements':
