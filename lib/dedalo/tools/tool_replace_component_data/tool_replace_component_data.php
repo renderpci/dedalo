7941d6891b3bcc933033e994d0ae2d31589fb8e8
Juan Francisco Onielfa Veneros <paco@render.es>
2018-09-30 09:47:02 +0200

    Added catch error when empty($this->search_options->search_query_object) context

--- lib/dedalo/tools/tool_replace_component_data/tool_replace_component_data.php
+++ lib/dedalo/tools/tool_replace_component_data/tool_replace_component_data.php
@@ -9,32 +9,33 @@
 	$label 					= $this->component_obj->get_label();
 	$traducible 			= $this->component_obj->get_traducible();	
 	$tool_name 				= get_class($this);
-	$modo 					= $this->get_modo();
-	$valor 					=  $this->component_obj->get_valor();
+	$modo 					= $this->get_modo();	
 	$file_name 				= $modo;
-
 
 	# TOOL CSS / JS MAIN FILES
 	css::$ar_url[] = DEDALO_LIB_BASE_URL."/tools/".$tool_name."/css/".$tool_name.".css";
 	js::$ar_url[]  = DEDALO_LIB_BASE_URL."/tools/".$tool_name."/js/".$tool_name.".js";
 
-	switch($modo) {
-		
+	# Case search options from search (list) are not available (session is closed)
+	if (empty($this->search_options->search_query_object)) {
+		echo "<div class=\"error\">Invalid list values. Please, go to list mode and select the records that you can change before replace.</div>";
+		return;
+	}
+
+	switch($modo) {		
 	
 		case 'button':
 					
-					$line_lenght = 90;	// Default value is 90
-
-					break;
+			$line_lenght = 90;	// Default value is 90
+			break;
 		
 		case 'page':
-					$total_records = (int)$this->search_options->search_query_object->full_count;
 
-					#$this->propagate_data();	
-					break;
+			$valor 			= $this->component_obj->get_valor();
+			$total_records 	= (int)$this->search_options->search_query_object->full_count;
+			break;
 		
-	}#end switch
-		
+	}//end switch		
 
 
 
@@ -42,5 +43,4 @@
 	$page_html	= DEDALO_LIB_BASE_PATH . '/tools/' . get_class($this).  '/html/' . get_class($this) . '_' . $file_name .'.phtml';
 	if( !include($page_html) ) {
 		echo "<div class=\"error\">Invalid mode $this->modo</div>";
-	}	
-?>
+	}
