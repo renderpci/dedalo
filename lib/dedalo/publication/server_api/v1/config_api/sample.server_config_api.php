87f78e7777655ad4dfbee6ad54c668698511aed7
Juan Francisco Onielfa Veneros <paco@render.es>
2018-11-07 10:05:33 +0100

    Update server_config_api file

--- lib/dedalo/publication/server_api/v1/config_api/sample.server_config_api.php
+++ lib/dedalo/publication/server_api/v1/config_api/sample.server_config_api.php
@@ -1,8 +1,21 @@
 <?php
-include dirname(dirname(dirname(dirname(__FILE__)))) .'/config/config4.php';
+include dirname(dirname(dirname(dirname(dirname(__FILE__))))) .'/config/config4.php';
+
 
 ################################################################
 # CONSTANTS
+
+	// API_WEB_USER_CODE . Verification user code (must be identical in config of client and server)
+	if(!defined('API_WEB_USER_CODE'))
+	define('API_WEB_USER_CODE' 			, $API_WEB_USER_CODE);
+			if (empty($code)) {
+				echo json_encode("Sorry. Empty user code");
+				die();
+			}
+			if ($code!==API_WEB_USER_CODE) {
+				echo json_encode("Sorry. Invalid user code");
+				die();
+			}
 	
 	// WEB_CURRENT_LANG_CODE . Current lang default. If request get 'lang' overwrite value
 	define('WEB_CURRENT_LANG_CODE' 		, !empty($_REQUEST['lang']) ? $_REQUEST['lang'] : 'lg-eng');	
@@ -12,13 +25,6 @@ include dirname(dirname(dirname(dirname(__FILE__)))) .'/config/config4.php';
 
 	// WEB_VIDEO_PATH
 	define('WEB_VIDEO_BASE_URL' 		, DEDALO_MEDIA_BASE_URL .'/'. DEDALO_AV_FOLDER .'/'. DEDALO_AV_QUALITY_DEFAULT );
-
-	// API_WEB_USER_CODE . Verification user code (must be identical in config of client and server)
-	define('API_WEB_USER_CODE' 			, 'aR17empa92sd546asBh2');
-	if (!isset($_REQUEST['code']) || $_REQUEST['code']!==API_WEB_USER_CODE) {
-		echo json_encode("Sorry. Invalid user code");
-		die(); 
-	}
 
 
 
@@ -38,30 +44,30 @@ include dirname(dirname(dirname(dirname(__FILE__)))) .'/config/config4.php';
 	define('PUBLICACION_FILTER_SQL' , " ");	
 
 	# Table names
-	define('TABLE_INTERVIEW'		 , 'interview');
-	define('TABLE_AUDIOVISUAL'		 , 'audiovisual');
-	define('TABLE_IMAGE'			 , 'image');
-	define('TABLE_INFORMANT'		 , 'informant');	
-	define('TABLE_THESAURUS'		 , 'themes');
-	define('TRANSCRIPTION_TIPO'		 , 'rsc36');
-	define('AV_TIPO'				 , 'rsc35');
-	define('AUDIOVISUAL_SECTION_TIPO', 'rsc167');
+		define('TABLE_INTERVIEW'		 , 'interview');
+		define('TABLE_AUDIOVISUAL'		 , 'audiovisual');
+		define('TABLE_IMAGE'			 , 'image');
+		define('TABLE_INFORMANT'		 , 'informant');	
+		define('TABLE_THESAURUS'		 , 'themes');
+		define('TRANSCRIPTION_TIPO'		 , 'rsc36');
+		define('AV_TIPO'				 , 'rsc35');
+		define('AUDIOVISUAL_SECTION_TIPO', 'rsc167');
 
 	# Fields
-	define('FIELD_TRANSCRIPTION'	, TRANSCRIPTION_TIPO);
-	define('FIELD_TERM'				, 'term');
-	define('FIELD_TERM_ID'			, 'terminoID');
-	define('FIELD_SUMMARY'			, 'abstract');
-	define('FIELD_INTERVIEW_DATE'	, 'date');	
-	define('FIELD_INFORMANT'		, 'informant');
-	define('FIELD_IMAGE'			, 'image');
-	define('FIELD_NAME'				, 'name');
-	define('FIELD_SURNAME'			, 'surname');
-	define('FIELD_BIRTHDATE'		, 'birthdate');
-	define('FIELD_BIRTHPLACE'		, 'birthplace');
-	define('FIELD_VIDEO'			, 'video');
-	define('FIELD_CODE'				, 'code');	
-	define('FIELD_NORDER'			, 'norder');
+		define('FIELD_TRANSCRIPTION'	, TRANSCRIPTION_TIPO);
+		define('FIELD_TERM'				, 'term');
+		define('FIELD_TERM_ID'			, 'terminoID');
+		define('FIELD_SUMMARY'			, 'abstract');
+		define('FIELD_INTERVIEW_DATE'	, 'date');	
+		define('FIELD_INFORMANT'		, 'informant');
+		define('FIELD_IMAGE'			, 'image');
+		define('FIELD_NAME'				, 'name');
+		define('FIELD_SURNAME'			, 'surname');
+		define('FIELD_BIRTHDATE'		, 'birthdate');
+		define('FIELD_BIRTHPLACE'		, 'birthplace');
+		define('FIELD_VIDEO'			, 'video');
+		define('FIELD_CODE'				, 'code');	
+		define('FIELD_NORDER'			, 'norder');
 
 
 
@@ -71,4 +77,10 @@ include dirname(dirname(dirname(dirname(__FILE__)))) .'/config/config4.php';
 
 
 
-?>
+################################################################
+# Access-Control-Allow-Origin
+# Specifies a URI that may access the resource. You may specify one or more origins, separated by commas.
+# Use * for requests without credentials,
+	#define('ACCESS_CONTROL_ALLOW_ORIGIN', '*');
+
+
