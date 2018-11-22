64f1d4eeaded842cf189be4b373ad66e12557137
Juan Francisco Onielfa Veneros <paco@render.es>
2018-11-22 10:22:31 +0100

    Added custom get_diffusion_sections_from_diffusion_element method

--- lib/dedalo/diffusion/class.diffusion_socrata.php
+++ lib/dedalo/diffusion/class.diffusion_socrata.php
@@ -195,5 +195,48 @@ class diffusion_socrata extends diffusion  {
 
 
 
+	/**
+	* GET_DIFFUSION_SECTIONS_FROM_DIFFUSION_ELEMENT
+	* @return array $ar_diffusion_sections
+	*/
+	public static function get_diffusion_sections_from_diffusion_element($diffusion_element_tipo) {
+		
+		$ar_diffusion_sections = array();
+
+		# tables. RecordObj_dd::get_ar_terminoID_by_modelo_name_and_relation($diffusion_element_tipo, $modelo_name='table', $relation_type='children_recursive', $search_exact=false);
+		$tables = RecordObj_dd::get_ar_terminoID_by_modelo_name_and_relation($diffusion_element_tipo, 'table', 'children_recursive', false);
+		foreach ($tables as $current_table_tipo) {
+
+			$modelo_name = RecordObj_dd::get_modelo_name_by_tipo($current_table_tipo,true);
+			switch ($modelo_name) {
+				case 'table_alias':
+					# First try section (thesaurus needed)
+					$ar_related = common::get_ar_related_by_model('section', $current_table_tipo);
+					if (!isset($ar_related[0])) {
+						# If not, We search 'table' now
+						$ar_table = common::get_ar_related_by_model('table', $current_table_tipo);
+						if (isset($ar_table[0])) {
+							$ar_related = common::get_ar_related_by_model('section', $ar_table[0]);
+						}
+					}
+					break;
+				
+				case 'table':
+				default:
+					# Pointer to section
+					$ar_related = common::get_ar_related_by_model('section', $current_table_tipo);
+					break;
+			}
+		
+			if (isset($ar_related[0])) {
+				$ar_diffusion_sections[] = $ar_related[0];
+			}
+		}
+
+		return $ar_diffusion_sections;
+	}//end get_diffusion_sections_from_diffusion_element
+
+
+
 }
 ?>
