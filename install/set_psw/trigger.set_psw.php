b8013ab6cdd43e34becde03cece0040a34aaa374
Juan Francisco Onielfa Veneros <paco@render.es>
2019-01-17 17:24:49 +0100

    Fixed loop when Dedalo is already configured but root password is empty

--- install/set_psw/trigger.set_psw.php
+++ install/set_psw/trigger.set_psw.php
@@ -2,39 +2,39 @@
 $TOP_TIPO=false;
 require_once( dirname(dirname(dirname(__FILE__))) .'/lib/dedalo/config/config4.php');
 
-#if(login::is_logged()!==true) die("<span class='error'> Auth error: please login </span>");
-
 # set vars
 	$vars = array('mode','username','password','reference');
 		foreach($vars as $name) $$name = common::setVar($name);
-	
 
 # mode
 if(empty($mode)) exit("<span class='error'> Trigger: Error Need mode..</span>");
 
 	
-
+/**
+* SET_PSW
+*/
 if($mode==='set_psw') {
 
 	if (empty($username) || empty($password) || empty($reference) ) {
 		exit("Error: few vars");
 	}
 
-	if ( $reference!='rfC'.$password ) {
+	if ( $reference!=='rfC'.$password ) {
 		exit("Error: bad reference");
 	}
 
 	$password_encripted = dedalo_encrypt_openssl($password);
 
-	# Test encrypt and decript data cycle
-	if (dedalo_decrypt_openssl($password_encripted) != $password) {
-		return "Error: sorry an error ocurred on UPDATE record. Encrytp and decrypt cylce is wrong!";
-	}	
+	// Test encrypt and decrypt data cycle
+		if (dedalo_decrypt_openssl($password_encripted) !== $password) {
+			echo "Error: sorry an error ocurred on UPDATE record. Encrytp and decrypt cylce is wrong!";
+			exit();
+		}	
 	
 	$section = section::get_instance( -1, DEDALO_SECTION_USERS_TIPO );
-	$dato = $section->get_dato();
-	$tipo = DEDALO_USER_PASSWORD_TIPO;
-	$lang = DEDALO_DATA_NOLAN;	
+	$dato 	 = $section->get_dato();
+	$tipo 	 = DEDALO_USER_PASSWORD_TIPO;
+	$lang 	 = DEDALO_DATA_NOLAN;	
 
 	# Set dato 
 	$dato->components->$tipo->dato->$lang  = $password_encripted;
@@ -48,19 +48,10 @@ if($mode==='set_psw') {
 		if(SHOW_DEBUG) {			
 			throw new Exception("Error Processing Save Update Request ". pg_last_error(), 1);
 		}
-		return "Error: sorry an error ocurred on UPDATE record. Data is not saved";
+		echo "Error: sorry an error ocurred on UPDATE record. Data is not saved";
+		exit();
 	}	
 	
 	unset($_SESSION['dedalo4']['auth']);
-	
-	$contents = file_get_contents(__FILE__);
-	file_put_contents(__FILE__,
-	    "<?php # Remove this line and the next line to re-configure the application
-	    die('Error: Sorry, the application has already been configured.'); 
-	    ?>\n" . $contents
-	);
-
-	#exit();
 
 } #if($mode=='set_psw')
-?>
