96f9f4b75ea9125d5a2515460cc1e1df5cef7e1c
Juan Francisco Onielfa Veneros <paco@render.es>
2019-03-26 11:03:32 +0100

    Fixed error on tool_image_versions when regular image_id is override by external image_id

--- lib/dedalo/tools/tool_image_versions/tool_image_versions.php
+++ lib/dedalo/tools/tool_image_versions/tool_image_versions.php
@@ -17,8 +17,9 @@
 	$initial_media_path 	= $this->component_obj->get_initial_media_path();
 	$external_source 		= $this->component_obj->get_external_source();
 	$modo 					= $this->get_modo();
-	$file_name 				= $modo;	
+	$file_name 				= $modo;
 
+	
 	# TOOL CSS / JS MAIN FILES
 	css::$ar_url[] = DEDALO_LIB_BASE_URL."/tools/".$tool_name."/css/".$tool_name.".css";
 	js::$ar_url[]  = DEDALO_LIB_BASE_URL."/tools/".$tool_name."/js/".$tool_name.".js";
