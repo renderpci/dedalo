1b83205ddea3c71fb422ffe184e070a823f0eb81
Juan Francisco Onielfa Veneros <paco@render.es>
2019-05-10 10:02:08 +0200

    Fix undefined component_iri 'id_wrapper' and 'input_name' in search mode

--- lib/dedalo/component_iri/component_iri.php
+++ lib/dedalo/component_iri/component_iri.php
@@ -118,7 +118,10 @@
 		case 'search':
 				# dato is injected by trigger search wen is needed
 				$dato 	= $this->get_dato();
-				$valor 	= $this->get_valor();				
+				$valor 	= $this->get_valor();
+
+				$id_wrapper 	= 'wrapper_'.$identificador_unico;
+				$input_name 	= $tipo.'_'.$parent;
 
 				# Search input name (var search_input_name is injected in search -> records_search_list.phtml)
 				# and recovered in component_common->get_search_input_name()
@@ -137,4 +140,5 @@
 	if( !include($page_html) ) {
 		echo "<div class=\"error\">Invalid mode $this->modo</div>";
 	}
-?>
+
+
