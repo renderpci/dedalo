8e9947374feb0132318e9121f61ce098b645013c
Juan Francisco Onielfa Veneros <paco@render.es>
2019-06-04 19:13:08 +0200

    Added page controller

--- lib/dedalo/page/page.php
+++ lib/dedalo/page/page.php
@@ -2,6 +2,13 @@
 
 	# PAGE CONTROLLER
 
+	$page_options = new StdClass();
+		$page_options->model 		= 'section';
+		$page_options->section_tipo = $_REQUEST['tipo'] ?? $_REQUEST['t'] ?? false;
+		$page_options->section_id 	= $_REQUEST['id'] ?? false;
+		$page_options->mode 		= $_REQUEST['mode'] ?? 'list';
+		$page_options->lang 		= $_REQUEST['lang'] ?? DEDALO_DATA_LANG;
+
 	$page_options_json = json_encode($page_options);
 
 	$page_html	= dirname(__FILE__) . '/html/page.phtml';	
