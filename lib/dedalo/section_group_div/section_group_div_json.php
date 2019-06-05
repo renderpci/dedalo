bdada2d009692157043ef129b872d504ec46db42
Alejandro Peña Carbonell <alex@render.es>
2019-06-05 16:14:33 +0200

    Add permisions to section_group_div_json

--- lib/dedalo/section_group_div/section_group_div_json.php
+++ lib/dedalo/section_group_div/section_group_div_json.php
@@ -1,11 +1,18 @@
 <?php
 // JSON data for section_group_div controller
 
+
+// configuration vars
+	// section_group_div don't has permisions, it's only a <div> element container for create group boxes in the dom
+	// it used only for order and possition the components inside it
+	$permissions		= 1;
+
+
 // context
 	$context = [];
 
 		// Element structure context (tipo, relations, properties, etc.)
-			$context[] = $this->get_structure_context();
+			$context[] = $this->get_structure_context($permissions);
 
 // data
 	$data = [];
