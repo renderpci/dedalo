d7e66738b24b6da37f6a8645d48734b9fb60e058
Lourdes Veros Álvarez <lur@render.es>
2019-06-13 11:32:26 +0200

    Added complete value list load to component_select_json

--- lib/dedalo/component_select/component_select_json.php
+++ lib/dedalo/component_select/component_select_json.php
@@ -1,13 +1,9 @@
 <?php
 // JSON data component controller
 
-
-
 // component configuration vars
 	$permissions		= $this->get_component_permissions();
 	$modo				= $this->get_modo();
-
-
 
 // context
 	$context = [];
@@ -19,10 +15,9 @@
 
 	}//end if($options->get_context===true)
 
-
-
 // data
 	$data = [];
+	$item_values = [];
 
 	if($options->get_data===true && $permissions>0){
 			
@@ -33,38 +28,51 @@
 					# Working here !
 					break;
 				case 'list':
-					$dato  = $this->get_dato();
-					if (empty($dato)) {
-						
-						$item_values = [];
-					
-					}else{
 
-						$value = reset($dato);
-						$label = $this->get_valor();
+					$ar_list_of_values	= $this->get_ar_list_of_values2();
+					$dato 				= $this->get_dato();
+					$tipo 				= $this->get_tipo();
 
-						$item_value = new stdClass();
-							$item_value->value 		= $value;
-							$item_value->label 		= $label;
-							$item_value->selected 	= true;
-						$item_values = [$item_value];
+					foreach ($ar_list_of_values->result as $key => $item) {
+
+						$label   = (string)$item->label;
+						$value = (object)clone $item->value;
+
+						if (!property_exists($value, 'type')) {
+							$value->type = DEDALO_RELATION_TYPE_LINK;
+						}
+						if (!property_exists($value, 'from_component_tipo')) {
+							$value->from_component_tipo = $tipo;
+						}
+
+						if (in_array($value, $dato)) {	# dato is array always
+							$selected = true;
+						}else{
+							$selected = false;
+						}
+
+						$item_value = new stdClass();			
+							$item_value->value 			= $value;
+							$item_value->label 			= $label;
+							$item_value->selected 		= $selected;
+
+						$item_values[]= $item_value;
 					}
+
 					break;
 			}
 
 		// item
-			$item = new stdClass();
-				$item->section_id 			= $this->get_section_id();
-				$item->tipo 				= $this->get_tipo();
-				$item->from_component_tipo 	= isset($this->from_component_tipo) ? $this->from_component_tipo : $item->tipo;
-				$item->section_tipo 		= $this->get_section_tipo();
-				$item->value 				= $item_values;
+		$item = new stdClass();
+			$item->section_id 			= $this->get_section_id();
+			$item->tipo 				= $this->get_tipo();
+			$item->from_component_tipo 	= isset($this->from_component_tipo) ? $this->from_component_tipo : $item->tipo;
+			$item->section_tipo 		= $this->get_section_tipo();
+			$item->value 				= $item_values;
 
-			$data[] = $item;
+		$data[] = $item;
 
 	}//end if($options->get_data===true && $permissions>0)
-
-
 	
 // JSON string
 	return common::build_element_json_output($context, $data);
