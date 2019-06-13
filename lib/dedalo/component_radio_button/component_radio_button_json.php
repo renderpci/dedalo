a9fd6d943f775b12e35358b1ae97c2a7e7bdd1bb
Lourdes Veros Álvarez <lur@render.es>
2019-06-13 11:38:37 +0200

    Appliying homogeneus structure to code and variables and add new test cases for component_select_json.

--- lib/dedalo/component_radio_button/component_radio_button_json.php
+++ lib/dedalo/component_radio_button/component_radio_button_json.php
@@ -17,60 +17,62 @@
 
 // data
 	$data = [];
-	$value_data = [];
+	$item_values = [];
 
 	if($options->get_data===true && $permissions>0){
+			
+		// item_values
+		$modo = $this->get_modo();
+		switch ($modo) {
+			case 'edit':
+				# Working here !
+				break;
+			case 'list':
 
-			$modo = $this->get_modo();
-			switch ($modo) {
-				case 'edit':
-					# Working here !
-					break;
-				case 'list':
-					
-					$ar_list_of_values	= $this->get_ar_list_of_values2();
-					$dato 				= $this->get_dato();
-					$tipo 				= $this->get_tipo();
+				$ar_list_of_values	= $this->get_ar_list_of_values2();
+				$dato 				= $this->get_dato();
+				$tipo 				= $this->get_tipo();
 
-					foreach ($ar_list_of_values->result as $key => $item) {
+				foreach ($ar_list_of_values->result as $key => $item) {
 
-						$label   = (string)$item->label;
-						$locator = (object)clone $item->value;
+					$label   = (string)$item->label;
+					$value = (object)clone $item->value;
 
-						if (!property_exists($locator, 'type')) {
-							$locator->type = DEDALO_RELATION_TYPE_LINK;
-						}
-						if (!property_exists($locator, 'from_component_tipo')) {
-							$locator->from_component_tipo = $tipo;
-						}
-
-						if (in_array($locator, $dato)) {	# dato is array always
-							$checked = true;
-						}else{
-							$checked = false;
-						}
-
-						$list_item = new stdClass();			
-							$list_item->value 			= $locator;
-							$list_item->label 			= $label;
-							$list_item->selected 		= $checked;
-
-						$value_data[]= $list_item;
+					if (!property_exists($value, 'type')) {
+						$value->type = DEDALO_RELATION_TYPE_LINK;
 					}
-					break;
-			}
+					if (!property_exists($value, 'from_component_tipo')) {
+						$value->from_component_tipo = $tipo;
+					}
 
-		// Value
+					if (in_array($value, $dato)) {	# dato is array always
+						$selected = true;
+					}else{
+						$selected = false;
+					}
+
+					$item_value = new stdClass();			
+						$item_value->value 			= $value;
+						$item_value->label 			= $label;
+						$item_value->selected 		= $selected;
+
+					$item_values[]= $item_value;
+				}
+
+				break;
+		}
+
+		// item
 		$item = new stdClass();
 			$item->section_id 			= $this->get_section_id();
 			$item->tipo 				= $this->get_tipo();
 			$item->from_component_tipo 	= isset($this->from_component_tipo) ? $this->from_component_tipo : $item->tipo;
 			$item->section_tipo 		= $this->get_section_tipo();
-			$item->value 				= $value_data; //$this->get_dato();
+			$item->value 				= $item_values;
 
 		$data[] = $item;
 
 	}//end if($options->get_data===true && $permissions>0)
-
+	
 // JSON string
 	return common::build_element_json_output($context, $data);
