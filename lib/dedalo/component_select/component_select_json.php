13140a61c70c9735c3de9b8cf47bc70fb6ba4384
Lourdes Veros Álvarez <lur@render.es>
2019-06-19 18:02:29 +0200

    Structure changes, added datalist and modified value.

--- lib/dedalo/component_select/component_select_json.php
+++ lib/dedalo/component_select/component_select_json.php
@@ -17,49 +17,19 @@
 
 // data
 	$data = [];
-	$item_values = [];
-
+	
 	if($options->get_data===true && $permissions>0){
 			
 		// item_values
-		$modo = $this->get_modo();
 		switch ($modo) {
 			case 'edit':
-				# Working here !
-				break;
-			case 'list':
-
-				$ar_list_of_values	= $this->get_ar_list_of_values2();
 				$dato 				= $this->get_dato();
-				$tipo 				= $this->get_tipo();
-
-				foreach ($ar_list_of_values->result as $key => $item) {
-
-					$label   = (string)$item->label;
-					$value = (object)clone $item->value;
-
-					if (!property_exists($value, 'type')) {
-						$value->type = DEDALO_RELATION_TYPE_LINK;
-					}
-					if (!property_exists($value, 'from_component_tipo')) {
-						$value->from_component_tipo = $tipo;
-					}
-
-					if (in_array($value, $dato)) {	# dato is array always
-						$selected = true;
-					}else{
-						$selected = false;
-					}
-
-					$item_value = new stdClass();			
-						$item_value->value 			= $value;
-						$item_value->label 			= $label;
-						$item_value->selected 		= $selected;
-
-					$item_values[]= $item_value;
-				}
-
+				$ar_list_of_values	= $this->get_ar_list_of_values2();
 				break;
+
+			case 'list':
+				$dato 				= $this->get_valor();
+		
 		}
 
 		// item
@@ -68,9 +38,14 @@
 			$item->tipo 				= $this->get_tipo();
 			$item->from_component_tipo 	= isset($this->from_component_tipo) ? $this->from_component_tipo : $item->tipo;
 			$item->section_tipo 		= $this->get_section_tipo();
-			$item->value 				= $item_values;
+			$item->value 				= $dato;
+			
 
-		$data[] = $item;
+	if (isset($ar_list_of_values)) {
+		$item->datalist 				= $ar_list_of_values->result;
+	}
+
+	$data[] = $item;
 
 	}//end if($options->get_data===true && $permissions>0)
 	
