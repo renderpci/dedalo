2cf4f5190be7d2181ffbf9660f5ff25769216001
Lourdes Veros Álvarez <lur@render.es>
2019-06-20 13:40:29 +0200

    Added mode switch and data structure creation dependent on it.

--- lib/dedalo/component_filter/component_filter_json.php
+++ lib/dedalo/component_filter/component_filter_json.php
@@ -1,13 +1,9 @@
 <?php
 // JSON data component controller
 
-
-
 // component configuration vars
 	$permissions		= $this->get_component_permissions();
 	$modo				= $this->get_modo();
-
-
 
 // context
 	$context = [];
@@ -19,28 +15,38 @@
 
 	}//end if($options->get_context===true)
 
-
-
 // data
 	$data = [];
 
 	if($options->get_data===true && $permissions>0){
-		
-		// Value
-			$value = $this->get_dato();
-			
-			$item = new stdClass();
-				$item->section_id 			= $this->get_section_id();
-				$item->tipo 				= $this->get_tipo();
-				$item->from_component_tipo 	= isset($this->from_component_tipo) ? $this->from_component_tipo : $item->tipo;
-				$item->section_tipo 		= $this->get_section_tipo();
-				$item->value 				= $value;
+				
+		switch ($modo) {
+				case 'edit':
+				$dato 				= $this->get_dato();
+				$ar_list_of_values	= $this->get_ar_list_of_values2();
+				break;
 
-			$data[] = $item;
+			case 'list':
+				$dato 				= $this->get_valor();
+		
+		}
+
+		// item
+		$item = new stdClass();
+			$item->section_id 			= $this->get_section_id();
+			$item->tipo 				= $this->get_tipo();
+			$item->from_parent 			= isset($this->from_parent) ? $this->from_parent : $item->tipo;
+			$item->section_tipo 		= $this->get_section_tipo();
+			$item->value 				= $dato;
+			
+
+	if (isset($ar_list_of_values)) {
+		$item->datalist 				= $ar_list_of_values->result;
+	}
+
+	$data[] = $item;
 
 	}//end if($options->get_data===true && $permissions>0)
-
-
-
+	
 // JSON string
 	return common::build_element_json_output($context, $data);
