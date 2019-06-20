b9fc059ad230bc45c61b07b18b30d3d5f3a33613
Lourdes Veros Álvarez <lur@render.es>
2019-06-20 16:43:00 +0200

    Rename from_component_tipo property to from_parent.

--- lib/dedalo/component_ip/component_ip_json.php
+++ lib/dedalo/component_ip/component_ip_json.php
@@ -32,7 +32,7 @@
 			$item = new stdClass();
 				$item->section_id 			= $this->get_section_id();
 				$item->tipo 				= $this->get_tipo();
-				$item->from_component_tipo 	= isset($this->from_component_tipo) ? $this->from_component_tipo : $item->tipo;
+				$item->from_parent 			= isset($this->from_parent) ? $this->from_parent : $item->tipo;
 				$item->section_tipo 		= $this->get_section_tipo();
 				$item->value 				= $value;
 
