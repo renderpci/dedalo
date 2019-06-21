50b72aaee978776e3e6221668b28db0814928793
Juan Francisco Onielfa Veneros <paco@render.es>
2019-06-21 18:09:33 +0200

    Added layout map resolve in section_group

--- lib/dedalo/section_group/section_group_json.php
+++ lib/dedalo/section_group/section_group_json.php
@@ -12,79 +12,32 @@
 
 		// Element structure context (tipo, relations, properties, etc.)
 			$context[] = $this->get_structure_context($permissions);
-			/*
-			// from requested context if exists
-				if (isset(dd_api::$ar_dd_objects)) {
-				 	$ar_request_dd_object = array_filter(dd_api::$ar_dd_objects, function($item) use($tipo, $section_tipo){
-						if ($item->parent===$tipo && $item->section_tipo===$section_tipo) {
-							return $item;
-						}
-					});
-				}
-
-			// subcontext from layout_map items
-				if (!empty($ar_request_dd_object)) {
-									
-					$ar_subcontext 	= [];
-					foreach ($ar_request_dd_object as $dd_object) {
-
-						$dd_object 				= (object)$dd_object;
-						$current_tipo 			= $dd_object->tipo;
-						$current_section_tipo 	= $dd_object->section_tipo;					
-						$mode 					= $dd_object->mode ?? 'list';
-						$model 					= RecordObj_dd::get_modelo_name_by_tipo($current_tipo,true);
-
-						switch (true) {
-							case (strpos($model, 'component_')===0):						
-								
-								$current_lang 	 = $dd_object->lang ?? component_common::get_component_lang($current_tipo, DEDALO_DATA_LANG);
-								
-								$related_element = component_common::get_instance( 	$model,
-																					$current_tipo,
-																					null,
-																					$mode,
-																					$current_lang,
-																					$current_section_tipo);
-								break;
-							
-							case (in_array($model, layout_map::$groupers)):
-								
-								$related_element = new $model($current_tipo, $current_section_tipo, $mode);											
-								break;
-
-							default:
-								debug_log(" Section json 1 [context]. Ignored model '$model' - current_tipo: '$current_tipo' ".to_string(), logger::WARNING);
-								break;
-						}
-						
-						if (isset($related_element)) {				
-						
-							// get the JSON context of the related component
-								$item_options = new stdClass();
-									$item_options->get_context 	 = true;
-									$item_options->get_data 	 = false;
-								$element_json = $related_element->get_json($options);
-
-							// temp ar_subcontext
-								$ar_subcontext = array_merge($ar_subcontext, $element_json->context);
-					
-						}
-					
-					}//end foreach ($layout_map as $section_tipo => $ar_list_tipos) foreach ($ar_list_tipos as $current_tipo)
-
-					// ar_subcontext add everyone
-						foreach ($ar_subcontext as $value) {
-							#if (!in_array($value, $context)) {
-								$context[] = $value;
-							#}
-						}
-
-				}//end if (!empty($ar_request_dd_object))
-			*/
-
+			
+		// subcontext from element layout_map items
+			$ar_subcontext = $this->get_ar_subcontext();
+			foreach ($ar_subcontext as $current_context) {
+				$context[] = $current_context;
+			}
 
 // data
 	$data = [];
+
+	if($options->get_data===true && $permissions>0){
+
+		// Value
+			$value = null;
+						
+			$item = new stdClass();
+				$item->section_id 			= $this->get_section_id();
+				$item->tipo 				= $this->get_tipo();
+				$item->from_parent 			= isset($this->from_parent) ? $this->from_parent : $this->get_parent();
+				$item->section_tipo 		= $this->get_section_tipo();
+				$item->model 				= get_class($this);
+				$item->value 				= $value;
+
+			$data[] = $item;
+			
+	}//end if $options->get_data===true && $permissions>0
 
 
 
