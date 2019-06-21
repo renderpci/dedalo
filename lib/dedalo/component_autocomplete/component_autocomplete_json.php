79ea56c6e415800186277746215436a195eba11f
Lourdes Veros Álvarez <lur@render.es>
2019-06-21 11:16:08 +0200

    Context and data load in the same way as section. Added mode switch and tested in both modes.

--- lib/dedalo/component_autocomplete/component_autocomplete_json.php
+++ lib/dedalo/component_autocomplete/component_autocomplete_json.php
@@ -1,12 +1,14 @@
 <?php
 // JSON data component controller
-return include(DEDALO_LIB_BASE_PATH.'/component_relation_common/component_relation_common_json.php');
 
 
-/*
+
 // component configuration vars
 	$permissions		= $this->get_component_permissions();
 	$modo				= $this->get_modo();
+	$section_tipo 		= $this->section_tipo;
+	$lang 				= $this->lang;
+	$tipo 				= $this->get_tipo();
 
 
 
@@ -18,93 +20,112 @@ return include(DEDALO_LIB_BASE_PATH.'/component_relation_common/component_relati
 		// Component structure context (tipo, relations, properties, etc.)
 			$context[] = $this->get_structure_context($permissions);
 
+		// subcontext from layout_map items
+			$ar_subcontext 	= [];
+			$layout_map 	= $this->get_layout_map(); 	dump($layout_map, ' layout_map ++ '.to_string());
+			foreach ($layout_map as $dd_object) {
+
+				$dd_object 		= (object)$dd_object;
+				$current_tipo 	= $dd_object->tipo;
+				$mode 			= $dd_object->mode ?? 'list';
+				$model 			= RecordObj_dd::get_modelo_name_by_tipo($current_tipo,true);
+				$current_lang 	= $dd_object->lang ?? component_common::get_component_lang($current_tipo, DEDALO_DATA_LANG);
+				
+				$related_component = component_common::get_instance( $model,
+																	 $current_tipo,
+																	 null,
+																	 $mode,
+																	 $current_lang,
+																	 $dd_object->section_tipo);
+
+				// Inject this tipo as related component from_parent
+					$related_component->from_parent = $tipo;
+
+				// get the JSON context of the related component
+					$get_json_options = new stdClass();
+						$get_json_options->get_context 	= true;
+						$get_json_options->get_data 	= false;
+					$component_json = $related_component->get_json($get_json_options);
+
+				// temp ar_subcontext
+					$ar_subcontext = array_merge($ar_subcontext, $component_json->context);
+			
+			}//end foreach ($layout_map as $section_tipo => $ar_list_tipos) foreach ($ar_list_tipos as $current_tipo)
+
+			// ar_subcontext add everyone
+				foreach ($ar_subcontext as $value) {
+					#if (!in_array($value, $context)) {
+						$context[] = $value;
+					#}
+				}
+	
 	}//end if($options->get_context===true)
 
-	
+
 
 // data
 	$data = [];
 
 	if($options->get_data===true && $permissions>0){
 
-		// Building real value
-			$dato = $this->get_dato();
-			if (!empty($dato)) {
-						
-				$ar_target_section_tipo = $this->get_ar_target_section_tipo();
-				$target_section_tipo	= reset($ar_target_section_tipo);
-				$max_records 			= 1;
-				$offset 				= 0;
+		switch ($modo) {
+			case 'edit':
+				$dato 				= $this->get_dato();				
+				//TODO - Get the proper list of values. Initially empty, dinamically apply some filter when needed
+				$ar_list_of_values	= [];//$this->get_ar_list_of_values2();
+				break;
 
-				// search_query_object_options 
-					$search_query_object_options = new stdClass();
-						$search_query_object_options->filter_by_locator  = (array)$dato;
-						$search_query_object_options->section_tipo 		 = $target_section_tipo;
-						$search_query_object_options->tipo 		 		 = $this->tipo;
-						
-						# paginations options
-						$search_query_object_options->limit 		 	= $max_records;
-						$search_query_object_options->offset 		 	= $offset;
+			case 'list':
+				$dato 				= $this->get_valor();
+		
+		}
 
-						// Order
-							$order_values = array_map(function($locator){
-								return (int)$locator->section_id;
-							}, $dato);					
-							$item = new stdClass();
-								$item->column_name 	 = 'section_id';
-								$item->column_values = $order_values;		
-							$search_query_object_options->order_custom = [$item];
+		// subdata
+			if (!empty($dato)) {			
 
-					$search_query_object = component_autocomplete::build_search_query_object($search_query_object_options);
-					$search_query_object->select = [];
+				// search self records to paginate
+					$rows_data = $this->get_portal_records($dato);
 
-				// search 
-					$search_development2 = new search_development2($search_query_object);
-					$rows_data 		 	 = $search_development2->search();
-
-				// layout_map 
-					$layout_map 	= $this->get_layout_map();
+				// subcontext from layout_map items
 					$ar_subcontext 	= [];
-					foreach ($layout_map as $section_tipo => $ar_list_tipos) foreach ($ar_list_tipos as $current_tipo) {
-							
-						$modo 			= 'list';
-						$model 			= RecordObj_dd::get_modelo_name_by_tipo($current_tipo,true);
-						$RecordObj_dd 	= new RecordObj_dd($current_tipo);
-						$lang 			= ($RecordObj_dd->get_traducible()==='si') ? DEDALO_DATA_LANG : DEDALO_DATA_NOLAN;
+					$layout_map 	= $this->get_layout_map();
+					foreach ($layout_map as $dd_object) {
 
-						foreach ($rows_data->ar_records as $current_record) {
+						$dd_object 		= (object)$dd_object;
+						$current_tipo 	= $dd_object->tipo;							
+						$modo 			= $dd_object->mode ?? 'list';
+						$model 			= RecordObj_dd::get_modelo_name_by_tipo($current_tipo,true);							
+						$current_lang 	= $dd_object->lang ?? component_common::get_component_lang($current_tipo, DEDALO_DATA_LANG);					
 						
-							$related_component = component_common::get_instance($model,
-																		 $current_tipo,
-																		 $current_record->section_id,
-																		 $modo,
-																		 $lang,
-																		 $current_record->section_tipo);
+						foreach ($rows_data->ar_records as $current_record) {
+							
+							$related_component = component_common::get_instance( $model,
+																				 $current_tipo,
+																				 $current_record->section_id,
+																				 $modo,
+																				 $current_lang,
+																				 $current_record->section_tipo);
 
 							// Inject this tipo as related component from_component_tipo
 								$related_component->from_component_tipo = $this->tipo;
 								$related_component->from_section_tipo 	= $this->section_tipo;
 
-							$component_json = $related_component->get_json();
+							// get json
+								$get_json_options = new stdClass();
+									$get_json_options->get_context 	= false;
+									$get_json_options->get_data 	= true;
+								$component_json = $related_component->get_json($get_json_options);
 
 							// Add data
 								$data = array_merge($data, $component_json->data);
 
-							// temp ar_subcontext
-								$ar_subcontext = array_merge($ar_subcontext, $component_json->context);
-
 						}//end foreach ($rows_data->ar_records as $current_record)
 					
 					}//end foreach ($layout_map as $section_tipo => $ar_list_tipos) foreach ($ar_list_tipos as $current_tipo)
-
 				// ar_subcontext add everyone
-					foreach ($ar_subcontext as $value) {
-						#if (!in_array($value, $context)) {
-							$context[] = $value;
-						#}
-					}
-
+				
 			}//end if (!empty($dato))
+
 
 		// Value
 			$value = $dato;
@@ -112,17 +133,21 @@ return include(DEDALO_LIB_BASE_PATH.'/component_relation_common/component_relati
 			$item = new stdClass();
 				$item->section_id 			= $this->get_section_id();
 				$item->tipo 				= $this->get_tipo();
-				$item->from_component_tipo 	= isset($this->from_component_tipo) ? $this->from_component_tipo : $item->tipo;
+				$item->from_parent 			= isset($this->from_parent) ? $this->from_parent : $item->tipo;
 				$item->section_tipo 		= $this->get_section_tipo();
 				$item->model 				= get_class($this);
 				$item->value 				= $value;
 
-			$data[] = $item;			
-	
-	}//end if($options->get_data===true && $permissions>0)
+
+			if (isset($ar_list_of_values)) {
+				$item->datalist 				= $ar_list_of_values->result;
+			}
+
+			$data[] = $item;
+			
+	}//end if $options->get_data===true && $permissions>0
 
 
 
 // JSON string
 	return common::build_element_json_output($context, $data);
-*/
