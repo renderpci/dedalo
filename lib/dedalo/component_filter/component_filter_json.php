9d7be854affbae5dd8e3cfac559fbb1b30f9cd1d
Lourdes Veros Álvarez <lur@render.es>
2019-06-26 18:12:50 +0200

    Modified so it is able to load datalist data.

--- lib/dedalo/component_filter/component_filter_json.php
+++ lib/dedalo/component_filter/component_filter_json.php
@@ -28,9 +28,11 @@
 	
 		switch ($modo) {
 			case 'edit':
-				$dato 				= $this->get_dato();
-				//TODO - Change function call to retrieve dalalist data
-				$ar_list_of_values	= $this->get_ar_projects_for_current_section();
+				$dato 					= $this->get_dato();
+				$ar_list_of_values = filter::get_user_authorized_projects($user_id, $this->tipo);	
+				//TODO - Change function call to retrieve dalalist data, $user_id is empty
+				//$ar_list_of_values		= $this->get_ar_projects_for_current_section();		
+			
 				break;
 			case 'list':
 				$dato 				= $this->get_valor();
@@ -45,15 +47,25 @@
 			$item->section_tipo 		= $this->get_section_tipo();
 			$item->value 				= $dato;
 
-			if (isset($ar_list_of_values) && isset($ar_list_of_values->result)) {
-				$item->datalist 		= $ar_list_of_values->result;
+			if (isset($ar_list_of_values)) {
+				foreach ($ar_list_of_values as $user_project) {		
+
+					$project_value = new stdClass();
+						$project_value->section_id 		= $user_project->locator->section_id;
+						$project_value->section_tipo 	= $user_project->locator->section_tipo;
+
+					$project = new stdClass();
+						$project->value 				= $project_value;
+						$project->label 				= $user_project->label;
+						$project->section_id 			= $user_project->locator->section_id;
+
+						$item->datalist[] = $project;
+					}				
 			}
 
 		$data[] = $item;
 
 	}//end if($options->get_data===true && $permissions>0)
-
-
 
 // JSON string
 	return common::build_element_json_output($context, $data);
