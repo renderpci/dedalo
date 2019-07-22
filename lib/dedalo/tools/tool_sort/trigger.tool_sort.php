e118fea0ec041b70aab2745bde4556277e10804e
Juan Francisco Onielfa Veneros <paco@render.es>
2019-07-22 17:49:34 +0200

    Tool sort fixes

--- lib/dedalo/tools/tool_sort/trigger.tool_sort.php
+++ lib/dedalo/tools/tool_sort/trigger.tool_sort.php
@@ -73,3 +73,80 @@ function assign_element($json_data) {
 
 
 
+/**
+* SET_ORIGINAL_DUPLICATE
+* @param $section_tipo
+* @param $original_id
+* @param $duplicates_id
+*/
+function set_original_duplicate($json_data) {
+	global $start_time;
+
+	# Write session to unlock session file
+	session_write_close();
+
+	$response = new stdClass();
+		$response->result 	= false;
+		$response->msg 		= 'Error. Request failed ['.__FUNCTION__.']';
+
+	$vars = array('section_tipo','original_id','duplicates_id');
+		foreach($vars as $name) {
+			$$name = common::setVarData($name, $json_data);
+			# DATA VERIFY
+			if ($name==='duplicates_id') continue; # Skip non mandatory
+			if (empty($$name)) {
+				$response->msg = 'Trigger Error: ('.__FUNCTION__.') Empty '.$name.' (is mandatory)';
+				return $response;
+			}
+		}
+	
+	// relation_related_tipo
+		$ar_component_relation_related = section::get_ar_children_tipo_by_modelo_name_in_section($section_tipo, ['component_relation_related'], $from_cache=true, $resolve_virtual=true, $recursive=true, $search_exact=true, $ar_tipo_exclude_elements=false);
+		if (empty($ar_component_relation_related)) {
+			$response->msg .= ' component_relation_related not found in section '.$section_tipo;
+			return $response;
+		}else{
+			$component_relation_related = $ar_component_relation_related[0];
+		}
+
+	// component. set dato and save
+		$component_tipo = $component_relation_related;		
+		$section_id 	= $original_id;
+		$modelo_name = RecordObj_dd::get_modelo_name_by_tipo($component_relation_related,true);
+		$component   = component_common::get_instance($modelo_name,
+													  $component_tipo,
+													  $section_id,
+													  "edit",
+													  DEDALO_DATA_NOLAN,
+													  $section_tipo);
+		$dato = [];
+		foreach ($duplicates_id as $current_section_id) {
+			
+			$locator = new locator();
+				$locator->set_section_tipo($section_tipo);
+				$locator->set_section_id($current_section_id);
+				$locator->set_from_component_tipo($component_relation_related);
+			
+			$dato[] = $locator;
+		}
+
+		$component->set_dato($dato);
+		$component->Save();
+
+	// response
+		$response->result 	= true;
+		$response->msg 		= 'Ok. Request done ['.__FUNCTION__.']';
+
+	// Debug
+		if(SHOW_DEBUG===true) {
+
+			$debug = new stdClass();
+				$debug->exec_time	= exec_time_unit($start_time,'ms')." ms";
+				foreach($vars as $name) {
+					$debug->{$name} = $$name;
+				}
+			$response->debug = $debug;
+		}
+	
+	return (object)$response;
+}//end set_original_duplicate
