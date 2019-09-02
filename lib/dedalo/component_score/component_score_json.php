51d74b9b76f6f896745fada9684df5aab6b958b3
Juan Francisco Onielfa Veneros <paco@render.es>
2019-09-02 19:08:04 +0200

    Added component_score_json controller

---
+++ lib/dedalo/component_score/component_score_json.php
@@ -0,0 +1,58 @@
+<?php
+// JSON data component controller
+
+
+
+// component configuration vars
+	$permissions		= $this->get_component_permissions();
+	$modo				= $this->get_modo();
+
+
+
+// context
+	$context = [];
+
+	if($options->get_context===true){
+
+		// Component structure context (tipo, relations, properties, etc.)
+			$context[] = $this->get_structure_context($permissions);
+
+		// add buttons
+			$context = array_merge($context, $this->get_structure_buttons($permissions));
+
+	}//end if($options->get_context===true)
+
+
+
+// context_simple
+	if($options->get_context_simple===true){
+
+		// Component structure context_simple (tipo, relations, properties, etc.)
+			$context[] = $this->get_structure_context_simple($permissions);
+
+	}//end if($options->get_context_simple===true)
+
+
+
+// data
+	$data = [];
+
+	if($options->get_data===true && $permissions>0){
+				
+		// Value
+		$value = $this->get_dato();
+		
+		
+		// data item
+		$item  = $this->get_data_item($value);
+			$item->parent_tipo 			= $this->get_tipo();
+			$item->parent_section_id 	= $this->get_section_id();
+
+		$data[] = $item;
+
+	}//end if($options->get_data===true && $permissions>0)
+
+
+
+// JSON string
+	return common::build_element_json_output($context, $data);
