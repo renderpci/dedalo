2bbbadadbf8648d25ed292cf81094315e70c18dd
Lourdes Veros Álvarez <lur@render.es>
2019-09-13 17:06:54 +0200

    Modified to match the new model and to be multivalue.

--- lib/dedalo/component_email/component_email_json.php
+++ lib/dedalo/component_email/component_email_json.php
@@ -20,7 +20,11 @@
 				break;
 			
 			default:
-				$context[] = $this->get_structure_context($permissions);
+				// Component structure context (tipo, relations, properties, etc.)
+					$context[] = $this->get_structure_context($permissions);
+
+				// add buttons
+					$context = array_merge($context, $this->get_structure_buttons($permissions));
 				break;
 		}
 	}//end if($options->get_context===true)
@@ -37,6 +41,8 @@
 		
 		// data item
 		$item  = $this->get_data_item($value);
+		$item->parent_tipo 			= $this->get_tipo();
+		$item->parent_section_id 	= $this->get_section_id();
 
 		$data[] = $item;
 
