bd8b48b3723c302b1eb6d279480198775b5e2a87
Juan Francisco Onielfa Veneros <paco@render.es>
2019-09-29 10:56:28 +0200

    Change page test config of edit mode to show 2 records instead one

--- lib/dedalo/page/page.php
+++ lib/dedalo/page/page.php
@@ -162,6 +162,8 @@
 				$section->set_lang($lang);
 				$sqo_context = $section->get_sqo_context();
 
+				if(MODE==='edit') $sqo_context->show[0]->limit = 2; // Force show 2 records
+
 				$page_element = new StdClass();
 					$page_element->model 		 = 'section';
 					$page_element->section_tipo  = $section_tipo;
