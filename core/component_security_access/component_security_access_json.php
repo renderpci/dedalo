7e42596b0b44badb7a89fb77e6f5119936276519
Alejandro Peña Carbonell <alex@render.es>
2019-12-26 12:23:55 +0100

    Add security_access_json file

---
+++ core/component_security_access/component_security_access_json.php
@@ -0,0 +1,64 @@
+<?php
+// JSON data component controller
+
+
+
+// component configuration vars
+	$permissions		= $this->get_component_permissions();
+	$modo				= $this->get_modo();
+	$properties			= $this->get_propiedades();
+
+
+
+// context
+	$context = [];
+
+	if($options->get_context===true){
+		switch ($options->context_type) {
+			case 'simple':
+				// Component structure context_simple (tipo, relations, properties, etc.)
+				$context[] = $this->get_structure_context_simple($permissions);
+				break;
+
+			default:
+				$sqo_context = isset($properties->unique) ? true : false;
+				// Component structure context (tipo, relations, properties, etc.)
+					$context[] = $this->get_structure_context($permissions, $sqo_context);
+
+				// add buttons
+					$context = array_merge($context, $this->get_structure_buttons($permissions));
+				break;
+		}
+	}//end if($options->get_context===true)
+
+
+
+// data
+	$data = [];
+
+	if($options->get_data===true && $permissions>0){
+
+		// Value
+		switch ($modo) {
+			case 'list':
+				$value = component_common::extract_component_dato_fallback($this, $lang=DEDALO_DATA_LANG, $main_lang=DEDALO_DATA_LANG_DEFAULT);
+				break;
+			case 'edit':
+			default:
+				$value = $this->get_dato();
+				break;
+		}
+
+		// data item
+		$item  = $this->get_data_item($value);
+		$item->parent_tipo 			= $this->get_tipo();
+		$item->parent_section_id 	= $this->get_section_id();
+
+		$data[] = $item;
+
+	}//end if($options->get_data===true && $permissions>0)
+
+
+
+// JSON string
+	return common::build_element_json_output($context, $data);
