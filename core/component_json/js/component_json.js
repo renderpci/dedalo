3c3b19efa6bc67d09f4c49efe7893b40c901d0ff
Juan Francisco Onielfa Veneros <paco@render.es>
2020-01-02 19:05:54 +0100

    Fix component_json old code to use jsoneditor lib as js module

--- core/component_json/js/component_json.js
+++ core/component_json/js/component_json.js
@@ -1,9 +1,19 @@
+/*global get_label, page_globals, SHOW_DEBUG, DEDALO_CORE_URL*/
+/*eslint no-undef: "error"*/
+
+
+
 // imports
+	import {data_manager} from '../../common/js/data_manager.js'
+	import {common,create_source} from '../../common/js/common.js'
+	import {component_common} from '../../component_common/js/component_common.js'
 	import {render_component_json} from '../../component_json/js/render_component_json.js'
-	import * as instances from '../../common/js/instances.js'
+
+
+
 
 export const component_json = function(){
-	
+
 	this.tipo
 	this.section_tipo
 	this.section_id
@@ -20,140 +30,203 @@ export const component_json = function(){
 
 }//end component_json
 
-/**
-* INIT
-* @return 
-*/
-component_json.prototype.init = function(options) {
-	
-	const self = this
 
-	self.mode 			= options.mode
-	self.lang 			= options.lang
-	self.section_lang 	= options.section_lang
-	self.model 			= options.model
-	self.tipo 			= options.tipo
-	self.section_tipo 	= options.section_tipo
-	self.section_id 	= options.section_id
-	self.parent 		= options.parent
-	self.id 			= options.id
-
-	// Options vars 
-	self.context = options.context || null
-	self.data 	 = options.data || []	
-
-	//console.log("component_json: init:",self);	
-
-	//event_manager.subscribe('stateChange', () => self.render())
-}//end init
 
 /**
-* LOAD_CONTEXT
-* @return 
+* COMMON FUNCTIONS
+* extend component functions from component common
 */
-component_json.prototype.load_context = function() {
+// prototypes assign
+	// lifecycle
+	component_json.prototype.init 	 			= component_common.prototype.init
+	component_json.prototype.build 	 			= component_common.prototype.build
+	component_json.prototype.render 			= common.prototype.render
+	component_json.prototype.refresh 			= common.prototype.refresh
+	component_json.prototype.destroy 	 		= common.prototype.destroy
 
-	const self = this
+	// change data
+	component_json.prototype.save 	 			= component_common.prototype.save
+	//component_json.prototype.load_data 			= component_common.prototype.load_data
+	//component_json.prototype.get_value 			= component_common.prototype.get_value
+	//component_json.prototype.set_value 			= component_common.prototype.set_value
+	component_json.prototype.update_data_value	= component_common.prototype.update_data_value
+	component_json.prototype.update_datum 		= component_common.prototype.update_datum
+	component_json.prototype.change_value 		= component_common.prototype.change_value
 
-	if (self.context) {
+	// render
+	component_json.prototype.list 				= render_component_json.prototype.list
+	component_json.prototype.edit 				= render_component_json.prototype.edit
+	component_json.prototype.edit_in_list		= render_component_json.prototype.edit
+	component_json.prototype.search 			= render_component_json.prototype.search
+	component_json.prototype.change_mode 		= component_common.prototype.change_mode
 
-		return new Promise(function(resolve) {
-		  resolve(self.context)
-		});
-	}
 
-	const options = {
-		model 			: 'section_record',
-		tipo 			: self.section_tipo,
-		section_tipo 	: self.section_tipo,
-		section_id		: self.section_id,
-		mode			: self.mode,
-		lang			: self.section_lang
-	}
 
-	const tipo = self.tipo		
+/**
+* INIT_EDITOR
+* @return
+*/
+component_json.prototype.init_editor = async function() {
 
-	// section instance
-		const js_promise = instances.get_instance(options).then(function(current_section_record){
+	// load dependences js/css
+		const load_promises = []
 
-			const context = current_section_record.get_component_context(tipo);	
+		const load_promise = import('../../../lib/jsoneditor/dist/jsoneditor.js')
+		load_promises.push( load_promise )
+		//self.JSONEditor = JSONEditor
 
-			//event_manager.publish('stateChange')
+		// const lib_js_file = DEDALO_ROOT_WEB + '/lib/leaflet/dist/leaflet.js'
+		// load_promises.push( common.prototype.load_script(lib_js_file) )
 
-			// set
-				self.context = context
+		const lib_css_file = DEDALO_ROOT_WEB + '/lib/jsoneditor/dist/jsoneditor.min.css'
+		load_promises.push( common.prototype.load_style(lib_css_file) )
+
+		await Promise.all(load_promises).then(async function(response){
+			//console.log("JSONEditor:",JSONEditor);
 		})
 
-	//event_manager.subscribe('stateChange', () => self.render())
+	return true
+};//end init_editor
 
-	return js_promise
-}//end load_context
 
-/**
-* LOAD_DATA
-* @return 
-*/
-component_json.prototype.load_data = function(){
 
-	const self = this
+// /**
+// * INIT
+// * @return
+// */
+// component_json.prototype.init = function(options) {
 
-	const options = {
-		model 			: 'section_record',
-		tipo 			: self.section_tipo,
-		section_tipo 	: self.section_tipo,
-		section_id		: self.section_id,
-		mode			: self.mode,
-		lang			: self.section_lang
-	}
+// 	const self = this
 
-	const tipo = self.tipo	
+// 	self.mode 			= options.mode
+// 	self.lang 			= options.lang
+// 	self.section_lang 	= options.section_lang
+// 	self.model 			= options.model
+// 	self.tipo 			= options.tipo
+// 	self.section_tipo 	= options.section_tipo
+// 	self.section_id 	= options.section_id
+// 	self.parent 		= options.parent
+// 	self.id 			= options.id
 
-	// section instance
-		const js_promise = instances.get_instance(options).then(function(current_section){
-		
-			self.data =	current_section.get_component_data(tipo);
+// 	// Options vars
+// 	self.context = options.context || null
+// 	self.data 	 = options.data || []
 
-			return self.data
-		})
+// 	//console.log("component_json: init:",self);
 
-	return js_promise
-}//end load_data
+// 	//event_manager.subscribe('stateChange', () => self.render())
+// }//end init
 
-/**
-* RENDER
-* @return promise
-*/
-component_json.prototype.render = function(){
 
-	const self = this
-	
-	const context = self.context
-	return self.load_data().then(function(){
 
-		return new Promise(function(resolve){
 
-			// render
-				const current_render = new render_component_json(self)
-				
-				let node = ""
-				const mode = self.mode	
-				switch (mode){
-					case 'list':
-						node = current_render.list()
-					break
+// /**
+// * LOAD_CONTEXT
+// * @return
+// */
+// component_json.prototype.load_context = function() {
 
-					case 'edit':
-					default :
-						node = current_render.edit()
-				}
+// 	const self = this
 
-			// set node
-				self.node = node
+// 	if (self.context) {
 
-			// return self
-				//setTimeout(function(){
-					resolve(self)
-				//},1000)			
-		})
-	})
-}//end render
+// 		return new Promise(function(resolve) {
+// 		  resolve(self.context)
+// 		});
+// 	}
+
+// 	const options = {
+// 		model 			: 'section_record',
+// 		tipo 			: self.section_tipo,
+// 		section_tipo 	: self.section_tipo,
+// 		section_id		: self.section_id,
+// 		mode			: self.mode,
+// 		lang			: self.section_lang
+// 	}
+
+// 	const tipo = self.tipo
+
+// 	// section instance
+// 		const js_promise = instances.get_instance(options).then(function(current_section_record){
+
+// 			const context = current_section_record.get_component_context(tipo);
+
+// 			//event_manager.publish('stateChange')
+
+// 			// set
+// 				self.context = context
+// 		})
+
+// 	//event_manager.subscribe('stateChange', () => self.render())
+
+// 	return js_promise
+// }//end load_context
+
+// /**
+// * LOAD_DATA
+// * @return
+// */
+// component_json.prototype.load_data = function(){
+
+// 	const self = this
+
+// 	const options = {
+// 		model 			: 'section_record',
+// 		tipo 			: self.section_tipo,
+// 		section_tipo 	: self.section_tipo,
+// 		section_id		: self.section_id,
+// 		mode			: self.mode,
+// 		lang			: self.section_lang
+// 	}
+
+// 	const tipo = self.tipo
+
+// 	// section instance
+// 		const js_promise = instances.get_instance(options).then(function(current_section){
+
+// 			self.data =	current_section.get_component_data(tipo);
+
+// 			return self.data
+// 		})
+
+// 	return js_promise
+// }//end load_data
+
+// /**
+// * RENDER
+// * @return promise
+// */
+// component_json.prototype.render = function(){
+
+// 	const self = this
+
+// 	const context = self.context
+// 	return self.load_data().then(function(){
+
+// 		return new Promise(function(resolve){
+
+// 			// render
+// 				const current_render = new render_component_json(self)
+
+// 				let node = ""
+// 				const mode = self.mode
+// 				switch (mode){
+// 					case 'list':
+// 						node = current_render.list()
+// 					break
+
+// 					case 'edit':
+// 					default :
+// 						node = current_render.edit()
+// 				}
+
+// 			// set node
+// 				self.node = node
+
+// 			// return self
+// 				//setTimeout(function(){
+// 					resolve(self)
+// 				//},1000)
+// 		})
+// 	})
+// }//end render
--- core/component_json/js/render_component_json.js
+++ core/component_json/js/render_component_json.js
@@ -9,6 +9,8 @@
 
 
 
+
+
 /**
 * Render_component
 * Manage the components logic and appearance in client side
@@ -25,29 +27,26 @@ export const render_component_json = function(options) {
 * Render node for use in list
 * @return DOM node
 */
-render_component_json.prototype.list = function(options) {
+render_component_json.prototype.list = function() {
 
 	const self = this
 
 	// Options vars
-		const context 			= self.context
-		const data 				= self.data
+		const data = self.data
+
+	// wrapper
+		const wrapper = ui.component.build_wrapper_list(self, {
+			autoload : false
+		})
 
 	// Value as string
 		const value_string = JSON.stringify(data.value)
 
-	// Node create
-		const node = common.create_dom_element({
-			element_type	: "div",
-			class_name		: self.model + '_list ' + self.tipo,
-			text_content 	: value_string
-		})
+	// Set value
+		wrapper.textContent = value_string
 
-	// Debug
-		//console.log("++ context", context);
-		//console.log("++ data:", data);
 
-	return node
+	return wrapper
 }//end list
 
 
@@ -57,66 +56,258 @@ render_component_json.prototype.list = function(options) {
 * Render node for use in edit
 * @return DOM node
 */
-render_component_json.prototype.edit = function(options) {
+render_component_json.prototype.edit = async function(options={render_level:'full'}) {
 
 	const self = this
 
-	// Options vars
-		const context 			= self.context
-		const data 				= self.data || []
-		const value 			= data.value || []
-		const label 			= context.label
-		const model 			= self.model
-		const mode 				= 'edit'
-		const tipo 				= context.tipo
-		const section_id 		= data.section_id
-		const id 				= self.id || 'id is not set'
+		console.log("self.data:",self.data);
+
+	// fix non value scenarios
+		self.data.value = (!self.data.value || self.data.value.length<1) ? [null] : self.data.value
+
+
+	const render_level 	= options.render_level
 
 	// content_data
-		const content_data = document.createElement("div")
-
-		// create ul
-		const ul = common.create_dom_element({
-			element_type	: 'ul',
-			parent 			: content_data
-		})
-
-		const inputs_value = (value.length<1) ? [''] : value
-		const value_length = inputs_value.length
-		for (let i = 0; i < value_length; i++) {
-
-			// create li
-			const li = common.create_dom_element({
-				element_type	: 'li',
-				parent 			: ul
-			})
-
-			const textarea = common.create_dom_element({
-				element_type	: 'div',
-				contenteditable	: true,
-				text_content 	: JSON.stringify(inputs_value[i]),
-				parent 			: li
-			})
-
-			const btn_save = common.create_dom_element({
-				element_type	: 'div',
-				text_content	: 'SAVE',
-				parent 			: li
-			})
-			const json_editor = common.create_dom_element({
-				element_type	: 'div',
-				text_content	: 'JSON',
-				parent 			: li
-			})
+		const current_content_data = await get_content_data_edit(self)
+		if (render_level==='content') {
+			return current_content_data
 		}
 
-	// ui build_edit returns component wrapper
+	// wrapper. ui build_edit returns component wrapper
 		const wrapper = ui.component.build_wrapper_edit(self, {
-			content_data : content_data
+			content_data : current_content_data
 		})
+
+	// add events
+		//add_events(self, wrapper)
 
 
 	return wrapper
 }//end edit
+
+
+
+/**
+* ADD_EVENTS
+*/
+const add_events = function(self, wrapper) {
+
+	// update value, subscription to the changes: if the dom input value was changed, observers dom elements will be changed own value with the observable value
+		self.events_tokens.push(
+			event_manager.subscribe('update_value_'+self.id, update_value)
+		)
+		function update_value (changed_data) {
+			//console.log("-------------- - event update_value changed_data:", changed_data);
+			// change the value of the current dom element
+			const changed_node = wrapper.querySelector('input[data-key="'+changed_data.key+'"]')
+			changed_node.value = changed_data.value
+		}
+
+	// change event, for every change the value in the imputs of the component
+		wrapper.addEventListener('change', async (e) => {
+			//e.stopPropagation()
+			alert("Changed ! [under construction]");
+
+			/*
+			// update
+			if (e.target.matches('input[type="text"].input_value')) {
+				//console.log("++update e.target:",JSON.parse(JSON.stringify(e.target.dataset.key)));
+				//console.log("++update e.target value:",JSON.parse(JSON.stringify(e.target.value)));
+
+				// // is_unique check
+				// if (self.context.properties.unique) {
+				// 	// const result = await check_duplicates(self, e.target.value, false)
+				// 	if (self.duplicates) {
+				// 		e.target.classList.add("duplicated")
+
+				// 		const message = ui.build_message("Warning. Duplicated value " + self.duplicates.section_id)
+				// 		wrapper.appedChild(message)
+
+				// 		return false
+				// 	}
+				// }
+
+				const changed_data = Object.freeze({
+					action	: 'update',
+					key		: JSON.parse(e.target.dataset.key),
+					value	: (e.target.value.length>0) ? e.target.value : null,
+				})
+				self.change_value({
+					changed_data : changed_data,
+					refresh 	 : false
+				})
+				.then((save_response)=>{
+					// event to update the dom elements of the instance
+					event_manager.publish('update_value_'+self.id, changed_data)
+				})
+
+				return true
+			}
+			*/
+
+		}, false)
+
+
+
+
+
+	return true
+}//end add_events
+
+
+
+/**
+* get_CONTENT_DATA_EDIT
+* @return DOM node content_data
+*/
+const get_content_data_edit = async function(self) {
+
+	const value = self.data.value
+	const mode 	= self.mode
+
+	const fragment = new DocumentFragment()
+
+	// inputs container
+		const inputs_container = ui.create_dom_element({
+			element_type	: 'ul',
+			class_name 		: 'inputs_container',
+			parent 			: fragment
+		})
+
+	// values (inputs)
+		const inputs_value = value//(value.length<1) ? [''] : value
+		const value_length = inputs_value.length
+		for (let i = 0; i < value_length; i++) {
+			get_input_element(i, inputs_value[i], inputs_container, self)
+			break; // only one is used
+		}
+
+	// buttons container
+		const buttons_container = ui.create_dom_element({
+			element_type	: 'div',
+			class_name 		: 'buttons_container',
+			parent 			: fragment
+		})
+
+	// button close
+		if(mode==='edit_in_list' && !ui.inside_tool(self)){
+			const button_close = ui.create_dom_element({
+				element_type	: 'span',
+				class_name 		: 'button close',
+				parent 			: buttons_container
+			})
+		}
+
+	// content_data
+		const content_data = document.createElement("div")
+			  content_data.classList.add("content_data", self.type, "nowrap")
+		content_data.appendChild(fragment)
+
+
+	return content_data
+}//end get_content_data_edit
+
+
+
+/**
+* GET_INPUT_ELEMENT
+* @return dom element li
+*/
+const get_input_element = async (i, current_value, inputs_container, self) => {
+
+	const mode = self.mode
+
+	let validated = false
+
+	// li
+		const li = ui.create_dom_element({
+			element_type : 'li',
+			parent 		 : inputs_container
+		})
+
+	// button_save
+		const button_save = ui.create_dom_element({
+			element_type : 'button',
+			class_name	 : 'button_save',
+			text_content : "Save",
+			parent 		 : li
+		})
+		button_save.addEventListener("click", function(e) {
+			this.blur()
+			if (validated!==true) {
+
+				alert("Error. Trying so save invalid json value!");
+				return false
+			}
+
+			const changed_data = Object.freeze({
+				action	: 'update',
+				key		: 0,
+				value	: editor.get()
+			})
+			self.change_value({
+				changed_data : changed_data,
+				refresh 	 : false
+			})
+			.then((save_response)=>{
+				// event to update the dom elements of the instance
+				event_manager.publish('update_value_'+self.id, changed_data)
+			})
+		})
+
+	// // editor_container
+	// 	const editor_container = ui.create_dom_element({
+	// 		element_type : 'div',
+	// 		parent 		 : li
+	// 	})
+
+	// load
+		await self.init_editor()
+
+	// create the editor
+		const editor_options = {
+			mode	: 'code',
+			modes	: ['code', 'form', 'text', 'tree', 'view'], // allowed modes
+			onError	: function (err) {
+				console.error("err:",err);
+				alert(err.toString());
+			},
+			onValidationError : function() {
+				validated = false
+			},
+			onValidate: function() {
+				validated = true
+
+		       //var json = editor.get();
+		       // Update hidden text area value
+		       //editor_text_area.value = editor.getText()
+
+				// const json = editor.get();
+				//      	console.log("json:",json);
+				//      	//console.log("json editor.get():",editor.get());
+				//      	console.log("text editor.getText():",editor.getText());
+
+				// const changed_data = Object.freeze({
+				// 	action	: 'update',
+				// 	key		: 0,
+				// 	value	: editor.get()
+				// })
+				// self.change_value({
+				// 	changed_data : changed_data,
+				// 	refresh 	 : false
+				// })
+				// .then((save_response)=>{
+				// 	// event to update the dom elements of the instance
+				// 	event_manager.publish('update_value_'+self.id, changed_data)
+				// })
+
+		    }
+		}
+		const editor = new JSONEditor(li, editor_options, current_value)
+
+
+	return li
+}//end get_input_element
 
 
