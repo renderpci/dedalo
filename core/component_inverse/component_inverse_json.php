082a0ee89d0f7d58d085628393db7db752b33380
Lourdes Veros Álvarez <lur@render.es>
2020-01-10 13:45:37 +0100

    Modified value structure and value rendering.

--- core/component_inverse/component_inverse_json.php
+++ core/component_inverse/component_inverse_json.php
@@ -32,13 +32,13 @@
 
 	if($options->get_data===true && $permissions>0){
 
-
 		// Value
 		$value = $this->get_dato();
-		$dato  = $value[0]->from_section_id;
+		
 		// data item
-		//$item  = $this->get_data_item($value);		
-		$data[] = $this->get_data_item($dato);//$item;
+		$item = $this->get_data_item($value);
+	
+		$data[] = $item;
 
 	}//end if($options->get_data===true && $permissions>0)
 
