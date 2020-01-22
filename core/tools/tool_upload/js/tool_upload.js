d3205054f3bf4e8d04bf62090702f82c9e4a3503
Juan Francisco Onielfa Veneros <paco@render.es>
2020-01-22 18:45:31 +0100

    Fixed tool upload issues

--- core/tools/tool_upload/js/tool_upload.js
+++ core/tools/tool_upload/js/tool_upload.js
@@ -3,7 +3,7 @@
 	import {get_instance, delete_instance} from '../../../common/js/instances.js'
 	import {common} from '../../../common/js/common.js'
 	import {tool_common} from '../../../tool_common/js/tool_common.js'
-	import {render_tool_upload, add_component} from './render_tool_upload.js'
+	import {render_tool_upload} from './render_tool_upload.js'
 
 
 
@@ -132,28 +132,54 @@ const get_system_info = async function(self) {
 
 
 
+// /**
+// * FILE_SELECTED
+// */
+// tool_upload.prototype.file_selected = function(file, content_data) {
+
+// 	const self = this
+
+// 	// check max file size
+// 		const file_size_bytes = file.size
+// 		if (file_size_bytes>self.max_size_bytes) {
+// 			alert("Error. File is too big. Max file size is " + Math.floor(self.max_size_bytes / (1024*1024)) + " MB and current file is " + Math.floor(file_size_bytes / (1024*1024)));
+// 			return false
+// 		}
+
+
+// 	// exec upload_file
+// 		self.upload_file(file, content_data)
+
+// 	return true
+// }//end file_selected
+
+
+
 /**
 * UPLOAD_FILE
 */
-tool_upload.prototype.upload_file = async function(e, content_data) {
+tool_upload.prototype.upload_file = async function(file, content_data, response_msg, preview_image, progress_bar_container) {
 
 	const self = this
 
-	// console.log("upload_file e:",e)
-	// console.log("upload_file e.target:",e.target);
-	// console.log("upload_file e.target.files:",e.target.files);
-	// console.log("size:",e.target.files[0].size);
-
-	// check max file size
-		const file_size_bytes = e.target.files[0].size
-		if (file_size_bytes>self.max_size_bytes) {
-			alert("Error. File is too big. Max file size is " + Math.floor(self.max_size_bytes / (1024*1024)) + " MB and current file is " + Math.floor(file_size_bytes / (1024*1024)));
+	// check file extension
+		const allowed_extensions = self.caller.context.allowed_extensions
+		const file_extension 	 = file.name.split('.').pop()
+		if (!allowed_extensions.includes(file_extension)) {
+			alert( get_label.extension_no_valida + ": \n" + file_extension + "\nUse any of: \n" + JSON.stringify(allowed_extensions) );
 			return false
 		}
 
-	// dom elements
-		const response_container = content_data.querySelector(".response_container")
+	// check max file size
+		const file_size_bytes = file.size
+		if (file_size_bytes>self.max_size_bytes) {
+			alert( get_label.fichero_demasiado_grande + " Max file size is " + Math.floor(self.max_size_bytes / (1024*1024)) + " MB and current file is " + Math.floor(file_size_bytes / (1024*1024)));
+			return false
+		}
 
+	// elemenmts
+		const progress_info = progress_bar_container.querySelector('.progress_info')
+		const progress_line = progress_bar_container.querySelector('.progress_line')
 
 	// FormData build
 		const fd = new FormData();
@@ -161,221 +187,115 @@ tool_upload.prototype.upload_file = async function(e, content_data) {
 			  fd.append("component_tipo", 	self.caller.tipo);
 			  fd.append("section_tipo", 	self.caller.section_tipo);
 			  fd.append("section_id", 		self.caller.section_id);
-			  fd.append("quality", 			self.caller.default_target_quality);
+			  fd.append("quality", 			self.caller.context.default_target_quality);
 			  // file
-			  fd.append("fileToUpload", e.target.files[0]);
+			  fd.append("fileToUpload", file);
 
-			  	// console.log("fd:",fd);
-			  	// console.log("self:",self);
-			  	// return;
+	// upload_loadstart
+		const upload_loadstart = function(evt) {
+			preview_image.src 	   = ''
+			progress_line.value    = 0;
+			response_msg.innerHTML = '<span class="blink">Loading file '+file.name+'</span>'
+		}//end upload_loadstart
 
-	try {
+	// upload_load
+		const upload_load = function(evt) {
+			response_msg.innerHTML = '<span class="blink">Processing file '+file.name+'</span>'
+		}//end upload_load
 
+	// upload_error
+		const upload_error = function(evt) {
+			response_msg.innerHTML = `<span class="error">${get_label.error_al_subir_el_archivo} ${file.name}</span>`
+		}//end upload_error
+
+	// upload_abort
+		const upload_abort = function(evt) {
+			response_msg.innerHTML = '<span class="error">User aborts upload</span>'
+		}//end upload_abort
+
+	// upload_progress
+		const upload_progress = function(evt) {
+			const percent = parseInt(evt.loaded/evt.total*100);
+			// info line show numerical percentage of load
+		    progress_info.innerHTML = 'Upload progress: ' + percent + ' %'
+		    // progress line show graphic percentage of load
+		    progress_line.value = percent
+		}//end upload_progress
+
+	// xhr_load
+		const xhr_load = function(evt) {
+
+			// parse response string as JSON
+				const response = JSON.parse(evt.target.response);
+				if (!response) {
+					console.error("Error in XMLHttpRequest load response. Nothing is received");
+					return false
+				}
+
+			// debug
+				if(SHOW_DEBUG===true) {
+					console.log("upload_file.XMLHttpRequest load response:", response);
+				}
+
+			// print message
+				response_msg.innerHTML = response.msg
+
+			// preview image
+				if (response.preview_url) {
+					preview_image.src = response.preview_url + '?' + (new Date().getTime())
+				}
+
+			// refresh component
+				self.caller.refresh().then((refresh_reponse)=>{
+
+				})
+
+			// close modal. On close, modal destroy the caller (this tool)
+				// setTimeout(()=>{
+				// 	const modal_container = document.querySelector('dd-modal')
+				// 	if (modal_container) {
+				// 		modal_container._closeModal()
+				// 	}
+				// }, 20000)
+
+			return true
+		}//end xhr_load
+
+
+	// XMLHttpRequest
 		const xhr = new XMLHttpRequest();
 
-				// progress event
-					// xhr.upload.addEventListener("progress", tool_upload.uploadProgress, false);
-					xhr.addEventListener("progress", function(e) {
-						upload_progress(e, content_data)
-					}, false);
+		// upload
+			// upload_loadstart (the upload begins)
+			xhr.upload.addEventListener("loadstart", upload_loadstart, false);
 
-				// load complete event
-					xhr.addEventListener("load", function(e) {
+			// upload_load file (the upload ends successfully)
+			xhr.upload.addEventListener("load", upload_load, false);
 
-						// parse response string as JSON
-							const response = JSON.parse(e.target.response);
-							if (!response) {
-								console.error("Error in XMLHttpRequest load response. Nothing is received");
-								return false
-							}
+			// upload_error (the upload ends in error)
+			xhr.upload.addEventListener("error", upload_error, false);
 
-						// debug
-							if(SHOW_DEBUG===true) {
-								console.log("upload_file.XMLHttpRequest load response:", response);
-							}
+			// upload_abort (the upload has been aborted by the user)
+			xhr.upload.addEventListener("abort", upload_abort, false);
 
-						// print message
-							response_container.innerHTML = response.msg
+			// progress
+			xhr.upload.addEventListener("progress", upload_progress, false);
 
-						// refresh component
-							self.caller.refresh().then((refresh_reponse)=>{
+		// hxr
+			// xhr_load (the XMLHttpRequest ends successfully)
+			xhr.addEventListener("load", xhr_load, false);
 
-							})
+			// open connection
+			xhr.open("POST", self.trigger_url, true);
 
-						// close modal. On close, modal destroy the caller (this tool)
-							setTimeout(()=>{
-								const modal_container = document.querySelector('dd-modal')
-								if (modal_container) {
-									modal_container._closeModal()
-								}
-							}, 5000)
+			// request header
+			xhr.setRequestHeader("X-File-Name", file.name);
 
-
-					}, false);
-
-				// failed event
-					// xhr.addEventListener("error", tool_upload.uploadFailed, false);
-
-				// cancel event
-					// xhr.addEventListener("abort", tool_upload.uploadCanceled, false);
-
-				// open connection
-					xhr.open("POST", self.trigger_url);
-
-				// send data
-					xhr.send(fd);
-
-		// intervalTimer = setInterval( tool_upload.updateTransferSpeed, 1000 );
-
-		// hide button submit
-		// document.getElementById('btn_upload').style.display = 'none';
-
-	}catch(error) {
-		console.error('ERROR uploadFile: ', error);
-	}
+			// send data
+			xhr.send(fd);
 
 
 	return true
 }//end upload_file
-
-
-
-/**
-* UPLOAD_PROGRESS
-*/
-const upload_progress = function(evt, content_data) {
-
-		console.log("++++++++++++++++++++++ upload_progress evt:",evt);
-
-		return
-
-	try {
-
-		if (evt.lengthComputable) {
-
-			bytesUploaded = evt.loaded;
-
-			bytesTotal = evt.total;
-
-			let percentComplete = Math.round(evt.loaded * 100 / evt.total);
-
-			// bytesTransfered
-				let bytesTransfered = '';
-
-				if (bytesUploaded > 1024*1024)
-					bytesTransfered = (Math.round(bytesUploaded * 100/(1024*1024))/100).toString() + 'MB';
-				else if (bytesUploaded > 1024)
-					bytesTransfered = (Math.round(bytesUploaded * 100/1024)/100).toString() + 'KB';
-				else
-					bytesTransfered = (Math.round(bytesUploaded * 100)/100).toString() + 'Bytes';
-
-
-			document.getElementById('progressNumber').innerHTML = percentComplete.toString() + '%';
-			document.getElementById('progressBar').style.width = (percentComplete * 3.9).toString() + 'px';
-			document.getElementById('transferBytesInfo').innerHTML = bytesTransfered;
-
-			if (percentComplete == 100) {
-
-				if(document.getElementById('progressInfo'))
-				document.getElementById('progressInfo').style.display = 'none';
-
-				const 	uploadResponse 		 		 = document.getElementById('uploadResponse');
-						uploadResponse.innerHTML 	 = '<div class="please_wait blink">'+get_label.por_favor_espere+'</div>';
-						uploadResponse.style.display = 'block';
-
-				// Redimensiona ventana
-				tool_upload.resize_window();
-			}
-
-		}else{
-
-			document.getElementById('progressBar').innerHTML = 'Unable to compute';
-		}
-
-	}catch(error) {
-		console.error('ERROR upload_progress:', error)
-	}
-
-	return true
-
-}//end upload_progress
-
-
-
-
-
-// /**
-// * UPLOAD_FILE
-// */
-// tool_upload.prototype.upload_file9999999 = async function(file) {
-
-// 	const self = this
-
-// 	uploading = 'si';
-
-// 	clearInterval(intervalTimer);
-// 	intervalTimer = 0;
-
-// 	previousBytesLoaded = 0;
-// 	document.getElementById('uploadResponse').style.display = 'none'
-// 	document.getElementById('progressNumber').innerHTML 	= ''
-
-// 	const progressBar = document.getElementById('progressBar')
-// 		  progressBar.style.display = 'block'
-// 		  progressBar.style.width   = '0px'
-
-
-// 	// If you want to upload only a file along with arbitary data that is not in the form, use this:
-
-// 		// var fd = new FormData();
-
-// 		// fd.append("author", "Shiv Kumar");
-
-// 		// fd.append("name", "Html 5 File API/FormData");
-
-// 		// fd.append("fileToUpload", document.getElementById('fileToUpload').files[0]);
-
-
-// 	// If you want to simply post the entire form, use this:
-
-// 		// var fd = document.getElementById('form_upload').getFormData();
-
-
-// 	const fd = new FormData(document.getElementById('form_upload'));
-
-// 	const validacion = tool_upload.validar_formulario();
-// 	if(validacion!==true) return false;
-
-
-// 	try {
-
-// 		const 	xhr = new XMLHttpRequest();
-
-// 				xhr.upload.addEventListener("progress", tool_upload.uploadProgress, false);
-
-// 				xhr.addEventListener("load", tool_upload.uploadComplete, false);
-
-// 				xhr.addEventListener("error", tool_upload.uploadFailed, false);
-
-// 				xhr.addEventListener("abort", tool_upload.uploadCanceled, false);
-
-// 				xhr.open("POST", trigger_url);
-
-// 				xhr.send(fd);
-
-// 		intervalTimer = setInterval( tool_upload.updateTransferSpeed, 1000 );
-
-// 		// hide button submit
-// 		document.getElementById('btn_upload').style.display = 'none';
-
-// 	}catch(error) {
-// 		console.error('ERROR uploadFile:')
-// 		console.error(error)
-// 	}
-
-// 	// tool_upload.resize_window()
-
-// 	return true
-// }//end upload_file
 
 
