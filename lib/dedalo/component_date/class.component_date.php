07692e77298c98e756f513447cc53736d2de8e43
Alejandro Peña Carbonell <alex@render.es>
2020-01-31 17:44:41 +0100

    Fix the search of the dates with ranges of years, months, days.

--- lib/dedalo/component_date/class.component_date.php
+++ lib/dedalo/component_date/class.component_date.php
@@ -1177,23 +1177,26 @@ class component_date extends component_common {
 		}
 
 		# Date
+		# the calculation of the seconds for the end of the period always need to be seconds -1 
+		# ex: year 2000 in seconds is: start = 64281600000 end = 64313740800 -1 or 64313740799 
+		# because 64313740800 = 2001
 		if (isset($dd_date->day)) {
 
 			$dd_date_clone = clone($dd_date);
 			$dd_date_clone->day = $dd_date_clone->day+1;
-			$final_search_range_seconds = dd_date::convert_date_to_seconds($dd_date_clone);
+			$final_search_range_seconds = dd_date::convert_date_to_seconds($dd_date_clone)-1;
 
 		}elseif (isset($dd_date->month)) {
 
 			$dd_date_clone = clone($dd_date);
 			$dd_date_clone->month = $dd_date_clone->month+1;
-			$final_search_range_seconds = dd_date::convert_date_to_seconds($dd_date_clone);
+			$final_search_range_seconds = dd_date::convert_date_to_seconds($dd_date_clone)-1;
 
 		}elseif (isset($dd_date->year)) {
 
 			$dd_date_clone = clone($dd_date);
 			$dd_date_clone->year = $dd_date_clone->year+1;
-			$final_search_range_seconds = dd_date::convert_date_to_seconds($dd_date_clone);
+			$final_search_range_seconds = dd_date::convert_date_to_seconds($dd_date_clone)-1;
 
 		}
 
