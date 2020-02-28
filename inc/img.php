11bcd3510bc3d2b37c6bf0870cedb4d64de3946a
Juan Francisco Onielfa Veneros <paco@render.es>
2020-02-28 13:41:00 +0100

    Clean code

--- inc/img.php
+++ inc/img.php
@@ -27,7 +27,7 @@ require_once(dirname(__FILE__).'/class.Thumb.php');
 $thumb 		= new Thumb($source);
 
 
-# FX 
+# FX
 
 	$req_w = isset($_REQUEST['w']) ? safe_xss($_REQUEST['w']) : false;
 	$req_h = isset($_REQUEST['h']) ? safe_xss($_REQUEST['h']) : false;
@@ -35,34 +35,34 @@ $thumb 		= new Thumb($source);
 
 	# crop image
 	if($fx=='crop') {
-		
+
 		# width
 		if(!isset($w)) $w = 64;
 		if($req_w!==false) $w = $req_w;
-		
+
 		# height
 		if(!isset($h)) $h = 48;
 		if($req_h!==false) $h = $req_h;
-		
+
 		# position
 		if(!isset($p)) $p = 'center';
 		if($req_p!==false) $p = $req_p;
-		
+
 		$thumb->crop($cwidth=$w, $cheight=$h, $pos=$p);
 	}
-	
+
 
 	# resize image
-	if($fx=='resize') {		
-		
+	if($fx=='resize') {
+
 		# value max
 		if(!isset($w)) $w = 484;
 		if($req_w!==false) $w = $req_w;
-		
+
 		# property (widht or height)
 		if(!isset($h)) $h = 390;
 		if($req_h!==false) $h = $req_h;
-		
+
 		#$thumb->resize($value=$rv, $prop=$rp);
 		$thumb->resizeWithLimits($maxWidht=$w,$maxHeight=$h) ;
 	}
@@ -80,6 +80,4 @@ header('Content-Type: image/jpeg');
 # show created image raw code without save the file
 $thumb->show();
 
-exit();
 
-?>
