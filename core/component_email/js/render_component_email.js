c76af3e6af7d5219226f7f6888e697cc55433062
Lourdes Veros Álvarez <lur@render.es>
2020-04-01 11:24:10 +0200

    Fixed remove and send email buttons visibility changes.

--- core/component_email/js/render_component_email.js
+++ core/component_email/js/render_component_email.js
@@ -158,6 +158,26 @@ const add_events = function(self, wrapper) {
 		wrapper.addEventListener("click", e => {
 			// e.stopPropagation()
 
+			// reset remove and email_send buttons view
+				const all_buttons_remove = wrapper.querySelectorAll('.remove, .email_send')
+					for (let i = all_buttons_remove.length - 1; i >= 0; i--) {
+						all_buttons_remove[i].classList.add("display_none")
+					}
+
+			// show current remove button
+				if (e.target.matches('input[type="text"')) {
+					// set the button_remove and button_email_send associated to the input selected to visible
+						const button_remove = e.target.parentNode.querySelector('.remove')
+						if (button_remove) {
+							button_remove.classList.remove("display_none")
+						}
+
+						const button_email_send = e.target.parentNode.querySelector('.email_send')
+						if (button_email_send) {
+							button_email_send.classList.remove("display_none")
+						}
+				}
+
 			// insert
 			if (e.target.matches('.button.add')) {
 
@@ -200,13 +220,6 @@ const add_events = function(self, wrapper) {
 
 				return true
 			}
-
-			// if (e.target.matches('.button.close')) {
-			// 	//change mode
-			// 	self.change_mode()
-
-			// 	return true
-			// }
 
 			if (e.target.matches('.button.email_send')) {
 
@@ -417,12 +430,12 @@ const get_input_element_edit = (i, current_value, inputs_container, self) => {
 
 	// button remove
 		if((mode==='edit' || 'edit_in_list') && !is_inside_tool){
-		const button_remove = ui.create_dom_element({
-			element_type	: 'div',
-			class_name 		: 'button remove display_none',
-			dataset			: { key : i },
-			parent 			: li
-		})
+			const button_remove = ui.create_dom_element({
+				element_type	: 'div',
+				class_name 		: 'button remove display_none',
+				dataset			: { key : i },
+				parent 			: li
+			})
 
 			// button email
 			const button_email = ui.create_dom_element({
