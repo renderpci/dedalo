957db665025345925479438f3f576cd8420c75dd
Lourdes Veros Álvarez <lur@render.es>
2020-04-01 18:34:46 +0200

    Modify prototypes, adding new ones and removing not needed.

--- core/component_radio_button/js/component_radio_button.js
+++ core/component_radio_button/js/component_radio_button.js
@@ -32,24 +32,27 @@ export const component_radio_button = function(){
 * extend component functions from component common
 */
 // prototypes assign
+	// lifecycle
 	component_radio_button.prototype.init 	 			= component_common.prototype.init
 	component_radio_button.prototype.build 				= component_common.prototype.build
-	component_radio_button.prototype.render 			= common.prototype.render
-	component_radio_button.prototype.destroy 	 		= common.prototype.destroy
+	component_radio_button.prototype.render 			= common.prototype.render	
 	component_radio_button.prototype.refresh 			= common.prototype.refresh
+	component_radio_button.prototype.destroy 	 		= common.prototype.destroy
+
+	// change data
 	component_radio_button.prototype.save 	 			= component_common.prototype.save
-	component_radio_button.prototype.load_data 			= component_common.prototype.load_data
-	component_radio_button.prototype.get_value 			= component_common.prototype.get_value
-	component_radio_button.prototype.set_value 			= component_common.prototype.set_value
 	component_radio_button.prototype.update_data_value 	= component_common.prototype.update_data_value
 	component_radio_button.prototype.update_datum		= component_common.prototype.update_datum
 	component_radio_button.prototype.change_value 		= component_common.prototype.change_value
 
-	// render
+	// render	
 	component_radio_button.prototype.list 				= render_component_radio_button.prototype.list
 	component_radio_button.prototype.edit 				= render_component_radio_button.prototype.edit
 	component_radio_button.prototype.edit_in_list		= render_component_radio_button.prototype.edit
+	component_radio_button.prototype.tm					= render_component_radio_button.prototype.edit
+	component_radio_button.prototype.search 			= render_component_radio_button.prototype.search
 	component_radio_button.prototype.change_mode 		= component_common.prototype.change_mode
+
 
 
 /**
