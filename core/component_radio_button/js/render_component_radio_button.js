64166348983561f210679b26cd803054077ba826
Lourdes Veros Álvarez <lur@render.es>
2020-04-02 10:23:01 +0200

    Fixed rendering for use in search.

--- core/component_radio_button/js/render_component_radio_button.js
+++ core/component_radio_button/js/render_component_radio_button.js
@@ -51,7 +51,7 @@ render_component_radio_button.prototype.list = async function() {
 
 /**
 * EDIT
-* Render node for use in edit
+* Render node for use in modes: edit, edit_in_list
 * @return DOM node
 */
 render_component_radio_button.prototype.edit = async function(options={render_level:'full'}) {
@@ -62,7 +62,7 @@ render_component_radio_button.prototype.edit = async function(options={render_le
 		self.data.value = (self.data.value.length<1) ? [null] : self.data.value
 
 	// render_level
-		const render_level = options.render_level
+		const render_level = options.render_level || 'full'
 
 	// content_data
 		const content_data = await get_content_data_edit(self)
@@ -79,7 +79,21 @@ render_component_radio_button.prototype.edit = async function(options={render_le
 			buttons 	 : buttons
 		})
 
-	// events delegated
+	// events
+		add_events(self, wrapper)
+
+	return wrapper
+}//end edit
+
+
+
+/**
+* ADD_EVENTS
+* @return bool
+*/
+const add_events = function(self, wrapper) {
+
+// events delegated
 	// update value, subscription to the changes: if the dom input value was changed, observers dom elements will be changed own value with the observable value
 		self.events_tokens.push(
 			event_manager.subscribe('update_value_'+self.id, update_value)
@@ -186,14 +200,6 @@ render_component_radio_button.prototype.edit = async function(options={render_le
 					return true
 				}
 
-			// mode change
-				if (e.target.matches('.button.close')) {
-					//change mode
-					self.change_mode('list', true)
-
-					return true
-				}
-
 
 		},false)
 
@@ -211,8 +217,86 @@ render_component_radio_button.prototype.edit = async function(options={render_le
 			}
 		},true)
 
+
+}
+
+
+/**
+* SEARCH
+* Render node for use in search
+* @return DOM node wrapper
+*/
+render_component_radio_button.prototype.search = async function() {
+
+	const self = this
+
+	// fix non value scenarios
+		self.data.value = (self.data.value.length<1) ? [null] : self.data.value
+
+	const content_data = await get_content_data_search(self)
+
+	// ui build_edit returns component wrapper
+		const wrapper = ui.component.build_wrapper_edit(self, {
+			content_data : content_data
+		})
+
+	// id
+		wrapper.id = self.id
+
+	// Events
+
+		// change event, for every change the value in the imputs of the component
+			wrapper.addEventListener('change', (e) => {
+				e.stopPropagation()
+
+				// input_value. The standard input for the value of the component
+				if (e.target.matches('input[type="text"].input_value')) {
+					//get the input node that has changed
+					const input = e.target
+					//the dataset.key has the index of correspondence self.data.value index
+					const i 	= input.dataset.key
+					// set the selected node for change the css
+					self.selected_node = wrapper
+					// set the changed_data for replace it in the instance data
+					// update_data_value. key is the posistion in the data array, the value is the new value
+					const value = (input.value.length>0) ? input.value : null
+					// set the changed_data for update the component data and send it to the server for change when save
+					const changed_data = {
+						action	: 'update',
+						key	  	: i,
+						value 	: value
+					}
+					// update the data in the instance previous to save
+					self.update_data_value(changed_data)
+					// set the change_data to the instance
+					self.data.changed_data = changed_data
+					// event to update the dom elements of the instance
+					event_manager.publish('change_search_element', self)
+					return true
+				}
+
+				// q_operator. get the input value of the q_operator
+				// q_operator: is a separate operator used with components that is impossible mark the operator in the input_value,
+				// like; radio_button, check_box, date, autocomplete, etc
+				if (e.target.matches('input[type="text"].q_operator')) {
+					//get the input node that has changed
+					const input = e.target
+					// set the changed_data for replace it in the instance data
+					// update_data_value. key is the posistion in the data array, the value is the new value
+					const value = (input.value.length>0) ? input.value : null
+					// update the data in the instance previous to save
+					self.data.q_operator = value
+					// event to update the dom elements of the instance
+					event_manager.publish('change_search_element', self)
+					return true
+				}
+			}, false)
+
+
+
 	return wrapper
-}//end edit
+}//end search
+
 
 
 /**
@@ -240,7 +324,7 @@ const get_content_data_edit = async function(self) {
 		const value_compare = value.length>0 ? value[0] : null
 		const length = datalist.length
 		for (let i = 0; i < length; i++) {
-			input_element(i, datalist[i], inputs_container, self)
+			get_input_element_edit(i, datalist[i], inputs_container, self)
 		}
 
 	// buttons
@@ -251,10 +335,11 @@ const get_content_data_edit = async function(self) {
 		})
 
 	// content_data
-		const content_data = ui.component.build_content_data(self)
-			  content_data.classList.add("nowrap")
-			  content_data.appendChild(fragment)
-
+		const content_data = ui.component.build_content_data(self, {
+			autoload : true
+		})
+		
+		content_data.appendChild(fragment)
 
 	return content_data
 }//end get_content_data_edit
@@ -283,7 +368,7 @@ const get_buttons = (self) => {
 		}
 
 	// button reset
-		if((mode==='edit' || mode==='edit_in_list') && !is_inside_tool){
+		if(mode==='edit' || mode==='edit_in_list'){// && !is_inside_tool){
 			ui.create_dom_element({
 				element_type	: 'span',
 				class_name 		: 'button reset',
@@ -307,10 +392,10 @@ const get_buttons = (self) => {
 
 
 /**
-* INPUT_ELEMENT
+* GET_INPUT_ELEMENT_EDIT
 * @return dom element li
 */
-const input_element = (i, current_value, inputs_container, self) => {
+const get_input_element_edit = (i, current_value, inputs_container, self) => {
 
 	const value  		 	= self.data.value || []
 	const value_length   	= value.length
@@ -358,4 +443,83 @@ const input_element = (i, current_value, inputs_container, self) => {
 
 	return li
 
-}//end input_element
+}//end get_input_element_edit
+
+
+/**
+* GET_CONTENT_DATA_SEARCH
+* @return DOM node content_data
+*/
+const get_content_data_search = async function(self) {
+
+	const value 		= self.data.value
+	const mode 			= self.mode
+	const datalist 		= self.data.datalist
+
+	const fragment 			= new DocumentFragment()
+	const is_inside_tool 	= ui.inside_tool(self)
+
+	// values (inputs)
+		const value_compare = value.length>0 ? value[0] : null
+		const length = datalist.length
+		for (let i = 0; i < value_length; i++) {
+			get_input_element_search(i, datalist[i], fragment, self)
+		}
+
+	// content_data
+		const content_data = ui.component.build_content_data(self, {
+			autoload : true
+		})
+		
+		content_data.classList.add("nowrap")
+		content_data.appendChild(fragment)
+
+
+	return content_data
+}//end get_content_data_search
+
+
+
+/**
+* GET_INPUT_ELEMENT_SEARCH
+* @return dom element input
+*/
+const get_input_element_search = (i, current_value, inputs_container, self) => {
+
+	const datalist_item  	= current_value
+	const datalist_value 	= datalist_item.value
+
+	// q operator (search only)
+		const q_operator = self.data.q_operator
+		const input_q_operator = ui.create_dom_element({
+			element_type 	: 'input',
+			type 		 	: 'text',
+			value 		 	: q_operator,
+			class_name 		: 'q_operator',
+			parent 		 	: inputs_container
+		})
+
+	// // input field
+	// 	const input = ui.create_dom_element({
+	// 		element_type 	: 'input',
+	// 		type 		 	: 'text',
+	// 		class_name 		: 'input_value',
+	// 		dataset 	 	: { key : i },
+	// 		value 		 	: current_value,
+	// 		parent 		 	: inputs_container
+	// 	})
+
+			// input checkbox
+		const option = ui.create_dom_element({
+			element_type	: 'input',
+			type 			: 'radio',
+			id 				: self.id +"_"+ i,
+			dataset 	 	: { key : i },
+			value 			: JSON.stringify(datalist_value),
+			name 			: self.id,
+			parent 			: inputs_container
+		})
+
+
+	return input
+}//end get_input_element_search
