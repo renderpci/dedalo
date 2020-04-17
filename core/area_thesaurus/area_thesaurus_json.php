684f7b874e30a0e707b0993e698d1be857234177
Juan Francisco Onielfa Veneros <paco@render.es>
2020-04-17 18:19:16 +0200

    Fix error on set from_parent_tipo (now is mandatory for areas too)

--- core/area_thesaurus/area_thesaurus_json.php
+++ core/area_thesaurus/area_thesaurus_json.php
@@ -15,6 +15,9 @@
 
 
 	if($options->get_context===true){
+
+		// set self from_parent
+			$this->from_parent = $tipo;
 			
 		// Component structure context (tipo, relations, properties, etc.)
 			$current_context = $this->get_structure_context($permissions, $sqo_context=true);
