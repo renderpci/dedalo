36f686f1859b3fd47a9c4271a117fd5382475f1c
Alejandro Peña Carbonell <alex@render.es>
2020-04-22 12:19:21 +0200

    Add tool pdf_extarctor

---
+++ core/tools/tool_pdf_extractor/class.tool_pdf_extractor.php
@@ -0,0 +1,119 @@
+<?php
+/*
+* CLASS TOOL_UPLOAD
+*
+*
+*/
+class tool_upload { // extends tool_common
+
+
+
+	/**
+	* __CONSTRUCT
+	*/
+	public function __construct($component_obj, $modo='button') {
+
+		// # Fix modo
+		// $this->modo = $modo;
+
+		// # Para unificar el acceso, se copia el componente a $this->component_obj
+		// $this->component_obj 	= $component_obj;
+
+		// # Fix component
+		// $this->source_component = $component_obj;
+		// $this->source_component->set_modo('tool_upload');
+		// #$this->source_component->set_variant( tool_upload::$source_variant );
+		// 	#dump($component_obj,'component_obj');
+
+		// $this->section_tipo = $component_obj->get_section_tipo();
+	}//end __construct
+
+
+
+	/**
+	* GET_SYSTEM_INFO
+	* @return
+	*/
+	public static function get_system_info() {
+
+		// Returns a file size limit in bytes based on the PHP upload_max_filesize
+		// and post_max_size
+		function file_upload_max_size() {
+		  static $max_size = -1;
+
+		  if ($max_size < 0) {
+			// Start with post_max_size.
+			$post_max_size = parse_size(ini_get('post_max_size'));
+			if ($post_max_size > 0) {
+			  $max_size = $post_max_size;
+			}
+
+			// If upload_max_size is less, then reduce. Except if upload_max_size is
+			// zero, which indicates no limit.
+			$upload_max = parse_size(ini_get('upload_max_filesize'));
+			if ($upload_max > 0 && $upload_max < $max_size) {
+			  $max_size = $upload_max;
+			}
+		  }
+		  return $max_size;
+		}
+
+		function parse_size($size) {
+		  $unit = preg_replace('/[^bkmgtpezy]/i', '', $size); // Remove the non-unit characters from the size.
+		  $size = preg_replace('/[^0-9\.]/', '', $size); // Remove the non-numeric characters from the size.
+		  if ($unit) {
+			// Find the position of the unit in the ordered string which is the power of magnitude to multiply a kilobyte by.
+			return round($size * pow(1024, stripos('bkmgtpezy', $unit[0])));
+		  }
+		  else {
+			return round($size);
+		  }
+		}
+
+
+		$upload_tmp_dir = ini_get('upload_tmp_dir');
+
+		$system_info = new stdClass();
+			$system_info->max_size_bytes 		= file_upload_max_size();
+			$system_info->sys_get_temp_dir 		= sys_get_temp_dir();
+			$system_info->upload_tmp_dir 		= $upload_tmp_dir;
+			$system_info->upload_tmp_perms 		= fileperms($upload_tmp_dir);
+			$system_info->session_cache_expire  = (int)ini_get('session.cache_expire');
+
+
+
+		return $system_info;
+	}//end get_system_info
+
+
+
+	/**
+	* ERROR_NUMBER_TO_TEXT
+	* @param $f_error_number int
+	* @return $f_error_text strint
+	*/
+	public static function error_number_to_text($f_error_number) {
+
+		if( $f_error_number===0 ) {
+						# all is ok
+						$f_error_text = label::get_label('archivo_subido_con_exito');
+		}else{
+			switch($f_error_number) {
+						# Error by number
+				case 1 : $f_error_text = label::get_label('el_archivo_subido_excede_de_la_directiva');	break;
+				case 2 : $f_error_text = label::get_label('el_archivo_subido_excede_el_tamano_maximo');	break;
+				case 3 : $f_error_text = label::get_label('el_archivo_subido_fue_solo_parcialmente_cargado');	break;
+				case 4 : $f_error_text = label::get_label('ningun_archivo_fue_subido');	break;
+				case 6 : $f_error_text = label::get_label('carpeta_temporal_no_accesible');	break;
+				case 7 : $f_error_text = label::get_label('no_se_pudo_escribir_el_archivo_en_el_disco');	break;
+				case 8 : $f_error_text = label::get_label('una_extension_de_php_detuvo_la_carga_de_archivos');	break;
+			}
+		}
+
+		return $f_error_text;
+	}//end error_number_to_text
+
+
+
+
+}//end class
---
+++ core/tools/tool_pdf_extractor/js/render_tool_pdf_extractor.js
@@ -0,0 +1,171 @@
+/*global get_label, page_globals, SHOW_DEBUG, DEDALO_CORE_URL*/
+/*eslint no-undef: "error"*/
+
+
+
+// imports
+	import {event_manager} from '../../../common/js/event_manager.js'
+	import {ui} from '../../../common/js/ui.js'
+
+
+
+/**
+* RENDER_TOOL_UPLOAD
+* Manages the component's logic and apperance in client side
+*/
+export const render_tool_pdf_extractor = function() {
+
+	return true
+}//end render_tool_pdf_extractor
+
+
+
+/**
+* RENDER_TOOL_upload
+* Render node for use like button
+* @return DOM node
+*/
+render_tool_pdf_extractor.prototype.edit = async function (options={render_level:'full'}) {
+
+	const self = this
+
+	const render_level 	= options.render_level
+
+	// content_data
+		const current_content_data = await get_content_data(self)
+		if (render_level==='content') {
+			return current_content_data
+		}
+
+	// wrapper. ui build_edit returns component wrapper
+		const wrapper = await ui.tool.build_wrapper_edit(self, {
+			content_data : current_content_data
+		})
+
+	// // buttons container
+	// 	const buttons_container = ui.create_dom_element({
+	// 		element_type	: 'div',
+	// 		class_name 		: 'buttons_container',
+	// 		parent 			: wrapper
+	// 	})
+
+
+	// tool_container
+		//const tool_container = document.getElementById('tool_container')
+		//if(tool_container!==null){
+		//	tool_container.appendChild(wrapper)
+		//}else{
+		//	const main = document.getElementById('main')
+		//	const new_tool_container = ui.create_dom_element({
+		//		id 				: 'tool_container',
+		//		element_type	: 'div',
+		//		parent 			: main
+		//	})
+		//	new_tool_container.appendChild(wrapper)
+		//}
+
+	// modal container
+		const header = wrapper.querySelector('.tool_header')
+		const modal  = ui.attach_to_modal(header, wrapper, null)
+		modal.on_close = () => {
+			self.destroy(true, true, true)
+		}
+
+	// events
+		// click
+			// wrapper.addEventListener("click", function(e){
+			// 	e.stopPropagation()
+			// 	console.log("e:",e);
+			// 	return
+			// })
+
+
+	return wrapper
+}//end render_tool_pdf_extractor
+
+
+
+/**
+* GET_CONTENT_DATA
+* @return DOM node content_data
+*/
+const get_content_data = async function(self) {
+
+
+	const fragment = new DocumentFragment()
+
+
+	// range page
+		const page_range = ui.create_dom_element({
+			element_type	: 'div',
+			class_name		: 'page_in',
+			text_content 	: '',
+			parent 			: fragment
+		})
+
+		// page_in
+			const page_in = ui.create_dom_element({
+				element_type	: 'input',
+				type 			: 'text',
+				class_name		: 'page_in',
+				text_content 	: '',
+				parent 			: page_range
+			})
+		// page_out
+			const page_in = ui.create_dom_element({
+				element_type	: 'input',
+				type 			: 'text',
+				class_name		: 'page_out',
+				text_content 	: '',
+				parent 			: page_range
+			})
+
+
+
+
+
+	// response_container
+		const response_container = ui.create_dom_element({
+			element_type	: 'div',
+			class_name		: 'response_container',
+			parent 			: fragment
+		})
+		// response_msg
+		const response_msg = ui.create_dom_element({
+			element_type	: 'div',
+			class_name		: 'response_msg',
+			parent 			: response_container
+		})
+
+	// info
+		// container info
+		const info = ui.create_dom_element({
+			element_type	: 'div',
+			class_name 		: 'info',
+			// text_content 	: '',
+			parent 			: fragment
+		})
+		// caller component
+		ui.create_dom_element({
+			element_type	: 'div',
+			inner_html	 	: '<label>Caller component</label>' + self.caller.model,
+			parent 			: info
+		})
+
+
+
+	// // buttons container
+	// 	const buttons_container = ui.create_dom_element({
+	// 		element_type	: 'div',
+	// 		class_name 		: 'buttons_container',
+	// 		parent 			: components_container
+	// 	})
+
+	// content_data
+		const content_data = document.createElement("div")
+			  content_data.classList.add("content_data", self.type)
+			  content_data.appendChild(fragment)
+
+
+	return content_data
+}//end get_content_data
