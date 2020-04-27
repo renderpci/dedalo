da5ecada95eb52f5db661a7d80f8c0a8c9c3b532
Juan Francisco Onielfa Veneros <paco@render.es>
2020-04-27 13:50:44 +0200

    Add tool common constructor

--- tools/tool_common/class.tool_common.php
+++ tools/tool_common/class.tool_common.php
@@ -1,39 +1,58 @@
 <?php
 /*
-* CLASS TOOL_UPLOAD
-*
+* CLASS TOOL_COMMON
+* Add basic methods for general use in tools
 *
 */
-abstract class tool_common{
+abstract class tool_common {
 
-    public $name;
-    public $config;
 
-    /**
-    * get_config
-    * @return
-    */
-    public function get_config() {
 
-        $tool_name = $this->name:
+	public $name;
+	public $config;
 
-        // get all tools config sections
+
+	/**
+	* __CONSTRUCT
+	* @return bool true
+	*/
+	public function __construct() {
+		
+		// set tool name as class name
+		$this->name = get_called_class();
+
+		return true;
+	}//end __construct
+
+
+
+	/**
+	* GET_CONFIG
+	* @return object | null
+	*/
+	public function get_config() {
+
+		$tool_name = $this->name;
+
+		// get all tools config sections
 			$ar_config = tools_register::get_all_config_tool();
 
-        // append config
-        $ar_tool_config = array_filter($ar_config, function($item) use($tool_name){
-            if($item->name === $tool_name) {
-                return $item;
-            }
-        });
-        $config = !empty($ar_tool_config[0])
-            ? $ar_tool_config[0]->config
-            : null;
+		// select current from all tool config
+			$ar_config = array_filter($ar_config, function($item) use($tool_name){
+				if($item->name===$tool_name) {
+					return $item;
+				}
+			});
+		
+		$config = !empty($ar_config[0])
+			? $ar_config[0]->config
+			: null;
 
-        $this->config = $config;
+		// fix value
+			$this->config = $config;
 
-        return $config;
-    };//end get_config
+		return $config;
+	}//end get_config
 
 
 }
