e88ea6a121048503330767e3435ecffddd68d371
Juan Francisco Onielfa Veneros <paco@render.es>
2020-05-02 17:55:20 +0200

    Improves  widget model in get_archive_weights

---
+++ core/component_info/widgets/widget_common.js
@@ -0,0 +1,46 @@
+/*global get_label, page_globals, SHOW_DEBUG*/
+/*eslint no-undef: "error"*/
+
+
+
+// imports
+	// import {event_manager} from '../../common/js/event_manager.js'
+	// import {data_manager} from '../../common/js/data_manager.js'
+	// import * as instances from '../../common/js/instances.js'
+	// import {create_source} from '../../common/js/common.js'
+	// import {ui} from '../../common/js/ui.js'
+
+
+
+export const widget_common = function(){
+
+	return true
+}//end widget_common
+
+
+
+/**
+* INIT
+* Common init prototype to use in components as default
+* @return bool true
+*/
+widget_common.prototype.init = async function(options) {
+
+	const self = this
+
+	// set vars
+		self.id				= options.section_tipo
+		self.section_tipo	= options.section_tipo
+		self.section_id		= options.section_id
+		self.lang			= options.lang
+		self.mode			= options.mode
+		self.value			= options.value
+
+	// status update
+		self.status = 'inited'
+
+	
+	return true
+}//end init
+
+
--- core/extras/numisdata/widgets/get_archive_weights/js/get_archive_weights.js
+++ core/extras/numisdata/widgets/get_archive_weights/js/get_archive_weights.js
@@ -5,7 +5,7 @@
 
 // imports
 	// import {data_manager} from '../../common/js/data_manager.js'
-	// import {common,create_source} from '../../common/js/common.js'
+	import {widget_common} from '../../component_info/widgets/widget_common.js'
 	import {render_get_archive_weights} from '../../get_archive_weights/js/render_get_archive_weights.js'
 
 
@@ -17,8 +17,13 @@ export const get_archive_weights = function(){
 	this.section_tipo
 	this.section_id
 	this.lang
+	this.mode
+
 	this.value
+
 	this.node
+
+	this.status
 
 	return true
 }//end get_archive_weights
@@ -27,114 +32,63 @@ export const get_archive_weights = function(){
 
 /**
 * COMMON FUNCTIONS
-* extend component functions from component common
+* extend functions from common
 */
 // prototypes assign
 	// lifecycle
-	// get_archive_weights.prototype.init 	 			= component_common.prototype.init
-	// get_archive_weights.prototype.build 	 			= component_common.prototype.build
-	get_archive_weights.prototype.render 				= common.prototype.render
-	// get_archive_weights.prototype.refresh 			= common.prototype.refresh
-	// get_archive_weights.prototype.destroy 	 		= common.prototype.destroy
-
-	// // change data
-	// get_archive_weights.prototype.save 	 			= component_common.prototype.save
-	// get_archive_weights.prototype.update_data_value	= component_common.prototype.update_data_value
-	// get_archive_weights.prototype.update_datum 		= component_common.prototype.update_datum
-	// get_archive_weights.prototype.change_value 		= component_common.prototype.change_value
-
+	// get_archive_weights.prototype.init 	 	= component_common.prototype.init
 	// render
-	get_archive_weights.prototype.list 					= render_get_archive_weights.prototype.list
-	get_archive_weights.prototype.edit 					= render_get_archive_weights.prototype.edit
-	// get_archive_weights.prototype.edit_in_list		= render_get_archive_weights.prototype.edit
-	// get_archive_weights.prototype.tm					= render_get_archive_weights.prototype.edit
-	// get_archive_weights.prototype.search 			= render_get_archive_weights.prototype.search
-	// get_archive_weights.prototype.change_mode 		= component_common.prototype.change_mode
+	get_archive_weights.prototype.edit 			= render_get_archive_weights.prototype.edit
 
 
 
 /**
-* ACTIVE
-* Custom active function triggered after ui.active has finish
+* INIT
 */
-get_archive_weights.prototype.active = function() {
-
-	//console.log("Yujuu! This is my component custom active test triggered after ui.active. id:", this.id )
-
-	return true
-}//end active
-
-
-
-/**
-* IS_UNIQUE
-* Check the value of the input_text with the all values in the database
-* @result bool
-* result:
-* 	true : unique value, it not has any record inside the section.
-* 	false: the value has almost 1 record inside the database, but it is not unique.
-*/
-get_archive_weights.prototype.is_unique = async function(new_value){
+get_archive_weights.prototype.init = async function(options) {
 
 	const self = this
 
-	if (new_value.length<1) return false
+	// call the generic commom init
+		const common_init = widget_common.prototype.init.call(this, options);
 
-	const unique_config = self.context.properties.unique
+	
+	// load dependences js/css
+		const load_promises = []
 
-	// search item rebuild filter q param and others
-		const sqo = self.sqo_context.search.find(item => item.typo==='sqo')
-		// set limit on sqo
-		sqo.limit = 1
-		// set skip_projects_filter as true
-		sqo.skip_projects_filter = true
+		// css file load
+			const lib_css_file = '../css/get_archive_weights.css'
+			load_promises.push( common.prototype.load_style(lib_css_file) )
 
-		const filter = sqo.filter['$and']
+	const js_promise = Promise.all(load_promises)
 
-		// filter item (expected one, under '$and' operator)
-			const filter_item = filter[0]
-			// add received value to filter
-			filter_item.q = "=" + new_value
-			// set the split words by spaces to false
-			filter_item.q_split = false
 
-		// exclude self record of search once
-			if (filter.length===1) {
-				const new_item_filter = {
-					q 	 : '!=' + self.section_id,
-					path : [
-						{
-							component_tipo  : 'section_id',
-							modelo 			: 'component_section_id',
-							name 			: 'Dummy section id',
-							section_tipo 	: [self.section_tipo]
-						}
-					]
-				}
-				filter.push(new_item_filter)
-			}
+	return js_promise
+}//end init
 
-			//console.log("filter_item:",filter_item);
-			//console.log("new_item_filter:",new_item_filter);
-			// console.log("sqo:",sqo);
 
-	// add self context as the unique ddo
-		//self.sqo_context.search.push(self.context)
 
-	// load data
-		const current_data_manager 	= new data_manager()
-		const api_response 			= await current_data_manager.section_load_data(self.sqo_context.search)
-		const data 					= api_response.result.data
+/**
+* RENDER
+*/
+get_archive_weights.prototype.render = async function(options={render_level:'full'}) {
 
-	// record data results from search
-		const record = data.find(item => item.tipo===self.tipo)
+	const self = this
 
-	// result. If results are found, value is NOT unique
-		//const result = (typeof record==="undefined") ? true : false
+	const render_level 	= options.render_level || 'full'
+	const render_mode 	= self.mode || 'edit'
 
-		if(SHOW_DEBUG===true) {
-			console.log("+++++ is_unique api_response data:",data, record);
-		}
+	const node = await self[render_mode]({
+		render_level : render_level
+	})
 
-	return record
-}//end is_unique
+	self.node.push(node)
+
+	return node
+}//end render
+
+
+
+
+
+
