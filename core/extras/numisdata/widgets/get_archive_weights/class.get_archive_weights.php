a68d247e31c5b3933f189b247f13d835de941a7f
Alejandro Peña Carbonell <alex@render.es>
2020-05-02 16:30:54 +0200

    Added widget get_archive_weights in alpha state

---
+++ core/extras/numisdata/widgets/get_archive_weights/class.get_archive_weights.php
@@ -0,0 +1,170 @@
+<?php
+
+
+/*
+* CLASS GET_ARCHIVE_WEIGHTS
+*
+*
+*/
+class get_archive_weights extends widget_common {
+
+	/**
+	* get_dato
+	* @return
+	*/
+	public function get_dato() {
+
+		$section_tipo 	= $this->section_tipo;
+		$section_id 	= $this->section_id;
+		$data_source 	= $this->data_source;
+
+		$component_source = array_reduce($data_source, function ($carry, $item){
+			if ($item->type==='source') {
+				return $item;
+			}
+			return $carry;
+		});
+
+		$current_component_tipo = $component_source->component_tipo;
+		$current_section_tipo 	= $component_source->section_tipo;
+
+		#
+		# PORTAL ROWS
+
+			$modelo_name 	  = RecordObj_dd::get_modelo_name_by_tipo($current_component_tipo,true); // Expected portal
+			$component_portal = component_common::get_instance($modelo_name,
+															   $current_component_tipo,
+															   $section_id,
+															   'list',
+															   DEDALO_DATA_NOLAN,
+															   $current_section_tipo);
+
+			$dato = $component_portal->get_dato();
+
+			if (empty($dato)) {
+				return 'Empty portal data';
+			}
+
+
+
+		$component_used = array_reduce($data_source, function ($carry, $item){
+
+			if ($item->type==='used') {
+				return $item;
+			}
+			return $carry;
+		});
+
+		$component_tipo_used 	= $component_used->component_tipo;
+		$section_tipo_used 		= $component_used->section_tipo;
+
+
+		$component_data_weights = array_reduce($data_source, function ($carry, $item){
+
+			if ($item->type==='data_weights') {
+				return $item;
+			}
+			return $carry;
+		});
+
+		$component_tipo_data_weights 	= $component_data_weights->component_tipo;
+
+
+		$component_data_diameter = array_reduce($data_source, function ($carry, $item){
+
+			if ($item->type==='data_diamenter') {
+				return $item;
+			}
+			return $carry;
+		});
+
+		$component_tipo_data_diameter 	= $component_data_diameter->component_tipo;
+
+
+
+		$weights = [];
+		$diameter = [];
+		#get the value of the component using portal dato
+			foreach ($dato as $current_locator) {
+
+				$section_id 	= $current_locator->section_id;
+				$section_tipo 	= $current_locator->section_tipo;
+
+				$used_modelo_name	= RecordObj_dd::get_modelo_name_by_tipo($component_tipo_used,true); // Expected portal
+				$used 				= component_common::get_instance($used_modelo_name,
+															   $component_tipo_used,
+															   $section_id,
+															   'list',
+															   DEDALO_DATA_NOLAN,
+															   $section_tipo);
+
+				$used_dato = $used->get_dato();
+
+				#if ($used_dato[0]->section_id==='2') continue;
+				if (empty($used_dato) || $used_dato[0]->section_id==='2') continue;
+
+				//weights
+				$data_weights_modelo_name	= RecordObj_dd::get_modelo_name_by_tipo($component_tipo_data_weights,true); // Expected portal
+				$data_weights				= component_common::get_instance($data_weights_modelo_name,
+															   $component_tipo_data_weights,
+															   $section_id,
+															   'list',
+															   DEDALO_DATA_NOLAN,
+															   $section_tipo);
+
+				$data_weights_dato 	= $data_weights->get_dato();
+				$data_weights_label = $data_weights->get_label();
+
+				if(!empty($data_weights_dato)){
+					$weights[] = $data_weights_dato;
+				}
+
+				//diameter
+				$data_diameter_modelo_name	= RecordObj_dd::get_modelo_name_by_tipo($component_tipo_data_diameter,true); // Expected portal
+				$data_diameter				= component_common::get_instance($data_diameter_modelo_name,
+															   $component_tipo_data_diameter,
+															   $section_id,
+															   'list',
+															   DEDALO_DATA_NOLAN,
+															   $section_tipo);
+
+				$data_diameter_dato = $data_diameter->get_dato();
+				$data_diameter_label = $data_diameter->get_label();
+
+				if(!empty($data_diameter_dato)){
+					$diameter[] = $data_diameter_dato;
+				}
+			}
+
+			if (!empty($weights)) {
+				$media_weight 			= round((array_sum($weights) / count($weights)),2);
+				$total_elements_weights = count($weights);
+				$max_weight 			= max($weights);
+				$min_weight 			= min($weights);
+			}else{
+				debug_log(__METHOD__." Empty weights. Sum ignored in widget get_archive_weights ".to_string(), logger::DEBUG);
+			}
+
+			if (!empty($diameter)) {
+				$media_diameter				= round((array_sum($diameter) / count($diameter)),2);
+				$total_elements_diameter 	= count($diameter);
+				$max_diameter 				= max($diameter);
+				$min_diameter				= min($diameter);
+			}else{
+				debug_log(__METHOD__." Empty diameter. Sum ignored in widget get_archive_weights ".to_string(), logger::DEBUG);
+			}
+
+		$dato = new stdClass();
+			$dato->media_weight 			= $media_weight 			?? null;
+			$dato->max_weight 				= $max_weight 				?? null;
+			$dato->min_weight 				= $min_weight 				?? null;
+			$dato->total_elements_weights 	= $total_elements_weights 	?? null;
+			$dato->media_diameter 			= $media_diameter 			?? null;
+			$dato->max_diameter 			= $max_diameter 			?? null;
+			$dato->min_diameter 			= $min_diameter 			?? null;
+			$dato->total_elements_diameter 	= $total_elements_diameter 	?? null;
+
+		return $dato;
+	}//end get_dato
+
+}//end get_archive_weights
--- core/extras/numisdata/widgets/get_archive_weights/js/get_archive_weights.js
+++ core/extras/numisdata/widgets/get_archive_weights/js/get_archive_weights.js
@@ -1,50 +1,140 @@
+/*global get_label, page_globals, SHOW_DEBUG, DEDALO_CORE_URL*/
+/*eslint no-undef: "error"*/
+
+
+
+// imports
+	// import {data_manager} from '../../common/js/data_manager.js'
+	// import {common,create_source} from '../../common/js/common.js'
+	import {render_get_archive_weights} from '../../get_archive_weights/js/render_get_archive_weights.js'
+
+
+
+export const get_archive_weights = function(){
+
+	this.id
+
+	this.section_tipo
+	this.section_id
+	this.lang
+	this.value
+	this.node
+
+	return true
+}//end get_archive_weights
 
 
 
 /**
-* GET_ARCHIVE_WEIGHTS
+* COMMON FUNCTIONS
+* extend component functions from component common
 */
-var get_archive_weights = new function() {
+// prototypes assign
+	// lifecycle
+	// get_archive_weights.prototype.init 	 			= component_common.prototype.init
+	// get_archive_weights.prototype.build 	 			= component_common.prototype.build
+	get_archive_weights.prototype.render 				= common.prototype.render
+	// get_archive_weights.prototype.refresh 			= common.prototype.refresh
+	// get_archive_weights.prototype.destroy 	 		= common.prototype.destroy
+
+	// // change data
+	// get_archive_weights.prototype.save 	 			= component_common.prototype.save
+	// get_archive_weights.prototype.update_data_value	= component_common.prototype.update_data_value
+	// get_archive_weights.prototype.update_datum 		= component_common.prototype.update_datum
+	// get_archive_weights.prototype.change_value 		= component_common.prototype.change_value
+
+	// render
+	get_archive_weights.prototype.list 					= render_get_archive_weights.prototype.list
+	get_archive_weights.prototype.edit 					= render_get_archive_weights.prototype.edit
+	// get_archive_weights.prototype.edit_in_list		= render_get_archive_weights.prototype.edit
+	// get_archive_weights.prototype.tm					= render_get_archive_weights.prototype.edit
+	// get_archive_weights.prototype.search 			= render_get_archive_weights.prototype.search
+	// get_archive_weights.prototype.change_mode 		= component_common.prototype.change_mode
 
 
-	/**
-	* ACTIVATE_LISTENER_VALUE
-	*/
-	this.activate_listener_value = function(component_target_tipo) {
-			
-		$(function() {
-			var reference_input	= document.querySelector('.css_wrap_portal[data-tipo="' + component_target_tipo + '"] > .content_data') // > .content_data .table_portal_rows_list
-			//  console.log(reference_input);
-			if (reference_input) {	
 
-				// Remove listener to avoid duplicates
-				reference_input.removeEventListener('change', get_archive_weights.update_state)				
-				
-				// Add listner change
-				reference_input.addEventListener("change", get_archive_weights.update_state, false);				
+/**
+* ACTIVE
+* Custom active function triggered after ui.active has finish
+*/
+get_archive_weights.prototype.active = function() {
+
+	//console.log("Yujuu! This is my component custom active test triggered after ui.active. id:", this.id )
+
+	return true
+}//end active
+
+
+
+/**
+* IS_UNIQUE
+* Check the value of the input_text with the all values in the database
+* @result bool
+* result:
+* 	true : unique value, it not has any record inside the section.
+* 	false: the value has almost 1 record inside the database, but it is not unique.
+*/
+get_archive_weights.prototype.is_unique = async function(new_value){
+
+	const self = this
+
+	if (new_value.length<1) return false
+
+	const unique_config = self.context.properties.unique
+
+	// search item rebuild filter q param and others
+		const sqo = self.sqo_context.search.find(item => item.typo==='sqo')
+		// set limit on sqo
+		sqo.limit = 1
+		// set skip_projects_filter as true
+		sqo.skip_projects_filter = true
+
+		const filter = sqo.filter['$and']
+
+		// filter item (expected one, under '$and' operator)
+			const filter_item = filter[0]
+			// add received value to filter
+			filter_item.q = "=" + new_value
+			// set the split words by spaces to false
+			filter_item.q_split = false
+
+		// exclude self record of search once
+			if (filter.length===1) {
+				const new_item_filter = {
+					q 	 : '!=' + self.section_id,
+					path : [
+						{
+							component_tipo  : 'section_id',
+							modelo 			: 'component_section_id',
+							name 			: 'Dummy section id',
+							section_tipo 	: [self.section_tipo]
+						}
+					]
+				}
+				filter.push(new_item_filter)
 			}
-		});	
-	};//end activate_listener_value
 
+			//console.log("filter_item:",filter_item);
+			//console.log("new_item_filter:",new_item_filter);
+			// console.log("sqo:",sqo);
 
+	// add self context as the unique ddo
+		//self.sqo_context.search.push(self.context)
 
-	/**
-	* UPDATE_STATE
-	*/
-	this.update_state = function(e) {
-		
-		// Select current widget element
-		var get_archive_weights = document.getElementById('get_archive_weights')
-		
-		// Update info (reload component info)
-		var js_promise = component_info.update_info(get_archive_weights)
-			//js_promise.then(function() { })
-	};//end update_state
-	
-	
+	// load data
+		const current_data_manager 	= new data_manager()
+		const api_response 			= await current_data_manager.section_load_data(self.sqo_context.search)
+		const data 					= api_response.result.data
 
-		
+	// record data results from search
+		const record = data.find(item => item.tipo===self.tipo)
 
-}//end get_archive_weights
+	// result. If results are found, value is NOT unique
+		//const result = (typeof record==="undefined") ? true : false
 
+		if(SHOW_DEBUG===true) {
+			console.log("+++++ is_unique api_response data:",data, record);
+		}
 
+	return record
+}//end is_unique
