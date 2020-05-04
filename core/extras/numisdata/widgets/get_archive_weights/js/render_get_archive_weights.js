e50b04ddf24f5cadcfab9fc70329d03a970d6d12
Alejandro Peña Carbonell <alex@render.es>
2020-05-04 19:03:38 +0200

    Widget: get_archive_weights full operative

--- core/extras/numisdata/widgets/get_archive_weights/js/render_get_archive_weights.js
+++ core/extras/numisdata/widgets/get_archive_weights/js/render_get_archive_weights.js
@@ -93,32 +93,200 @@ const get_value_element = (i, current_value, values_container, self) => {
 
 	// li
 		const li = ui.create_dom_element({
-			element_type : 'li',
-			parent 		 : values_container
+			element_type	: 'li',
+			class 			: 'get_archive_weights',
+			parent 			: values_container
 		})
 
-	// iterate object properties
-		for (let [label, value] of Object.entries(current_value)) {
+	//weights
+		const archive_weights = ui.create_dom_element({
+			type 		: 'div',
+			parent 		: li
+		})
 
-			// label
-				const span_label = ui.create_dom_element({
+		// general
+		const sum_weights = ui.create_dom_element({
+			type 		: 'div',
+			class 		: 'sum_weights',
+			parent 		: archive_weights
+		})
+			//media_weight
+				// label
+					const media_weight_label = ui.create_dom_element({
+						type 		: 'span',
+						class_name	: 'label',
+						inner_html 	: get_label.weight,
+						parent 		: sum_weights
+					})
+
+				// value
+					const media_weight_value = ui.create_dom_element({
+						type 		: 'span',
+						class_name	: 'value',
+						inner_html 	: current_value.media_weight,
+						parent 		: sum_weights
+					})
+
+		// detail
+		const get_archive_weights = ui.create_dom_element({
+			type 		: 'span',
+			class 		: 'get_archive_weights',
+			parent 		: archive_weights
+		})
+			//max_weight
+				// label
+				const max_weight_label = ui.create_dom_element({
+					type 		: 'span',
+					class_name	: 'label_range',
+					inner_html 	: 'max: ',
+					parent 		: get_archive_weights
+				})
+
+				// value
+				const max_weight_value = ui.create_dom_element({
+					type 		: 'span',
+					class_name	: 'value',
+					inner_html 	: current_value.max_weight,
+					parent 		: get_archive_weights
+				})
+
+			// min_weight
+				// label
+				const min_weight_label = ui.create_dom_element({
+					type 		: 'span',
+					class_name	: 'label_range',
+					inner_html 	: ' | min: ',
+					parent 		: get_archive_weights
+				})
+
+				// value
+				const min_weight_value = ui.create_dom_element({
+					type 		: 'span',
+					class_name	: 'value',
+					inner_html 	: current_value.min_weight,
+					parent 		: get_archive_weights
+				})
+
+			// total_elements_weights
+				// label
+				const total_weight_label = ui.create_dom_element({
 					type 		: 'span',
 					class_name	: 'label',
-					inner_html 	: label,
-					parent 		: li
+					inner_html 	: ' | n: ',
+					parent 		: get_archive_weights
+				})
+
+				// value
+				const total_weight_value = ui.create_dom_element({
+					type 		: 'span',
+					class_name	: 'value',
+					inner_html 	: current_value.total_elements_weights,
+					parent 		: get_archive_weights
+				})
+
+
+	//Diameter
+		// general
+		const archive_diameter = ui.create_dom_element({
+			type 		: 'div',
+			class 		: 'get_archive_diameter',
+			parent 		: li
+		})
+
+		const sum_diameter = ui.create_dom_element({
+			type 		: 'div',
+			class 		: 'sum_diameter',
+			parent 		: archive_diameter
+		})
+		// media_diameter
+			// label
+				const media_diameter_label = ui.create_dom_element({
+					type 		: 'span',
+					class_name	: 'label',
+					inner_html 	: get_label.diameter,
+					parent 		: sum_diameter
 				})
 
 			// value
-				const span_value = ui.create_dom_element({
+				const media_diameter_value = ui.create_dom_element({
 					type 		: 'span',
 					class_name	: 'value',
-					inner_html 	: JSON.stringify(value),
-					parent 		: li
+					inner_html 	: current_value.media_diameter,
+					parent 		: sum_diameter
 				})
-		}
+		// detail
+		const get_archive_diameter = ui.create_dom_element({
+			type 		: 'span',
+			class 		: 'get_archive_diameter',
+			parent 		: archive_diameter
+		})
+			// max_diameter
+				// label
+				const max_diameter_label = ui.create_dom_element({
+					type 		: 'span',
+					class_name	: 'label_range',
+					inner_html 	: 'max: ',
+					parent 		: get_archive_diameter
+				})
+
+				// value
+				const max_diameter_value = ui.create_dom_element({
+					type 		: 'span',
+					class_name	: 'value',
+					inner_html 	: current_value.max_diameter,
+					parent 		: get_archive_diameter
+				})
+
+			// min_diameter
+				// label
+				const min_diameter_label = ui.create_dom_element({
+					type 		: 'span',
+					class_name	: 'label_range',
+					inner_html 	: ' | min: ',
+					parent 		: get_archive_diameter
+				})
+
+				// value
+				const min_diameter_value = ui.create_dom_element({
+					type 		: 'span',
+					class_name	: 'value',
+					inner_html 	: current_value.min_diameter,
+					parent 		: get_archive_diameter
+				})
+
+
+			//total_elements_diameter
+				// label
+				const total_diameter_label = ui.create_dom_element({
+					type 		: 'span',
+					class_name	: 'label',
+					inner_html 	: ' | n: ',
+					parent 		: get_archive_diameter
+				})
+
+				// value
+				const total_diameter_value = ui.create_dom_element({
+					type 		: 'span',
+					class_name	: 'value',
+					inner_html 	: current_value.total_elements_diameter,
+					parent 		: get_archive_diameter
+				})
+
+
+		// even manager model to use in other widgets_properties
+		// this widget don't use it, because the info is not in the same section
+		// than the components that changed our value
+		// the user don't see the info and the imput componets at same time
 		event_manager.subscribe('update_widget_value_'+self.id, (changed_data) =>{
-			console.log("change_data", changed_data);
-			span_value.innerHTML = JSON.stringify(changed_data)
+			media_weight_value.innerHTML 	= changed_data[0].media_weight
+			max_weight_value.innerHTML 		= changed_data[0].max_weight
+			min_weight_value.innerHTML 		= changed_data[0].min_weight
+			total_weight_value.innerHTML 	= changed_data[0].total_elements_weights
+
+			media_diameter_value.innerHTML 	= changed_data[0].media_diameter
+			max_diameter_value.innerHTML 	= changed_data[0].max_diameter
+			min_diameter_value.innerHTML 	= changed_data[0].min_diameter
+			total_diameter_value.innerHTML 	= changed_data[0].total_elements_diameter
 		})
 
 	return li
