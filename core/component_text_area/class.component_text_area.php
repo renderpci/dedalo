95399fbb9373c025d5fc488aa13f7242cbe49895
Alejandro Peña Carbonell <alex@render.es>
2020-05-25 17:39:56 +0200

    remove the old: render_list_value from component_text_area

--- core/component_text_area/class.component_text_area.php
+++ core/component_text_area/class.component_text_area.php
@@ -1496,39 +1496,6 @@ class component_text_area extends component_common {
 	}//end get_diffusion_obj
 
 
-
-	/**
-	* RENDER_LIST_VALUE
-	* Overwrite for non default behavior
-	* Receive value from section list and return proper value to show in list
-	* Sometimes is the same value (eg. component_input_text), sometimes is calculated (e.g component_portal)
-	* @param string $value
-	* @param string $tipo
-	* @param int $parent
-	* @param string $modo
-	* @param string $lang
-	* @param string $section_tipo
-	* @param int $section_id
-	*
-	* @return string $list_value
-	*/ // ($value, $tipo, $parent, $modo, $lang, $section_tipo, $section_id, $caller_component_tipo=null) ? locator ??
-	public static function render_list_value($value, $tipo, $parent, $modo, $lang, $section_tipo, $section_id, $locator=null, $caller_component_tipo=null) {
-
-		return ''; // SKIP CALCLULATE LIST VALUE
-
-		$component 		= component_common::get_instance(__CLASS__,
-														 $tipo,
-													 	 $parent,
-													 	 $modo,
-														 $lang,
-													 	 $section_tipo);
-		$value = $component->get_html();
-
-		return $value;
-	}//end render_list_value
-
-
-
 	/**
 	* GET_DIFFUSION_VALUE
 	* Overwrite component common method
