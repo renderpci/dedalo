c146fd102a3aa4645d98a965e496529618493ab0
Alejandro Peña Carbonell <alex@render.es>
2020-05-26 13:50:29 +0200

    Remove old files into area_developer

--- core/area_development/html/info.php
+++
@@ -1,7 +0,0 @@
-<?php
-include dirname(dirname(dirname(dirname(__FILE__)))) .'/config/config.php';
-
-# LOGIN VERIFICATION
-if(login::is_logged()!==true) die("<span class='error'> Auth error: please login </span>");
-
-phpinfo();
