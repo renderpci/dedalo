40088469cf44a38aefd9376e4401b2eb15a667c3
Juan Francisco Onielfa Veneros <paco@render.es>
2020-08-11 00:14:20 +0200

    Clean code

--- core/component_pdf/js/render_component_pdf.js
+++ core/component_pdf/js/render_component_pdf.js
@@ -251,36 +251,35 @@ const get_input_element_edit = async (i, current_value, inputs_container, self) 
 			// })
 			// iframe.setAttribute('data', viewer_url)
 
-			//
 			// // when the standard html of pdf.js is loaded, is possible get the library and set the pdf
-			 document.addEventListener("webviewerloaded",  (e) =>{
+			document.addEventListener("webviewerloaded",  (e) =>{
 
-			// shadow.addEventListener('load', (e) =>{
-				// DEDALO_APPLICATION_LANG
-				const locale = 'es-ES'
-				// Libraries are loaded via <script> tag, create shortcut to access PDF.js exports.
-				// the pdf_js is not necesary load here, we will use only the viewer
-				// self.pdf_js 						= iframe.contentWindow['pdfjs-dist/build/pdf'];
-				const PDFViewerApplicationOptions 	= iframe.contentWindow['PDFViewerApplicationOptions'];
-				self.pdf_viewer 					= iframe.contentWindow['PDFViewerApplication'];
+				// shadow.addEventListener('load', (e) =>{
+					// DEDALO_APPLICATION_LANG
+					const locale = 'es-ES'
+					// Libraries are loaded via <script> tag, create shortcut to access PDF.js exports.
+					// the pdf_js is not necesary load here, we will use only the viewer
+					// self.pdf_js 						= iframe.contentWindow['pdfjs-dist/build/pdf'];
+					const PDFViewerApplicationOptions 	= iframe.contentWindow['PDFViewerApplicationOptions'];
+					self.pdf_viewer 					= iframe.contentWindow['PDFViewerApplication'];
 
-				// remove the first page / default page of the library
-				PDFViewerApplicationOptions.set('defaultUrl', '');
-				PDFViewerApplicationOptions.set('locale', 'es-ES');
-					// console.log("PDFViewerApplicationOptions", PDFViewerApplicationOptions);
-
-				// load the pdf in the viewer
-				self.pdf_viewer.open(pdf_url).then(function (pdfDocument) {
-					// PDFViewerApplicationOptions.document.webL10n.setLanguage('es-ES')
-					// PDFViewerApplicationOptions.set('locale', 'es-ES');
-					// PDFViewerApplicationOptions.locale = 'es-ES'
-					if(SHOW_DEBUG===true) {
-						// console.log("slef.pdf_viewer.PdfViewer", self.pdf_viewer);
-						// console.log("PDFViewerApplicationOptions", PDFViewerApplicationOptions.get('locale'));
-					}
+					// remove the first page / default page of the library
+					PDFViewerApplicationOptions.set('defaultUrl', '');
 					PDFViewerApplicationOptions.set('locale', 'es-ES');
-					// console.log("PDFViewerApplication.pagesCount", self.pdf_viewer.pagesCount);
-				});
+						// console.log("PDFViewerApplicationOptions", PDFViewerApplicationOptions);
+
+					// load the pdf in the viewer
+					self.pdf_viewer.open(pdf_url).then(function (pdfDocument) {
+						// PDFViewerApplicationOptions.document.webL10n.setLanguage('es-ES')
+						// PDFViewerApplicationOptions.set('locale', 'es-ES');
+						// PDFViewerApplicationOptions.locale = 'es-ES'
+						if(SHOW_DEBUG===true) {
+							// console.log("slef.pdf_viewer.PdfViewer", self.pdf_viewer);
+							// console.log("PDFViewerApplicationOptions", PDFViewerApplicationOptions.get('locale'));
+						}
+						PDFViewerApplicationOptions.set('locale', 'es-ES');
+						// console.log("PDFViewerApplication.pagesCount", self.pdf_viewer.pagesCount);
+					});
 			})
 
 			iframe.src = viewer_url
