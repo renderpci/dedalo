ae1dd728f2ca33d5ed63ee165b59664fa4bb9d3d
Juan Francisco Onielfa Veneros <paco@render.es>
2020-08-11 00:14:20 +0200

    Clean code

--- core/services/service_tinymce/js/dd-tiny.js
+++ core/services/service_tinymce/js/dd-tiny.js
@@ -28,6 +28,10 @@ class DDTiny extends HTMLElement {
 			const textarea = document.createElement('textarea')
 				  textarea.insertAdjacentHTML('afterbegin', value);
 				  self.appendChild(textarea)
+		
+		// event_manager.when_in_dom(self.options.container, function(){
+		// 	console.log("DOM ready:",self.options.container);
+		// });
 
 		// tinymce editor init
 			tinymce.init({
@@ -36,22 +40,22 @@ class DDTiny extends HTMLElement {
 				// cache_suffix. Force remove chache when version changes
 				cache_suffix			: "?"+page_globals.dedalo_version,
 				// plugins. Tiny plugins custom array
-				plugins					: this.options.plugins,
+				plugins					: self.options.plugins,
 				// toolbar. Editor buttons to show in toolbar
-				toolbar					: this.options.toolbar,
+				toolbar					: self.options.toolbar,
 				// toolbar_items_size. Set toolbar buttons size
-				toolbar_items_size		: this.options.toolbar_items_size || 'small',
+				toolbar_items_size		: self.options.toolbar_items_size || 'small',
 				// menubar. specify which menus should appear and the order that they
 				// appear in the menu bar at the top of editor
-				menubar					: this.options.menubar || false,
+				menubar					: self.options.menubar || false,
 				// statusbar. allows you to specify whether or not TinyMCE should
 				// display the status bar at the bottom of the editor. (true|false)
-				// statusbar 				: this.options.statusbar || false,
+				// statusbar 				: self.options.statusbar || false,
 				// skin_url. enables you to specify the location of the skin file
-				skin_url				: this.options.skin_url
+				skin_url				: self.options.skin_url
 					|| DEDALO_ROOT_WEB + "/lib/tinymce/js/tinymce/skins/lightgray",
 				// theme_url. enables you to specify the location of the theme file
-				theme_url 				: this.options.theme_url
+				theme_url 				: self.options.theme_url
 					|| DEDALO_ROOT_WEB + "/lib/tinymce/js/tinymce/themes/modern/theme.min.js?" +page_globals.dedalo_version,
 				// entity_encoding. Allows you to get XML escaped content out of TinyMCE.
 				// By setting this option to xml, posted content will be converted to an XML
@@ -62,21 +66,21 @@ class DDTiny extends HTMLElement {
 				// automatically it will instead produce br elements and Shift+Enter will produce a p.
 				forced_root_block		: false,
 				// width. Set the width of the editor in pixels
-					// width 					: this.options.width || null,
+					// width 					: self.options.width || null,
 				// height. sets the height of the editable area in pixels.
-					// height 					: this.options.height || null,
+					// height 					: self.options.height || null,
 				// content_css
-				content_css				: this.options.content_css
+				content_css				: self.options.content_css
 					|| DEDALO_CORE_URL + '/component_text_area/css/mce_editor_default.css?' + page_globals.dedalo_version,
 				// body_class
-				body_class				: this.options.body_class || null,
+				body_class				: self.options.body_class || null,
 				// relative_urls. If this option is set to true, all URLs returned from the MCFileManager will be relative from the specified
 				// document_base_url. If it's set to false all URLs will be converted to absolute URLs.
 				relative_urls			: false,
 				// convert_urls. enables you to control whether TinyMCE is to be smart and restore URLs to their original values.
 				convert_urls			: false,
 				// browser_spellcheck. enables TinyMCE to use the browser's native spell checker.
-				browser_spellcheck		: this.options.browser_spellcheck || true,
+				browser_spellcheck		: self.options.browser_spellcheck || true,
 				// schema. enables you to switch between the HTML4 and HTML5 schema
 				schema					: 'html5-strict',
 				// setup. called whens editor setup
@@ -91,7 +95,8 @@ class DDTiny extends HTMLElement {
 					// update dd-tiny element id with new editor id
 					self.id = editor.id
 				}
-			})
+			})		
+
 	}
 }
 customElements.define('dd-tiny', DDTiny)
