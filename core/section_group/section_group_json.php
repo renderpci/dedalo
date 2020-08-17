8f98bc74017abd21b9a3b9f69ef47b6717ec646e
Juan Francisco Onielfa Veneros <paco@render.es>
2020-08-17 18:55:10 +0200

    Fix error on get subdata (section) that added duplicate ddo

--- core/section_group/section_group_json.php
+++ core/section_group/section_group_json.php
@@ -25,10 +25,10 @@
 					$context[] = $this->get_structure_context($permissions);
 
 				// subcontext from element layout_map items (from_parent_tipo, parent_grouper)
-					$ar_subcontext = $this->get_ar_subcontext($section_tipo, $tipo);
-					foreach ($ar_subcontext as $current_context) {
-						$context[] = $current_context;
-					}
+					// $ar_subcontext = $this->get_ar_subcontext($section_tipo, $tipo);
+					// foreach ($ar_subcontext as $current_context) {
+					// 	$context[] = $current_context;
+					// }
 				break;
 		}
 	}//end if($options->get_context===true)
