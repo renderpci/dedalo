a8e8ee988bf1487451de14f6d38713f68563914c
Alejandro Peña Carbonell <alex@render.es>
2020-08-18 14:40:43 +0200

    Add render mini in compoment_check_box

--- core/component_check_box/js/render_component_check_box.js
+++ core/component_check_box/js/render_component_check_box.js
@@ -19,6 +19,34 @@ export const render_component_check_box = function() {
 };//end render_component_check_box
 
 
+/**
+* MINI
+* Render node to be used by service autocomplete or any datalist
+* @return DOM node
+*/
+render_component_check_box.prototype.mini = async function() {
+
+	const self = this
+
+	// Options vars
+		const context 	= self.context
+		const data 		= self.data
+		const value 	= data.value || []
+
+	// wrapper
+		const wrapper = ui.component.build_wrapper_mini(self)
+
+	// Value as string
+		const value_string = value.join(self.divisor)
+
+	// Set value
+		wrapper.textContent = value_string
+
+
+	return wrapper
+};//end mini
+
+
 
 /**
 * LIST
