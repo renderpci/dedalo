02cf4d26c2826dcc6ecc0ed2d005786e61dcc120
Alejandro Peña Carbonell <alex@render.es>
2020-08-18 13:00:28 +0200

    remove the sesion in dd_core_api

--- core/api/v1/common/class.dd_core_api.php
+++ core/api/v1/common/class.dd_core_api.php
@@ -1014,7 +1014,9 @@ class dd_core_api {
 
 							// store search_query_object
 								//$context[] = $current_sqo;
-								$_SESSION['dedalo']['config']['sqo'][$sqo_id] = $search_query_object;
+								if ($model==='section') {
+									// $_SESSION['dedalo']['config']['sqo'][$sqo_id] = $search_query_object;
+								}
 							break;
 
 						case 'get_data':
