0fd9cb52a3d8c8587f121e1d64fff69ca8c4997b
Alejandro Peña Carbonell <alex@render.es>
2020-08-18 14:40:28 +0200

    Add render mini in compoment_av

--- core/component_av/js/render_component_av.js
+++ core/component_av/js/render_component_av.js
@@ -19,6 +19,39 @@ export const render_component_av = function() {
 };//end  render_component_av
 
 
+/**
+* MINI
+* Render node to be used by service autocomplete or any datalist
+* @return DOM node
+*/
+render_component_av.prototype.mini = async function() {
+
+	const self = this
+
+	// Options vars
+		const context 	= self.context
+		const data 		= self.data
+
+	// wrapper
+		const wrapper = ui.component.build_wrapper_mini(self)
+
+	// url
+		const posterframe_url 	= data.posterframe_url
+		const url 				= posterframe_url // (!posterframe_url || posterframe_url.length===0) ? DEDALO_LIB_URL + "/themes/default/0.jpg" : posterframe_url
+
+	// image
+		const image = ui.create_dom_element({
+			element_type	: "img",
+			src 			: url,
+			parent 			: wrapper
+		})
+		// ui.component.add_image_fallback(image)
+
+
+	return wrapper
+};//end  mini
+
+
 
 /**
 * LIST
