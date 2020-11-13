d7aa69b7fd8609044b4ad8e20666a5b758732a31
Juan Francisco Onielfa Veneros <paco@render.es>
2020-11-13 13:01:30 +0100

    Fix lib lessc.inc.php legacy issues with modern php versions >=7.3

--- vendor/leafo/lessphp/lessc.inc.php
+++ vendor/leafo/lessphp/lessc.inc.php
@@ -746,7 +746,7 @@ class lessc {
 					if ($suffix !== null &&
 						$subProp[0] == "assign" &&
 						is_string($subProp[1]) &&
-						$subProp[1]{0} != $this->vPrefix)
+						$subProp[1][0] != $this->vPrefix)
 					{
 						$subProp[2] = array(
 							'list', ' ',
@@ -1857,7 +1857,7 @@ class lessc {
 		$this->pushEnv();
 		$parser = new lessc_parser($this, __METHOD__);
 		foreach ($args as $name => $strValue) {
-			if ($name{0} != '@') $name = '@'.$name;
+			if ($name[0] != '@') $name = '@'.$name;
 			$parser->count = 0;
 			$parser->buffer = (string)$strValue;
 			if (!$parser->propertyValue($value)) {
@@ -2515,8 +2515,8 @@ class lessc_parser {
 			if (is_null($block->type)) {
 				$hidden = true;
 				if (!isset($block->args)) {
-					foreach ($block->tags as $tag) {
-						if (!is_string($tag) || $tag{0} != $this->lessc->mPrefix) {
+					foreach ($block->tags as $tag) {						
+						if (!is_string($tag) || $tag[0] != $this->lessc->mPrefix) {
 							$hidden = false;
 							break;
 						}
