0e97a04377c2270a8ca599557919fa591e9e5cd3
Paco <paco@render.es>
2021-07-25 10:31:19 +0200

    Clean code

--- core/component_check_box/class.component_check_box.php
+++ core/component_check_box/class.component_check_box.php
@@ -62,6 +62,9 @@ class component_check_box extends component_relation_common {
 
 
 
+	/**
+	* GET_DATO_AS_STRING
+	*/
 	public function get_dato_as_string() {
 
 		return json_handler::encode($this->get_dato());
@@ -88,11 +91,16 @@ class component_check_box extends component_relation_common {
 
 
 
+	/**
+	* GET_DATAFRAME_VALUE
+	*/
 	public function get_dataframe_value($type){
 
 		$dataframe_value = RecordObj_dd::get_termino_by_tipo($type,DEDALO_APPLICATION_LANG, true);
 
 		return $dataframe_value;
+	}//end get_dataframe_value
 
-	}
-}
+
+
+}//end component_check_box
