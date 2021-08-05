1d023eb57a94f08f23683964f4844af4f766123f
Paco <paco@render.es>
2021-08-05 13:00:22 +0200

    Unified tool load component calls (centralized in tool_common generic method)

--- tools/tool_lang/class.tool_lang.php
+++ tools/tool_lang/class.tool_lang.php
@@ -11,27 +11,41 @@ class tool_lang extends tool_common {
 
 	/**
 	* AUTOMATIC_TRANSLATION
+	* Exec a translation request against the translator service given (babel, google, etc.)
+	* and save the result to the target component in the target lang.
+	* Note that translator config is stored in the tool section data (tools_register)
+	* @param object $request_options
 	* @return object $response
 	*/
-	public function automatic_translation($request_options) {
+	public static function automatic_translation($request_options) {
 
 		$response = new stdClass();
-			$response->result 	= false;
-			$response->msg 		= 'Error. Request failed ['.__FUNCTION__.']';
+			$response->result	= false;
+			$response->msg		= 'Error. Request failed ['.__FUNCTION__.']';
 
-		$options = new stdClass();
-			$options->source_lang 		= null;
-			$options->target_lang 		= null;
-			$options->component_tipo	= null;
-			$options->section_id 		= null;
-			$options->section_tipo 		= null;
-			$options->translator 		= null;
-			foreach ($request_options as $key => $value) {if (property_exists($options, $key)) $options->$key = $value;}
+		// options
+			$options = new stdClass();
+				$options->source_lang		= null;
+				$options->target_lang		= null;
+				$options->component_tipo	= null;
+				$options->section_id		= null;
+				$options->section_tipo		= null;
+				$options->translator		= null;
+				$options->config			= null;
+				foreach ($request_options as $key => $value) {if (property_exists($options, $key)) $options->$key = $value;}
 
-		// config json . Must be compatible with tool properties translator_engine data
-			$config 				= $this->get_config();
-			$ar_translator_configs 	= $config->translator_config->value;
-			$translator_name 		= $options->translator->name;
+		// config
+			// get all tools config sections
+				$ar_config = tools_register::get_all_config_tool();
+			// select current from all tool config matching tool name
+				$tool_name	= get_called_class(); // tool_lang
+				$config		= array_find($ar_config, function($el) use($tool_name) {
+					return $el->name===$tool_name;
+				});
+
+		// config JSON . Must be compatible with tool properties translator_engine data
+			$ar_translator_configs	= $config->config->translator_config->value;
+			$translator_name		= $options->translator->name;
 			// search current translator config in tool config (stored in database, section 'dd996' Tools configuration)
 			$translator_config = array_find($ar_translator_configs, function($item) use($translator_name) {
 				return $item->name===$translator_name;
@@ -41,7 +55,7 @@ class tool_lang extends tool_common {
 			$uri = $translator_config->uri;
 			$key = $translator_config->key;
 
-		// Source text . Get source text from component
+		// Source text . Get source text from component (source_lang)
 			$model 		= RecordObj_dd::get_modelo_name_by_tipo($options->component_tipo,true);
 			$component	= component_common::get_instance($model,
 														 $options->component_tipo,
@@ -64,11 +78,11 @@ class tool_lang extends tool_common {
 					case 'babel':
 					default:
 						$translate = babel::translate([
-							'uri' 			=> $uri,
-							'key' 			=> $key,
-							'source_lang' 	=> $options->source_lang,
-							'target_lang' 	=> $options->target_lang,
-							'text' 			=> $value,
+							'uri'			=> $uri,
+							'key'			=> $key,
+							'source_lang'	=> $options->source_lang,
+							'target_lang'	=> $options->target_lang,
+							'text'			=> $value
 						]);
 						$result	= $translate->result;
 						if ($result===false) {
@@ -83,7 +97,7 @@ class tool_lang extends tool_common {
 			}//end foreach ($dato as $key => $value)
 
 
-		// Save result on target component
+		// Save result on target component (target_lang)
 			$component	= component_common::get_instance($model,
 														 $options->component_tipo,
 														 $options->section_id,
@@ -91,11 +105,12 @@ class tool_lang extends tool_common {
 														 $options->target_lang,
 														 $options->section_tipo);
 			$component->set_dato($translated_data);
-			$component->Save(false); // (!) Important: send arg 'false' to save for avoid alter other langs tags (propagate)
+			$component->Save(false); // (!) Important: send argument 'false' to save to prevent alter other langs tags (propagate)
 
 
-		$response->result 	= true;
-		$response->msg 		= 'Ok. Request done ['.__FUNCTION__.']';
+		// response
+			$response->result	= true;
+			$response->msg		= 'OK. Request done ['.__FUNCTION__.']';
 
 
 		return (object)$response;
