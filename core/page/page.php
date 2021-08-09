a2d119f6ff09df309e64ef215cb7b1509502dca1
Alejandro Peña Carbonell <alex@render.es>
2021-08-09 21:55:33 +0200

    standardize the tool definition in properties

--- core/page/page.php
+++ core/page/page.php
@@ -70,13 +70,15 @@
 						$config	= $properties->config ?? null;
 
 					// tool_context
-						$tool_name = $properties->config->tool_name ?? false;
+						$tool_name = isset($properties->tool_config) && is_object($properties->tool_config)
+							? key($properties->tool_config)
+							: false;
 						if ($tool_name) {
 							$ar_tool_object	= common::get_client_registered_tools([$tool_name]);
 							if (empty($ar_tool_object)) {
 								debug_log(__METHOD__." ERROR. No tool found for tool '$tool_name' in section_tool_tipo ".to_string($section_tool_tipo), logger::ERROR);
 							}else{
-								$tool_config	= $properties->config->tool_config->$tool_name ?? false;
+								$tool_config	= $properties->tool_config->{$tool_name} ?? false;
 								$tool_context	= common::create_tool_context($ar_tool_object[0], $tool_config);
 								$config->tool_context = $tool_context;
 								// dump($current_area->config, ' ++++++++++++++++++++++++++++++++++++++ current_area->config ++ '.to_string($section_tool_tipo));
