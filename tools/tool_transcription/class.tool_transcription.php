a5490f5594032bdd0eb00315cb21b8588fd4f4cb
Alejandro Peña Carbonell <alex@render.es>
2021-08-09 16:27:46 +0200

    Added tool transcription

--- tools/tool_transcription/class.tool_transcription.php
+++ tools/tool_transcription/class.tool_transcription.php
@@ -33,49 +33,6 @@ class tool_transcription extends tool_common {
 	}//end __construct
 
 
-
-	/**
-	* GET_COMPONENT_RELATED_OBJ
-	* @return object $component_related_obj
-	* Returns a full component (expected is text area)
-	*/
-	protected function get_component_related_obj() {
-
-		if(isset($this->component_related_obj)) return $this->component_related_obj;
-
-		# media info
-		$tipo 			= $this->component_obj->get_tipo();
-		$parent 		= $this->component_obj->get_parent();
-		$section_tipo 	= $this->component_obj->get_section_tipo();			
-	
-		# método acceso directo al componente. buscamos probablemente el componente text_area para transcribir (puede no haber)
-		$ar_terminos_relacionados = $this->component_obj->RecordObj_dd->get_relaciones();				
-		if(empty($ar_terminos_relacionados)) {
-			#throw new Exception("Component related not exists. Please configure dependencies", 1);
-			return null;
-		}
-			
-		foreach ($ar_terminos_relacionados as $modelo => $termino_relacionado_tipo) {
-			$termino_relacionado_tipo = reset($termino_relacionado_tipo);
-			break;
-		}
-
-		# Create final related component
-		$modelo_name 		 = RecordObj_dd::get_modelo_name_by_tipo($termino_relacionado_tipo,true);
-		$modo 		 		 = 'edit';	// 'tool_transcription' | 'edit'
-		$component_text_area = component_common::get_instance($modelo_name,
-															  $termino_relacionado_tipo,
-															  $parent,
-															  $modo,
-															  DEDALO_DATA_LANG,
-															  $section_tipo);
-
-
-		$this->component_related_obj = $component_text_area;
-
-		return $this->component_related_obj;
-	}//end get_component_related_obj
-
 	
 	
 	/**
