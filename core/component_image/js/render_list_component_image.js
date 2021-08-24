27a6ce672e3ed91312ea1e694f70e360108f9101
Paco <paco@render.es>
2021-08-24 15:25:40 +0200

    component_image render has been divided into modes

---
+++ core/component_image/js/render_list_component_image.js
@@ -0,0 +1,70 @@
+/*global get_label, page_globals, SHOW_DEBUG, DEDALO_CORE_URL*/
+/*eslint no-undef: "error"*/
+
+
+
+// imports
+	// import {event_manager} from '../../common/js/event_manager.js'
+	import {ui} from '../../common/js/ui.js'
+
+
+
+/**
+* RENDER_LIST_COMPONENT_IMAGE
+* Manage the components logic and appearance in client side
+*/
+export const render_list_component_image = function(component) {
+
+	return true
+};//end render_list_component_image
+
+
+
+/**
+* LIST
+* Render node for use in list
+* @return DOM node
+*/
+render_list_component_image.prototype.list = function(options) {
+
+	const self = this
+
+	// Options vars
+		const context 	= self.context
+		const datalist 	= self.data.datalist || []
+
+	// wrapper
+		const wrapper = ui.component.build_wrapper_list(self, {
+			autoload : false
+		})
+
+	// url
+		// const value		= data.value
+		const quality		= "1.5MB"
+		const url_object	= datalist.filter(item => item.quality===quality)[0]
+		const url			= (typeof url_object==="undefined")
+			? DEDALO_CORE_URL + "/themes/default/0.jpg"
+			: url_object.url
+
+	// image
+		const image = ui.create_dom_element({
+			element_type	: "img",
+			src				: url,
+			parent			: wrapper
+		})
+		ui.component.add_image_fallback(image)
+
+	//const image_div = ui.create_dom_element({
+	//	element_type	: "div",
+	//	//class_name		: node_class_name,
+	//	style 			: {
+	//		"background-image" : "url("+url+")"
+	//	},
+	//	parent 			: wrapper
+	//})
+
+
+	return wrapper
+};//end list
+
+
