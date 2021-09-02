72b1c475229e7667aed124c2bf6b3146497685e4
Paco <paco@render.es>
2021-09-02 20:06:21 +0200

    Added component_section_id render files (modes)

---
+++ core/component_section_id/js/render_edit_component_section_id.js
@@ -0,0 +1,77 @@
+/*global get_label, page_globals, SHOW_DEBUG, DEDALO_CORE_URL*/
+/*eslint no-undef: "error"*/
+
+
+
+// import
+	import {ui} from '../../common/js/ui.js'
+	import {event_manager} from '../../common/js/event_manager.js'
+
+
+
+/**
+* RENDER_EDIT_COMPONENT_SECTION_ID
+* Manage the components logic and appearance in client side
+*/
+export const render_edit_component_section_id = function(component) {
+
+	return true
+};//end render_edit_component_section_id
+
+
+
+/**
+* EDIT
+* Render node for use in edit
+* @return DOM node
+*/
+render_edit_component_section_id.prototype.edit = async function(options={render_level:'full'}) {
+
+	const self = this
+
+	// render_level
+		const render_level = options.render_level
+
+	// content_data
+		const content_data = await get_content_data_edit(self)
+		if (render_level==='content') {
+			return content_data
+		}
+
+	// wrapper. ui build_edit returns component wrapper
+		const wrapper = ui.component.build_wrapper_edit(self, {
+			content_data : content_data
+		})
+
+	// add events
+		//add_events(self, wrapper)
+
+
+	return wrapper
+};//end edit
+
+
+
+/**
+* CONTENT_DATA_EDIT
+* @return DOM node content_data
+*/
+const get_content_data_edit = async function(self) {
+
+	const value = self.data.value
+	const mode 	= self.mode
+
+	// content_data
+		const content_data = ui.component.build_content_data(self)
+
+		const div_value = ui.create_dom_element({
+			element_type	: 'div',
+			class_name		: 'section_id',
+			text_content 	: value,
+			parent 			: content_data
+		})
+
+	return content_data
+};//end get_content_data_edit
+
+
