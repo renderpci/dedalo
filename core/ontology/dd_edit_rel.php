fae87efc1f2b37f91062cf5e1e79356acf23cd6e
Paco <paco@render.es>
2021-10-06 13:11:49 +0200

    Clean dd_edit_rel code

--- core/ontology/dd_edit_rel.php
+++ core/ontology/dd_edit_rel.php
@@ -8,126 +8,145 @@ require_once( dirname(__FILE__) . '/lang/lang_code.php' );
 /**
 * LOGIN
 */
-$is_logged	= login::is_logged();
-	
-if($is_logged!==true) {
-	$url =  DEDALO_ROOT_WEB ."/main/";
-	header("Location: $url");
-	exit();
-}
-$permissions = (int)security::get_security_permissions(DEDALO_TESAURO_TIPO, DEDALO_TESAURO_TIPO);
-if ($permissions<1) {
-	$url =  DEDALO_ROOT_WEB ."/main/";
-	header("Location: $url");
-	exit();
-}
+	$is_logged	= login::is_logged();
+
+	if($is_logged!==true) {
+		$url =  DEDALO_ROOT_WEB ."/main/";
+		header("Location: $url");
+		exit();
+	}
+	$permissions = (int)security::get_security_permissions(DEDALO_TESAURO_TIPO, DEDALO_TESAURO_TIPO);
+	if ($permissions<1) {
+		$url =  DEDALO_ROOT_WEB ."/main/";
+		header("Location: $url");
+		exit();
+	}
+
+
 
 require_once(dirname(dirname(__FILE__)) . '/db/class.RecordObj_dd.php');
 require_once(dirname(__FILE__) . '/class.dd.php');
-#require_once(DEDALO_ROOT . '/inc/funciones.php'); 
 
 
 
-# set vars
-$vars = array('terminoID','terminoID_to_link','terminoID_to_unlink','ts_lang','type','accion');
-foreach($vars as $name)	$$name = common::setVar($name);
+// set main request vars
+	$vars = array('terminoID','terminoID_to_link','terminoID_to_unlink','ts_lang','type','accion');
+	foreach($vars as $name)	$$name = common::setVar($name);
 
 
 
+// linkTS. Link ts (add termino relacionado)
+	if($accion=='linkTS') {
 
-
-# LINK TS (ADD TERMINO RELACIONADO)
-if($accion=='linkTS') {
-		
-	#$ts->insertTR($terminoID,$terminoID_to_link);
-	if(!$terminoID || !$terminoID_to_link) exit("$accion Need more vars: terminoID: $terminoID, terminoID_to_link: $terminoID_to_link");
-	
-	$RecordObj_dd	= new RecordObj_dd($terminoID); 
-	
-	$ar_relaciones	= $RecordObj_dd->get_relaciones();								#var_dump($ar_relaciones); die();
-	
-	function in_array_r($needle, $haystack, $strict = true) {
-		if(is_array($haystack)) foreach ($haystack as $item) {
-			if (($strict ? $item === $needle : $item == $needle) || (is_array($item) && in_array_r($needle, $item, $strict))) {
-				return true;
+		// verify mandatory vars
+			if(!$terminoID || !$terminoID_to_link) {
+				exit("$accion Need more vars: terminoID: $terminoID, terminoID_to_link: $terminoID_to_link");
 			}
-		}	
-		return false;
-	}
-	
-	# añadimos al array el nuevo valor
-	if( !in_array_r($terminoID_to_link, $ar_relaciones) && $terminoID_to_link!=$terminoID ) {
-		$RecordObj_dd2 		= new RecordObj_dd($terminoID_to_link);
-		$modeloID 			= $RecordObj_dd2->get_modelo();
-		$ar_relaciones[]	= array($modeloID => $terminoID_to_link);
-		
-		# guardamos el resultado
-		$RecordObj_dd->set_relaciones($ar_relaciones);									#var_dump($ar_relaciones); die();
-		$RecordObj_dd->Save();
-	}
-	
-}
 
+		$RecordObj_dd	= new RecordObj_dd($terminoID);
+		$ar_relaciones	= $RecordObj_dd->get_relaciones();
 
-# DELETE
-if($accion=='unlinkTS') {
-		
-	#$relID	= safe_xss($_REQUEST["relID"]);
-	#$ts->deleteTR($relID);	
-	
-	if(!$terminoID || !$terminoID_to_unlink) exit("$accion Need more vars: terminoID: $terminoID, terminoID_to_unlink: $terminoID_to_unlink");
-	
-	/*
-	$RecordObj_dd	= new RecordObj_dd($terminoID);	
-	$ar_relaciones	= $RecordObj_dd->get_relaciones();
-	
-	# eliminamos del array el valor recibido
-	$ar_final = NULL;
-	if(is_array($ar_relaciones)) foreach($ar_relaciones as $key => $ar_values) {
-		
-		foreach($ar_values as $modeloID => $terminoID) {
-			
-			if($terminoID != $terminoID_to_unlink) $ar_final[] =  array($modeloID => $terminoID);	
-		}			
-	}
-	
-	# guardamos el resultado
-	$RecordObj_dd->set_relaciones($ar_final);
-	$RecordObj_dd->Save();
-	*/
-	
-	$RecordObj_dd	= new RecordObj_dd($terminoID);	
-	$RecordObj_dd->remove_element_from_ar_terminos_relacionados($terminoID_to_unlink);
-	$RecordObj_dd->Save();
-	
-}
-	
-
-# LIST
-# Búsqueda de descriptores relacionados con el actual
-$ar_terminos_relacionados 	= RecordObj_dd::get_ar_terminos_relacionados($terminoID,$cache=false);		#echo " n:".count($ar_terminos_relacionados)." <br>";print_r($ar_terminos_relacionados);die();
-$n_terminos_relacionados	= count($ar_terminos_relacionados); 
-$html = '';
-	
-if($n_terminos_relacionados > 0) {	
-	
-	$arTR = array();
-	foreach($ar_terminos_relacionados as $ar_terminos) {
-				
-		if(is_array($ar_terminos)) foreach($ar_terminos as $terminoID) {
-					
-			$arTR['terminoID'][]= $terminoID ;
-			$arTR['termino'][]	= RecordObj_dd::get_termino_by_tipo($terminoID) ;
+		function in_array_r($needle, $haystack, $strict = true) {
+			if(is_array($haystack)) foreach ($haystack as $item) {
+				if (($strict ? $item === $needle : $item == $needle) || (is_array($item) && in_array_r($needle, $item, $strict))) {
+					return true;
+				}
+			}
+			return false;
 		}
+
+		# añadimos al array el nuevo valor
+		if( !in_array_r($terminoID_to_link, $ar_relaciones) && $terminoID_to_link!==$terminoID ) {
+
+			// to related term model
+				$RecordObj_dd_related	= new RecordObj_dd($terminoID_to_link);
+				$modeloID				= $RecordObj_dd_related->get_modelo(); // get modelo term_id like 'dd96'
+
+			// model. Verify is really model
+				$RecordObj_dd_model	= new RecordObj_dd($modeloID);
+				$esmodelo			= $RecordObj_dd_model->get_esmodelo(); // get 'esmodelo' param value like 'si' / 'no'
+				if ($esmodelo!=='si') {
+					$msg = 'Error. term '.$modeloID.' is not model. Expected esmodelo value is \'si\' and current is: '.$esmodelo;
+					debug_log(__METHOD__." $msg ".to_string(), logger::ERROR);
+					exit($msg);
+				}
+
+			// new_relation. Relations format is array of objects like: [{"dd91": "mht70"},{"dd94": "rsc852"}]
+				$new_relation = (object)[
+					$modeloID => $terminoID_to_link
+				];
+
+			// add to existing relations
+				$ar_relaciones[] = $new_relation;
+
+			// Set whole relations array and save it
+				$RecordObj_dd->set_relaciones($ar_relaciones);
+				$RecordObj_dd->Save();
+		}
+
+		exit();
+	}//end if($accion=='linkTS')
+
+
+
+// unlinkTS. Delete
+	if($accion=='unlinkTS') {
+
+		#$relID	= safe_xss($_REQUEST["relID"]);
+		#$ts->deleteTR($relID);
+
+		if(!$terminoID || !$terminoID_to_unlink) exit("$accion Need more vars: terminoID: $terminoID, terminoID_to_unlink: $terminoID_to_unlink");
+
+		/*
+		$RecordObj_dd	= new RecordObj_dd($terminoID);
+		$ar_relaciones	= $RecordObj_dd->get_relaciones();
+
+		# eliminamos del array el valor recibido
+		$ar_final = NULL;
+		if(is_array($ar_relaciones)) foreach($ar_relaciones as $key => $ar_values) {
+
+			foreach($ar_values as $modeloID => $terminoID) {
+
+				if($terminoID != $terminoID_to_unlink) $ar_final[] =  array($modeloID => $terminoID);
+			}
+		}
+
+		# guardamos el resultado
+		$RecordObj_dd->set_relaciones($ar_final);
+		$RecordObj_dd->Save();
+		*/
+
+		$RecordObj_dd	= new RecordObj_dd($terminoID);
+		$RecordObj_dd->remove_element_from_ar_terminos_relacionados($terminoID_to_unlink);
+		$RecordObj_dd->Save();
+
+		exit();
+	}//end if($accion=='unlinkTS')
+
+
+
+// LIST
+// Búsqueda de descriptores relacionados con el actual
+	$ar_terminos_relacionados	= RecordObj_dd::get_ar_terminos_relacionados($terminoID, $cache=false);
+	$n_terminos_relacionados	= count($ar_terminos_relacionados);
+	if($n_terminos_relacionados > 0) {
+
+		$arTR = array();
+		foreach($ar_terminos_relacionados as $ar_terminos) {
+
+			if(is_array($ar_terminos)) foreach($ar_terminos as $terminoID) {
+
+				$arTR['terminoID'][]	= $terminoID ;
+				$arTR['termino'][]		= RecordObj_dd::get_termino_by_tipo($terminoID) ;
+			}
+		}
+
+		# ordenamos alfabéticamente el array obtenido
+		asort($arTR['termino']);
 	}
-	
-	# ordenamos alfabéticamente el array obtenido
-	asort($arTR['termino']);	
-}	
-	
-	
-$page_html	= 'html/dd_edit_rel.phtml';	
-include($page_html);
-#return $page_html;
-#exit();
-?>
+
+	// include page phtml
+	$page_html = dirname(__FILE__) . '/html/dd_edit_rel.phtml';
+	include $page_html;
+
+
