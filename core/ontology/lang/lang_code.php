1aa45fd469e73f8d147c0a0cbc58587febc16fee
Paco <paco@render.es>
2021-10-07 10:48:47 +0200

    Clean dd lang_code

--- core/ontology/lang/lang_code.php
+++ core/ontology/lang/lang_code.php
@@ -1,26 +1,25 @@
 <?php
-# D3 COMPATIBILY
+// D3 COMPATIBILY
 
 
-# SYNC LANG
-$lang = $_SESSION['lang'] = tools::convert_d4_to_d3_lang(DEDALO_APPLICATION_LANG);
+// SYNC LANG
+	$lang = $_SESSION['lang'] = tools::convert_d4_to_d3_lang(DEDALO_APPLICATION_LANG);
 
 
-# path to file
-$lang_file = $lang;
+// path to file
+	$lang_file	= $lang;
+	$lang_path	= dirname(__FILE__) . '/'.$lang_file.'.php';
 
 
-$lang_path = dirname(__FILE__) . '/'.$lang_file.'.php';
+// load lang file
+	if(!include_once $lang_path) {
+		$codHeader = '<meta http-equiv="Content-Type" content="text/html; charset=UTF-8" />';
+		$msg = "$codHeader Dedalo Language '$lang' is not defined! ";
+		// debug
+		if(SHOW_DEVELOPER===true) {
+			$msg .= '<br>lang_path: '.$lang_path;
+			$msg .= "<br><a href=\"javascript:top.location='?lang=lg-eng';\"> Back </a>";
+		}
 
-
-# load lang file
-if(file_exists($lang_path)) {	
-	require_once($lang_path);
-}else{
-	$codHeader = '<meta http-equiv="Content-Type" content="text/html; charset=UTF-8" />';
-	$msg = "$codHeader Dedalo Language not defined! ";
-	if(NIVEL==10)
-	$msg .= $lang_path;
-	$msg .= " <br><a href=\"javascript:top.location='?lang=en';\"> Back </a> ";
-	die($msg);
-}
+		die($msg);
+	}
