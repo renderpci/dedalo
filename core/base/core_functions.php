5cf8e0c3d953ea6d573f4168d968677a657ec318
Paco <paco@render.es>
2021-10-27 12:33:26 +0200

    Added core functions log color mark to errors

--- core/base/core_functions.php
+++ core/base/core_functions.php
@@ -171,27 +171,62 @@ function wrap_html($string, $htmlspecialchars=true) {
 * DEBUG_LOG
 */
 function debug_log($info, $level=logger::DEBUG) {
-	if(!SHOW_DEBUG) return false;
-	/* level ref
-	const DEBUG 	= 100;
-	const INFO 		= 75;
-	const NOTICE 	= 50;
-	const WARNING 	= 25;
-	const ERROR 	= 10;
-	const CRITICAL 	= 5;
-	*/
-	if( $level > LOGGER_LEVEL ) {
+
+	if(SHOW_DEBUG!==true) {
+		return false;
+	}
+		// level ref
+			// const DEBUG		= 100;
+			// const INFO			= 75;
+			// const NOTICE		= 50;
+			// const WARNING	= 25;
+			// const ERROR		= 10;
+			// const CRITICAL	= 5;
+
+	if($level > LOGGER_LEVEL) {
 		return false;
 	}
 
 	$msg = 'DEBUG_LOG ['.logger::level_to_string($level).'] '. $info;
 
 	if ($level<11) {
-		$msg = str_repeat("!", 10) . ' ' . $msg . ' ' . str_repeat("!", 90);
+		// $msg = $msg . PHP_EOL . str_repeat("!", 10);
+
+		$colorFormats = array(
+				// styles
+				// italic and blink may not work depending of your terminal
+				'bold'				=> "\033[1m%s\033[0m",
+				'dark'				=> "\033[2m%s\033[0m",
+				'italic'			=> "\033[3m%s\033[0m",
+				'underline'		=> "\033[4m%s\033[0m",
+				'blink'				=> "\033[5m%s\033[0m",
+				'reverse'			=> "\033[7m%s\033[0m",
+				'concealed'		=> "\033[8m%s\033[0m",
+				// foreground colors
+				'black'				=> "\033[30m%s\033[0m",
+				'red'					=> "\033[31m%s\033[0m",
+				'green'				=> "\033[32m%s\033[0m",
+				'yellow'			=> "\033[33m%s\033[0m",
+				'blue'				=> "\033[34m%s\033[0m",
+				'magenta'			=> "\033[35m%s\033[0m",
+				'cyan'				=> "\033[36m%s\033[0m",
+				'white'				=> "\033[37m%s\033[0m",
+				// background colors
+				'bg_black'		=> "\033[40m%s\033[0m",
+				'bg_red'			=> "\033[41m%s\033[0m",
+				'bg_green'		=> "\033[42m%s\033[0m",
+				'bg_yellow'		=> "\033[43m%s\033[0m",
+				'bg_blue'			=> "\033[44m%s\033[0m",
+				'bg_magenta'	=> "\033[45m%s\033[0m",
+				'bg_cyan'			=> "\033[46m%s\033[0m",
+				'bg_white'		=> "\033[47m%s\033[0m"
+    );
+    $msg = sprintf($colorFormats['bg_yellow'], $msg);
 	}
 	error_log($msg);
 
 	// $GLOBALS['log_messages'][] = $msg;
+	return true;
 }//end debug_log
 
 
