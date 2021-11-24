59037872445dc4c1cd64a5d1fb9fe280d7b75e7e
Paco <paco@render.es>
2021-11-24 11:42:42 +0100

    Added component_json search mode

--- lib/dedalo/component_json/class.component_json.php
+++ lib/dedalo/component_json/class.component_json.php
@@ -1,6 +1,6 @@
 <?php
 /*
-* CLASS COMPONENT LAYOUT
+* CLASS COMPONENT_JSON
 *
 *
 */
@@ -12,7 +12,10 @@ class component_json extends component_common {
 
 
 	
-	# CONSTRUCT
+	/**
+	* __CONSTRUCT
+	* Call parent component_common constructor
+	*/
 	public function __construct($tipo=null, $parent=null, $modo='edit', $lang=DEDALO_DATA_NOLAN, $section_tipo=null) {
 		
 		# Force always DEDALO_DATA_NOLAN
@@ -26,16 +29,15 @@ class component_json extends component_common {
 
 	/**
 	* GET_DATO
+	* @return obejct $dato
 	*/
 	public function get_dato() {
+
 		// Compressed dato to avoid postgresql change index order
 		$dato = parent::get_dato();
 
-		// De-Compress dato 
-		#if(!empty($dato)) $dato = json_decode( base64_encode($dato) );
+		// De-Compress dato
 		$dato = json_decode($dato);
-		#$dato = unserialize($dato);
-		#dump($dato, ' dato ++ '.to_string());
 
 		if(!empty($dato) && !is_object($dato) && !is_array($dato)) {			
 			trigger_error("Error. dato converted to empty object because is not as expected object. ". gettype($dato));			
@@ -55,10 +57,15 @@ class component_json extends component_common {
 		if (!empty($dato)) {
 
 			if (is_string($dato)) {
-				if (!$dato = json_decode($dato)) {
-					trigger_error("Error. Only valid JSON is accepted as dato");
-					return false;
-				}			
+
+				if ($this->modo==='search') {
+					$dato = [$dato];
+				}else{
+					if (!$dato = json_decode($dato)) {
+						trigger_error("Error. Only valid JSON is accepted as dato");
+						return false;
+					}
+				}
 			}
 
 			if(!is_object($dato) && !is_array($dato)) {
@@ -79,7 +86,7 @@ class component_json extends component_common {
 
 	/**
 	* GET_VALOR
-	* @return 
+	* @return string $valor
 	*/
 	public function get_valor() {
 		$dato  = $this->get_dato();
@@ -90,5 +97,172 @@ class component_json extends component_common {
 
 
 
-};//end class
-?>
+	/**
+	* RESOLVE_QUERY_OBJECT_SQL
+	* @param object $query_object
+	* @return object $query_object
+	*	Edited/parsed version of received object
+	*/
+	public static function resolve_query_object_sql($query_object) {
+		#debug_log(__METHOD__." query_object ".to_string($query_object), logger::DEBUG);
+
+		$q = $query_object->q;
+		if (isset($query_object->type) && $query_object->type==='jsonb') {
+			$q = json_decode($q);
+		}
+
+		# Always set fixed values
+		$query_object->type = 'string';
+		$query_object->unaccent = false;
+
+		$q = pg_escape_string(stripslashes($q));
+
+		$q_operator = isset($query_object->q_operator) ? $query_object->q_operator : null;
+
+		$component		= end($query_object->path);
+		$component_tipo	= $component->component_tipo;
+
+		switch (true) {
+			# EMPTY VALUE (in current lang data)
+			case ($q==='!*'):
+				$operator = 'IS NULL';
+				$q_clean  = '';
+				$query_object->operator = $operator;
+				$query_object->q_parsed	= $q_clean;
+				$query_object->lang 	= 'all';
+
+				$logical_operator = '$or';
+				$new_query_json = new stdClass;
+					$new_query_json->$logical_operator = [$query_object];
+
+				// Search empty only in current lang
+				// Resolve lang based on if is translatable
+					$path_end		= end($query_object->path);
+					$component_tipo	= $path_end->component_tipo;
+					$lang			= DEDALO_DATA_NOLAN;
+
+					$clone = clone($query_object);
+						$clone->operator = '=';
+						$clone->q_parsed = '\'[]\'';
+						$clone->lang 	 = $lang;
+
+					$new_query_json->$logical_operator[] = $clone;
+
+					$clone2 = clone($query_object);
+						$clone2->operator = '=';
+						$clone2->q_parsed = '\'{}\'';
+						$clone2->lang 	 = $lang;
+
+					$new_query_json->$logical_operator[] = $clone2;
+
+					// legacy data (set as null instead [])
+					$clone = clone($query_object);
+						$clone->operator = 'IS NULL';
+						$clone->lang 	 = $lang;
+
+					$new_query_json->$logical_operator[] = $clone;
+
+				# override
+				$query_object = $new_query_json;
+				break;
+			# NOT EMPTY (in any project lang data)
+			case ($q==='*'):
+				$operator = 'IS NOT NULL';
+				$q_clean  = '';
+				$query_object->operator = $operator;
+				$query_object->q_parsed	= $q_clean;
+
+				$logical_operator ='$and';
+				$new_query_json = new stdClass;
+					$new_query_json->$logical_operator = [$query_object];
+
+				// langs check
+					$ar_query_object	= [];
+					$ar_all_langs		= [DEDALO_DATA_NOLAN]; // Added no lang also
+					foreach ($ar_all_langs as $current_lang) {
+
+						$clone = clone($query_object);
+							$clone->operator = '!=';
+							$clone->q_parsed = '\'[]\'';
+							$clone->lang 	 = $current_lang;
+
+						$clone2 = clone($query_object);
+							$clone2->operator = '!=';
+							$clone2->q_parsed = '\'{}\'';
+							$clone2->lang 	 = $current_lang;
+
+						$current_operator ='$and';
+						$current_langs_query_json = new stdClass;
+							$current_langs_query_json->$current_operator = [$clone, $clone2];
+
+						$ar_query_object[] = $current_langs_query_json;
+					}
+
+					$logical_operator ='$or';
+					$langs_query_json = new stdClass;
+						$langs_query_json->$logical_operator = $ar_query_object;
+
+				# override
+				$query_object = [$new_query_json, $langs_query_json];
+				break;
+			# NOT CONTAIN
+			case (strpos($q, '-')===0 || $q_operator==='-'):
+				$operator	= '!~*';
+				$q_clean	= str_replace('-', '', $q);
+				$query_object->operator = $operator;
+				$query_object->q_parsed	= '\'.*'.$q_clean.'.*\'';
+				break;
+			# CONTAIN EXPLICIT
+			case (substr($q, 0, 1)==='*' && substr($q, -1)==='*'):
+				$operator	= '~*';
+				$q_clean	= str_replace('*', '', $q);
+				$query_object->operator = $operator;
+				$query_object->q_parsed	= '\'.*'.$q_clean.'.*\'';
+				break;
+			# LITERAL
+			case (substr($q, 0, 1)==="'" && substr($q, -1)==="'"):
+				$operator	= '~';
+				$q_clean	= str_replace("'", '', $q);
+				$query_object->operator = $operator;
+				$query_object->q_parsed	= '\'.*"'.$q_clean.'".*\'';
+				break;
+			# DEFAULT CONTAIN
+			default:
+				$operator	= 'ILIKE';
+				$q_clean	= $q;
+				$query_object->operator = $operator;
+				$query_object->q_parsed	= '\'%'.$q_clean.'%\'';
+				break;
+		}//end switch (true)
+
+
+		return $query_object;
+	}//end resolve_query_object_sql
+
+
+
+	/**
+	* SEARCH_OPERATORS_INFO
+	* Return valid operators for search in current component
+	* @return array $ar_operators
+	*/
+	public function search_operators_info() {
+
+		$ar_operators = [
+			'*' 	 => 'no_vacio', // not null
+			'!*' 	 => 'campo_vacio', // null
+			// '=' 	 => 'similar_a',
+			// '!=' 	 => 'distinto_de',
+			'-' 	 => 'no_contiene',
+			// '*text*' => 'contiene',
+			// 'text*'  => 'empieza_con',
+			// '*text'  => 'acaba_con',
+			// '\'text\'' => 'literal',
+		];
+
+		return $ar_operators;
+	}//end search_operators_info
+
+
+
+}//end class component_json
--- lib/dedalo/component_json/component_json.php
+++ lib/dedalo/component_json/component_json.php
@@ -2,78 +2,78 @@
 	
 	# CONTROLLER
 
-	$tipo 					= $this->get_tipo();
-	$parent 				= $this->get_parent();
-	$section_tipo 			= $this->get_section_tipo();
+	$tipo					= $this->get_tipo();
+	$parent					= $this->get_parent();
+	$section_tipo			= $this->get_section_tipo();
 	$modo					= $this->get_modo();
-	$lang					= $this->get_lang();		
-	$dato 					= $this->get_dato();
+	$lang					= $this->get_lang();
+	$dato					= $this->get_dato();
 	$valor					= $this->get_valor();
-	$label 					= $this->get_label();
+	$label					= $this->get_label();
 	$required				= $this->get_required();
 	$debugger				= $this->get_debugger();
 	$permissions			= $this->get_component_permissions();
-	$ejemplo				= NULL;
-	$html_title				= "Info about $tipo";	
+	$ejemplo				= null;
+	$html_title				= "Info about $tipo";
 	
 	$identificador_unico	= $this->get_identificador_unico();
 	$component_name			= get_class($this);
-	$context 				= $this->get_context();
-	$context_name 			= $context->context_name;	
+	$context				= $this->get_context();
+	$context_name			= $context->context_name;
 	
 	if($permissions===0) return null;
 	
 	# Verify component content record is inside section record filter
 	if ($this->get_filter_authorized_record()===false) return NULL ;
 	
-	$file_name				= $modo;	
+	$file_name = $modo;
 
 	
 	switch($modo) {
 		
 		case 'edit' :
-				$id_wrapper		= 'wrapper_'.$identificador_unico;
-				$input_name		= "{$tipo}_{$parent}";
-				$component_info	= $this->get_component_info('json');
-				$propiedades	= $this->get_propiedades('json');
-				#return "WORK IN PROGRESS..";
+			$id_wrapper		= 'wrapper_'.$identificador_unico;
+			$input_name		= "{$tipo}_{$parent}";
+			$component_info	= $this->get_component_info('json');
+			$propiedades	= $this->get_propiedades('json');
+			#return "WORK IN PROGRESS..";
 
-				# Aditional css / js
-				css::$ar_url[] = DEDALO_ROOT_WEB."/lib/jsoneditor/jsoneditor.min.css";
-				js::$ar_url[]  = DEDALO_ROOT_WEB."/lib/jsoneditor/jsoneditor.min.js";
+			# Aditional css / js
+			css::$ar_url[]	= DEDALO_ROOT_WEB."/lib/jsoneditor/jsoneditor.min.css";
+			js::$ar_url[]	= DEDALO_ROOT_WEB."/lib/jsoneditor/jsoneditor.min.js";
 
-				# reference
-				#$dato = new stdClass();
-				#$dato->print = '<div class="test_class"> test print </div>';
-				#$dato->edit = '<div class="test_class"> test edit </div>';
-				break;
+			# reference
+			#$dato = new stdClass();
+			#$dato->print = '<div class="test_class"> test print </div>';
+			#$dato->edit = '<div class="test_class"> test edit </div>';
+			break;
 
-		case 'tool_time_machine' :	
-				return null;	
-				break;						
-						
+		case 'tool_time_machine' :
+			return null;
+			break;
+
 		case 'search' :
-				# dato is injected by trigger search wen is needed
-				$dato = isset($this->dato) ? $this->dato : null;
-				
-				# Search input name (var search_input_name is injected in search -> records_search_list.phtml)
-				# and recovered in component_common->get_search_input_name()
-				# Normally is section_tipo + component_tipo, but when in portal can be portal_tipo + section_tipo + component_tipo
-				$search_input_name = $this->get_search_input_name();
-				
-				return null;
-				break;
+			# dato is injected by trigger search wen is needed
+			$value = isset($this->dato)
+				? json_decode($this->dato)[0] // dato in mode search is always set as array json encoded
+				: null;
+
+			# Search input name (var search_input_name is injected in search -> records_search_list.phtml)
+			# and recovered in component_common->get_search_input_name()
+			# Normally is section_tipo + component_tipo, but when in portal can be portal_tipo + section_tipo + component_tipo
+			$search_input_name = $this->get_search_input_name();
+			break;
 
 		case 'list_tm' :
-				$file_name = 'list';							
+			$file_name = 'list';
 		case 'list' :
-				echo "WORK IN PROGRESS..";
-				return ;
-				break;		
+			echo "WORK IN PROGRESS..";
+			return ;
+			break;
 		
 	}
 		
-	$page_html	= DEDALO_LIB_BASE_PATH .'/'. get_class($this) . '/html/' . get_class($this) . '_' . $file_name . '.phtml';
+	$page_html = DEDALO_LIB_BASE_PATH .'/'. get_class($this) . '/html/' . get_class($this) . '_' . $file_name . '.phtml';
 	if( !include($page_html) ) {
 		echo "<div class=\"error\">Invalid mode $this->modo</div>";
 	}
