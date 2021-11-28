36e486f5db163ad915af21a5b1d40600ca41aa68
Paco <paco@render.es>
2021-11-28 11:41:09 +0100

    Clean ts_object code

--- core/ts_object/trigger.ts_object.php
+++ core/ts_object/trigger.ts_object.php
@@ -1,6 +1,6 @@
 <?php
 // Turn off output buffering
-	ini_set('output_buffering', 'off');
+ini_set('output_buffering', 'off');
 
 $start_time=microtime(1);
 include(dirname(dirname(dirname(__FILE__))).'/config/config.php');
@@ -8,9 +8,6 @@ include(DEDALO_CORE_PATH . '/ts_object/class.ts_object.php');
 
 # TRIGGER_MANAGER. Add trigger_manager to receive and parse requested data
 $options = new stdClass();
-// $options->source = (isset($_GET['mode']) && $_GET['mode']==='get_children_data')
-// 	? 'GET'
-// 	: 'php://input';
 common::trigger_manager($options);
 
 # IGNORE_USER_ABORT
@@ -29,11 +26,11 @@ function get_children_data($json_data) {
 	session_write_close();
 
 	$response = new stdClass();
-		$response->result 	= false;
-		$response->msg 		= 'Error. Request failed ['.__FUNCTION__.']';
+		$response->result	= false;
+		$response->msg		= 'Error. Request failed ['.__FUNCTION__.']';
 
 	# set vars
-	$vars = array('section_tipo','section_id','node_type','tipo');
+		$vars = array('section_tipo','section_id','node_type','tipo');
 		foreach($vars as $name) {
 			$$name = common::setVarData($name, $json_data);
 			# DATA VERIFY
@@ -44,7 +41,6 @@ function get_children_data($json_data) {
 			}
 		}
 
-
 	if($node_type==='hierarchy_node') {
 
 		// Children are the same current data
@@ -52,27 +48,21 @@ function get_children_data($json_data) {
 			$locator->set_section_tipo($section_tipo);
 			$locator->set_section_id($section_id);
 		$children = array($locator);
-			#dump($children, ' children ++ '.to_string());
 
 	}else{
 
 		// Calculate children from parent
-		$modelo_name	= 'component_relation_children';
-		$modo			= 'list_thesaurus';
-		$lang			= DEDALO_DATA_NOLAN;
-		$component_relation_children = component_common::get_instance($modelo_name,
-																	  $tipo,
-																	  $section_id,
-																	  $modo,
-																	  $lang,
-																	  $section_tipo);
-		$dato 	   	= $component_relation_children->get_dato();
-		$children 	= $dato;
-
-		# sort_elements
-		#if(SHOW_DEBUG===true) $start_time = start_time();
-		#$children = ts_object::sort_elements($children, 'asc');
-		#if(SHOW_DEBUG===true) debug_log(__METHOD__." Time to sort children ".count($children)." - ".exec_time($start_time,""), logger::DEBUG);
+		$modelo_name					= 'component_relation_children';
+		$modo							= 'list_thesaurus';
+		$lang							= DEDALO_DATA_NOLAN;
+		$component_relation_children	= component_common::get_instance($modelo_name,
+																		 $tipo,
+																		 $section_id,
+																		 $modo,
+																		 $lang,
+																		 $section_tipo);
+		$dato		= $component_relation_children->get_dato();
+		$children	= $dato;
 	}
 
 	// model
@@ -91,11 +81,10 @@ function get_children_data($json_data) {
 
 			$ts_object			= new ts_object( $section_id, $section_tipo, $options );
 			$children_object	= $ts_object->get_children_data();
-			#debug_log(__METHOD__." children_object ".to_string($children_object), logger::DEBUG);
 
 			# Add only descriptors
 			#if ($children_object->is_descriptor===true) {
-				$children_data[]	= $children_object;
+				$children_data[] = $children_object;
 			#}
 		}
 
@@ -104,10 +93,9 @@ function get_children_data($json_data) {
 
 	}catch(Exception $e) {
 
-		$response->result 	= false;
-		$response->msg 		= 'Error. Caught exception: '.$e->getMessage();
+		$response->result	= false;
+		$response->msg		= 'Error. Caught exception: '.$e->getMessage();
 	}
-
 
 
 	// debug
@@ -140,8 +128,8 @@ function add_child($json_data) {
 	global $start_time;
 
 	$response = new stdClass();
-	$response->result 	= false;
-	$response->msg 		= 'Error. Request failed ['.__FUNCTION__.']';
+		$response->result	= false;
+		$response->msg		= 'Error. Request failed ['.__FUNCTION__.']';
 
 	// set vars
 		$vars = array('section_tipo','section_id','node_type','tipo');
@@ -156,11 +144,11 @@ function add_child($json_data) {
 			}
 
 	// new section. Create a new empty section
-		$new_section 	= section::get_instance(null,$section_tipo);
+		$new_section	= section::get_instance(null,$section_tipo);
 		$new_section_id	= $new_section->Save();
 						if (empty($new_section_id)) {
 							#debug_log(__METHOD__." Error on create new section from parent. Stoped add_child process !".to_string(), logger::ERROR);
-							$response->msg 		= 'Error on create new section from parent. Stoped add_child process !';
+							$response->msg = 'Error on create new section from parent. Stoped add_child process !';
 							return $response;
 						}
 
@@ -226,8 +214,8 @@ function add_child($json_data) {
 			$component_relation_children->Save();
 
 			# All is ok. Result is new created section section_id
-			$response->result  	= (int)$new_section_id;
-			$response->msg 		= 'Ok. Request done ['.__FUNCTION__.']';
+			$response->result	= (int)$new_section_id;
+			$response->msg		= 'Ok. Request done ['.__FUNCTION__.']';
 
 			// debug
 				if(SHOW_DEBUG===true) {
@@ -254,8 +242,8 @@ function add_child_from_hierarchy($json_data) {
 	global $start_time;
 
 	$response = new stdClass();
-		$response->result 	= false;
-		$response->msg 		= 'Error. Request failed ['.__FUNCTION__.']';
+		$response->result	= false;
+		$response->msg		= 'Error. Request failed ['.__FUNCTION__.']';
 
 	// vars
 		$vars = array('section_tipo','section_id','target_section_tipo','tipo');
@@ -270,7 +258,7 @@ function add_child_from_hierarchy($json_data) {
 			}
 
 	// new section
-		$new_section 	= section::get_instance(null,$target_section_tipo);
+		$new_section	= section::get_instance(null,$target_section_tipo);
 		$new_section_id	= $new_section->Save();
 						if (empty($new_section_id)) {
 							debug_log(__METHOD__." Error on create new section from parent. Stoped add_child process !".to_string(), logger::ERROR);
@@ -285,9 +273,9 @@ function add_child_from_hierarchy($json_data) {
 			debug_log(__METHOD__." Invalid section_map 'is_descriptor' property from section $target_section_tipo ".to_string($section_map), logger::DEBUG);
 		}else{
 			if ($section_map->thesaurus->is_descriptor!==false) {
-				$component_tipo = $section_map->thesaurus->is_descriptor;
-				$modelo_name 	= RecordObj_dd::get_modelo_name_by_tipo($component_tipo,true);
-				$component 	 	= component_common::get_instance($modelo_name,
+				$component_tipo	= $section_map->thesaurus->is_descriptor;
+				$modelo_name	= RecordObj_dd::get_modelo_name_by_tipo($component_tipo,true);
+				$component		= component_common::get_instance($modelo_name,
 																 $component_tipo,
 																 $new_section_id,
 																 'edit', // note mode edit autosave default value
@@ -302,9 +290,9 @@ function add_child_from_hierarchy($json_data) {
 			debug_log(__METHOD__." Invalid section_map 'is_indexable' property from section $target_section_tipo ".to_string($section_map), logger::DEBUG);
 		}else{
 			if ($section_map->thesaurus->is_indexable!==false) {
-				$component_tipo = $section_map->thesaurus->is_indexable;
-				$modelo_name 	= RecordObj_dd::get_modelo_name_by_tipo($component_tipo,true);
-				$component 	 	= component_common::get_instance($modelo_name,
+				$component_tipo	= $section_map->thesaurus->is_indexable;
+				$modelo_name	= RecordObj_dd::get_modelo_name_by_tipo($component_tipo,true);
+				$component		= component_common::get_instance($modelo_name,
 																 $component_tipo,
 																 $new_section_id,
 																 'edit', // note mode edit autosave default value
@@ -315,8 +303,8 @@ function add_child_from_hierarchy($json_data) {
 		}
 
 	// component_relation_children
-		$modelo_name 	= 'component_relation_children';
-		$modo 			= 'edit';
+		$modelo_name	= 'component_relation_children';
+		$modo			= 'edit';
 		$lang			= DEDALO_DATA_NOLAN;
 		$component_relation_children = component_common::get_instance($modelo_name,
 																	  $tipo,
@@ -360,8 +348,8 @@ function delete($json_data) {
 	global $start_time;
 
 	$response = new stdClass();
-		$response->result 	= false;
-		$response->msg 		= 'Error. Request failed ['.__FUNCTION__.']';
+		$response->result	= false;
+		$response->msg		= 'Error. Request failed ['.__FUNCTION__.']';
 
 	$vars = array('section_tipo','section_id','node_type');
 		foreach($vars as $name) {
@@ -376,10 +364,10 @@ function delete($json_data) {
 
 
 	# CHILDREN . Verify that current term don't have children. If yes, stop process.
-	$modelo_name 		= 'component_relation_children';
-	$modo 				= 'edit';
+	$modelo_name		= 'component_relation_children';
+	$modo				= 'edit';
 	$lang				= DEDALO_DATA_NOLAN;
-	$ar_children_tipo 	= section::get_ar_children_tipo_by_modelo_name_in_section($section_tipo, array($modelo_name), $from_cache=true, $resolve_virtual=true, $recursive=true, $search_exact=true);
+	$ar_children_tipo	= section::get_ar_children_tipo_by_modelo_name_in_section($section_tipo, array($modelo_name), $from_cache=true, $resolve_virtual=true, $recursive=true, $search_exact=true);
 	foreach ($ar_children_tipo as $current_tipo) {
 
 	 	$component_relation_children = component_common::get_instance($modelo_name,
@@ -404,12 +392,12 @@ function delete($json_data) {
 
 	# RECORD . Finally, delete target section
 	$section_to_remove	= section::get_instance($section_id, $section_tipo);
-	$result 			= (bool)$section_to_remove->Delete('delete_record');
+	$result				= (bool)$section_to_remove->Delete('delete_record');
 
 	debug_log(__METHOD__." Removed section $section_id, $section_tipo ".to_string(), logger::DEBUG);
 
 	$response->result	= $result;
-	$response->msg 		= 'Ok. Request done ['.__FUNCTION__.']';
+	$response->msg		= 'Ok. Request done ['.__FUNCTION__.']';
 
 	# Debug
 	if(SHOW_DEBUG===true) {
@@ -438,8 +426,8 @@ function update_parent_data($json_data) {
 	global $start_time;
 
 	$response = new stdClass();
-		$response->result 	= false;
-		$response->msg 		= 'Error. Request failed ['.__FUNCTION__.']';
+		$response->result	= false;
+		$response->msg		= 'Error. Request failed ['.__FUNCTION__.']';
 
 	# set vars
 	$vars = array('section_tipo','section_id','old_parent_section_id','old_parent_section_tipo','parent_section_id','parent_section_tipo','parent_node_type','tipo');
@@ -455,16 +443,16 @@ function update_parent_data($json_data) {
 		$locator = new locator();
 			$locator->set_section_tipo($old_parent_section_tipo);
 			$locator->set_section_id($old_parent_section_id);
-		$filter   = array($locator);
+		$filter = array($locator);
 		$relation_response = component_relation_common::remove_parent_references($section_tipo, $section_id, $filter);
 		if ($relation_response->result===true) {
 			debug_log(__METHOD__." Removed me as children from old parent  ".to_string(), logger::DEBUG);
 		}
 
 	# Add me as children of new parent
-		$modelo_name 	= 'component_relation_children';
-		#$tipo 			= ($parent_node_type=='root') ? DEDALO_HIERARCHY_CHILDREN_TIPO : DEDALO_THESAURUS_RELATION_CHIDREN_TIPO;
-		$modo 			= 'edit';
+		$modelo_name	= 'component_relation_children';
+		#$tipo			= ($parent_node_type=='root') ? DEDALO_HIERARCHY_CHILDREN_TIPO : DEDALO_THESAURUS_RELATION_CHIDREN_TIPO;
+		$modo			= 'edit';
 		$lang			= DEDALO_DATA_NOLAN;
 		$component_relation_children = component_common::get_instance($modelo_name,
 																	  $tipo,
@@ -481,8 +469,8 @@ function update_parent_data($json_data) {
 			debug_log(__METHOD__." Added dropped element as children of target wrap ".to_string(), logger::DEBUG);
 
 			# All is ok. Result is new created section section_id
-			$response->result 	= true;
-			$response->msg 		= 'Ok. Request done ['.__FUNCTION__.']';
+			$response->result	= true;
+			$response->msg		= 'Ok. Request done ['.__FUNCTION__.']';
 
 			# Debug
 			if(SHOW_DEBUG===true) {
@@ -507,48 +495,48 @@ function update_parent_data($json_data) {
 * SHOW_INDEXATIONS | Moved to dd_api 27-07-2021
 * @return object $response
 */
-// function show_indexations_DES($json_data) {
-// 	global $start_time;
+	// function show_indexations_DES($json_data) {
+	// 	global $start_time;
 
-// 	session_write_close();
+	// 	session_write_close();
 
-// 	$response = new stdClass();
-// 		$response->result 	= false;
-// 		$response->msg 		= 'Error. Request failed ['.__FUNCTION__.']';
+	// 	$response = new stdClass();
+	// 		$response->result 	= false;
+	// 		$response->msg 		= 'Error. Request failed ['.__FUNCTION__.']';
 
-// 	# set vars
-// 	$vars = array('section_tipo','section_id','component_tipo');
-// 		foreach($vars as $name) {
-// 			$$name = common::setVarData($name, $json_data);
-// 			# DATA VERIFY
-// 			#if ($name==='dato') continue; # Skip non mandatory
-// 			if (empty($$name)) {
-// 				$response->msg = 'Trigger Error: ('.__FUNCTION__.') Empty '.$name.' (is mandatory)';
-// 				return $response;
-// 			}
-// 		}
+	// 	# set vars
+	// 	$vars = array('section_tipo','section_id','component_tipo');
+	// 		foreach($vars as $name) {
+	// 			$$name = common::setVarData($name, $json_data);
+	// 			# DATA VERIFY
+	// 			#if ($name==='dato') continue; # Skip non mandatory
+	// 			if (empty($$name)) {
+	// 				$response->msg = 'Trigger Error: ('.__FUNCTION__.') Empty '.$name.' (is mandatory)';
+	// 				return $response;
+	// 			}
+	// 		}
 
-// 	# DIFFUSION_INDEX_TS
-// 	$diffusion_index_ts = new diffusion_index_ts($section_tipo, $section_id, $component_tipo);
-// 	$html 				= $diffusion_index_ts->get_html();
+	// 	# DIFFUSION_INDEX_TS
+	// 	$diffusion_index_ts = new diffusion_index_ts($section_tipo, $section_id, $component_tipo);
+	// 	$html 				= $diffusion_index_ts->get_html();
 
-// 	$response->result 	= $html;
-// 	$response->msg 		= "Request done successufully";
+	// 	$response->result 	= $html;
+	// 	$response->msg 		= "Request done successufully";
 
-// 	# Debug
-// 	if(SHOW_DEBUG===true) {
-// 		$debug = new stdClass();
-// 			$debug->exec_time	= exec_time_unit($start_time,'ms')." ms";
-// 			foreach($vars as $name) {
-// 				$debug->{$name} = $$name;
-// 			}
+	// 	# Debug
+	// 	if(SHOW_DEBUG===true) {
+	// 		$debug = new stdClass();
+	// 			$debug->exec_time	= exec_time_unit($start_time,'ms')." ms";
+	// 			foreach($vars as $name) {
+	// 				$debug->{$name} = $$name;
+	// 			}
 
-// 		$response->debug = $debug;
-// 	}
+	// 		$response->debug = $debug;
+	// 	}
 
 
-// 	return (object)$response;
-// }//end show_indexations
+	// 	return (object)$response;
+	// }//end show_indexations
 
 
 
@@ -560,8 +548,8 @@ function save_order($json_data) {
 	global $start_time;
 
 	$response = new stdClass();
-		$response->result 	= false;
-		$response->msg 		= 'Error. Request failed ['.__FUNCTION__.']';
+		$response->result	= false;
+		$response->msg		= 'Error. Request failed ['.__FUNCTION__.']';
 
 	# set vars
 	$vars = array('section_tipo','section_id','component_tipo','ar_locators');
@@ -597,8 +585,8 @@ function save_order($json_data) {
 	$result = $component_relation_children->Save();
 
 
-	$response->result 	= $result;
-	$response->msg 		= 'Ok. Request done ['.__FUNCTION__.']';
+	$response->result	= $result;
+	$response->msg		= 'Ok. Request done ['.__FUNCTION__.']';
 
 	# Debug
 	if(SHOW_DEBUG===true) {
@@ -639,3 +627,5 @@ public function link_term($json_data) {
 		$locator->set_section_id($section_id);
 }//end link_term
 */
+
+
