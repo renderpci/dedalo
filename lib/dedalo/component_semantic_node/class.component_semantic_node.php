f9cd0d3215821df1a4aad4391106bb88a0ae19d4
Paco <paco@render.es>
2021-12-10 20:02:20 +0100

    Added component_semantic_node compatibility options (update_portal_dato)

--- lib/dedalo/component_semantic_node/class.component_semantic_node.php
+++ lib/dedalo/component_semantic_node/class.component_semantic_node.php
@@ -36,12 +36,89 @@ class component_semantic_node extends component_relation_common {
 		// fix whole full locator
 		$this->row_locator = $row_locator;
 
+		// compatibility
+			// converts old format
+			//	{
+			//      "oh89": [
+			//        {
+			//          "section_id": "2",
+			//          "section_tipo": "ds1"
+			//        }
+			//      ]
+			//  }
+			// to the new one:
+			// {
+			//      [
+			//        {
+			//          "section_id": "2",
+			//          "section_tipo": "ds1",
+			//          "from_component_tipo": "oh89"
+			//        }
+			//      ]
+			//  }
+			if (!empty($row_locator->ds) && is_object($row_locator->ds) && is_array($row_locator->ds->{$this->tipo})) {
+
+				$ds = [];
+				foreach ($row_locator->ds->{$this->tipo} as $ds_object) {
+
+					$new_value = (object)[
+						'section_id'			=> $ds_object->section_id,
+						'section_tipo'			=> $ds_object->section_tipo,
+						'from_component_tipo'	=> $this->tipo,
+						'type'					=> $this->relation_type
+					];
+					$ds[] = $new_value;
+				}
+				$row_locator->ds = $ds;
+				debug_log(__METHOD__." Changed value of ds from OLD format to the new one ".to_string($row_locator), logger::WARNING);
+				// update portal data (!)
+				$this->update_portal_dato($row_locator);
+			}
+
 		// dato in this component, is the portal row locator portion called as property 'ts'
 		$this->dato = $row_locator->ds ?? null;
 
 		// set as db loaded
 		$this->bl_loaded_matrix_data = true;
 	}//end set_dato
+
+
+
+	/**
+	* UPDATE_PORTAL_DATO
+	* @return bool
+	*/
+	public function update_portal_dato($new_row_locator) {
+
+		$portal_tipo = $new_row_locator->from_component_tipo;
+
+		// portal update
+			$modelo_name = RecordObj_dd::get_modelo_name_by_tipo($portal_tipo,true);
+			$component = component_common::get_instance( $modelo_name,
+														 $portal_tipo,
+														 $this->parent,
+														 'list',
+														 DEDALO_DATA_NOLAN,
+														 $this->section_tipo);
+			$current_dato = $component->get_dato();
+			foreach ($current_dato as $key => $current_locator) {
+				if ($current_locator->section_id==$new_row_locator->section_id &&
+					$current_locator->section_tipo==$new_row_locator->section_tipo
+				) {
+					// replace old locator
+					$current_dato[$key] = $new_row_locator;
+
+					$new_dato = array_values($current_dato);
+					$component->set_dato($new_dato);
+					$component->Save();
+					debug_log(__METHOD__." Updated portal value with updated new_row_locator ".to_string($current_dato), logger::WARNING);
+
+					return true;
+				}
+			}
+
+		return false;
+	}//end update_portal_dato
 
 
 
