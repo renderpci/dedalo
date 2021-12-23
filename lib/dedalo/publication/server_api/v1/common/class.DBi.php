62e3ddaee92a71e6c92a7e57667af91996cdcfb8
Paco <paco@render.es>
2021-12-23 17:10:26 +0100

    Clean Publication class DBi code

--- lib/dedalo/publication/server_api/v1/common/class.DBi.php
+++ lib/dedalo/publication/server_api/v1/common/class.DBi.php
@@ -25,49 +25,27 @@ abstract class DBi {
 			return($mysqli);
 		}
 
-				/*
-				$mysqli = new mysqli($host, $user, $password, $database, $port);
-				if ($mysqli->connect_errno) {
-				    echo "Failed to connect to MySQL: (" . $mysqli->connect_errno . ") " . $mysqli->connect_error;
-				    die();
-				}
-				#echo $mysqli->host_info . "\n";
-
-				return $mysqli;
-				*/
-
-		# Oculta el mensaje 'MySQL extension is deprecated & will be removed in the future of PHP' cuando se usa con PHP >=5
-		# error_reporting(E_ERROR | E_PARSE);
-
 		# INIT
 		$mysqli = mysqli_init();
 
 		if (!$mysqli) {
-			#die('Dedalo '.__METHOD__ . ' Failed mysqli_init');
 			throw new Exception(' Dedalo '.__METHOD__ . ' Failed mysqli_init ', 1);
 		}
 
-		#$mysqli->options(MYSQLI_OPT_INT_AND_FLOAT_NATIVE, 1);
-
 		# AUTOCOMMIT : SET AUTOCOMMIT (Needed for InnoDB save)
 		if (!$mysqli->options(MYSQLI_INIT_COMMAND, 'SET AUTOCOMMIT = 1')) {
-			#die('Dedalo '.'Setting MYSQLI_INIT_COMMAND failed');
 			throw new Exception(' Connect Error. Setting MYSQLI_INIT_COMMAND failed ', 1);
 		}
 
 		# TIMEOUT : SET CONNECT_TIMEOUT
 		if (!$mysqli->options(MYSQLI_OPT_CONNECT_TIMEOUT, 10)) {
-			#die('Dedalo '.'Setting MYSQLI_OPT_CONNECT_TIMEOUT failed');
 			throw new Exception(' Connect Error. Setting MYSQLI_OPT_CONNECT_TIMEOUT failed ', 1);
 		}
 
 		# CONNECT
-		if (!$mysqli->real_connect($host, $user, $password, $database,  $port, $socket)) {
+		if (!$mysqli->real_connect($host, $user, $password, $database, $port, $socket)) {
 			throw new Exception(' Connect Error on mysqli->real_connect '.mysqli_connect_errno().' - '.mysqli_connect_error(), 1);
-			#die( wrap_pre('Dedalo '.'Connect Error (' . mysqli_connect_errno() . ') ' . mysqli_connect_error()) );
 		}
-
-		#echo 'Success... ' . $mysqli->host_info . "\n";
 
 		# UTF8 : Change character set to utf8
 		if (!$mysqli->set_charset("utf8")) {
@@ -80,4 +58,4 @@ abstract class DBi {
 
 
 
-}
+}//end DBi class
