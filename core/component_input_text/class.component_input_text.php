d91ccdee7723788a2f72a837facacd5860c0865b
Paco <paco@render.es>
2021-12-24 18:22:19 +0100

    Fix component_input_text issue on resolve_query_object_sql when q value is array

--- core/component_input_text/class.component_input_text.php
+++ core/component_input_text/class.component_input_text.php
@@ -275,9 +275,10 @@ class component_input_text extends component_common {
 	public static function resolve_query_object_sql($query_object) {
 		#debug_log(__METHOD__." query_object ".to_string($query_object), logger::DEBUG);
 
-		// $q = is_array($query_object->q) ? reset($query_object->q) : $query_object->q;
 
-		$q = $query_object->q;
+		// $q = $query_object->q;
+		$q = is_array($query_object->q) ? reset($query_object->q) : $query_object->q;
+
 		if (isset($query_object->type) && $query_object->type==='jsonb') {
 			$q = json_decode($q);
 		}
