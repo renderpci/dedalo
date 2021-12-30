1aa9470029dc0342784c909ae9af79d6c3251881
Alejandro Peña Carbonell <alex@render.es>
2021-12-30 18:56:48 +0100

    reformulate the render list for portals

--- core/component_portal/js/render_list_component_portal.js
+++ core/component_portal/js/render_list_component_portal.js
@@ -25,86 +25,6 @@ export const render_list_component_portal = function() {
 };//end render_list_component_portal
 
 
-
-
-/**
-* LIST
-* Render node for use in list
-* @return DOM node wrapper
-*/
-	// render_list_component_portal.prototype.list = async function() {
-
-	// 	const self = this
-
-	// 	// build all section_record instances
-	// 		const ar_section_record = await self.get_ar_instances()
-
-	// 	const fragment = new DocumentFragment();
-
-	// 	// add all section_record nodes
-	// 		const ar_promises				= []
-	// 		const ar_section_record_length	= ar_section_record.length
-	// 		for (let i = 0; i < ar_section_record_length; i++) {
-
-	// 			// sequential mode
-	// 				// // section_record node. Await to preserve order
-	// 				// const section_record_node = await ar_section_record[i].render()
-	// 				// 	// console.log("section_record_node:",section_record_node);
-	// 				// 	if (section_record_node) {
-	// 				// 		fragment.appendChild(section_record_node)
-	// 				// 	}else{
-	// 				// 		console.error("Invalid section_record_node:",section_record_node, ar_section_record[i]);
-	// 				// 	}
-
-	// 			// parallel mode
-	// 				const current_promise = new Promise(function(resolve){
-	// 					ar_section_record[i].render()
-	// 					.then(function(section_record_node){
-	// 						resolve(section_record_node)
-	// 					}).catch((errorMsg) => {
-	// 						console.error(errorMsg);
-	// 					})
-	// 				})
-	// 				ar_promises.push(current_promise)
-	// 		}
-
-	// 		// nodes. Await all nodes are parallel rendered and add to fragment
-	// 			await Promise.all(ar_promises).then(function(ar_section_record_node){
-	// 				const ar_section_record_node_length = ar_section_record_node.length
-	// 				for (let k = 0; k < ar_section_record_node_length; k++) {
-	// 					const section_record_node = ar_section_record_node[k]
-	// 					if (section_record_node) {
-	// 						fragment.appendChild(section_record_node)
-	// 					}else{
-	// 						console.error("Invalid section_record_node:",section_record_node, ar_section_record_node[k]);
-	// 					}
-	// 				}
-	// 			})
-
-	// 	// events
-	// 		// dblclick
-	// 			// wrapper.addEventListener("dblclick", function(e){
-	// 			// 	// e.stopPropagation()
-	// 			//
-	// 			// 	// change mode
-	// 			// 	self.change_mode('edit_in_list', true)
-	// 			// })
-
-	// 	// wrapper
-	// 		const wrapper = ui.component.build_wrapper_list(self, {
-	// 			autoload : false
-	// 		})
-
-	// 	// CSS
-
-	// 	wrapper.appendChild(fragment)
-
-	// 	return wrapper
-	// };//end  list
-
-
-
-
 /**
 * LIST
 * Render node for use in list
@@ -119,86 +39,73 @@ render_list_component_portal.prototype.list = async function(options) {
 		const render_level = options.render_level || 'full'
 
 	// view
-		const view	= self.view || null
+		const mode	= self.context.view === 'line'
+			? 'mini'
+			: 'list'
 
+		const ar_section_record	= await self.get_ar_instances({
+			mode : mode
+		})
 
-	const wrapper = (async ()=>{
+		// content_data
+			const content_data = await get_content_data(self, ar_section_record)
+			if (render_level==='content') {
+				return content_data
+			}
 
-		switch(view) {
+		// columns_map
+			const columns_map = await self.columns_map
 
-			case 'view_autocomplete99':
-				return view_autocomplete(self, options)
+		const fragment = new DocumentFragment()
 
-			default:
-				// reset service state portal_active
-					// self.portal_active = false
+		// list_body
+			const list_body = ui.create_dom_element({
+				element_type	: 'div',
+				class_name		: 'list_body',
+				parent			: fragment
+			})
+			// const items				= flat_column_items(columns_map)
+			// const template_columns	= `auto ${items.join(' ')}`
+			// const template_columns = `repeat(${columns_map.length}, 1fr)`
+			// flat columns create a sequence of grid widths taking care of sub-column space
+			// like 1fr 1fr 1fr 3fr 1fr
+			const items				= ui.flat_column_items(columns_map)
+			const template_columns	= `${items.join(' ')}`
+			Object.assign(
+				list_body.style,
+				{
+					"grid-template-columns": template_columns
+				}
+			)
 
-				const ar_section_record	= await self.get_ar_instances()
+		// header
+			// const list_header_node = build_header(columns_map, ar_section_record, self)
+			// const list_header_node = ui.render_list_header(columns_map, self, false)
+			// list_body.appendChild(list_header_node)
 
-				// content_data
-					const content_data = await get_content_data(self, ar_section_record)
-					if (render_level==='content') {
-						return content_data
-					}
+		// content_data append
+			list_body.appendChild(content_data)
 
-				// columns_map
-					const columns_map = await self.columns_map
+		// wrapper. ui build_edit returns component wrapper
+			// const _wrapper = ui.component.build_wrapper_edit(self, {
+			// 	// autoload	: false,
+			// 	// buttons		: buttons,
+			// 	list_body	: list_body
+			// 	// top		: top
+			// })
+			// _wrapper.classList.add("portal")
 
-				const fragment = new DocumentFragment()
+		// wrapper
+			const wrapper = ui.create_dom_element({
+				element_type	: 'div',
+				id				: self.id,
+				//class_name	: self.model + ' ' + self.tipo + ' ' + self.mode
+				class_name		: 'wrapper_' + self.type + ' ' + self.model + ' ' + self.tipo + ' portal ' + self.mode
+			})
+			wrapper.appendChild(fragment)
 
-				// list_body
-					const list_body = ui.create_dom_element({
-						element_type	: 'div',
-						class_name		: 'list_body',
-						parent			: fragment
-					})
-					// const items				= flat_column_items(columns_map)
-					// const template_columns	= `auto ${items.join(' ')}`
-					// const template_columns = `repeat(${columns_map.length}, 1fr)`
-					// flat columns create a sequence of grid widths taking care of sub-column space
-					// like 1fr 1fr 1fr 3fr 1fr
-					const items				= ui.flat_column_items(columns_map)
-					const template_columns	= `${items.join(' ')}`
-					Object.assign(
-						list_body.style,
-						{
-							"grid-template-columns": template_columns
-						}
-					)
-
-				// header
-					// const list_header_node = build_header(columns_map, ar_section_record, self)
-					// const list_header_node = ui.render_list_header(columns_map, self, false)
-					// list_body.appendChild(list_header_node)
-
-				// content_data append
-					list_body.appendChild(content_data)
-
-				// wrapper. ui build_edit returns component wrapper
-					// const _wrapper = ui.component.build_wrapper_edit(self, {
-					// 	// autoload	: false,
-					// 	// buttons		: buttons,
-					// 	list_body	: list_body
-					// 	// top		: top
-					// })
-					// _wrapper.classList.add("portal")
-
-				// wrapper
-					const _wrapper = ui.create_dom_element({
-						element_type	: 'div',
-						id				: self.id,
-						//class_name	: self.model + ' ' + self.tipo + ' ' + self.mode
-						class_name		: 'wrapper_' + self.type + ' ' + self.model + ' ' + self.tipo + ' portal ' + self.mode
-					})
-					_wrapper.appendChild(fragment)
-
-				// events
-					// add_events(self, _wrapper)
-
-				return _wrapper;
-		}//end switch(view)
-	})()
-
+		// events
+			// add_events(self, wrapper)
 
 	return wrapper
 };//end list
