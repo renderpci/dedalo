c4df973681d89c6defe6b13cf9465844374fdeda
Paco <paco@render.es>
2022-01-22 10:58:16 +0100

    Clean server get_context functions and cache code

--- core/sections/sections_json.php
+++ core/sections/sections_json.php
@@ -9,71 +9,125 @@
 	$section_class		= 'section';
 
 
+
 // context
 
-	
-	// if($options->get_context===true){
-	// 	switch ($options->context_type) {
-	// 		case 'simple':
-	// 			// Component structure context_simple (tipo, relations, properties, etc.)
-	// 			$context[] = $this->get_structure_context_simple($permissions);
-	// 			break;
+	// DES
+		// if($options->get_context===true){
+		// 	switch ($options->context_type) {
+		// 		case 'simple':
+		// 			// Component structure context_simple (tipo, relations, properties, etc.)
+		// 			$context[] = $this->get_structure_context_simple($permissions);
+		// 			break;
 
-	// 		default:
-	// 			foreach ((array)$ar_section_tipo as $current_section_tipo) {
-					
-	// 				$section = $section_class::get_instance(null, $current_section_tipo, $modo);
+		// 		default:
+		// 			foreach ((array)$ar_section_tipo as $current_section_tipo) {
 
-	// 				// set dd_request always to allow components know request context
-	// 					// $section->set_dd_request( $this->get_dd_request() ); // inject dd_request
+		// 				$section = $section_class::get_instance(null, $current_section_tipo, $modo);
 
-	// 				// pagination. fix pagination vars (defined in class component_common)
-	// 					$limit	= $this->search_query_object->limit;
-	// 					$offset	= $this->search_query_object->offset;
-	// 					$pagination = new stdClass();
-	// 						$pagination->limit	= $limit;
-	// 						$pagination->offset	= $offset;
-	// 					$section->pagination = $pagination;
-				
-	// 				// section JSON context
-	// 					$section_options = new stdClass();
-	// 						$section_options->get_context	= true;
-	// 						$section_options->get_data 	 	= false;
-	// 					$section_json = $section->get_json($section_options);
+		// 				// set dd_request always to allow components know request context
+		// 					// $section->set_dd_request( $this->get_dd_request() ); // inject dd_request
 
-	// 				$context = array_merge($context, $section_json->context);
-	// 			}
-				
-	// 			// $dd_request = dd_core_api::$dd_request;
-	// 			// $request_ddo = array_find($dd_request, function($item){
-	// 			// 	return $item->typo==='request_ddo';
-	// 			// });
-	// 			// // when no empty request_ddo->value
-	// 			// if ($request_ddo && !empty($request_ddo->value)) {
-	// 			// 	dd_core_api::$context_dd_objects = $request_ddo->value;
-	// 			// 	$context						 = $request_ddo->value;
-	// 			// }
-	// 			break;
-	// 	}
-	// }//end if($options->get_context===true)
+		// 				// pagination. fix pagination vars (defined in class component_common)
+		// 					$limit	= $this->search_query_object->limit;
+		// 					$offset	= $this->search_query_object->offset;
+		// 					$pagination = new stdClass();
+		// 						$pagination->limit	= $limit;
+		// 						$pagination->offset	= $offset;
+		// 					$section->pagination = $pagination;
+
+		// 				// section JSON context
+		// 					$section_options = new stdClass();
+		// 						$section_options->get_context	= true;
+		// 						$section_options->get_data 	 	= false;
+		// 					$section_json = $section->get_json($section_options);
+
+		// 				$context = array_merge($context, $section_json->context);
+		// 			}
+
+		// 			// $dd_request = dd_core_api::$dd_request;
+		// 			// $request_ddo = array_find($dd_request, function($item){
+		// 			// 	return $item->typo==='request_ddo';
+		// 			// });
+		// 			// // when no empty request_ddo->value
+		// 			// if ($request_ddo && !empty($request_ddo->value)) {
+		// 			// 	dd_core_api::$context_dd_objects = $request_ddo->value;
+		// 			// 	$context						 = $request_ddo->value;
+		// 			// }
+		// 			break;
+		// 	}
+		// }//end if($options->get_context===true)
+
 
 
 // data
 	$context	= [];
 	$data		= [];
 
-	// if($options->get_data===true){
-	
-		// dato is the full result of a search using the search_query_object
-		$dato = $this->get_dato();
+	// dato is the full result of a search using the search_query_object
+	$dato = $this->get_dato();
 
-		if (empty($dato)) {
+	if (empty($dato)) {
 
-			$ar_section_tipo	= $this->get_ar_section_tipo();
+		$ar_section_tipo = $this->get_ar_section_tipo();
 
-			foreach ((array)$ar_section_tipo as $current_section_tipo) {
-					
-				$section = $section_class::get_instance(null, $current_section_tipo, $modo);
+		foreach ((array)$ar_section_tipo as $current_section_tipo) {
+
+			$section = $section_class::get_instance(null, $current_section_tipo, $modo);
+
+			// pagination. fix pagination vars (defined in class component_common)
+				$limit	= $this->search_query_object->limit;
+				$offset	= $this->search_query_object->offset;
+				$pagination = new stdClass();
+					$pagination->limit	= $limit;
+					$pagination->offset	= $offset;
+				$section->pagination = $pagination;
+
+			// section JSON context
+				$section_json = $section->get_json();
+
+			$context = array_merge($context, $section_json->context);
+		}
+	}else{
+		// data item
+			$value = array_map(function($item) use($modo){
+
+				$locator = new stdClass();
+					$locator->section_tipo	= $item->section_tipo;
+					$locator->section_id	= $item->section_id;
+
+				// tm case
+					if($modo==='tm'){
+						$locator->matrix_id	= $item->id;
+						$locator->timestamp	= $item->timestamp;
+						$locator->state		= $item->state;
+					}
+
+				return $locator;
+			}, $dato);
+
+			$ar_section_tipo = array_map(function($locator){
+				return $locator->section_tipo;
+			}, $dato);
+
+
+
+			$item = new stdClass();
+				$item->typo			= 'sections';
+				$item->section_tipo	= $ar_section_tipo;
+				$item->tipo			= $this->caller_tipo;
+				$item->value		= $value;
+
+			$data[] = $item;
+
+		// subdatum
+			foreach ($dato as $key => $current_record) {
+
+				$section_id		= $current_record->section_id;
+				$section_tipo	= $current_record->section_tipo;
+
+				// section instance
+					$section = $section_class::get_instance($section_id, $section_tipo, $modo, $cache=true);
 
 				// pagination. fix pagination vars (defined in class component_common)
 					$limit	= $this->search_query_object->limit;
@@ -82,106 +136,48 @@
 						$pagination->limit	= $limit;
 						$pagination->offset	= $offset;
 					$section->pagination = $pagination;
-			
-				// section JSON context
+
+				// set dato
+				if ($modo==='tm') {
+					$section->set_record($current_record); // inject whole db record as var
+				}else{
+					// inject dato to section when the dato come from db and set as loaded
+					$datos = $current_record->datos ?? null;
+					if (!is_null($datos)) {
+						$section->set_dato($datos);
+						$section->set_bl_loaded_matrix_data(true);
+					}else{
+						// inject dato when the dato come from ar_locators
+						$section->set_dato($current_record);
+					}
+				}
+
+				// get the JSON data of the related component
 					$section_json = $section->get_json();
 
-				$context = array_merge($context, $section_json->context);
-			}
-	
-
-		}else{
-				// data item
-				$value = array_map(function($item) use($modo){
-					
-					$locator = new stdClass();
-						$locator->section_tipo	= $item->section_tipo;
-						$locator->section_id	= $item->section_id;
-
-					// tm case
-						if($modo==='tm'){
-							$locator->matrix_id	= $item->id;
-							$locator->timestamp	= $item->timestamp;
-							$locator->state		= $item->state;
-						}
-
-					return $locator;
-				}, $dato);
-
-				$ar_section_tipo = array_map(function($locator){
-					return $locator->section_tipo;
-				}, $dato);
-
-
-				
-				$item = new stdClass();
-					$item->typo			= 'sections';
-					$item->section_tipo	= $ar_section_tipo;
-					$item->tipo			= $this->caller_tipo;
-					$item->value		= $value;
-
-				$data[] = $item;
-
-
-			// subdatum
-				foreach ($dato as $key => $current_record) {
-
-					$section_id		= $current_record->section_id;
-					$section_tipo	= $current_record->section_tipo;
-
-					// section instance
-						$section = $section_class::get_instance($section_id, $section_tipo, $modo, $cache=true);
-
-					// pagination. fix pagination vars (defined in class component_common)
-						$limit	= $this->search_query_object->limit;
-						$offset	= $this->search_query_object->offset;
-						$pagination = new stdClass();
-							$pagination->limit	= $limit;
-							$pagination->offset	= $offset;
-						$section->pagination = $pagination;
-	
-					//set dato
-					if ($modo==='tm') {
-						$section->set_record($current_record); // inject whole db record as var
-					}else{
-						// inject dato to section when the dato come from db and set as loaded
-						$datos = $current_record->datos ?? null;
-						if (!is_null($datos)) {
-							$section->set_dato($datos);
-							$section->set_bl_loaded_matrix_data(true);
-						}else{
-							// inject dato when the dato come from ar_locators
-							$section->set_dato($current_record);
+				// context. prevent duplicated context. Get the unique context and subcontext that will be need to used in client.
+				// it's necessary to have all context called but only one it's necessary, in a list the context its calculated for every row and column, getting duplicated context and subcontext
+				// include the context that wasn't included in the previous loops.
+					$current_context = $section_json->context;
+					foreach ($current_context as $context_item) {
+						$found = array_find($context, function($el) use($context_item){
+							return 	$el->tipo===$context_item->tipo &&
+									$el->section_tipo===$context_item->section_tipo &&
+									$el->mode===$context_item->mode;
+						});
+						if ($found===null) {
+							// add if not already exists
+							$context[] = $context_item;
 						}
 					}
 
-					// get the JSON data of the related component
-						$section_json = $section->get_json();
-
-					// context. prevent duplicated context. Get the unique context and subcontext that will be need to used in client.
-					// it's necessary to have all context called but only one it's necessary, in a list the context its calculated for every row and column, getting duplicated context and subcontext
-					// include the context that wasn't included in the previous loops.
-						$current_context = $section_json->context;
-						foreach ($current_context as $context_item) {
-							$found = array_find($context, function($el) use($context_item){
-								return $el->tipo===$context_item->tipo && $el->section_tipo===$context_item->section_tipo && $el->mode===$context_item->mode;
-							});
-							if ($found===null) {
-								// add
-								$context[] = $context_item;
-							}
-						}
-
-
-					$data = array_merge($data, $section_json->data);
-
-				}//end foreach ($dato as $current_record)
-
-
-		}
-	// }// end if $permissions > 0
+				$data = array_merge($data, $section_json->data);
+			}//end foreach ($dato as $current_record)
+	}//end if (empty($dato))
 
 
 
 // JSON string
 	return common::build_element_json_output($context, $data);
+
+
