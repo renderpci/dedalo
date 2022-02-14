e1e90561cb60cad92013e33643fe747286f85bd1
Paco <paco@render.es>
2022-02-14 19:50:01 +0100

    Added v5 converted tool tool_posterframe

---
+++ tools/tool_posterframe/js/render_tool_posterframe.js
@@ -0,0 +1,225 @@
+/*global get_label, page_globals, SHOW_DEBUG, DEDALO_CORE_URL, tool_posterframe */
+/*eslint no-undef: "error"*/
+
+
+
+// imports
+	import {event_manager} from '../../../core/common/js/event_manager.js'
+	import {ui} from '../../../core/common/js/ui.js'
+
+
+
+/**
+* RENDER_TOOL_POSTERFRAME
+* Manages the component's logic and appearance in client side
+*/
+export const render_tool_posterframe = function() {
+
+	return true
+};//end render_tool_posterframe
+
+
+
+/**
+* EDIT
+* Render tool DOM nodes
+* This function is called by render common attached in 'tool_posterframe.js'
+* @param object options
+* @return DOM node
+*/
+render_tool_posterframe.prototype.edit = async function(options) {
+
+	const self = this
+
+	// options
+		const render_level = options.render_level || 'full'
+
+	// content_data
+		const content_data = await get_content_data(self)
+		if (render_level==='content') {
+			return content_data
+		}
+
+	// wrapper. ui build_edit returns a standard built tool wrapper
+		const wrapper = ui.tool.build_wrapper_edit(self, {
+			content_data : content_data
+		})
+
+	// modal container
+		const header = wrapper.querySelector('.tool_header') // is created by ui.tool.build_wrapper_edit
+		const modal  = ui.attach_to_modal(header, wrapper, null)
+		modal.on_close = () => {
+			self.caller.refresh()
+			// when closing the modal, common destroy is called to remove tool and elements instances
+			self.destroy(true, true, true)
+		}
+
+
+	return wrapper
+};//end edit
+
+
+
+/**
+* GET_CONTENT_DATA
+* Render tool body or 'content_data'
+* @param instance self
+* @return DOM node content_data
+*/
+const get_content_data = async function(self) {
+
+	const fragment = new DocumentFragment()
+
+	// main_component_container
+		const main_component_container = ui.create_dom_element({
+			element_type	: 'div',
+			class_name		: 'main_component_container',
+			parent			: fragment
+		})
+		self.main_component.render()
+		.then(function(component_node){
+			main_component_container.appendChild(component_node)
+		})
+
+	// buttons_container
+		const buttons_container = ui.create_dom_element({
+			element_type	: 'div',
+			class_name		: 'buttons_container',
+			parent			: fragment
+		})
+		const buttons_wrapper = get_buttons(self)
+		buttons_container.appendChild(buttons_wrapper)
+
+	// content_data
+		const content_data = document.createElement("div")
+			  content_data.classList.add("content_data", self.type)
+			  content_data.appendChild(fragment)
+
+
+	return content_data
+};//end get_content_data
+
+
+
+/**
+* GET_BUTTONS
+* @param object instance self
+* @return DOM node buttons_wrapper
+*/
+const get_buttons = function(self) {
+
+	const fragment = new DocumentFragment()
+
+	// identifying_image_block
+		const identifying_image_block = ui.create_dom_element({
+			element_type	: 'div',
+			class_name		: 'identifying_image_block',
+			parent			: fragment
+		})
+
+		// button_create_identifying_image
+			const button_create_identifying_image = ui.create_dom_element({
+				element_type	: 'button',
+				class_name		: 'light create_identifying_image',
+				inner_html		: get_label.crear_imagen_identificativa || 'Create identifying image',
+				parent			: identifying_image_block
+			})
+			button_create_identifying_image.addEventListener("click", async function(){
+				identifying_image_block.classList.add('loading')
+				const item_value	= JSON.parse(identifying_image_selector.value)
+				const current_time	= self.main_component.video.currentTime
+				await self.create_identifying_image(item_value, current_time)
+				identifying_image_block.classList.remove('loading')
+			})
+
+		// identifying_image_selector
+			const identifying_image_selector = ui.create_dom_element({
+				element_type	: 'select',
+				class_name		: 'identifying_image_selector',
+				parent			: identifying_image_block
+			})
+			const ar_identifying_image_length = self.ar_identifying_image.length
+			for (let i = 0; i < ar_identifying_image_length; i++) {
+
+				const item = self.ar_identifying_image[i]
+				// option
+				ui.create_dom_element({
+					element_type	: 'option',
+					value			: JSON.stringify(item),
+					text_content	: item.label + ' - ' + item.section_id,
+					parent			: identifying_image_selector
+				})
+			}
+
+
+	// manage_posterframe_block
+		const manage_posterframe_block = ui.create_dom_element({
+			element_type	: 'div',
+			class_name		: 'manage_posterframe_block',
+			parent			: fragment
+		})
+
+		// button_create_posterframe
+			const button_create_posterframe = ui.create_dom_element({
+				element_type	: 'button',
+				class_name		: 'warning gear create_posterframe',
+				inner_html		: get_label.crear || 'Create',
+				parent			: manage_posterframe_block
+			})
+			button_create_posterframe.addEventListener("click", async function(){
+				image_posterframe.classList.add('loading')
+				const current_time = self.main_component.video.currentTime
+				await self.create_posterframe(current_time)
+				if (self.main_component.data.posterframe_url===page_globals.fallback_image) {
+					// initial no posterframe case
+					await self.main_component.refresh()
+				}
+				image_posterframe.src = self.main_component.data.posterframe_url + '?' + Math.random()
+				image_posterframe.classList.remove('loading')
+			})
+
+		// button_delete_posterframe
+			const button_delete_posterframe = ui.create_dom_element({
+				element_type	: 'button',
+				class_name		: 'light delete delete_posterframe',
+				inner_html		: get_label.borrar || 'Delete',
+				parent			: manage_posterframe_block
+			})
+			button_delete_posterframe.addEventListener("click", async function(){
+				image_posterframe.classList.add('loading')
+				const deleted = await self.delete_posterframe()
+				image_posterframe.src = deleted===true
+					? page_globals.fallback_image
+					: self.main_component.data.posterframe_url + '?' + Math.random()
+				image_posterframe.classList.remove('loading')
+			})
+
+		// image_posterframe
+			const image_posterframe = ui.create_dom_element({
+				element_type	: 'img',
+				class_name		: 'image_posterframe',
+				src				: self.main_component.data.posterframe_url + '?' + Math.random(),
+				parent			: fragment
+			})
+			// const token = event_manager.subscribe('render_'+self.main_component.id, fn_update_posterframe)
+			// self.events_tokens.push(token)
+			// function fn_update_posterframe() {
+			// 	image_posterframe.src = self.main_component.data.posterframe_url + '?' + Math.random()
+			// 	console.log("updated image_posterframe.src:", image_posterframe.src);
+			// 	event_manager.unsubscribe(token)
+			// }
+
+
+	// buttons_wrapper
+		const buttons_wrapper = ui.create_dom_element({
+			element_type	: 'div',
+			class_name		: 'buttons_wrapper'
+		})
+		buttons_wrapper.appendChild(fragment)
+
+
+	return buttons_wrapper
+}//end get_buttons
+
+
+
---
+++ tools/tool_posterframe/js/tool_posterframe.js
@@ -0,0 +1,292 @@
+/*global get_label, page_globals, SHOW_DEBUG, DEDALO_CORE_URL */
+/*eslint no-undef: "error"*/
+
+
+
+// import needed modules
+	import {clone, dd_console} from '../../../core/common/js/utils/index.js'
+	import {data_manager} from '../../../core/common/js/data_manager.js'
+	// import {get_instance, delete_instance} from '../../../core/common/js/instances.js'
+	import {common, create_source} from '../../../core/common/js/common.js'
+	// import {ui} from '../../../core/common/js/ui.js'
+	import {tool_common} from '../../tool_common/js/tool_common.js'
+	import {render_tool_posterframe} from './render_tool_posterframe.js' // self tool rendered (called from render common)
+
+
+
+/**
+* TOOL_POSTERFRAME
+* Tool to make interesting things
+*/
+export const tool_posterframe = function () {
+
+	this.id				= null
+	this.model			= null
+	this.mode			= null
+	this.node			= null
+	this.ar_instances	= null
+	this.events_tokens	= null
+	this.status			= null
+	this.main_component	= null
+	this.type			= null
+	this.source_lang	= null
+	this.target_lang	= null
+	this.langs			= null
+	this.caller			= null
+
+
+	return true
+};//end page
+
+
+
+/**
+* COMMON FUNCTIONS
+* extend component functions from component common
+*/
+// prototypes assign
+	// render : using common render entry point
+	tool_posterframe.prototype.render	= common.prototype.render
+	// destroy							: using common destroy method
+	tool_posterframe.prototype.destroy	= common.prototype.destroy
+	// refresh							: using common refresh method
+	tool_posterframe.prototype.refresh	= common.prototype.refresh
+	// render mode edit (default). Set the tool custom manager to build the DOM nodes view
+	tool_posterframe.prototype.edit		= render_tool_posterframe.prototype.edit
+
+
+
+/**
+* INIT
+* Custom tool init
+*/
+tool_posterframe.prototype.init = async function(options) {
+
+	const self = this
+
+	// call the generic common tool init
+		const common_init = tool_common.prototype.init.call(this, options);
+
+	// set the self specific vars not defined by the generic init (in tool_common)
+
+
+
+	return common_init
+};//end init
+
+
+
+/**
+* BUILD
+* Custom tool build
+*/
+tool_posterframe.prototype.build = async function(autoload=false) {
+
+	const self = this
+
+	// call generic common tool build
+		const common_build = await tool_common.prototype.build.call(this, autoload);
+
+	// specific actions.. like fix main_component for convenience
+		// main_component
+			const main_component_ddo	= self.tool_config.ddo_map.find(el => el.role==="main_component")
+			self.main_component			= self.ar_instances.find(el => el.tipo===main_component_ddo.tipo)
+			// rebuild it in 'player' mode to get stream info (allow navidation frame by frame)
+			self.main_component.mode	= 'player'
+			await self.main_component.build(true)
+
+		// ar_identifying_image (used to fullfil the identifying_image selector options)
+			self.ar_identifying_image = await self.get_ar_identifying_image()
+			console.log("//// tool self:",self);
+
+	return common_build
+};//end build_custom
+
+
+
+/**
+* CREATE_POSTERFRAME
+* 	Creates a new posterframe file from current_time overwriting old file if exists
+* @param float current_time
+* 	Fom main_component video current_time value
+* @return promise > bool
+*/
+tool_posterframe.prototype.create_posterframe = function(current_time) {
+
+	const self = this
+
+	// source. Note that second argument is the name of the function to manage the tool request like 'apply_value'
+	// this generates a call as my_tool_name::my_function_name(arguments)
+		const source = create_source(self, 'create_posterframe')
+		// add the necessary arguments used in the given function
+		source.arguments = {
+			tipo			: self.main_component.tipo,
+			section_tipo	: self.main_component.section_tipo,
+			section_id		: self.main_component.section_id,
+			current_time	: current_time
+		}
+
+	// rqo
+		const rqo = {
+			dd_api	: 'dd_utils_api',
+			action	: 'tool_request',
+			source	: source
+		}
+
+	// call to the API, fetch data and get response
+		return new Promise(function(resolve){
+
+			const current_data_manager = new data_manager()
+			current_data_manager.request({body : rqo})
+			.then(function(response){
+				dd_console("-> create_posterframe API response:",'DEBUG',response);
+
+				const result = response.result // array of objects
+
+				resolve(result)
+			})
+		})
+};//end create_posterframe
+
+
+
+/**
+* DELETE_POSTERFRAME
+* 	Delete the posterframe file
+* @return promise > array
+*/
+tool_posterframe.prototype.delete_posterframe = async function() {
+
+	const self = this
+
+	// confirm dialog
+		if ( !confirm( (get_label.seguro || 'Sure?') ) ) {
+			return false
+		}
+
+	// source. Note that second argument is the name of the function to manage the tool request like 'apply_value'
+	// this generates a call as my_tool_name::my_function_name(arguments)
+		const source = create_source(self, 'delete_posterframe')
+		// add the necessary arguments used in the given function
+		source.arguments = {
+			tipo			: self.main_component.tipo,
+			section_tipo	: self.main_component.section_tipo,
+			section_id		: self.main_component.section_id
+		}
+
+	// rqo
+		const rqo = {
+			dd_api	: 'dd_utils_api',
+			action	: 'tool_request',
+			source	: source
+		}
+
+	// call to the API, fetch data and get response
+		return new Promise(function(resolve){
+
+			const current_data_manager = new data_manager()
+			current_data_manager.request({body : rqo})
+			.then(function(response){
+				dd_console("-> delete_posterframe API response:",'DEBUG',response);
+
+				const result = response.result // array of objects
+
+				resolve(result)
+			})
+		})
+};//end delete_posterframe
+
+
+
+/**
+* GET_AR_IDENTIFYING_IMAGE
+* 	Get identifying_image elements possibles from section inverse locators
+* 	Used by identifying_image selector in render
+* @return promise > array
+*/
+tool_posterframe.prototype.get_ar_identifying_image = async function() {
+
+	const self = this
+
+	// source. Note that second argument is the name of the function to manage the tool request like 'apply_value'
+	// this generates a call as my_tool_name::my_function_name(arguments)
+		const source = create_source(self, 'get_ar_identifying_image')
+		// add the necessary arguments used in the given function
+		source.arguments = {
+			section_tipo	: self.main_component.section_tipo,
+			section_id		: self.main_component.section_id
+		}
+
+	// rqo
+		const rqo = {
+			dd_api	: 'dd_utils_api',
+			action	: 'tool_request',
+			source	: source
+		}
+
+	// call to the API, fetch data and get response
+		return new Promise(function(resolve){
+
+			const current_data_manager = new data_manager()
+			current_data_manager.request({body : rqo})
+			.then(function(response){
+				dd_console("-> get_ar_identifying_image API response:",'DEBUG',response);
+
+				const result = response.result // array of objects
+
+				resolve(result)
+			})
+		})
+};//end get_ar_identifying_image
+
+
+
+/**
+* CREATE_IDENTIFYING_IMAGE
+* 	Create a new idenfifing image in target portal based on current item_value selection ad av current_time
+* @return promise > array
+*/
+tool_posterframe.prototype.create_identifying_image = async function(item_value, current_time) {
+
+	const self = this
+
+	// confirm dialog
+		if ( !confirm( (get_label.seguro || 'Sure?') ) ) {
+			return false
+		}
+
+	// source. Note that second argument is the name of the function to manage the tool request like 'apply_value'
+	// this generates a call as my_tool_name::my_function_name(arguments)
+		const source = create_source(self, 'create_identifying_image')
+		// add the necessary arguments used in the given function
+		source.arguments = {
+			tipo			: self.main_component.tipo,
+			section_tipo	: self.main_component.section_tipo,
+			section_id		: self.main_component.section_id,
+			item_value		: item_value,
+			current_time	: current_time
+		}
+
+	// rqo
+		const rqo = {
+			dd_api	: 'dd_utils_api',
+			action	: 'tool_request',
+			source	: source
+		}
+
+	// call to the API, fetch data and get response
+		return new Promise(function(resolve){
+
+			const current_data_manager = new data_manager()
+			current_data_manager.request({body : rqo})
+			.then(function(response){
+				dd_console("-> create_identifying_image API response:",'DEBUG',response);
+
+				const result = response.result // array of objects
+
+				resolve(result)
+			})
+		})
+};//end create_identifying_image
+
+
