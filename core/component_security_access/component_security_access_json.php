ea05b214bef8e43ecd5216751230054ddc6703a6
Paco <paco@render.es>
2022-02-18 18:23:49 +0100

    Improved component_security_access render

--- core/component_security_access/component_security_access_json.php
+++ core/component_security_access/component_security_access_json.php
@@ -4,9 +4,9 @@
 
 
 // component configuration vars
-	$permissions		= $this->get_component_permissions();
-	$modo				= $this->get_modo();
-	$properties			= $this->get_properties();
+	$permissions	= $this->get_component_permissions();
+	$modo			= $this->get_modo();
+	$properties		= $this->get_properties();
 
 
 
@@ -14,15 +14,17 @@
 	$context = [];
 
 	if($options->get_context===true && $permissions>0){
+
 		$add_rqo = isset($properties->unique) ? true : false;
 		switch ($options->context_type) {
+
 			case 'simple':
 				// Component structure context_simple (tipo, relations, properties, etc.)
 				$context[] = $this->get_structure_context_simple($permissions, $add_rqo);
 				break;
 
 			default:
-				
+
 				// Component structure context (tipo, relations, properties, etc.)
 					$context[] = $this->get_structure_context($permissions, $add_rqo);
 
@@ -41,29 +43,29 @@
 
 		// Value
 		switch ($modo) {
+
 			case 'list':
 				$value = component_common::extract_component_dato_fallback($this, $lang=DEDALO_DATA_LANG, $main_lang=DEDALO_DATA_LANG_DEFAULT);
 				break;
+
 			case 'edit':
 			default:
-				$value 		= $this->get_dato();
-				$datalist 	= $this->get_datalist();
+				$value		= $this->get_dato();
+				$datalist	= $this->get_datalist();
 				break;
 		}
 
 		// data item
-		$item  = $this->get_data_item($value);
-		$item->parent_tipo 			= $this->get_tipo();
-		$item->parent_section_id 	= $this->get_section_id();
+			$item = $this->get_data_item($value);
+				$item->parent_tipo			= $this->get_tipo();
+				$item->parent_section_id	= $this->get_section_id();
 
-
-		// dataset
-		if (isset($datalist) && isset($datalist->result)) {
-			$item->datalist = $datalist->result;
-		}
+		// datalist
+			if (isset($datalist)) {
+				$item->datalist = $datalist;
+			}
 
 		$data[] = $item;
-
 	}//end if($options->get_data===true && $permissions>0)
 
 
--- core/component_security_access/js/component_security_access.js
+++ core/component_security_access/js/component_security_access.js
@@ -6,10 +6,13 @@
 // imports
 	import {common} from '../../common/js/common.js'
 	import {component_common} from '../../component_common/js/component_common.js'
-	import {render_edit_component_security_access} from '../../component_security_access/js/render_edit_component_security_access.js'
+	import {clone} from '../../common/js/utils/index.js'
+	// (!) Using version 2 to test !
+	import {render_edit_component_security_access} from '../../component_security_access/js/render_edit2_component_security_access.js'
 	import {render_list_component_security_access} from '../../component_security_access/js/render_list_component_security_access.js'
 	import {render_mini_component_security_access} from '../../component_security_access/js/render_mini_component_security_access.js'
 	import {render_search_component_security_access} from '../../component_security_access/js/render_search_component_security_access.js'
+
 
 
 export const component_security_access = function(){
@@ -65,3 +68,96 @@ export const component_security_access = function(){
 	component_security_access.prototype.change_mode			= component_common.prototype.change_mode
 
 
+
+/**
+* UPDATE_VALUE
+* Update component var self.changed_value with received value
+* Note that component property 'changed_value' begins as copy of
+* DB self.data.value first element (key zero)
+* Will be used to save component data in a compact block
+*
+* @param object item
+* 	datalist item witht info about tipo, model, value as
+	{
+		label: "Descripción"
+		model: "section_group"
+		order: 2
+		parent: "mht39"
+		tipo: "mht55"
+		value: "2"
+	}
+* @param int input_value
+*
+* @return array changed_value
+*  array of objects like:
+	[
+		{
+			parent: "oh1"
+			tipo: "oh1"
+			type: "area"
+			value: 3
+		}
+	]
+*/
+component_security_access.prototype.update_value = function(item, input_value) {
+
+	const self = this
+
+	const changed_value = clone(self.changed_value || self.data.value[0] || [])
+
+	// find if already exists
+	const found = changed_value.find(el => el.tipo===item.tipo)
+	if (found) {
+		// uddate
+		found.value = input_value
+	}else{
+		// add
+		item.value = input_value
+		changed_value.push(item)
+	}
+
+	// fix changed_value
+		self.changed_value = changed_value
+
+
+	return changed_value
+}//end update_value
+
+
+
+/**
+* SAVE_CHANGES
+* @return bool true
+*/
+// save timer vars
+let save_timer;			// Timer identifier
+const wait_time = 0;	// Wait time in milliseconds before save
+component_security_access.prototype.save_changes = function() {
+
+	const self = this
+
+	// clear timer on each call to prevent multiple actions by user
+	// in sort time, generating multiple saves
+	clearTimeout(save_timer);
+
+	// wait for X ms and, if no new call is received, then process the request
+	save_timer = setTimeout(() => {
+
+		const action = typeof self.data.value[0]!=='undefined' ? 'update' : 'insert'
+		const changed_data = Object.freeze({
+			action	: action,
+			key		: 0,
+			value	: self.changed_value
+		})
+		// console.log("changed_data:",changed_data);
+		self.change_value({
+			changed_data	: changed_data,
+			refresh			: false
+		})
+		.then((save_response)=>{
+			console.log("save_response:",save_response);
+		})
+	}, wait_time);
+
+	return true
+}//end save_changes
