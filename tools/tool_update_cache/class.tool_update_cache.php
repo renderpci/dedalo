b862262e3a352b3af4e3f2e271f37d81cae58165
Paco <paco@render.es>
2022-02-28 14:16:38 +0100

    Added rebuilded tool from v5 'tool_update_cache'

---
+++ tools/tool_update_cache/class.tool_update_cache.php
@@ -0,0 +1,183 @@
+<?php
+// includes. Include another files if need
+	// include( dirname(__FILE__) . '/additional/class.additional.php');
+
+
+
+/*
+* CLASS TOOL_UPDATE_CACHE
+* Manages Dédalo cache clean actions
+*
+*/
+class tool_update_cache extends tool_common {
+
+
+
+	/**
+	* UPDATE_CACHE
+	* Exec a custom action called from client
+	* Note that tool config is stored in the tool section data (tools_register)
+	* @param object $request_options
+	* @return object $response
+	*/
+	public static function update_cache($request_options) {
+
+		$response = new stdClass();
+			$response->result	= false;
+			$response->msg		= 'Error. Request failed ['.__FUNCTION__.']';
+
+		// options
+			$options = new stdClass();
+				$options->section_tipo		= null;
+				$options->ar_component_tipo	= null;
+				foreach ($request_options as $key => $value) {if (property_exists($options, $key)) $options->$key = $value;}
+
+		// short vars
+			$section_tipo		= $options->section_tipo;
+			$ar_component_tipo	= $options->ar_component_tipo;
+
+		// Disable logging activity and time machine # !IMPORTANT
+			logger_backend_activity::$enable_log = false;
+			RecordObj_time_machine::$save_time_machine_version = false;
+
+		// RECORDS. Use actual list search options as base to build current search
+			$sqo_id	= implode('_', ['section', $section_tipo]);
+			if (empty($_SESSION['dedalo']['config']['sqo'][$sqo_id])) {
+				$response->msg .= ' section session sqo is not found!';
+				return $response;
+			}
+			$sqo			= $_SESSION['dedalo']['config']['sqo'][$sqo_id];
+			$sqo->limit		= 0;
+			$sqo->offset	= 0;
+
+		// search
+			$search		= search::get_instance($sqo);
+			$rows_data	= $search->search();
+
+		// result records iterate
+			foreach ($rows_data->ar_records as $key => $row) {
+
+				$section_id = $row->section_id;
+
+				foreach ($ar_component_tipo as $current_component_tipo) {
+
+					// model
+						$model = RecordObj_dd::get_modelo_name_by_tipo($current_component_tipo,true);
+						if (strpos($model, 'component_')===false) {
+							debug_log(__METHOD__." Skipped element '$model' tipo: $current_component_tipo (is not a component) ".to_string(), logger::DEBUG);
+							continue;
+						}
+
+					// component
+						$current_component = component_common::get_instance(
+							$model,
+							$current_component_tipo,
+							$section_id,
+							'edit',
+							DEDALO_DATA_LANG,
+							$section_tipo,
+							false // cache
+						);
+
+					// regenerate data
+						$current_component->get_dato(); # !! Important get dato before regenerate
+						$result = $current_component->regenerate_component();
+						if ($result!==true) {
+							debug_log(__METHOD__." Error on regenerate componet $model - $current_component_tipo - $section_tipo - $section_id ".to_string(), logger::ERROR);
+						}
+
+				}//end foreach ($related_terms as $current_component_tipo)
+
+			}//end foreach ($records_data->result as $key => $ar_value)
+
+
+		// Enable logging activity and time machine # !IMPORTANT
+			logger_backend_activity::$enable_log = true;
+			RecordObj_time_machine::$save_time_machine_version = true;
+
+		// response
+			$response->result	= true;
+			$response->msg		= "Updated cache of section $section_tipo successufully. Total records: ".count($rows_data->ar_records)." where components count: ".count($ar_component_tipo);
+
+
+		return $response;
+	}//end update_cache
+
+
+
+	/**
+	* GET_COMPONENT_LIST
+	* List of components ready to update cache
+	*
+	* @return object $response
+	* 	->result = array of objects
+	*/
+	public static function get_component_list($request_options) {
+
+		$response = new stdClass();
+			$response->result	= false;
+			$response->msg		= 'Error. Request failed ['.__FUNCTION__.']';
+
+		// options
+			$options = new stdClass();
+				$options->section_tipo	= null;
+				$options->lang			= DEDALO_DATA_LANG;
+				foreach ($request_options as $key => $value) {if (property_exists($options, $key)) $options->$key = $value;}
+
+		// short vars
+			$section_tipo	= $options->section_tipo;
+			$lang			= $options->lang;
+
+		# All section components
+			$related_terms = section::get_ar_children_tipo_by_modelo_name_in_section(
+				$section_tipo, // section_tipo
+				['component_'], // ar_modelo_name_required
+				true, // from_cache
+				true, // resolve_virtual
+				true, // recursive
+				false, // search_exact
+				false // ar_tipo_exclude_elements
+			);
+
+		// Only section list defined components
+			// $ar_section_list_tipo = section::get_ar_children_tipo_by_modelo_name_in_section($section_tipo, 'section_list', true);
+			// if (!isset($section_list_tipo[0])) {
+			// 	// throw new Exception("Error Processing Request. Section list not found for $section_tipo", 1);
+			// 	$msg = " Error Processing Request. Section list not found for section_tipo: $section_tipo";
+			// 	trigger_error($msg);
+			// 	$response->msg .= $msg;
+			// 	return $response;
+			// }
+			// $section_list_tipo	= $ar_section_list_tipo[0];
+			// $RecordObj_dd		= new RecordObj_dd($section_list_tipo);
+			// $related_terms		= $RecordObj_dd->get_ar_terminos_relacionados($section_list_tipo, $cache=true, $simple=true);
+
+		// component_list
+			$component_list = [];
+			foreach ($related_terms as $current_component_tipo) {
+				$model = RecordObj_dd::get_modelo_name_by_tipo($current_component_tipo,true);
+				if (strpos($model, 'component_')===false) {
+					debug_log(__METHOD__." Skipped element '$model' tipo: $current_component_tipo (is not a component) ".to_string(), logger::DEBUG);
+					continue;
+				}
+
+				$component_list[] = (object)[
+					'tipo'	=> $current_component_tipo,
+					'model'	=> $model,
+					'label'	=> RecordObj_dd::get_termino_by_tipo($current_component_tipo, $lang)
+				];
+			}
+
+		// response
+			$response->result	= $component_list;
+			$response->msg		= 'OK. Request done successfully';
+
+
+		return $response;
+	}//end get_component_list
+
+
+
+}//end class
+
+
---
+++ tools/tool_update_cache/js/tool_update_cache.js
@@ -0,0 +1,186 @@
+/*global get_label, page_globals, SHOW_DEBUG, DEDALO_CORE_URL */
+/*eslint no-undef: "error"*/
+
+
+
+// import needed modules
+	import {clone, dd_console} from '../../../core/common/js/utils/index.js'
+	import {data_manager} from '../../../core/common/js/data_manager.js'
+	// import {get_instance, delete_instance} from '../../../core/common/js/instances.js'
+	import {common, create_source} from '../../../core/common/js/common.js'
+	// import {ui} from '../../../core/common/js/ui.js'
+	import {tool_common} from '../../tool_common/js/tool_common.js'
+	import {render_tool_update_cache} from './render_tool_update_cache.js' // self tool rendered (called from render common)
+
+
+
+/**
+* TOOL_UPDATE_CACHE
+* Tool to make interesting things
+*/
+export const tool_update_cache = function () {
+
+	this.id				= null
+	this.model			= null
+	this.mode			= null
+	this.node			= null
+	this.ar_instances	= null
+	this.events_tokens	= null
+	this.status			= null
+	this.main_component	= null
+	this.type			= null
+	this.source_lang	= null
+	this.target_lang	= null
+	this.langs			= null
+	this.caller			= null
+
+	this.component_list = null
+
+
+	return true
+};//end page
+
+
+
+/**
+* COMMON FUNCTIONS
+* extend component functions from component common
+*/
+// prototypes assign
+	// render : using common render entry point
+	tool_update_cache.prototype.render	= common.prototype.render
+	// destroy							: using common destroy method
+	tool_update_cache.prototype.destroy	= common.prototype.destroy
+	// refresh							: using common refresh method
+	tool_update_cache.prototype.refresh	= common.prototype.refresh
+	// render mode edit (default). Set the tool custom manager to build the DOM nodes view
+	tool_update_cache.prototype.edit	= render_tool_update_cache.prototype.edit
+
+
+
+/**
+* INIT
+* Custom tool init
+*/
+tool_update_cache.prototype.init = async function(options) {
+
+	const self = this
+
+	// call the generic common tool init
+		const common_init = tool_common.prototype.init.call(this, options);
+
+	// set the self specific vars not defined by the generic init (in tool_common)
+
+
+
+	return common_init
+};//end init
+
+
+
+/**
+* BUILD
+* Custom tool build
+*/
+tool_update_cache.prototype.build = async function(autoload=false) {
+
+	const self = this
+
+	// call generic common tool build
+		const common_build = await tool_common.prototype.build.call(this, autoload);
+
+
+	// specific actions.. like fix main_component for convenience
+		self.component_list = await self.get_component_list()
+
+
+	return common_build
+};//end build_custom
+
+
+
+/**
+* GET_COMPONENT_LIST
+* 	Get the llist of section components selectables to update cache
+* @return promise > bool
+*/
+tool_update_cache.prototype.get_component_list = function() {
+
+	const self = this
+
+	const section_tipo = self.caller.section_tipo
+
+	// source. Note that second argument is the name of the function to manage the tool request like 'apply_value'
+	// this generates a call as my_tool_name::my_function_name(arguments)
+		const source = create_source(self, 'get_component_list')
+		// add the necessary arguments used in the given function
+		source.arguments = {
+			section_tipo : section_tipo
+		}
+
+	// rqo
+		const rqo = {
+			dd_api	: 'dd_utils_api',
+			action	: 'tool_request',
+			source	: source
+		}
+
+	// call to the API, fetch data and get response
+		return new Promise(function(resolve){
+
+			const current_data_manager = new data_manager()
+			current_data_manager.request({body : rqo})
+			.then(function(response){
+				dd_console("-> get_component_list API response:",'DEBUG',response);
+
+				const result = response.result // array of objects
+
+				resolve(result)
+			})
+		})
+};//end get_component_list
+
+
+
+/**
+* update_cache
+* 	Get the llist of section components selectables to update cache
+* @return promise > bool
+*/
+tool_update_cache.prototype.update_cache = function(ar_component_tipo) {
+
+	const self = this
+
+	const section_tipo = self.caller.section_tipo
+
+	// source. Note that second argument is the name of the function to manage the tool request like 'apply_value'
+	// this generates a call as my_tool_name::my_function_name(arguments)
+		const source = create_source(self, 'update_cache')
+		// add the necessary arguments used in the given function
+		source.arguments = {
+			section_tipo		: section_tipo,
+			ar_component_tipo	: ar_component_tipo,
+			lang				: page_globals.dedalo_application_lang
+		}
+
+	// rqo
+		const rqo = {
+			dd_api	: 'dd_utils_api',
+			action	: 'tool_request',
+			source	: source
+		}
+
+	// call to the API, fetch data and get response
+		return new Promise(function(resolve){
+
+			const current_data_manager = new data_manager()
+			current_data_manager.request({body : rqo})
+			.then(function(response){
+				dd_console("-> update_cache API response:",'DEBUG',response);
+
+				const result = response.result // array of objects
+
+				resolve(result)
+			})
+		})
+};//end update_cache
