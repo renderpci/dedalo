27ed9f3a497ecb5201c8ac945e681e1f5ef2529c
Paco <paco@render.es>
2022-03-04 11:27:41 +0100

    Clean code

--- core/diffusion/class.diffusion.php
+++ core/diffusion/class.diffusion.php
@@ -449,43 +449,43 @@ abstract class diffusion  {
 				#dump($ar_diffusion_element_tipo, ' ar_diffusion_element_tipo ++ '.to_string());
 			foreach ($ar_diffusion_element_tipo as $element_tipo) {
 
-				$RecordObj_dd = new RecordObj_dd($element_tipo);
-					$properties  = json_decode($RecordObj_dd->get_properties());
-					$diffusion_class_name = isset($properties->diffusion->class_name) ? $properties->diffusion->class_name : null;
-					$name = RecordObj_dd::get_termino_by_tipo($element_tipo, DEDALO_STRUCTURE_LANG, true, false);
+				$RecordObj_dd			= new RecordObj_dd($element_tipo);
+				$properties				= $RecordObj_dd->get_properties();
+				$diffusion_class_name	= isset($properties->diffusion->class_name) ? $properties->diffusion->class_name : null;
+				$name					= RecordObj_dd::get_termino_by_tipo($element_tipo, DEDALO_STRUCTURE_LANG, true, false);
 
-					# Database of current diffusion element
-					$ar_children = RecordObj_dd::get_ar_terminoID_by_modelo_name_and_relation($element_tipo, $modelo_name='database', $relation_type='children', $search_exact=true);
+				# Database of current diffusion element
+				$ar_children = RecordObj_dd::get_ar_terminoID_by_modelo_name_and_relation($element_tipo, $modelo_name='database', $relation_type='children', $search_exact=true);
 
-					$diffusion_database_tipo = !empty($ar_children)
-						? reset($ar_children)
-						: null;
+				$diffusion_database_tipo = !empty($ar_children)
+					? reset($ar_children)
+					: null;
 
-					// database_alias case try
-						if (empty($diffusion_database_tipo)) {
-							$ar_children			= RecordObj_dd::get_ar_terminoID_by_modelo_name_and_relation($element_tipo, $modelo_name='database_alias', $relation_type='children', $search_exact=true);
-							$database_alias_tipo	= reset($ar_children);
-							$ar_real_database_tipo	= RecordObj_dd::get_ar_terminoID_by_modelo_name_and_relation($database_alias_tipo, 'database', 'termino_relacionado', false);
+				// database_alias case try
+					if (empty($diffusion_database_tipo)) {
+						$ar_children			= RecordObj_dd::get_ar_terminoID_by_modelo_name_and_relation($element_tipo, $modelo_name='database_alias', $relation_type='children', $search_exact=true);
+						$database_alias_tipo	= reset($ar_children);
+						$ar_real_database_tipo	= RecordObj_dd::get_ar_terminoID_by_modelo_name_and_relation($database_alias_tipo, 'database', 'termino_relacionado', false);
 
-							$diffusion_database_tipo = reset($ar_real_database_tipo);
-							$diffusion_database_name = RecordObj_dd::get_termino_by_tipo($database_alias_tipo, DEDALO_STRUCTURE_LANG, true, false);
-							// dump($ar_real_database_tipo, ' ar_real_database_tipo ++ diffusion_database_name: '.to_string($diffusion_database_name));
+						$diffusion_database_tipo = reset($ar_real_database_tipo);
+						$diffusion_database_name = RecordObj_dd::get_termino_by_tipo($database_alias_tipo, DEDALO_STRUCTURE_LANG, true, false);
+						// dump($ar_real_database_tipo, ' ar_real_database_tipo ++ diffusion_database_name: '.to_string($diffusion_database_name));
 
-							// overwrite element_tipo (!)
-							// $element_tipo = $diffusion_database_tipo;
+						// overwrite element_tipo (!)
+						// $element_tipo = $diffusion_database_tipo;
 
-						}else{
-							$diffusion_database_name = RecordObj_dd::get_termino_by_tipo($diffusion_database_tipo, DEDALO_STRUCTURE_LANG, true, false);
-						}
+					}else{
+						$diffusion_database_name = RecordObj_dd::get_termino_by_tipo($diffusion_database_tipo, DEDALO_STRUCTURE_LANG, true, false);
+					}
 
-					$data = new stdClass();
-						$data->element_tipo		= $element_tipo;
-						$data->name				= $name;
-						$data->class_name		= $diffusion_class_name;
-						$data->database_name	= $diffusion_database_name;
-						$data->database_tipo	= $diffusion_database_tipo;
+				$data = new stdClass();
+					$data->element_tipo		= $element_tipo;
+					$data->name				= $name;
+					$data->class_name		= $diffusion_class_name;
+					$data->database_name	= $diffusion_database_name;
+					$data->database_tipo	= $diffusion_database_tipo;
 
-					$ar_diffusion_map->{$diffusion_group_tipo}[] = $data;
+				$ar_diffusion_map->{$diffusion_group_tipo}[] = $data;
 
 			}#foreach ($ar_diffusion_element_tipo as $element_tipo)
 
