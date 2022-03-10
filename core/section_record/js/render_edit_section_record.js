1c8d9314e3633646cfd5ef2ac83afccb0be2e71b
Paco <paco@render.es>
2022-03-10 17:06:15 +0100

    Added section_group content_data property to avoid DOM selections in section_record

--- core/section_record/js/render_edit_section_record.js
+++ core/section_record/js/render_edit_section_record.js
@@ -76,33 +76,32 @@ const get_content_data_edit = async function(self, ar_instances) {
 
 	const fragment = new DocumentFragment()
 
-	// add all section_record rendered nodes
-		// loop the instances for select the parent node
+	// render . Render all instances node in parallel
 		const ar_instances_length = ar_instances.length
-		// render all instances in parallel before iterate
-			const ar_promises = []
-			for (let i = 0; i < ar_instances_length; i++) {
-				const current_promise = new Promise(function(resolve){
-					const current_instance = ar_instances[i]
+		const ar_promises = []
+		for (let i = 0; i < ar_instances_length; i++) {
+			const current_promise = new Promise(function(resolve){
+				const current_instance = ar_instances[i]
 
-					// already rendered case
-					if (current_instance.status==='rendered' && typeof current_instance.node[0]!=='undefined') {
+				// already rendered case
+				if (current_instance.status==='rendered' && typeof current_instance.node[0]!=='undefined') {
+					resolve(true)
+				}else{
+					current_instance.render()
+					.then(function(current_instance){
+						// current_instance.instance_order_key = i
 						resolve(true)
-					}else{
-						current_instance.render()
-						.then(function(current_instance){
-							// current_instance.instance_order_key = i
-							resolve(true)
-						}).catch((errorMsg) => {
-							console.error(errorMsg);
-						})
-					}
-				})
-				ar_promises.push(current_promise)
-			}
-			// nodes. Await all instances are parallel rendered
-			await Promise.all(ar_promises) // render work done safely
+					}).catch((errorMsg) => {
+						console.error(errorMsg);
+					})
+				}
+			})
+			ar_promises.push(current_promise)
+		}
+		// nodes. Await all instances are parallel rendered
+		await Promise.all(ar_promises) // render work done safely
 
+	// hierarchize nodes. Distribute nodes to parents
 		for (let i = 0; i < ar_instances_length; i++) {
 
 			if (typeof ar_instances[i]==="undefined") {
@@ -114,49 +113,51 @@ const get_content_data_edit = async function(self, ar_instances) {
 			const current_instance		= ar_instances[i]
 			const current_instance_node	= current_instance.node[0] || await current_instance.render()
 
+			// component_filter case . Send to inspector
+				if (current_instance.model==='component_filter') {
+					// render_component_filter_xx event is observed by inspector init
+					// to get the component DOM node and to place it into the inspector container
+					event_manager.publish('render_component_filter_' + current_instance.section_tipo, current_instance_node)
+					continue;
+				}
+
 			// parent_grouper. get the parent node inside the context
 				const parent_grouper = current_instance.context.parent_grouper
 
-			// if the item has the parent the section_tipo is direct children of the section_record
-			// else we has other item parent
-			if(parent_grouper===self.section_tipo || self.mode==="list"){
+			// if the item has the parent, the section_tipo is direct children of the section_record
+			// else it has another item parent
+			if(parent_grouper===self.section_tipo) { //  || self.mode==='list'
 
+				// direct root level case
 				fragment.appendChild(current_instance_node)
 
 			}else{
-
-				// component_filter case . Send to inspector
-					if (current_instance.model==='component_filter') {
-						// render_component_filter_xx event is observed by inspector init
-						// to get the component DOM node and to place it into the inspector container
-						event_manager.publish('render_component_filter_' + current_instance.section_tipo, current_instance_node)
-						continue;
-					}
 
 				// get the parent instance like section group or others
 				const parent_instance = ar_instances.find(instance =>  instance.tipo===parent_grouper
 																	&& instance.section_id===current_instance.section_id
 																	&& instance.section_tipo===current_instance.section_tipo)
-				// if parent_istance exist go to append the current instance to it.
-				if(typeof parent_instance!=="undefined"){
+				// if parent_istance exist, go to append the current instance to it.
+				if(typeof parent_instance!=='undefined') {
 
 					const parent_node = parent_instance.node[0] || await parent_instance.render()
-
 					// move the node to his father
-					if (parent_instance.type==="grouper" && self.mode!=="list") {
-						// append inside content data of groupper
-						if (!parent_node || !current_instance_node) {
-							console.log("---error: parent_node:",parent_node, ' - current_instance_node:',current_instance_node);
-						}
-							//console.log("============== parent_node:",parent_node);
-							//console.log("==============  current_instance_node:",current_instance_node);
-						const grouper_content_data_node = parent_node.querySelector(":scope >.content_data")
+					if (parent_instance.type==='grouper') { //  && self.mode!=='list'
+						// check valid parameters
+							// if (!parent_node || !current_instance_node) {
+							// 	console.error("---error: parent_node:",parent_node, ' - current_instance_node:',current_instance_node);
+							// }
+						// append inside content data of grouper
+						// Note that 'content_data' is attached to grouper wrapper as a property to avoid DOM search
+						// const grouper_content_data_node = parent_node.querySelector(':scope >.content_data')
+						const grouper_content_data_node = parent_node.content_data
 						grouper_content_data_node.appendChild(current_instance_node)
 					}else{
+						// direct attach (safe fallback)
 						parent_node.appendChild(current_instance_node)
 					}
 				}else{
-					// direct attach (list mode and safe fallback)
+					// direct attach (safe fallback)
 					fragment.appendChild(current_instance_node)
 				}
 			}
@@ -164,8 +165,8 @@ const get_content_data_edit = async function(self, ar_instances) {
 
 
 	// content_data
-		const content_data = document.createElement("div")
-			  content_data.classList.add("content_data", self.mode, self.type)
+		const content_data = document.createElement('div')
+			  content_data.classList.add('content_data', self.mode, self.type)
 			  content_data.appendChild(fragment)
 
 
