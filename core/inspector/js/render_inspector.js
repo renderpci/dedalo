8c86ae6e655e96c01f3678b04d93f3b0cdd5139f
Alejandro Peña Carbonell <alex@render.es>
2022-04-30 19:28:42 +0200

    Change the node_info in the inspector to get the new util

--- core/inspector/js/render_inspector.js
+++ core/inspector/js/render_inspector.js
@@ -8,6 +8,7 @@
 	import {download_url} from '../../common/js/data_manager.js'
 	import {event_manager} from '../../common/js/event_manager.js'
 	// import {data_manager} from '../../common/js/data_manager.js'
+	import {render_node_info} from '../../common/js/utils/notifications.js'
 	import * as instances from '../../common/js/instances.js'
 
 
@@ -902,57 +903,8 @@ const render_activity_info = function(self) {
 			event_manager.subscribe('save', fn_saved)
 		)
 		function fn_saved(options){
-			// options
-				const instance		= options.instance
-				const api_response	= options.api_response // object or null
-				const event_msg		= options.msg
-
-			// node_info. create temporal node info
-				const node_info = ui.create_dom_element({
-					element_type	: 'div',
-					class_name		: 'node_info_save_msg'
-				})
-				// add at top
-				activity_info_body.prepend(node_info)
-				node_info.addEventListener("click", function(){
-					node_info.remove()
-				})
-
-			// msg. Based on API response result
-				if(api_response) {
-					if (api_response.result===false) {
-						node_info.classList.add('error')
-						const text = `${get_label.fail_to_save || 'Failed to save'} <br>${instance.label}`
-						node_info.insertAdjacentHTML('afterbegin', text)
-						// error msg
-							const msg = []
-							if (api_response.error) {
-								msg.push(api_response.error)
-							}
-							if (api_response.msg) {
-								msg.push(api_response.msg)
-							}
-							if (msg.length>0) {
-								node_info.insertAdjacentHTML('beforeend', '<br>' + msg.join('<br>') )
-							}
-					}else{
-						node_info.classList.add('ok')
-						const text = `${instance.label} ${get_label.guardado || 'Saved'}`
-						node_info.insertAdjacentHTML('afterbegin', text)
-						setTimeout(function(){
-							node_info.remove()
-						}, 15000)
-					}
-				}else{
-					// saved false case
-					node_info.classList.add('warning')
-					const text = `${event_msg} <br>${instance.label}`
-					node_info.insertAdjacentHTML('afterbegin', text)
-					setTimeout(function(){
-						node_info.remove()
-					}, 30000)
-				}
-
+			const node_info = render_node_info(options)
+			activity_info_body.prepend(node_info)
 		}
 
 	// track collapse toggle state of content
