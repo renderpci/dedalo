62a9b6a878f82761719a3e584a4c9e98a062b5da
Paco <paco@render.es>
2022-05-28 11:59:09 +0200

    Added section option to disable inspector. Set as false

--- core/section/js/section.js
+++ core/section/js/section.js
@@ -490,7 +490,7 @@ section.prototype.build = async function(autoload=false) {
 		}//end if (!self.paginator)
 
 	// inspector
-		if (self.mode==='edit' && !self.inspector && self.permissions) {
+		if (self.mode==='edit' && self.inspector===null && self.permissions) {
 			// if (initiator && initiator.model==='component_portal') {
 
 			// 	self.inspector = null
