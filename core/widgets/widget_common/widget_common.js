d7342ce21feee747bdb4d36117b3b5085f6e5752
Paco <paco@render.es>
2022-08-09 09:53:57 +0200

    Added main.less widgets load instead self widget css load

--- core/widgets/widget_common/widget_common.js
+++ core/widgets/widget_common/widget_common.js
@@ -63,8 +63,8 @@ widget_common.prototype.build = async function(autoload=false) {
 		self.status = 'building'
 
 	// load self style
-		const tool_css_url = DEDALO_CORE_URL + '/widgets' + self.properties.path + "/css/" + self.name + ".css"
-		common.prototype.load_style(tool_css_url) // returns promise
+		// const tool_css_url = DEDALO_CORE_URL + '/widgets' + self.properties.path + "/css/" + self.name + ".css"
+		// common.prototype.load_style(tool_css_url) // returns promise
 
 	// autoload
 		if (autoload===true) {
