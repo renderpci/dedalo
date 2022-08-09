06e391574c280b859d69e0e0f3c9dd25ad6a4ee8
Alejandro Peña Carbonell <alex@render.es>
2022-08-09 21:47:42 +0200

    Change the list render to manage views in component_image

--- core/component_image/js/render_list_component_image.js
+++ core/component_image/js/render_list_component_image.js
@@ -5,9 +5,10 @@
 
 // imports
 	// import {event_manager} from '../../common/js/event_manager.js'
-	import {ui} from '../../common/js/ui.js'
-	import {open_tool} from '../../../tools/tool_common/js/tool_common.js'
-
+	// import {ui} from '../../common/js/ui.js'
+	// import {open_tool} from '../../../tools/tool_common/js/tool_common.js'
+	import {render_list_view_default} from './render_list_view_default.js'
+	import {render_list_view_mosaic} from './render_list_view_mosaic.js'
 
 
 /**
@@ -26,74 +27,30 @@ export const render_list_component_image = function() {
 * Render node for use in list
 * @return DOM node wrapper
 */
-render_list_component_image.prototype.list = function() {
+render_list_component_image.prototype.list = function(options) {
 
 	const self = this
 
-	// short vars
-		const data		= self.data || {}
-		const datalist	= data.datalist || []
+	// options
+		// const render_level = options.render_level
+
+	// view
+		const view	= self.context.view || 'table'
 
 	// wrapper
-		const wrapper = ui.component.build_wrapper_list(self, {
-			autoload : false
-		})
-		wrapper.classList.add('media')
+		let wrapper
+		switch(view) {
 
-	// url
-		// const value		= data.value
-		const quality		= page_globals.dedalo_image_thumb_default // '1.5MB'
-		const url_object	= datalist.find(item => item.quality===quality)
-		const url			= (typeof url_object==="undefined")
-			? DEDALO_CORE_URL + "/themes/default/0.jpg"
-			: url_object.url
+			case 'mosaic':
+				wrapper = render_list_view_mosaic.render(self, options)
+				break;
 
-	// image
-		const image = ui.create_dom_element({
-			element_type	: 'img',
-			class_name		: 'hidden', // loading
-			parent			: wrapper
-		})
-		image.draggable = false
-		// image.loading = 'lazy'
-		// image.setAttribute('crossOrigin', 'Anonymous');
-		// ui.component.add_image_fallback(image)
 
-	// image background color
-		image.addEventListener('load', set_bg_color, false)
-		function set_bg_color() {
-			this.removeEventListener('load', set_bg_color, false)
-			ui.set_background_image(this, wrapper)
-			image.classList.remove('hidden')
+			case 'default':
+			default:
+				wrapper = render_list_view_default.render(self, options)
+				break;
 		}
-
-	// set src
-		image.src = url
-
-	// open viewer
-		image.addEventListener('mouseup', function (evt) {
-
-			const file_exist = data.datalist.find(item => item.file_exist === true)
-				console.log("data:",data);
-			// if the datalist doesn't has any quality with file, fire the tool_upload, enable it, so it could be used
-			// else open the player to show the image
-			if(!file_exist){
-				evt.stopPropagation();
-				// get the upload tool to be fired
-				const tool_upload = self.tools.find(el => el.model === 'tool_upload')
-
-				// open_tool (tool_common)
-					open_tool({
-						tool_context	: tool_upload,
-						caller			: self
-					})
-			}else{
-				const url = DEDALO_CORE_URL + `/page/?tipo=${self.tipo}&section_tipo=${self.section_tipo}&id=${self.section_id}&mode=viewer&menu=false`
-				const current_window = window.open(url,"image_viewer","width=10,height=10")
-				current_window.focus()
-			}
-		})
-
 
 	return wrapper
 }//end list
