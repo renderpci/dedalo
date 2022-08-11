8e7554d417962b2f53e8af690eb7eb43ba731611
Alejandro Peña Carbonell <alex@render.es>
2022-08-11 20:53:40 +0200

    Add mandatory state and move events to input node in component_email

--- core/component_email/js/render_edit_component_email.js
+++ core/component_email/js/render_edit_component_email.js
@@ -66,132 +66,25 @@ render_edit_component_email.prototype.edit = async function(options={render_leve
 const add_events = function(self, wrapper) {
 
 	// update value, subscription to the changes: if the dom input value was changed, observers dom elements will be changed own value with the observable value
-		self.events_tokens.push(
-			event_manager.subscribe('update_value_'+self.id, update_value)
-		)
-		function update_value (changed_data) {
-			// change the value of the current dom element
-			const changed_node = wrapper.querySelector('input[data-key="'+changed_data.key+'"]')
-			changed_node.value = changed_data.value
-		}
+		// 	self.events_tokens.push(
+		// 		event_manager.subscribe('update_value_'+self.id, update_value)
+		// 	)
+		// 	function update_value (changed_data) {
+		// 		// change the value of the current dom element
+		// 		const changed_node = wrapper.querySelector('input[data-key="'+changed_data.key+'"]')
+		// 		changed_node.value = changed_data.value
+		// 	}
 
 	// remove element, subscription to the events
-				// self.events_tokens.push(
-				// 	event_manager.subscribe('remove_element_'+self.id, remove_element)
-				// )
-				// async function remove_element(component) {
-				// 	// change all elements inside of content_data
-				// 	const new_content_data = await render_content_data(component)
-				// 	// replace the content_data with the refresh dom elements (imputs, delete buttons, etc)
-				// 	wrapper.childNodes[2].replaceWith(new_content_data)
-				// }
-
-	// change event, for every change the value in the imputs of the component
-		wrapper.addEventListener('change', (e) => {
-			// e.stopPropagation()
-
-			// update
-			if (e.target.matches('input[type="text"].input_value')) {
-				//console.log("++update e.target:", clone(e.target.dataset.key));
-				//console.log("++update e.target value:", clone(e.target.value));
-
-				const validated = self.verify_email(e.target.value)
-				ui.component.error(!validated, e.target)
-
-				if (validated) {
-					const changed_data = Object.freeze({
-						action	: 'update',
-						key		: JSON.parse(e.target.dataset.key),
-						value	: (e.target.value.length>0) ? e.target.value : null,
-					})
-					self.change_value({
-						changed_data : changed_data,
-						refresh 	 : false
-					})
-					.then((save_response)=>{
-						// event to update the dom elements of the instance
-						event_manager.publish('update_value_'+self.id, changed_data)
-					})
-				}
-
-				return true
-			}
-		})
-
-	// click event [click]
-		wrapper.addEventListener("click", e => {
-
-			// // insert
-			// if (e.target.matches('.button.add')) {
-
-			// 	const changed_data = Object.freeze({
-			// 		action	: 'insert',
-			// 		key		: self.data.value.length,//self.data.value.length>0 ? self.data.value.length : 1,
-			// 		value	: null
-			// 	})
-			// 	self.change_value({
-			// 		changed_data : changed_data,
-			// 		refresh 	 : false
-			// 	})
-			// 	.then((save_response)=>{
-			// 		// event to update the dom elements of the instance
-			// 		event_manager.publish('add_element_'+self.id, changed_data)
-			// 	})
-
-			// 	return true
-			// }
-
-			// remove
-			// if (e.target.matches('.button.remove')) {
-
-			// 	// force possible input change before remove
-			// 	document.activeElement.blur()
-
-			// 	const changed_data = Object.freeze({
-			// 		action	: 'remove',
-			// 		key		: e.target.dataset.key,
-			// 		value	: null,
-			// 		refresh : true
-			// 	})
-			// 	self.change_value({
-			// 		changed_data : changed_data,
-			// 		label 		 : e.target.previousElementSibling.value,
-			// 		refresh 	 : true
-			// 	})
-			// 	.then(()=>{
-			// 	})
-
-			// 	return true
-			// }
-
-			if (e.target.matches('.button.email')) {
-
-				self.send_email(e.target)
-
-				return true
-
-			}
-
-		})
-
-	// keyup event
-		wrapper.addEventListener("keyup", async (e) => {
-
-			// page unload event
-				if (e.key!=='Enter') {
-					const key				= e.target.dataset.key
-					const original_value	= self.db_data.value[key]
-					const new_value			= e.target.value
-					if (new_value!==original_value) {
-						// set_before_unload (bool) add
-						set_before_unload(true)
-					}else{
-						// set_before_unload (bool) remove
-						set_before_unload(false)
-					}
-				}
-		})//end keyup
-
+		// self.events_tokens.push(
+		// 	event_manager.subscribe('remove_element_'+self.id, remove_element)
+		// )
+		// async function remove_element(component) {
+		// 	// change all elements inside of content_data
+		// 	const new_content_data = await render_content_data(component)
+		// 	// replace the content_data with the refresh dom elements (imputs, delete buttons, etc)
+		// 	wrapper.childNodes[2].replaceWith(new_content_data)
+		// }
 
 	return true
 }//end add_events
@@ -359,6 +252,10 @@ const get_input_element_edit = (i, current_value, self) => {
 
 	const mode				= self.mode
 	const is_inside_tool	= self.is_inside_tool
+	// check if the component is mandatory and it doesn't has value
+	const add_class 		= self.context.properties.mandatory && !current_value
+		? ' mandatory'
+		: ''
 
 	// li
 		const li = ui.create_dom_element({
@@ -366,22 +263,67 @@ const get_input_element_edit = (i, current_value, self) => {
 		})
 
 	// input field
-		ui.create_dom_element({
+		const input_email = ui.create_dom_element({
 			element_type	: 'input',
 			type			: 'text',
-			class_name		: 'input_value',
-			dataset			: { key : i },
+			class_name		: 'input_value' + add_class,
 			value			: current_value,
 			parent			: li
 		})
+	//events
+		input_email.addEventListener('change',function(e){
+			const validated = self.verify_email(input_email.value)
+			ui.component.error(!validated, input_email)
 
-	// button remove
+			if (validated) {
+				const new_value = (input_email.value.length>0) ? input_email.value : null
+				const changed_data = Object.freeze({
+					action	: 'update',
+					key		: i,
+					value	: new_value,
+				})
+				self.change_value({
+					changed_data : changed_data,
+					refresh 	 : false
+				})
+				.then((save_response)=>{
+					// event to update the dom elements of the instance
+					event_manager.publish('update_value_'+self.id, changed_data)
+				})
+				// check if the new value is empty or not to remove the mandatory class
+				if(new_value){
+					input_email.classList.remove('mandatory')
+				}else{
+					input_email.classList.add('mandatory')
+				}
+
+			}
+		})//end change
+		input_email.addEventListener('keyup',async function(e){
+			// page unload event
+				if (e.key!=='Enter') {
+					const key				= i
+					const original_value	= self.db_data.value[key]
+					const new_value			= input_email.value
+					if (new_value!==original_value) {
+						// set_before_unload (bool) add
+						set_before_unload(true)
+					}else{
+						// set_before_unload (bool) remove
+						set_before_unload(false)
+					}
+				}
+		})//end keyup
+
+
+	// add buttons to the email row
 		if((mode==='edit' || 'edit_in_list') && !is_inside_tool){
 			const button_remove = ui.create_dom_element({
 				element_type	: 'span',
 				class_name		: 'button remove hidden_button',
 				parent			: li
 			})
+			// button remove
 			button_remove.addEventListener('mouseup',function(e){
 				// force possible input change before remove
 				document.activeElement.blur()
