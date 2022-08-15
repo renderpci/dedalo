ef54adb78ffab1bebe3a3c0fbd4c359bcb5b12ef
Paco <paco@render.es>
2022-08-15 12:32:59 +0200

    Fixed no rendered node exists cases in ui and component_common

--- core/component_common/js/component_common.js
+++ core/component_common/js/component_common.js
@@ -464,13 +464,14 @@ component_common.prototype.save = async function(changed_data) {
 		}
 
 	// remove previous success/error css class if exists
-		if (self.node.classList.contains("error")) {
-			self.node.classList.remove("error")
+		if (self.node) {
+			if (self.node.classList.contains("error")) {
+				self.node.classList.remove("error")
+			}
+			if (self.node.classList.contains("save_success")) {
+				self.node.classList.remove("save_success")
+			}
 		}
-		if (self.node.classList.contains("save_success")) {
-			self.node.classList.remove("save_success")
-		}
-
 
 
 	// send_data
