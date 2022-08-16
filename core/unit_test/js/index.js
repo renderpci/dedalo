d11176017031ca135241d869fd0434ccbdfec6a2
Paco <paco@render.es>
2022-08-16 13:04:52 +0200

    Refactorized unit_test to easy files code location

---
+++ core/unit_test/js/index.js
@@ -0,0 +1,11 @@
+/*global page_globals, mocha assert */
+/*eslint no-undef: "error"*/
+
+
+
+/**
+* UNIT_TEST
+* 	To check basic functionalities of Dédalo elements
+*/
+export * from './data.js'
+export * from './unit_test.js'
