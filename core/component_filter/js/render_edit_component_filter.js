0caad3a1059a1eeb8ee0a3d1d893cec518b33eb5
Alejandro Peña Carbonell <alex@render.es>
2022-08-18 22:14:55 +0200

    Reformulate events delegate for component_filter

--- core/component_filter/js/render_edit_component_filter.js
+++ core/component_filter/js/render_edit_component_filter.js
@@ -61,120 +61,120 @@ render_edit_component_filter.prototype.edit = async function(options={render_lev
 */
 const add_events = function(self, wrapper) {
 
-	// update value, subscription to the changes: if the dom input value was changed, observers dom elements will be changed own value with the observable value
-		self.events_tokens.push(
-			event_manager.subscribe('update_value_'+self.id, update_value)
-		)
-		function update_value (component) {
-			// change the value of the current dom element
-			const changed_data = component.data.changed_data
-			const changed_node = wrapper.querySelector('input[data-key="'+component.selected_key+'"]')
-			changed_node.checked = (changed_data.value === null) ? false : true
-		}
+	// // update value, subscription to the changes: if the dom input value was changed, observers dom elements will be changed own value with the observable value
+	// 	self.events_tokens.push(
+	// 		event_manager.subscribe('update_value_'+self.id, update_value)
+	// 	)
+	// 	function update_value (component) {
+	// 		// change the value of the current dom element
+	// 		const changed_data = component.data.changed_data
+	// 		const changed_node = wrapper.querySelector('input[data-key="'+component.selected_key+'"]')
+	// 		changed_node.checked = (changed_data.value === null) ? false : true
+	// 	}
 
-	// add button element, subscription to the events
-		self.events_tokens.push(
-			event_manager.subscribe('edit_element_'+self.id, edit_element)
-		)
-		function edit_element(component) {
-			// change the value of the current dom element
-			//const changed_data = component.data.changed_data
-			//const inputs_container = wrapper.querySelector('.inputs_container')
-			//input_element(changed_data.key, changed_data.value, inputs_container)
-		}
+	// // add button element, subscription to the events
+	// 	self.events_tokens.push(
+	// 		event_manager.subscribe('edit_element_'+self.id, edit_element)
+	// 	)
+	// 	function edit_element(component) {
+	// 		// change the value of the current dom element
+	// 		//const changed_data = component.data.changed_data
+	// 		//const inputs_container = wrapper.querySelector('.inputs_container')
+	// 		//input_element(changed_data.key, changed_data.value, inputs_container)
+	// 	}
 
-	// remove button element, subscription to the events
-		self.events_tokens.push(
-			event_manager.subscribe('reset_element_'+self.id, reset_element)
-		)
-		async function reset_element(instance) {
-			// change all elements inside of content_data
-			const new_content_data = await get_content_data(instance)
-			// replace the content_data with the refresh dom elements (imputs, delete buttons, etc)
-			wrapper.childNodes[1].replaceWith(new_content_data)
-		}
+	// // remove button element, subscription to the events
+	// 	self.events_tokens.push(
+	// 		event_manager.subscribe('reset_element_'+self.id, reset_element)
+	// 	)
+	// 	async function reset_element(instance) {
+	// 		// change all elements inside of content_data
+	// 		const new_content_data = await get_content_data(instance)
+	// 		// replace the content_data with the refresh dom elements (imputs, delete buttons, etc)
+	// 		wrapper.childNodes[1].replaceWith(new_content_data)
+	// 	}
 
 	// change event, for every change the value in the imputs of the component
-		wrapper.addEventListener('change', (e) => {
-			// e.stopPropagation()
+		// wrapper.addEventListener('change', (e) => {
+			// // e.stopPropagation()
 
-			// update / remove
-				if (e.target.matches('input[type="checkbox"]')) {
+			// // update / remove
+			// 	if (e.target.matches('input[type="checkbox"]')) {
 
-					const action 		= (e.target.checked===true) ? 'insert' : 'remove'
-					const parsed_value 	= JSON.parse(e.target.value)
-					const changed_key 	= self.get_changed_key(action, parsed_value)
-					const changed_value = (action==='insert') ? parsed_value : null
+			// 		const action 		= (e.target.checked===true) ? 'insert' : 'remove'
+			// 		const parsed_value 	= JSON.parse(e.target.value)
+			// 		const changed_key 	= self.get_changed_key(action, parsed_value)
+			// 		const changed_value = (action==='insert') ? parsed_value : null
 
-					const changed_data = Object.freeze({
-						action  : action,
-						key 	: changed_key,
-						value 	: changed_value
-					})
-					self.change_value({
-						changed_data	: changed_data,
-						//label			: e.target.nextElementSibling.textContent,
-						refresh			: false,
-						remove_dialog	: false
-					})
-					.then((api_response)=>{
-						self.selected_key = e.target.dataset.key
-						// event to update the dom elements of the instance
-						event_manager.publish('update_value_'+self.id, self)
-					})
+			// 		const changed_data = Object.freeze({
+			// 			action  : action,
+			// 			key 	: changed_key,
+			// 			value 	: changed_value
+			// 		})
+			// 		self.change_value({
+			// 			changed_data	: changed_data,
+			// 			//label			: e.target.nextElementSibling.textContent,
+			// 			refresh			: false,
+			// 			remove_dialog	: false
+			// 		})
+			// 		.then((api_response)=>{
+			// 			self.selected_key = e.target.dataset.key
+			// 			// event to update the dom elements of the instance
+			// 			event_manager.publish('update_value_'+self.id, self)
+			// 		})
 
-					return true
-				}
-		})
+			// 		return true
+			// 	}
+		// })
 
 	// click event
-		wrapper.addEventListener("click", e => {
+		// wrapper.addEventListener("click", e => {
 			// e.stopPropagation()
 
 			// remove all
-				if (e.target.matches('.button.reset')) {
+				// if (e.target.matches('.button.reset')) {
 
-					if (self.data.value.length===0) {
-						return true
-					}
+				// 	if (self.data.value.length===0) {
+				// 		return true
+				// 	}
 
-					const changed_data = Object.freeze({
-						action  : 'remove',
-						key 	: false,
-						value 	: null
-					})
-					self.change_value({
-						changed_data : changed_data,
-						label  		 : 'All',
-						refresh 	 : true
-					})
-					.then((api_response)=>{
-						// rebuild and save the component
-						// event_manager.publish('reset_element_'+self.id, self)
-						// event_manager.publish('save_component_'+self.id, self)
-					})
+				// 	const changed_data = Object.freeze({
+				// 		action  : 'remove',
+				// 		key 	: false,
+				// 		value 	: null
+				// 	})
+				// 	self.change_value({
+				// 		changed_data : changed_data,
+				// 		label  		 : 'All',
+				// 		refresh 	 : true
+				// 	})
+				// 	.then((api_response)=>{
+				// 		// rebuild and save the component
+				// 		// event_manager.publish('reset_element_'+self.id, self)
+				// 		// event_manager.publish('save_component_'+self.id, self)
+				// 	})
 
-					return true
-				}
+				// 	return true
+				// }
 
 			// edit target section
-				if (e.target.matches('.button.edit')) {
+				// if (e.target.matches('.button.edit')) {
 
-					// rebuild_nodes. event to render the component again
-					event_manager.publish('edit_element_'+self.id, self)
+				// 	// rebuild_nodes. event to render the component again
+				// 	event_manager.publish('edit_element_'+self.id, self)
 
-					return true
-				}
+				// 	return true
+				// }
 
 			// change_mode
-				if (e.target.matches('.button.close')) {
-					//change mode
-					self.change_mode('list', true)
+				// if (e.target.matches('.button.close')) {
+				// 	//change mode
+				// 	self.change_mode('list', true)
 
-					return true
-				}
-		})
-
+				// 	return true
+				// }
+		// })
+//
 	// dblclick event
 		//wrapper.addEventListener("dblclick", function(e){
 		//	e.stopPropagation()
@@ -186,18 +186,18 @@ const add_events = function(self, wrapper) {
 		//})
 
 	// focus event
-		wrapper.addEventListener("focus", e => {
-			// e.stopPropagation()
+		// wrapper.addEventListener("focus", e => {
+		// 	// e.stopPropagation()
 
-			// selected_node. fix selected node
-			self.selected_node = wrapper
+		// 	// selected_node. fix selected node
+		// 	self.selected_node = wrapper
 
-			if (e.target.matches('input[type="checkbox"]')) {
-			 	event_manager.publish('active_component', self)
+		// 	if (e.target.matches('input[type="checkbox"]')) {
+		// 	 	event_manager.publish('active_component', self)
 
-			 	return true
-			}
-		},true)
+		// 	 	return true
+		// 	}
+		// },true)
 
 	return true
 }//end add_events
@@ -322,10 +322,33 @@ const get_buttons = (self) => {
 		}
 
 	// button reset
-		ui.create_dom_element({
+		const button_reset = ui.create_dom_element({
 			element_type	: 'span',
 			class_name 		: 'button reset',
 			parent 			: fragment
+		})
+		button_reset.addEventListener('click',function(){
+			if (self.data.value.length===0) {
+				return true
+			}
+
+			const changed_data = Object.freeze({
+				action  : 'remove',
+				key 	: false,
+				value 	: null
+			})
+			self.change_value({
+				changed_data : changed_data,
+				label  		 : 'All',
+				refresh 	 : true
+			})
+			.then((api_response)=>{
+				// rebuild and save the component
+				// event_manager.publish('reset_element_'+self.id, self)
+				// event_manager.publish('save_component_'+self.id, self)
+			})
+
+			return true
 		})
 
 	// buttons tools
@@ -426,10 +449,32 @@ const get_input_element = (i, current_value, self) => {
 			element_type	: 'input',
 			type			: 'checkbox',
 			id				: self.id +"_"+ i,
-			dataset			: { key : i },
-			value			: JSON.stringify(datalist_value),
 			parent			: li
 		})
+		option.addEventListener('change',function() {
+			const action 		= (option.checked===true) ? 'insert' : 'remove'
+			const changed_key 	= self.get_changed_key(action, datalist_value) // find the data.value key (could be different of datalist key)
+			const changed_value = (action==='insert') ? datalist_value : null
+
+			const changed_data = Object.freeze({
+				action  : action,
+				key 	: changed_key,
+				value 	: changed_value
+			})
+			self.change_value({
+				changed_data	: changed_data,
+				refresh			: false,
+				remove_dialog	: ()=>{
+					return true
+				}
+			})
+			.then((api_response)=>{
+				self.selected_key = i
+				// event to update the dom elements of the instance
+				event_manager.publish('update_value_'+self.id, self)
+			})
+		})//end change event
+
 		// checked option set on match
 		for (let j = 0; j < value_length; j++) {
 			if (value[j] && datalist_value &&
