5e1b6084a8fcd0d1d29efd011ae9886e6cd32cf2
Alejandro Peña Carbonell <alex@render.es>
2022-08-18 20:57:50 +0200

    Comment the Changed_key in component_check_box

--- core/component_check_box/js/component_check_box.js
+++ core/component_check_box/js/component_check_box.js
@@ -98,6 +98,9 @@ export const component_check_box = function(){
 
 /**
 * GET_CHANGED_KEY
+* Find the key of the data with the selected value of the datalist
+* The key of datalist has all possible values of the components
+* Key of the data is the active options in data.
 * @param string action
 * @param object value
 */
