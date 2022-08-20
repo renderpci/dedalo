7afb403d86e6d918781e69e02652d148d4877b06
Paco <paco@render.es>
2022-08-20 17:02:23 +0200

    Fixed component_inverse CSS and DOM nodes issues with new data_value containers

--- core/component_inverse/js/render_edit_component_inverse.js
+++ core/component_inverse/js/render_edit_component_inverse.js
@@ -19,35 +19,26 @@ export const render_edit_component_inverse = function() {
 * Render node for use in edit
 * @return DOM node
 */
-render_edit_component_inverse.prototype.edit = function() {
+render_edit_component_inverse.prototype.edit = function(options) {
 
 	const self = this
 
-	const value = self.data.value
+	// options
+		const render_level = options.render_level || 'full'
 
 	// content_data
-		const content_data = ui.component.build_content_data(self)
-
-	// inputs
-		const inputs_container = ui.create_dom_element({
-			element_type	: 'ul',
-			class_name 		: 'inputs_container',
-			parent 			: content_data
-		})
-
-	// build values
-		const inputs_value = value
-		const value_length = inputs_value.length
-
-		for (let i = 0; i < value_length; i++) {
-			const input_element_node = get_input_element(i, inputs_value[i])
-			inputs_container.appendChild(input_element_node)
+		const content_data = get_content_data(self)
+		if (render_level==='content') {
+			return content_data
 		}
 
-	// ui build_edit returns component wrapper
+	// wrapper. ui build_edit returns component wrapper
 		const wrapper = ui.component.build_wrapper_edit(self, {
 			content_data : content_data
 		})
+		// set pointers
+		wrapper.content_data = content_data
+
 
 	return wrapper
 }//end edit
@@ -55,39 +46,71 @@ render_edit_component_inverse.prototype.edit = function() {
 
 
 /**
-* GET_INPUT_ELEMENT
-* @return dom element li
+* GET_CONTENT_DATA
+* @return DOM node content_data
 */
-const get_input_element = (i, current_value) => {
+const get_content_data = function(self) {
 
-	// li
-		const li = ui.create_dom_element({
-			element_type : 'li'
+	// short vars
+		const data	= self.data || {}
+		const value	= data.value || []
+
+	// content_data
+		const content_data = ui.component.build_content_data(self)
+
+	// values (inputs)
+		const inputs_value	= (value.length<1) ? [null] : value // force one empty input at least
+		const value_length	= inputs_value.length
+		for (let i = 0; i < value_length; i++) {
+			const content_value = get_content_value(i, inputs_value[i])
+			content_data.appendChild(content_value)
+			// set the pointer
+			content_data[i] = content_value
+		}
+
+
+	return content_data
+}//end get_content_data
+
+
+
+/**
+* GET_CONTENT_VALUE
+* @return DOM node content_value
+*/
+const get_content_value = (i, current_value) => {
+
+	// short vars
+		const locator	= current_value.locator
+		const datalist	= current_value.datalist
+
+	// content_value
+		const content_value = ui.create_dom_element({
+			element_type	: 'div',
+			class_name		: 'content_value'
 		})
 
 	// span field section_id from related inverse section
 		ui.create_dom_element({
 			element_type	: 'span',
 			class_name		: 'inverse_show_section_id',
-			dataset			: { key : i },
-			text_node		: current_value.locator.from_section_id,
-			parent			: li
+			text_node		: locator.from_section_id,
+			parent			: content_value
 		})
 
 	// build span fields with other values from related inverse section
-		const span_datalist_length 	= current_value.datalist.length
+		const span_datalist_length = datalist.length
 		for (let j = 0; j < span_datalist_length; j++) {
 			// span_value
+			const parsed_value = datalist[j].label.concat(': ', datalist[j].value)
 			ui.create_dom_element({
 				element_type	: 'span',
 				class_name		: 'inverse_show_values',
-				text_node		: current_value.datalist[j].label.concat(': ', current_value.datalist[j].value),
-				parent			: li
+				text_node		: parsed_value,
+				parent			: content_value
 			})
-
 		}
 
-	return li
-}//end get_input_element
 
-
+	return content_value
+}//end get_content_value
