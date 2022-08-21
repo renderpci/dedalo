1b82e0a32433b0ec1a310749f66d962efca6d1c4
Paco <paco@render.es>
2022-08-21 20:05:52 +0200

    Clean up code

--- core/component_geolocation/js/render_search_component_geolocation.js
+++ core/component_geolocation/js/render_search_component_geolocation.js
@@ -5,45 +5,45 @@
 // UNDER CONSTRUCTION. (!) NOTE THAT CURRENTLY, THIS COMPONENT IS NOT SHOWED IN SEARCH LIST
 
 
-// // imports
-// 	// import {event_manager} from '../../common/js/event_manager.js'
-// 	import {ui} from '../../common/js/ui.js'
-// 	import {
-// 		get_content_data_edit
-// 	}
-// 	from '../../component_geolocation/js/render_edit_component_geolocation.js'
+// imports
+	// import {event_manager} from '../../common/js/event_manager.js'
+	import {ui} from '../../common/js/ui.js'
+	import {
+		get_content_data_edit
+	}
+	from '../../component_geolocation/js/render_edit_component_geolocation.js'
 
 
-// /**
-// * RENDER_SEARCH_COMPONENT_GEOLOCATION
-// * Manages the component's logic and apperance in client side
-// */
-// export const render_search_component_geolocation = function() {
+/**
+* RENDER_SEARCH_COMPONENT_GEOLOCATION
+* Manages the component's logic and apperance in client side
+*/
+export const render_search_component_geolocation = function() {
 
-// 	return true
-// }//end render_search_component_geolocation
-
-
-
-// /**
-// * SEARCH
-// * Render node for use in edit
-// * @return DOM node wrapper
-// */
-// render_search_component_geolocation.prototype.search = async function() {
-
-// 	const self = this
-
-// 	// content data
-// 		const content_data = get_content_data_edit(self)
-
-// 	// wrapper. ui build_edit returns component wrapper
-// 		const wrapper = ui.component.build_wrapper_search(self, {
-// 			content_data : content_data
-// 		})
-// 		// set pointers
-// 		wrapper.content_data = content_data
+	return true
+}//end render_search_component_geolocation
 
 
-// 	return wrapper
-// }//end search
+
+/**
+* SEARCH
+* Render node for use in edit
+* @return DOM node wrapper
+*/
+render_search_component_geolocation.prototype.search = async function() {
+
+	const self = this
+
+	// content data
+		const content_data = get_content_data_edit(self)
+
+	// wrapper. ui build_edit returns component wrapper
+		const wrapper = ui.component.build_wrapper_search(self, {
+			content_data : content_data
+		})
+		// set pointers
+		wrapper.content_data = content_data
+
+
+	return wrapper
+}//end search
