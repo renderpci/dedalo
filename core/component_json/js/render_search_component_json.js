000ff68a70e40b19564781cbbcbaa4fd9633f831
Alejandro Peña Carbonell <alex@render.es>
2022-08-21 17:59:00 +0200

    Remove old events delegate code in component_json

--- core/component_json/js/render_search_component_json.js
+++ core/component_json/js/render_search_component_json.js
@@ -44,65 +44,6 @@ render_search_component_json.prototype.search = async function() {
 }//end search
 
 
-
-/**
-* ADD_EVENTS
-*/
-	// const add_events = function(self, wrapper) {
-
-	// 	// change event, for every change the value in the inputs of the component
-	// 	wrapper.addEventListener('change', (e) => {
-
-	// 		// input_value. The standard input for the value of the component
-	// 			// if (e.target.matches('input[type="text"].input_value')) {
-
-	// 			// 	// input. Get the input node that has changed
-	// 			// 		const input = e.target
-
-	// 			// 	// parsed_value
-	// 			// 		const parsed_value = (input.value.length>0) ? input.value : null
-
-	// 			// 	// changed_data
-	// 			// 		const changed_data = Object.freeze({
-	// 			// 			action	: 'update',
-	// 			// 			key		: JSON.parse(input.dataset.key),
-	// 			// 			value	: parsed_value
-	// 			// 		})
-
-	// 			// 	// update the instance data (previous to save)
-	// 			// 		self.update_data_value(changed_data)
-	// 			// 	// set data.changed_data. The change_data to the instance
-	// 			// 		self.data.changed_data = changed_data
-	// 			// 	// publish search. Event to update the dom elements of the instance
-	// 			// 		event_manager.publish('change_search_element', self)
-
-	// 			// 	return true
-	// 			// }
-
-	// 		// q_operator. get the input value of the q_operator
-	// 			// // q_operator: is a separate operator used with components that is impossible mark the operator in the input_value,
-	// 			// // like; radio_button, check_box, date, autocomplete, etc
-	// 			// // (!) Not used in input text
-	// 			// if (e.target.matches('input[type="text"].q_operator')) {
-
-	// 			// 	// input. Get the input node that has changed
-	// 			// 		const input = e.target
-	// 			// 	// value
-	// 			// 		const value = (input.value.length>0) ? input.value : null
-	// 			// 	// q_operator. Fix the data in the instance previous to save
-	// 			// 		self.data.q_operator = value
-	// 			// 	// publish search. Event to update the dom elements of the instance
-	// 			// 		event_manager.publish('change_search_element', self)
-
-	// 			// 	return true
-	// 			// }
-	// 	})
-
-	// 	return true
-	// };//end add_events
-
-
-
 /**
 * GET_CONTENT_DATA
 * @return DOM node content_data
