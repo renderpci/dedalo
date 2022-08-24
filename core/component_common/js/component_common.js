d0b32ac1dba180382218ee6a4a54cb4b255aa77d
Alejandro Peña Carbonell <alex@render.es>
2022-08-24 17:23:37 +0200

    Add ayer_data_change control for tags in geolocation

--- core/component_common/js/component_common.js
+++ core/component_common/js/component_common.js
@@ -446,7 +446,6 @@ component_common.prototype.save = async function(changed_data) {
 		if (changed_data.action==='update') {
 			const original_value	= self.db_data.value[changed_data.key]
 			const new_value			= changed_data.value
-			// console.log("original_value:", original_value, new_value, new_value==original_value);
 			if (new_value==original_value) {
 
 				// dispatch event save
