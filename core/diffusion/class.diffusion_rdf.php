9aa59d5c4844ca29f2818cb3f7ad7e52320dfa4a
Alejandro Peña Carbonell <alex@render.es>
2022-09-08 18:49:30 +0200

    Change divisor for value_separator in diffusion_rdf

--- core/diffusion/class.diffusion_rdf.php
+++ core/diffusion/class.diffusion_rdf.php
@@ -505,7 +505,7 @@ class diffusion_rdf extends diffusion {
 																				DEDALO_DATA_LANG,
 																				$section_tipo,
 																				false);
-							# get_valor($lang=DEDALO_DATA_LANG, $format='string', $ar_related_terms=false, $divisor="<br> ")
+							# get_valor($lang=DEDALO_DATA_LANG, $format='string', $ar_related_terms=false, $value_separator="<br> ")
 							$dato = $component_obj->get_valor(DEDALO_DATA_LANG, $format='string', $separator_fields=', ',$separator_rows='<br>', $ar_related_terms=(array)$object_uri->value);	//  Ref. $lang=DEDALO_DATA_LANG, $format='string', $ar_related_terms=false
 							$value = $dato;
 							break;
