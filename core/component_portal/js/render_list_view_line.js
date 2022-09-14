8bfd67e8068f5c48e28675ac4cdd08e7adb30614
Alejandro Peña Carbonell <alex@render.es>
2022-09-14 13:28:03 +0200

    Add new render_list_view_line to component_portal

---
+++ core/component_portal/js/render_list_view_line.js
@@ -0,0 +1,122 @@
+/*global get_label, DEDALO_CORE_URL, Promise */
+/*eslint no-undef: "error"*/
+
+
+
+// imports
+	import {ui} from '../../common/js/ui.js'
+	import {event_manager} from '../../common/js/event_manager.js'
+	import {
+		activate_autocomplete,
+		render_references
+	} from './render_edit_component_portal.js'
+
+
+
+/**
+* RENDER_LIST_VIEW_LINE
+* Manage the components logic and appearance in client side
+*/
+export const render_list_view_line = function() {
+
+	return true
+}//end render_list_view_line
+
+
+
+
+/**
+* RENDER
+* Manages the component's logic and appearance in client side
+* @param component_portal instance self
+* @param object options
+* @return promise
+* 	DOM node wrapper
+*/
+render_list_view_line.render = async function(self, options) {
+
+	// options
+		const render_level = options.render_level || 'full'
+
+	// view
+		const children_view	= self.context.children_view || self.context.view || 'default'
+
+	// ar_section_record
+		const ar_section_record	= await self.get_ar_instances({
+			view	: children_view
+		})
+		// store to allow destroy later
+		self.ar_instances.push(...ar_section_record)
+
+	// content_data
+		const content_data = await get_content_data(self, ar_section_record)
+		if (render_level==='content') {
+			return content_data
+		}
+
+	// wrapper. ui build_edit returns component wrapper
+		const wrapper = ui.component.build_wrapper_edit(self, {
+			content_data	: content_data
+		})
+		wrapper.classList.add('portal', 'list_view_line')
+		// set pointers
+		wrapper.content_data = content_data
+
+	// autocomplete
+		wrapper.addEventListener('click', function() {
+			activate_autocomplete(self, wrapper)
+		})
+
+
+	return wrapper
+}//end render
+
+
+
+/**
+* GET_CONTENT_DATA
+* Render all received section records and place it into a new div 'content_data'
+* @return DOM node content_data
+*/
+const get_content_data = async function(self, ar_section_record) {
+
+	// build_values
+		const fragment = new DocumentFragment()
+
+		// add all section_record rendered nodes
+			const ar_section_record_length	= ar_section_record.length
+			if (ar_section_record_length===0) {
+
+				// no records found case
+				// const row_item = no_records_node()
+				// fragment.appendChild(row_item)
+			}else{
+
+				const ar_promises = []
+				for (let i = 0; i < ar_section_record_length; i++) {
+					const render_promise = ar_section_record[i].render()
+					ar_promises.push(render_promise)
+				}
+				await Promise.all(ar_promises).then(function(values) {
+				  for (let i = 0; i < ar_section_record_length; i++) {
+
+					const section_record = values[i]
+					fragment.appendChild(section_record)
+				  }
+				});
+			}//end if (ar_section_record_length===0)
+
+		// build references
+			if(self.data.references && self.data.references.length > 0){
+				const references_node = render_references(self.data.references)
+				fragment.appendChild(references_node)
+			}
+
+	// content_data
+		const content_data = ui.component.build_content_data(self)
+			  content_data.appendChild(fragment)
+
+
+	return content_data
+}//end get_content_data
+
