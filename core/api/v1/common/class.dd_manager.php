b2f0ba4dc7aee3d47f389ca36da686ed03df62bd
Alejandro Peña Carbonell <alex@render.es>
2022-10-03 12:34:21 +0200

    Add install and get_login_context exception to be logged in API manager

--- core/api/v1/common/class.dd_manager.php
+++ core/api/v1/common/class.dd_manager.php
@@ -43,9 +43,11 @@ final class dd_manager {
 
 		// logged check
 			$no_loggin_needed_actions = [
+				'install',
 				'start',
 				'change_lang',
-				'login'
+				'login',
+				'get_login_context',
 			];
 			if (in_array($rqo->action, $no_loggin_needed_actions)) {
 				// do not check login here
