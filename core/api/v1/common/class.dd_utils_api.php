de350fb191cc6a2064ee18cb519b162305cee45e
Alejandro Peña Carbonell <alex@render.es>
2022-10-03 12:30:29 +0200

    Add get_login_context to API utils to be called before login

--- core/api/v1/common/class.dd_utils_api.php
+++ core/api/v1/common/class.dd_utils_api.php
@@ -46,37 +46,44 @@ final class dd_utils_api {
 
 
 	/**
-	* GET_LOGIN (!) No longer used. Login now only need context, no data to render
+	* GET_LOGIN_CONTEXT
+	* This function is not used in normal login behavior (login is called directly in start API),
+	* it could be caller when the instance of the login has been build with autoload in true.
+	* This function could be caller by external processes as install to get the context of the login to create the login instance
+	* Login only need context, it not need data to be render.
+	* @param object $request_options
 	* @return object $response
 	*/
-		// public static function get_login($request_options=null) {
-		// 	$start_time = start_time();
+	public static function get_login_context(object $request_options) : object {
 
-		// 	$response = new stdClass();
-		// 		$response->result 	= false;
-		// 		$response->msg 		= 'Error. Request failed ['.__FUNCTION__.']';
+		$response = new stdClass();
+			$response->result 	= false;
+			$response->msg 		= 'Error. Request failed ['.__FUNCTION__.']';
 
-		// 	$login = new login();
+		$login = new login();
 
-		// 	// login json
-		// 		$get_json_options = new stdClass();
-		// 			$get_json_options->get_context	= true;
-		// 			$get_json_options->get_data		= true;
-		// 		$login_json = $login->get_json($get_json_options);
+		// login json
+			$get_json_options = new stdClass();
+				$get_json_options->get_context	= true;
+				$get_json_options->get_data		= false;
+			$login_json = $login->get_json($get_json_options);
 
-		// 	$response->msg		= 'Ok. Request done';
-		// 	$response->result	= $login_json;
+		// context add
+			$context = $login_json->context;
 
-		// 	// Debug
-		// 		if(SHOW_DEBUG===true) {
-		// 			$debug = new stdClass();
-		// 				$debug->exec_time		= exec_time_unit($start_time,'ms').' ms';
-		// 				$debug->request_options	= $request_options;
-		// 			$response->debug = $debug;
-		// 		}
+		// response
+			$response->result	= $context;
+			$response->msg		= 'Ok. Request done';
 
-		// 	return $response;
-		// }//end get_login
+		// Debug
+			if(SHOW_DEBUG===true) {
+				$debug = new stdClass();
+					$debug->request_options	= $request_options;
+				$response->debug = $debug;
+			}
+
+		return $response;
+	}//end get_login_context
 
 
 
