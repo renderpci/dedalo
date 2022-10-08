fda08970288cb4993770beb9d346fb5ec6c6c229
Paco <paco@render.es>
2022-10-08 19:58:25 +0200

    Added uni_test login control

--- core/unit_test/js/exec.js
+++ core/unit_test/js/exec.js
@@ -1,18 +1,33 @@
 /*global page_globals, mocha */
 /*eslint no-undef: "error"*/
-
+// import {get_instance} from '../../common/js/instances.js'
 
 
 // exec mocha
 	if (page_globals.is_logged!==true) {
 
 		// user is not logged
-		const container = document.getElementById('mocha')
-		if (container) {
-			container.innerHTML = `Please, login`
-		}
+
+		import ('../../common/js/instances.js')
+		.then(async function(instances){
+
+			// login instance add
+				instances.get_instance({
+					model	: 'login',
+					tipo	: 'dd229',
+					mode	: 'edit',
+					lang	: page_globals.dedalo_application_lang
+				})
+				.then(instance => instance.build(true))
+				.then(instance => instance.render())
+				.then(wrapper => {
+					document.body.appendChild(wrapper)
+				})
+		})
 
 	}else{
+
+		// let's go run
 
 		mocha.checkLeaks(false)
 		mocha.setup({globals: [
