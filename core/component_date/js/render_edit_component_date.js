80deda98b79c16ae2623b85909bc3c65659c91ea
Alejandro Peña Carbonell <alex@render.es>
2022-10-15 20:58:15 +0200

    Add view_line_edit to component_date

--- core/component_date/js/render_edit_component_date.js
+++ core/component_date/js/render_edit_component_date.js
@@ -7,6 +7,9 @@
 	import {view_default_edit_date} from './view_default_edit_date.js'
 	import {view_text_date} from './view_text_date.js'
 	import {view_mini_date} from './view_mini_date.js'
+	import {view_line_edit_date} from './view_line_edit_date.js'
+	import {ui} from '../../common/js/ui.js'
+	// import {event_manager} from '../../common/js/event_manager.js'
 
 /**
 * RENDER_EDIT_COMPONENT_DATE
@@ -39,6 +42,9 @@ render_edit_component_date.prototype.edit = async function(options) {
 
 		case 'text':
 			return view_text_date.render(self, options)
+
+		case 'line':
+			return view_line_edit_date.render(self, options)
 
 		case 'default':
 		default:
@@ -131,3 +137,414 @@ export const get_ar_raw_data_value = (self) => {
 
 	return ar_raw_value
 }//end get_input_element_edit
+
+
+
+/**
+* INPUT_ELEMENT_DATE
+*/
+export const input_element_date = (i, current_value, self) => {
+
+	const input_value = (current_value && current_value.start)
+		? self.date_to_string(current_value.start)
+		: ''
+
+	const node = get_input_date_node(i, 'start', input_value, self)
+
+	return node
+}//end input_element_date
+
+
+
+/**
+* INPUT_ELEMENT_RANGE
+* @return DOM DocumentFragment
+*/
+export const input_element_range = (i, current_value, self) => {
+
+	const fragment = new DocumentFragment()
+
+	// const date_mode = self.get_date_mode()
+
+	const input_value_start	= (current_value && current_value.start)
+		? self.date_to_string(current_value.start)
+		: ''
+	const input_value_end	= (current_value && current_value.end)
+		? self.date_to_string(current_value.end)
+		: ''
+
+	// start node
+		const node_start = get_input_date_node(i, 'start', input_value_start, self)
+		fragment.appendChild(node_start)
+
+	// dates_separator node
+		ui.create_dom_element({
+			element_type	: 'span',
+			class_name		: 'dates_separator',
+			text_content	: '<>',
+			parent			: fragment
+		})
+
+	// end_node
+		const node_end = get_input_date_node(i, 'end', input_value_end, self)
+		fragment.appendChild(node_end)
+
+
+	return fragment
+}//end input_element_range
+
+
+
+/**
+* INPUT_ELEMENT_PERIOD
+*/
+export const input_element_period = (i, current_value, self) => {
+
+	const period = (current_value && current_value.period) ? current_value.period : null
+
+	const year	= (period) ? period.year : ''
+	const month	= (period) ? period.month : ''
+	const day	= (period) ? period.day : ''
+
+	const label_year	= (year!=='' && year>1) 	? get_label.years : get_label.year
+	const label_month	= (month!=='' && month>1) 	? get_label.months : get_label.month
+	const label_day		= (day!=='' && day>1) 		? get_label.days : get_label.day
+
+	// create div end
+		const input_wrap = ui.create_dom_element({
+			element_type	: 'div',
+			class_name		: 'input-group',
+		})
+
+		// year
+			const input_year = ui.create_dom_element({
+				element_type	: 'input',
+				type			: 'text',
+				class_name		: 'input_period',
+				value			: year,
+				placeholder		: 'Y',
+				parent			: input_wrap
+			})
+			input_year.addEventListener('focus', function() {
+				// force activate on input focus (tabulating case)
+				if (!self.active) {
+					ui.component.activate(self)
+				}
+			})
+			input_year.addEventListener('keyup', function(e) {
+				if (e.key==='Enter') {
+					this.dispatchEvent(new Event('change'));
+				}
+			})
+			input_year.addEventListener('change', function(){
+				collect_data(input_year, input_month, input_day)
+			})
+			// year label
+			ui.create_dom_element({
+				element_type	: 'label',
+				inner_html		: label_year,
+				parent			: input_wrap
+			})
+
+		// month
+			const input_month = ui.create_dom_element({
+				element_type	: 'input',
+				type			: 'text',
+				class_name		: 'input_period',
+				value			: month,
+				placeholder		: 'M',
+				parent			: input_wrap
+			})
+			input_month.addEventListener('focus', function() {
+				// force activate on input focus (tabulating case)
+				if (!self.active) {
+					ui.component.activate(self)
+				}
+			})
+			input_month.addEventListener('keyup', function(e) {
+				if (e.key==='Enter') {
+					this.dispatchEvent(new Event('change'));
+				}
+			})
+			input_month.addEventListener('change', function(){
+				collect_data(input_year, input_month, input_day)
+			})
+			// month label
+			ui.create_dom_element({
+				element_type	: 'label',
+				inner_html		: label_month,
+				parent			: input_wrap
+			})
+
+		// day
+			const input_day = ui.create_dom_element({
+				element_type	: 'input',
+				type			: 'text',
+				class_name		: 'input_period',
+				value			: day,
+				placeholder		: 'D',
+				parent			: input_wrap
+			})
+			input_day.addEventListener('focus', function() {
+				// force activate on input focus (tabulating case)
+				if (!self.active) {
+					ui.component.activate(self)
+				}
+			})
+			input_day.addEventListener('keyup', function(e) {
+				if (e.key==='Enter') {
+					this.dispatchEvent(new Event('change'));
+				}
+			})
+			input_day.addEventListener('change', function(){
+				collect_data(input_year, input_month, input_day)
+			})
+			// day label
+			ui.create_dom_element({
+				element_type	: 'label',
+				inner_html		: label_day,
+				parent			: input_wrap
+			})
+
+
+	// collect_data function. Mix all fields data and saves
+		const collect_data = function(input_year, input_month, input_day) {
+
+			const new_year = (input_year.value)
+				? input_year.value
+				: null
+
+			const new_month = (input_month.value)
+				? input_month.value
+				: null
+
+			const new_day = (input_day.value)
+				? input_day.value
+				: null
+
+			const value = {
+				period: {}
+			}
+
+			if(new_year){
+				value.period.year = new_year
+			}
+			if(new_month){
+				value.period.month = new_month
+			}
+			if(new_day){
+				value.period.day = new_day
+			}
+
+			const changed_data = [Object.freeze({
+				action	: 'update',
+				key		: i,
+				value	: value
+			})]
+			self.change_value({
+				changed_data	: changed_data,
+				refresh			: false
+			})
+		}
+
+
+	return input_wrap
+}//end input_element_period
+
+
+
+/**
+* INPUT_ELEMENT_TIME
+* @return DOM node input_wrap
+*/
+export const input_element_time = (i, current_value, self) => {
+
+	// const date_mode = self.get_date_mode()
+
+	const input_value = (current_value)
+		? self.time_to_string(current_value.start)
+		: ''
+
+	// create div end
+	const input_wrap = ui.create_dom_element({
+		element_type	: 'div',
+		class_name		: 'flatpickr input-group',
+	})
+
+	const input = ui.create_dom_element({
+		element_type	: 'input',
+		type			: 'text',
+		class_name		: 'input_time',
+		value			: input_value,
+		placeholder		: self.get_placeholder_value(),
+		parent			: input_wrap
+	})
+	input.addEventListener('focus', function() {
+		// force activate on input focus (tabulating case)
+		if (!self.active) {
+			ui.component.activate(self)
+		}
+	})
+	input.addEventListener('keyup', function(e) {
+		if (e.key==='Enter') {
+			this.dispatchEvent(new Event('change'));
+		}
+	})
+	input.addEventListener('change', function(){
+		const response = self.parse_string_time(input.value)
+		if(response.error){
+			alert(response.error[0].msg)
+			ui.component.error(true, input_wrap)
+			return false
+		}
+		ui.component.error(false, input_wrap)
+
+		const value = {start:response.result}
+
+		const changed_data = [Object.freeze({
+			action	: 'update',
+			key		: i,
+			value	: value
+		})]
+		self.change_value({
+			changed_data	: changed_data,
+			refresh			: false
+		})
+	})
+
+	// button_calendar
+		const button_calendar = ui.create_dom_element({
+			element_type	: 'a',
+			class_name		: 'input-group-addon button calendar hidden_button ',
+			parent			: input_wrap
+		})
+		button_calendar.addEventListener('mouseup', function() {
+
+			const default_time		= input.value
+			const ar_time_format	= ['H','i','S']
+			const time_format		= ar_time_format.join(self.time_separator)
+
+			const datePicker = flatpickr(button_calendar, {
+				enableTime		: true,
+				noCalendar		: true,
+				time_24hr		: true,
+				enableSeconds	: true,
+				dateFormat		: time_format,
+				defaultDate		: default_time,
+				// onClose		: close_flatpickr,
+				// onValueUpdate
+				onClose			: function(selectedDates, dateStr, instance){
+					ui.component.error(false, input_wrap)
+					input.value = dateStr
+					input.dispatchEvent(new Event('change'))
+					// self.update_value_flatpickr(selectedDates, dateStr, instance, self, e.target)
+				}
+			})
+			datePicker.open()
+		})
+
+
+	return input_wrap
+}//end input_element_time
+
+
+
+/**
+* GET_INPUT_DATE_NODE
+* @return DOM node input_wrap
+*/
+export const get_input_date_node = (i, mode, input_value, self) => {
+
+	// create div end
+		const input_wrap = ui.create_dom_element({
+			element_type	: 'div',
+			class_name		: 'input-group'
+		})
+
+	// input field
+		const input = ui.create_dom_element({
+			element_type	: 'input',
+			type			: 'text',
+			class_name		: 'input_date',
+			value			: input_value,
+			placeholder		: self.get_placeholder_value(),
+			parent			: input_wrap
+		})
+		input.addEventListener('focus', function() {
+			// force activate on input focus (tabulating case)
+			if (!self.active) {
+				ui.component.activate(self)
+			}
+		})
+		input.addEventListener('keyup', function(e) {
+			if (e.key==='Enter') {
+				this.dispatchEvent(new Event('change'));
+			}
+		})
+		input.addEventListener('change', function() {
+
+			const response = self.parse_string_date(input.value)
+			if(response.error){
+				alert(response.error[0].msg)
+				ui.component.error(true, input_wrap)
+				return false
+			}
+			ui.component.error(false, input_wrap)
+
+			const value = self.data.value[i]
+				? JSON.parse(JSON.stringify(self.data.value[i]))
+				: {mode}
+
+			const new_value = (response.result.year)
+				? response.result
+				: ''
+
+			value[mode] = new_value
+
+			const changed_data = [Object.freeze({
+				action	: 'update',
+				key		: i,
+				value	: value
+			})]
+			self.change_value({
+				changed_data	: changed_data,
+				refresh			: false
+			})
+		})
+
+	// button_calendar
+		const button_calendar = ui.create_dom_element({
+			element_type	: 'a',
+			class_name		: 'input-group-addon button calendar hidden_button ',
+			parent			: input_wrap
+		})
+		button_calendar.addEventListener('mouseup', function() {
+			const dd_date_format = page_globals.DEDALO_DATE_ORDER  || 'dmy'
+
+			const ar_date_format = (dd_date_format === 'dmy')
+				? ['d','m','Y']
+				: (dd_date_format === 'ymd')
+					? ['Y','m','d']
+					: (dd_date_format === 'mdy')
+						? ['m','d','Y']
+						: ''
+			const date_format = ar_date_format.join(self.date_separator)
+			const default_date = input.value
+
+			const datePicker = flatpickr(button_calendar, {
+				dateFormat	: date_format,
+				defaultDate	: default_date,
+				allowInput	: true,
+				// onClose 	  : close_flatpickr,
+				onValueUpdate : function(selectedDates, dateStr, instance){
+					ui.component.error(false, input_wrap)
+					input.value = dateStr
+					input.dispatchEvent(new Event('change'))
+				}
+			})
+			datePicker.open()
+		})
+
+
+	return input_wrap
+}//end get_input_date_node
