36448a92ff310d2c6b1551313c6e77ade44f6c0b
Alejandro Peña Carbonell <alex@render.es>
2022-10-18 21:51:46 +0200

    Preserve context when the instance has previous context

--- core/area/js/area.js
+++ core/area/js/area.js
@@ -91,7 +91,23 @@ area.prototype.build = async function(autoload=true) {
 				self.datum	= api_response.result
 
 			// set context and data to current instance
-				self.context	= self.datum.context.find(el => el.tipo===self.tipo)
+				// context is only set when it's empty the origin context,
+				// if the instance has previous context, it will need to preserve.
+				// because the context could be modified by ddo configuration and it can no be changed
+				// ddo_map -----> context
+				// ex: oh27 define the specific ddo_map for rsc368
+				// 		{ mode: list, view: line, children_view: text ... }
+				// if you call to API to get the context of the rsc368 the context will be the default config
+				// 		{ mode: edit, view: default }
+				// but it's necessary preserve the specific ddo_map configuration in the new context.
+				// Context is set and changed in section_record.js to get the ddo_map configuration
+				if(!self.context){
+					const context	= self.datum.context.find(el => el.tipo===self.tipo)
+					if (!context) {
+						console.error("context not found in api_response:", api_response);
+					}
+					self.context = context
+				}
 				self.data		= self.datum.data.find(el => el.tipo===el.section_tipo)
 				self.widgets	= self.datum.context.filter(el => el.parent===self.tipo && el.typo==='widget')
 
