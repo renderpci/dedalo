148a54f91245739396fb23fee9b6ca017491c0af
Paco <paco@render.es>
2022-10-20 14:45:45 +0200

    Added default DEDALO_INSTALL_STATUS value as false in sample.config_core.php file

--- config/sample.config_core.php
+++ config/sample.config_core.php
@@ -1,2 +1,4 @@
 <?php
-// Important!: this file is setup by the core system, do not change it manually
+// Important!: this file is setup by the core system.
+// Do not change it manually unless you know what you're doing
+define('DEDALO_INSTALL_STATUS', false);
