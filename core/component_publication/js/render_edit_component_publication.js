b6a10d7e5d7391e7de1e06d08dfa040f041e46ac
Paco <paco@render.es>
2022-10-20 14:48:59 +0200

    Added view_line_edit render files to component_publication

--- core/component_publication/js/render_edit_component_publication.js
+++ core/component_publication/js/render_edit_component_publication.js
@@ -4,8 +4,10 @@
 
 
 // imports
-	// import {event_manager} from '../../common/js/event_manager.js'
+	import {ui} from '../../common/js/ui.js'
+	import {event_manager} from '../../common/js/event_manager.js'
 	import {view_default_edit_publication} from './view_default_edit_publication.js'
+	import {view_line_edit_publication} from './view_line_edit_publication.js'
 
 
 
@@ -34,6 +36,9 @@ render_edit_component_publication.prototype.edit = async function(options) {
 
 	switch(view) {
 
+		case 'line':
+			return view_line_edit_publication.render(self, options)
+			break;
 		case 'default':
 		default:
 			return view_default_edit_publication.render(self, options)
@@ -41,3 +46,133 @@ render_edit_component_publication.prototype.edit = async function(options) {
 
 	return null
 }//end edit
+
+
+
+/**
+* GET_CONTENT_DATA
+* @return DOM node content_data
+*/
+export const get_content_data = function(self) {
+
+	// short vars
+		const value = self.data.value || []
+
+	// content_data
+		const content_data = ui.component.build_content_data(self, {
+			button_close : null // set to null to prevent it from being created
+		})
+		content_data.classList.add('nowrap')
+
+	// build values
+		const inputs_value	= (value.length<1) ? [''] : value
+		const value_length	= inputs_value.length
+		for (let i = 0; i < value_length; i++) {
+			const content_value = get_content_value(i, inputs_value[i], self)
+			content_data.appendChild(content_value)
+			// set the pointer
+			content_data[i] = content_value
+		}
+
+
+	return content_data
+}//end get_content_data
+
+
+
+/**
+* GET_CONTENT_VALUE
+* Render the current value DOM nodes
+* @param int i
+* 	Value key
+* @param object current_value
+* 	Current locator value as:
+* 	{type: 'dd151', section_id: '1', section_tipo: 'dd64', from_component_tipo: 'rsc20'}
+* @param object self
+*
+* @return DOM element content_value
+*/
+const get_content_value = (i, current_value, self) => {
+
+	// content_value
+		const content_value = ui.create_dom_element({
+			element_type	: 'div',
+			class_name		: 'content_value'
+		})
+
+	// div_switcher
+		const div_switcher = ui.create_dom_element({
+			element_type	: 'label',
+			class_name		: 'switcher_publication text_unselectable',
+			parent			: content_value
+		})
+
+	// input checkbox
+		const input = ui.create_dom_element({
+			element_type	: 'input',
+			type			: 'checkbox',
+			value			: JSON.stringify(current_value),
+			parent			: div_switcher
+		})
+		input.addEventListener('change', function() {
+
+			const checked		= input.checked
+			const changed_value	= (checked===true)
+				? self.data.datalist.filter(item => item.section_id==1)[0].value
+				: self.data.datalist.filter(item => item.section_id==2)[0].value
+
+			const changed_data = [Object.freeze({
+				action	: 'update',
+				key		: i,
+				value	: changed_value
+			})]
+			self.change_value({
+				changed_data	: changed_data,
+				refresh			: false
+			})
+			.then(()=>{
+			// publish the publication locator value. (ex: used to change state of notes tag)
+				event_manager.publish('change_publication_value_'+self.id_base, changed_value)
+			})
+		})
+		// set checked from current value
+		if (current_value.section_id==1) {
+			input.setAttribute('checked', true)
+		}
+
+	// switch_label
+		ui.create_dom_element({
+			element_type	: 'i',
+			parent			: div_switcher
+		})
+
+
+	return content_value
+}//end get_content_value
+
+
+
+/**
+* GET_BUTTONS
+* @param object instance
+* @return DOM node buttons_container
+*/
+export const get_buttons = (self) => {
+
+	const is_inside_tool	= self.is_inside_tool
+	const mode				= self.mode
+
+	const fragment = new DocumentFragment()
+
+	// buttons tools
+		if (!is_inside_tool && mode==='edit') {
+			ui.add_tools(self, fragment)
+		}
+
+	// buttons container
+		const buttons_container = ui.component.build_buttons_container(self)
+		buttons_container.appendChild(fragment)
+
+
+	return buttons_container
+}//end get_buttons
