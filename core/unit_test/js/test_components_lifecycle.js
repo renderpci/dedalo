4e60630745d28a88822d7824c864bd1e1b033b78
Paco <paco@render.es>
2022-10-20 11:31:23 +0200

    Updated unit_test lifecycle assertions

--- core/unit_test/js/test_components_lifecycle.js
+++ core/unit_test/js/test_components_lifecycle.js
@@ -91,46 +91,77 @@ async function life_cycle_test(element) {
 			const expected = 'initiated'
 			assert.equal(new_instance.status, expected);
 			assert.equal(new_instance.mode, element.mode);
+			assert.equal(new_instance.context, null);
+			assert.equal(new_instance.node, null);
+			assert.equal(new_instance.active, false);
+			assert.equal(new_instance.is_data_changed, false);
+
+			assert.notEqual(new_instance.model, null);
+			assert.notEqual(new_instance.tipo, null);
+			assert.notEqual(new_instance.section_tipo, null);
+			assert.notEqual(new_instance.mode, null);
+			assert.notEqual(new_instance.lang, null);
+			assert.notEqual(new_instance.standalone, null);
 		});
 
 		it(`${element.model} BUILD (autoload=true) ${element.mode}`, async function() {
 
-			const expected = 'built'
-
 			// init instance
 				await new_instance.build(true)
 
-			assert.equal(new_instance.status, expected);
+			assert.equal(new_instance.status, 'built');
 			// assert.equal(new_instance.mode, element_mode);
+			assert.notEqual(new_instance.context, null);
+
+			assert.notEqual(new_instance.type, null);
+			assert.notEqual(new_instance.label, null);
+			assert.notEqual(new_instance.tools, null);
+			assert.notEqual(new_instance.permissions, null);
+			assert.notEqual(new_instance.view, null);
+			assert.notEqual(new_instance.rqo_test, null);
+			assert.notEqual(new_instance.data, null);
+			assert.notEqual(new_instance.db_data, null);
 		});
 
 		it(`${element.model} RENDER ${element.mode}`, async function() {
 
-			const expected = 'rendered'
-
-			// init instance
+			// render instance
 				const new_node = await new_instance.render()
+				// console.log('new_node:', new_node);
 
 			// insert in DOM
 				// if (new_instance.mode==='list') {
 				// 	content.prepend(new_node)
 				// }
 
-			assert.equal(new_instance.status, expected);
+			assert.equal(new_instance.status, 'rendered');
+			assert.notEqual(new_instance.node, null);
+
+			if (new_instance.mode==='edit') {
+				assert.notEqual(new_instance.node.content_data, null);
+				assert.notEqual(new_instance.node.content_data, undefined);
+				assert.notEqual(new_instance.node.querySelector('.label'), null);
+				assert.notEqual(new_instance.node.querySelector('.buttons_container'), null);
+				assert.notEqual(new_instance.node.querySelector('.content_data'), null);
+			}
+			else if(new_instance.mode==='list') {
+				assert.Equal(new_instance.node.content_data, undefined);
+				assert.Equal(new_instance.node.querySelector('.label'), null);
+				assert.Equal(new_instance.node.querySelector('.buttons_container'), null);
+				assert.Equal(new_instance.node.querySelector('.content_data'), null);
+			}
 		});
 
 		it(`${element.model} DESTROY ${element.mode}`, async function() {
 
-			const expected = 'destroyed'
-
-			// init instance
+			// destroy instance
 				await new_instance.destroy(
 					true,  // delete_self . default true
 					true, // delete_dependencies . default false
 					true // remove_dom . default false
 				)
 
-			assert.equal(new_instance.status, expected)
+			assert.equal(new_instance.status, 'destroyed')
 			assert.deepEqual(new_instance.ar_instances, [])
 			assert.deepEqual(new_instance.node, null)
 		});
