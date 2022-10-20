b315d3a093e5c63d0b7101653f36cf2b2b3db524
Paco <paco@render.es>
2022-10-20 14:49:40 +0200

    Added view_line_edit render files to component_select

--- core/component_select/js/view_line_edit_select.js
+++ core/component_select/js/view_line_edit_select.js
@@ -8,20 +8,19 @@
 	// import {object_to_url_vars} from '../../common/js/utils/index.js'
 	import {ui} from '../../common/js/ui.js'
 	import {
-		get_content_data,
-		get_buttons
+		get_content_data
 	} from './render_edit_component_select.js'
 
 
 
 /**
-* view_default_EDIT_SELECT
+* VIEW_LINE_EDIT_SELECT
 * Manages the component's logic and appearance in client side
 */
-export const view_default_edit_select = function() {
+export const view_line_edit_select = function() {
 
 	return true
-}//end view_default_edit_select
+}//end view_line_edit_select
 
 
 
@@ -30,24 +29,25 @@ export const view_default_edit_select = function() {
 * Render node for view
 * @return DOM node
 */
-view_default_edit_select.render = async function(self, options) {
+view_line_edit_select.render = async function(self, options) {
 
 	// options
 		const render_level = options.render_level || 'full'
 
+	// button_exit_edit
+		const button_exit_edit = ui.component.build_button_exit_edit(self)
+
 	// content_data
 		const content_data = get_content_data(self)
+		content_data.appendChild(button_exit_edit)
 		if (render_level==='content') {
 			return content_data
 		}
 
-	// buttons
-		const buttons = get_buttons(self)
-
 	// wrapper. ui build_edit returns component wrapper
 		const wrapper = ui.component.build_wrapper_edit(self, {
 			content_data	: content_data,
-			buttons			: buttons
+			label			: null
 		})
 		// set pointers
 		wrapper.content_data = content_data
