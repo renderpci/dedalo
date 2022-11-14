c0b2640b0549e6511eac4cd5ef0cc68010047a57
Paco <paco@render.es>
2022-11-14 21:31:54 +0100

    Re-enabled ui.flat_column_items component_image default width ('102px')

--- core/common/js/ui.js
+++ core/common/js/ui.js
@@ -2656,7 +2656,7 @@ export const ui = {
 			const width_defaults = {
 				component_publication	: '5rem',
 				component_info			: 'minmax(9rem, 1fr)',
-				// component_image			: '102px',
+				component_image			: '102px',
 				component_av			: '102px',
 				component_svg			: '102px',
 				component_pdf			: '102px'
