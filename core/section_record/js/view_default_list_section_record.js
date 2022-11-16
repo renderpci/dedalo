f364a8150fe86e754f12afcee947c3f27ef4c680
Paco <paco@render.es>
2022-11-16 12:51:27 +0100

    Improved section_record row hilite. Now is added when the wrapper is in DOM and styles are calculated only once and recalculated on window resize

--- core/section_record/js/view_default_list_section_record.js
+++ core/section_record/js/view_default_list_section_record.js
@@ -7,7 +7,7 @@
 	// import {event_manager} from '../../common/js/event_manager.js'
 	// import {clone} from '../../common/js/utils/index.js'
 	// import {data_manager} from '../../common/js/data_manager.js'
-	// import {when_in_dom} from '../../common/js/events.js'
+	import {when_in_dom} from '../../common/js/events.js'
 	import {ui} from '../../common/js/ui.js'
 	// import {open_tool} from '../../../tools/tool_common/js/tool_common.js'
 
@@ -201,54 +201,62 @@ view_default_list_section_record.render = async function(self, options) {
 		// hilite_row. User mouse enter/mouseleave creates an DOM node to hilite current row
 		// Note that only is activated when self.caller is a section to prevent deep portals issues
 			if (self.caller.model==='section' || self.caller.model==='time_machine' || self.caller.model==='service_time_machine') {
-				let hilite_row
+				when_in_dom(wrapper, function(){
 
-				// remove_hilite (if is set)
-					const remove_hilite = (e) => {
-						// e.stopPropagation()
-						if (hilite_row) {
-							hilite_row.remove()
-							hilite_row = null
+					let hilite_row
+
+					// row_style
+						const wrapper_first_column	= wrapper.firstChild
+						const wrapper_last_column	= wrapper.lastChild
+						const firstChild_el_rect	= wrapper_first_column.getBoundingClientRect();
+						const lastChild_el_rect		= wrapper_last_column.getBoundingClientRect();
+						const row_style = {
+							left	: parseFloat(firstChild_el_rect.x) + 'px',
+							top		: parseFloat(firstChild_el_rect.y + window.pageYOffset) + 'px',
+							height	: parseFloat(firstChild_el_rect.height) + 'px',
+							width	: parseFloat(lastChild_el_rect.x + lastChild_el_rect.width - firstChild_el_rect.x) + 'px'
 						}
-					}
-					wrapper.addEventListener('mouseleave', remove_hilite);
-					wrapper.addEventListener('mousedown', remove_hilite);
-
-				// add hilite
-					const fn_mouseenter = (e) => {
-						// remove previous hilite if exist
-							remove_hilite(e)
-
-						// small screen case. Do not add hilite
-							const width = window.innerWidth || document.documentElement.clientWidth || document.body.clientWidth;
-							if (width<960) {
-								return
-							}
-
-						// hilite style. Mimic row size using first and last columns size and position
-							const wrapper_first_column	= wrapper.firstChild
-							const wrapper_last_column	= wrapper.lastChild
+						window.addEventListener('resize', function() {
+							// update row_style values on resize
 							const firstChild_el_rect	= wrapper_first_column.getBoundingClientRect();
 							const lastChild_el_rect		= wrapper_last_column.getBoundingClientRect();
-							// console.log('firstChild_el_rect:',firstChild_el_rect, wrapper.firstChild);
-							// console.log('wrapper_first_column:', wrapper_first_column, 'wrapper_last_column:', wrapper_last_column);
+							row_style.left				= parseFloat(firstChild_el_rect.x) + 'px',
+							row_style.top				= parseFloat(firstChild_el_rect.y + window.pageYOffset) + 'px',
+							row_style.height			= parseFloat(firstChild_el_rect.height) + 'px',
+							row_style.width				= parseFloat(lastChild_el_rect.x + lastChild_el_rect.width - firstChild_el_rect.x) + 'px'
+						})
 
-							const style = {
-								left	: parseFloat(firstChild_el_rect.x) + 'px',
-								top		: parseFloat(firstChild_el_rect.y + window.pageYOffset) + 'px',
-								height	: parseFloat(firstChild_el_rect.height) + 'px',
-								width	: parseFloat(lastChild_el_rect.x + lastChild_el_rect.width - firstChild_el_rect.x) + 'px'
+					// fn_remove_hilite (if is set)
+						const fn_remove_hilite = () => {
+							if (hilite_row) {
+								hilite_row.remove()
+								hilite_row = null
 							}
+						}
+						wrapper.addEventListener('mouseleave', fn_remove_hilite);
+						wrapper.addEventListener('mousedown', fn_remove_hilite);
 
-						// hilite_row. create and append
-							hilite_row = ui.create_dom_element({
-								element_type	: 'div',
-								class_name		: 'hilite_row',
-								style			: style
-							})
-							wrapper.prepend(hilite_row)
-					}
-					wrapper.addEventListener('mouseenter', fn_mouseenter);
+					// fn_hilite. Add hilite
+						const fn_hilite = (e) => {
+							// remove previous hilite if exist
+								fn_remove_hilite(e)
+
+							// small screen case. Do not add hilite
+								const width = window.innerWidth || document.documentElement.clientWidth || document.body.clientWidth;
+								if (width<960) {
+									return
+								}
+
+							// hilite_row. create and append
+								hilite_row = ui.create_dom_element({
+									element_type	: 'div',
+									class_name		: 'hilite_row',
+									style			: row_style
+								})
+								wrapper.prepend(hilite_row)
+						}
+						wrapper.addEventListener('mouseenter', fn_hilite);
+				})
 			}//end if (self.caller.model==='section' || self.caller.model==='time_machine')
 
 	// wrapper css
