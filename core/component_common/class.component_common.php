c08b3963b35d4c7d1d511f8c5ba7f89260bb6988
Alejandro Peña Carbonell <alex@render.es>
2022-11-20 21:58:57 +0100

    Remove old way to construct pagination in component_common

--- core/component_common/class.component_common.php
+++ core/component_common/class.component_common.php
@@ -415,55 +415,58 @@ abstract class component_common extends common {
 
 		// pagination
 			$this->pagination = new stdClass();
-			$request_config = ( isset($properties->source->request_config) )
-				? $properties->source->request_config
-				: [];
-			$found = array_find($request_config, function($el){
-				return isset($el->api_engine) && $el->api_engine==='dedalo';
-			});
-			$rqo = !empty($found)
-				? $found
-				: (isset($request_config[0])
-					? $request_config[0]
-					: null);
+			$this->pagination->offset = 0;
+			$this->pagination->limit = null;
 
-			if (empty($rqo)) {
-				// v5 config
-				// limit
-					// $this->pagination->limit = isset($properties->max_records)
-					// 	? (int)$properties->max_records
-					// 	: 5;
+			// $request_config = ( isset($properties->source->request_config) )
+			// 	? $properties->source->request_config
+			// 	: [];
+			// $found = array_find($request_config, function($el){
+			// 	return isset($el->api_engine) && $el->api_engine==='dedalo';
+			// });
+			// $rqo = !empty($found)
+			// 	? $found
+			// 	: (isset($request_config[0])
+			// 		? $request_config[0]
+			// 		: null);
 
-					$this->pagination->limit = $this->modo==='edit'
-						? 1
-						: 10;
+			// if (empty($rqo)) {
+			// 	// v5 config
+			// 	// limit
+			// 		// $this->pagination->limit = isset($properties->max_records)
+			// 		// 	? (int)$properties->max_records
+			// 		// 	: 5;
 
-				// offset
-					$this->pagination->offset = isset($properties->offset)
-						? (int)$properties->offset
-						: 0;
+			// 		$this->pagination->limit = $this->modo==='edit'
+			// 			? 10
+			// 			: 1;
 
-			}else{
-				// v6 config
-				// limit
-					// $this->pagination->limit = (isset($rqo->sqo) && isset($rqo->sqo->limit))
-					// 	? (int)$rqo->sqo->limit
-					// 	: ((isset($rqo->show) && isset($rqo->show->sqo_config->limit))
-					// 		// show limit
-					// 		? (int)$rqo->show->sqo_config->limit
-					// 		: 5);
+			// 	// offset
+			// 		$this->pagination->offset = isset($properties->offset)
+			// 			? (int)$properties->offset
+			// 			: 0;
 
-					$this->pagination->limit = $this->modo==='edit'
-						? 1
-						: 10;
+			// }else{
+			// 	// v6 config
+			// 	// limit
+			// 		$this->pagination->limit = (isset($rqo->sqo) && isset($rqo->sqo->limit))
+			// 			? (int)$rqo->sqo->limit
+			// 			: ((isset($rqo->show) && isset($rqo->show->sqo_config->limit))
+			// 				// show limit
+			// 				? (int)$rqo->show->sqo_config->limit
+			// 				: 1);
 
-				// offset
-					$this->pagination->offset = (isset($rqo->sqo) && isset($rqo->sqo->offset))
-						? (int)$rqo->sqo->offset
-						: ((isset($rqo->show) && isset($rqo->show->sqo_config->offset))
-							? (int)$rqo->show->sqo_config->offset
-							: 0);
-			}
+			// 		if($this->modo==='list'){
+			// 			$this->pagination->limit = 1;
+			// 		}
+
+			// 	// offset
+			// 		$this->pagination->offset = (isset($rqo->sqo) && isset($rqo->sqo->offset))
+			// 			? (int)$rqo->sqo->offset
+			// 			: ((isset($rqo->show) && isset($rqo->show->sqo_config->offset))
+			// 				? (int)$rqo->show->sqo_config->offset
+			// 				: 0);
+			// }
 
 
 		return true;
