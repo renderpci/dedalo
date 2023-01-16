be368a5400d174f9467928f600519fb96f4ade15
Paco <paco@render.es>
2023-01-16 16:42:57 +0100

    Fixed error were update time_machine data generates an error on json decode empty values

--- core/base/upgrade/class.time_machine_v5_to_v6.php
+++ core/base/upgrade/class.time_machine_v5_to_v6.php
@@ -78,15 +78,20 @@ class time_machine_v5_to_v6 {
 
 				while($rows = pg_fetch_assoc($result)) {
 
-					$id				= $rows['id'];
-					// $id_matrix		= $rows['id_matrix'];
-					// $section_id		= $rows['section_id'];
-					// $section_tipo	= $rows['section_tipo'];
-					$tipo			= $rows['tipo'];
-					// $lang			= $rows['lang'];
-					$dato			= json_decode($rows['dato']);
+					// columns
+						$id					= $rows['id'];
+						// $id_matrix		= $rows['id_matrix'];
+						// $section_id		= $rows['section_id'];
+						// $section_tipo	= $rows['section_tipo'];
+						$tipo				= $rows['tipo'];
+						// $lang			= $rows['lang'];
 
+					// skip empty data
+						if (empty($rows['dato'])) {
+							continue;
+						}
 
+					$dato = json_decode($rows['dato']);
 					if (!empty($dato)) {
 
 						$model_name = RecordObj_dd::get_modelo_name_by_tipo($tipo);
@@ -96,6 +101,9 @@ class time_machine_v5_to_v6 {
 
 						switch ($model_name) {
 							case 'component_text_area':
+								if (is_array($dato)) {
+									continue 2;
+								}
 								$format_dato	= '<p>'.$dato.'</p>';
 								$changed_dato	= preg_replace('/(<\/? ?br>)/i', '</p><p>', $format_dato);
 								// fix final dato with new format as array
@@ -103,29 +111,47 @@ class time_machine_v5_to_v6 {
 								break;
 
 							case 'component_number':
+								if (is_array($dato)) {
+									continue 2;
+								}
 								// fix final dato with new format as array
 								$new_dato = [$dato];
 								break;
 
 							case 'component_json':
+								if (!is_string($dato)) {
+									continue 2;
+								}
 								// fix final dato with new format as array
-								$new_dato = json_decode($dato);
+								try {
+									$new_dato = json_decode($dato);
+								} catch (Exception $e) {
+									error_log( 'Caught exception: ' . $e->getMessage() );
+									debug_log(__METHOD__." ERROR: invalid JSON data id: ($id) ".$e->getMessage(), logger::ERROR);
+									continue 2;
+								}
 								break;
 
 							default:
-								// code...
+								debug_log(__METHOD__." model_name not valid ".to_string(), logger::ERROR);
+								continue 2;
 								break;
 						}
 
-						$strQuery 	= "UPDATE $table SET datos = $new_dato WHERE id = $id ";
-						$result 	= pg_query_params(DBi::_getConnection(), $strQuery, array( $section_data_encoded, $id ));
-						if(!$result) {
+						// data_encoded : JSON ENCODE ALWAYS !!!
+							$data_encoded = json_handler::encode($new_dato);
+							// prevent null encoded errors
+							$safe_data = str_replace(['\\u0000','\u0000'], ' ', $data_encoded);
+
+						$strQuery2		= "UPDATE $table SET dato = $1 WHERE id = $2 ";
+						$result2		= pg_query_params(DBi::_getConnection(), $strQuery2, array( $safe_data, $id ));
+						if($result2===false) {
 							$msg = "Failed Update section_data $i";
 							debug_log(__METHOD__." ERROR: $msg ".to_string(), logger::ERROR);
 							continue;
 						}
 					}else{
-						debug_log(__METHOD__." ERROR: Empty datos from: $table - $id ".to_string(), logger::ERROR);
+						debug_log(__METHOD__." Empty datos from: $table - $id ".to_string(), logger::WARNING);
 					}
 				}
 
@@ -133,7 +159,7 @@ class time_machine_v5_to_v6 {
 					if ($i_ref===0) {
 						debug_log(__METHOD__." Partial update of section data table: $table - id: $id - total: $n_rows - total time secs: ".exec_time_unit($start_time,'sec'), logger::DEBUG);
 					}else{
-						$i_ref = ($i_ref>1000) ? 0 : $i_ref + 1;
+						$i_ref = ($i_ref>10000) ? 0 : $i_ref + 1;
 					}
 			}
 			#break; // stop now
