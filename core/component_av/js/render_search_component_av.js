184eb318e480f574322133647969695f4b207c13
Paco <paco@render.es>
2023-01-21 19:19:00 +0100

    Added component_av search render

---
+++ core/component_av/js/render_search_component_av.js
@@ -0,0 +1,124 @@
+/*global */
+/*eslint no-undef: "error"*/
+
+
+
+// imports
+	import {event_manager} from '../../common/js/event_manager.js'
+	import {ui} from '../../common/js/ui.js'
+
+
+
+/**
+* RENDER_SEARCH_COMPONENT_AV
+* Manages the component's logic and appearance in client side
+*/
+export const render_search_component_av = function() {
+
+	return true
+}//end render_search_component_av
+
+
+
+/**
+* search
+* Render node for use in modes: search
+* @return DOM node wrapper
+*/
+render_search_component_av.prototype.search = async function(options) {
+
+	const self = this
+
+	// options
+		// const render_level = options.render_level || 'full'
+
+	// content data
+		const content_data = get_content_data(self)
+
+	// ui build_edit returns component wrapper
+		const wrapper = ui.component.build_wrapper_search(self, {
+			content_data : content_data
+		})
+		// set pointers
+		wrapper.content_data = content_data
+
+
+	return wrapper
+}//end search
+
+
+
+/**
+* GET_CONTENT_DATA
+* @param object self
+* @return DOM node content_data
+*/
+const get_content_data = function(self) {
+
+	// short vars
+		const data	= self.data || {}
+		const value	= data.value || []
+
+	// content_data
+		const content_data = ui.component.build_content_data(self)
+
+	// values (inputs)
+		const inputs_value	= value.length>0 ? value : ['']
+		const value_length	= inputs_value.length
+		for (let i = 0; i < value_length; i++) {
+			const input_element_node = get_content_value(i, inputs_value[i], self)
+			content_data.appendChild(input_element_node)
+			// set the pointer
+			content_data[i] = input_element_node
+		}
+
+
+	return content_data
+}//end get_content_data
+
+
+
+/**
+* GET_CONTENT_VALUE
+* @return DOM node content_value
+*/
+const get_content_value = (i, current_value, self) => {
+
+	// content_value
+		const content_value = ui.create_dom_element({
+			element_type	: 'div',
+			class_name		: 'content_value'
+		})
+
+	// input field
+		const input = ui.create_dom_element({
+			element_type	: 'input',
+			type			: 'text',
+			class_name		: 'input_value',
+			value			: current_value,
+			parent			: content_value
+		})
+		input.addEventListener('change', function() {
+
+			// parsed_value
+				const parsed_value = (input.value.length>0) ? input.value : null
+
+			// changed_data
+				const changed_data_item = Object.freeze({
+					action	: 'update',
+					key		: i,
+					value	: parsed_value
+				})
+
+			// update the instance data (previous to save)
+				self.update_data_value(changed_data_item)
+			// set data.changed_data. The change_data to the instance
+				// self.data.changed_data = changed_data
+			// publish search. Event to update the dom elements of the instance
+				event_manager.publish('change_search_element', self)
+		})//end event change
+
+
+	return content_value
+}//end get_content_value
+
