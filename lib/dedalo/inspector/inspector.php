131b241bfeb4540df1e41219b44b89ccffe8b21c
Alejandro Peña Carbonell <alex@render.es>
2023-01-27 15:30:24 +0100

    Check tm permisions to show or hide tm_list and component_history in inspector

--- lib/dedalo/inspector/inspector.php
+++ lib/dedalo/inspector/inspector.php
@@ -49,11 +49,12 @@
 			$time_machine_tipo = section::get_ar_childrens_by_model($section_tipo, $model_required_tm);
 			$time_machine_name = RecordObj_dd::get_termino_by_tipo($time_machine_tipo, DEDALO_APPLICATION_LANG);
 			if($time_machine_tipo){
-				$time_machine_list 		= new time_machine_list($time_machine_tipo, $section_id, $section_tipo, $modo='button');
-				$time_machine_button_html = $time_machine_list->get_html();
+				$time_machine_list			= new time_machine_list($time_machine_tipo, $section_id, $section_tipo, $modo='button');
+				$time_machine_button_html	= $time_machine_list->get_html();
+				$tm_history_permisions		= common::get_permissions($section_tipo, $time_machine_tipo);
 			}
 			
-
+			
 			//dump($time_machine_list,'$relation_list_button_html');
 
 			//$relation_list_name = RecordObj_dd::get_termino_by_tipo($relation_list,DEDALO_APPLICATION_LANG, true);
