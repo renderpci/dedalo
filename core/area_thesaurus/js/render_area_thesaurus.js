6114bbe3f0339ba36c70cb42208313fccecd7d4a
Alejandro Peña Carbonell <alex@render.es>
2023-01-29 19:04:18 +0100

    Removed old and unused code in area_thesaurus

--- core/area_thesaurus/js/render_area_thesaurus.js
+++ core/area_thesaurus/js/render_area_thesaurus.js
@@ -5,8 +5,6 @@
 
 // imports
 	import {event_manager} from '../../common/js/event_manager.js'
-	// import {data_manager} from '../../common/js/data_manager.js'
-	// import {common} from '../../common/js/common.js'
 	import {ui} from '../../common/js/ui.js'
 
 
@@ -48,24 +46,6 @@ render_area_thesaurus.prototype.list = async function(options) {
 
 			if (data.ts_search) {
 
-				// event_manager.when_in_dom(content_data, function(){
-				// 	ts_object.parse_search_result(data.ts_search.result, null, false)
-				// })
-
-				// const observer = new IntersectionObserver(function(entries) {
-				// 	const entry = entries[1] || entries[0]
-				// 	if (entry.isIntersecting===true || entry.intersectionRatio > 0) {
-				// 		observer.disconnect();
-				// 		ts_object.parse_search_result(data.ts_search.result, null, false)
-				// 	}
-				// }, { threshold: [0] });
-				// observer.observe(content_data);
-
-				// event_manager.subscribe('render_'+self.id, exec_search)
-				// function exec_search() {
-				// 	ts_object.parse_search_result(data.ts_search.result, null, false)
-				// }
-
 				self.ts_object.parse_search_result(data.ts_search.result, null, false)
 				// prevent to recreate content_data again
 				const content_data = self.node.querySelector('.content_data.area')
@@ -94,11 +74,6 @@ render_area_thesaurus.prototype.list = async function(options) {
 				parent			: fragment
 			})
 			self.search_container = search_container
-			// self.filter.build().then(()=>{
-			// 	self.filter.render().then(filter_wrapper =>{
-			// 		search_container.appendChild(filter_wrapper)
-			// 	})
-			// })
 		}
 
 	// content_data
@@ -111,22 +86,6 @@ render_area_thesaurus.prototype.list = async function(options) {
 		})
 		wrapper.prepend(fragment)
 
-	// change the mode of the thesaurus
-	// when the user do click publish the tipo to go and set the mode in list
-	// the action can be executed mainly in page, but it can be used for any instance.
-
-	// swicht_term_model
-		// const swicht_term_model = ui.create_dom_element({
-		// 	element_type	: 'div',
-		// 	class_name		:'swicht_term_model',
-		// 	inner_html		: 'term',
-		// 	parent			: wrapper
-		// })
-		// swicht_term_model.addEventListener("click", e => {
-		// 	self.build_options.terms_are_model = self.build_options.terms_are_model ? false : true
-		// 	self.refresh()
-		// })
-
 	// ts_search case
 		if (data.ts_search) {
 			event_manager.subscribe('render_'+self.filter.id, exec_search)
@@ -135,41 +94,8 @@ render_area_thesaurus.prototype.list = async function(options) {
 			}
 		}
 
-
 	return wrapper
 }//end list
-
-
-
-/**
-* EDIT
-* Render node for use in edit
-* @return DOM node
-*/
-	// render_area_thesaurus.prototype.edit = async function(options={render_level:'full'}) {
-
-	// 	const self = this
-
-	// 	// const render_level = options.render_level
-
-	// 	// // content_data
-	// 	// 	const content_data = await content_data(self)
-	// 	// 	if (render_level==='content') {
-	// 	// 		return content_data
-	// 	// 	}
-
-	// 	// // buttons
-	// 	// 	//const current_buttons = buttons(self);
-
-	// 	// // wrapper. ui build_edit returns component wrapper
-	// 	// 	const wrapper =	ui.area.build_wrapper_edit(self, {
-	// 	// 		content_data : content_data,
-	// 	// 		//buttons 	 : current_buttons
-	// 	// 	})
-
-
-	// 	return wrapper
-	// }//end edit
 
 
 
@@ -180,16 +106,6 @@ render_area_thesaurus.prototype.list = async function(options) {
 const render_content_data = function(self) {
 
 	const fragment = new DocumentFragment()
-
-	// widgets
-		// const widgets_lenght = self.widgets.length
-		// for (let i = 0; i < widgets_lenght; i++) {
-
-		// 	const widget = self.widgets[i]
-
-		// 	const widget_dom = build_widget(widget, self);
-		// 	fragment.appendChild(widget_dom)
-		// }
 
 	// container for list
 		const ul = ui.create_dom_element({
@@ -304,26 +220,6 @@ const render_content_data = function(self) {
 				}
 		}//end for (let i = 0; i < typology_length; i++)
 
-		// const hierarchy_root_nodes   = hierarchy_nodes.map(node => node.section_tipo+'_'+node.section_id+'_root')
-		// const hierarchy_nodes_length = hierarchy_root_nodes.length
-		// for (let i = 0; i < hierarchy_nodes_length; i++) {
-
-		// 	const root_node_id = hierarchy_root_nodes[i]
-
-		// 	// Launch ajax call promise
-		// 		ts_object.get_children(root_node_id)
-		// }
-
-
-	// for (let i = 0; i < typology_length; i++) {
-	// 	const current_typology = typology_nodes[i]
-	// 	const ul = ui.create_dom_element({
-	// 		class_name 	 : "tipology_name",
-	// 		element_type : 'div',
-	// 		parent 		 : li,
-	// 	})
-	// }
-
 	// content_data
 		const content_data = document.createElement("div")
 			  content_data.classList.add("content_data", self.type)
@@ -332,158 +228,6 @@ const render_content_data = function(self) {
 
 	return content_data
 }//end render_content_data
-
-
-
-/**
-* BUILD_WIDGET
-*/
-	// const build_widget = (item, self) => {
-
-	// 	const container = ui.create_dom_element({
-	// 		id				: item.id,
-	// 		element_type	: 'div',
-	// 		dataset			: {},
-	// 		class_name		: "widget_container"
-	// 	})
-
-	// 	// label
-	// 		const label = ui.create_dom_element({
-	// 			element_type	: 'div',
-	// 			class_name		: "widget_label",
-	// 			parent			: container,
-	// 			inner_html		: item.label || ''
-	// 		})
-	// 		label.addEventListener("dblclick", function(e){
-	// 			const body = e.target.nextElementSibling
-	// 			body.classList.contains("display_none") ? body.classList.remove("display_none") : body.classList.add("display_none")
-	// 		})
-
-	// 	// body
-	// 		const body = ui.create_dom_element({
-	// 			element_type	: 'div',
-	// 			class_name		: "widget_body",
-	// 			parent			: container
-	// 		})
-
-	// 		// item info
-	// 		if (item.info) {
-	// 			const widget_info = ui.create_dom_element({
-	// 				element_type	: 'div',
-	// 				class_name		: "link",
-	// 				parent			: body,
-	// 				inner_html		: item.info || ''
-	// 			})
-
-	// 			// action
-	// 				widget_info.addEventListener('mouseup',  async function(e){
-	// 					e.stopPropagation()
-
-	// 					// confirm optional
-	// 						if (item.confirm && !confirm(item.confirm)) {
-	// 							return false
-	// 						}
-
-	// 					widget_info.classList.add("lock")
-	// 					body_response.classList.add("preload")
-
-	// 					// data_manager
-	// 					const api_response = await data_manager.request({
-	// 						body : {
-	// 							dd_api	: item.trigger.dd_api,
-	// 							action	: item.trigger.action,
-	// 							options	: item.trigger.options
-	// 						}
-	// 					})
-	// 					// console.log("api_response:",api_response);
-
-	// 					print_response(body_response, api_response)
-
-	// 					widget_info.classList.remove("lock")
-	// 					body_response.classList.remove("preload")
-	// 				})
-	// 		}//end if (item.info) {
-
-	// 		// body info
-	// 			const body_info = ui.create_dom_element({
-	// 				element_type	: 'div',
-	// 				class_name		: "body_info",
-	// 				parent			: body,
-	// 				inner_html		: item.body || ''
-	// 			})
-	// 		// body response
-	// 			const body_response = ui.create_dom_element({
-	// 				element_type	: 'div',
-	// 				class_name		: "body_response",
-	// 				parent			: body
-	// 			})
-
-	// 	// run widget scripts
-	// 		if(item.run) {
-	// 			for (let i = 0; i < item.run.length; i++) {
-
-	// 				const func			= item.run[i].fn
-	// 				const func_options	= item.run[i].options
-
-	// 				const js_promise = self[func].apply(self, [{
-	// 					...item,
-	// 					...func_options,
-	// 					body_response  : body_response,
-	// 					print_response : print_response
-	// 				}])
-	// 			}
-	// 		}
-
-
-	// 	return container
-	// }//end  build_widget
-
-
-
-/**
-* PRINT_RESPONSE
-*/
-	// const print_response = (container, api_response) => {
-
-	// 	// clean container
-	// 		while (container.firstChild) {
-	// 			container.removeChild(container.firstChild);
-	// 		}
-
-	// 	// clean (eraser)
-	// 		const eraser = ui.create_dom_element({
-	// 			element_type : 'span',
-	// 			class_name 	 : "clean",
-	// 			parent 		 : container
-	// 		})
-	// 		eraser.addEventListener("mouseup", function(e){
-	// 			e.stopPropagation();
-
-	// 			while (container.firstChild) {
-	// 				container.removeChild(container.firstChild);
-	// 			}
-	// 		})
-
-	// 	// msg
-	// 		const msg = ui.create_dom_element({
-	// 			element_type : 'div',
-	// 			class_name 	 : "",
-	// 			parent 		 : container,
-	// 			inner_html 	 : api_response.msg
-	// 		})
-
-	// 	// json response result
-	// 		const result = ui.create_dom_element({
-	// 			element_type : 'pre',
-	// 			class_name 	 : "",
-	// 			parent 		 : container,
-	// 			inner_html 	 : JSON.stringify(api_response, null, " ").replace(/\\n/g, "<br>")
-	// 		})
-
-	// 	container.classList.remove("preload")
-
-	// 	return container
-	// }//end print_response
 
 
 
