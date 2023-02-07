37fab85b3b83ccbc14de9f116052599a922b694a
Alejandro Peña Carbonell <alex@render.es>
2023-02-07 22:53:48 +0100

    Removed old code in tool_import_files

--- tools/tool_import_files/js/render_tool_import_files.js
+++ tools/tool_import_files/js/render_tool_import_files.js
@@ -298,81 +298,8 @@ const get_content_data_edit = async function(self) {
 		const template = await create_template(self)
 		template_container.appendChild(template)
 
-		// DES
-			// const template_container = ui.create_dom_element({
-			// 	element_type	: 'div',
-			// 	class_name 		: 'template_container',
-			// 	inner_html 		: `
 
-			// 		<div id="actions" class="row">
-
-
-			// 	      <div class="col-lg-7">
-			// 	        <!-- The fileinput-button span is used to style the file input field as button -->
-			// 	        <span class="btn btn-success fileinput-button dz-clickable">
-			// 	            <i class="glyphicon glyphicon-plus"></i>
-			// 	            <span>Add files...</span>
-			// 	        </span>
-			// 	        <button type="submit" class="btn btn-primary start">
-			// 	            <i class="glyphicon glyphicon-upload"></i>
-			// 	            <span>Start upload</span>
-			// 	        </button>
-			// 	        <button type="reset" class="btn btn-warning cancel">
-			// 	            <i class="glyphicon glyphicon-ban-circle"></i>
-			// 	            <span>Cancel upload</span>
-			// 	        </button>
-			// 	      </div>
-
-			// 	      <div class="col-lg-5">
-			// 	        <!-- The global file processing state -->
-			// 	        <span class="fileupload-process">
-			// 	          <div id="total-progress" class="progress progress-striped active" role="progressbar" aria-valuemin="0" aria-valuemax="100" aria-valuenow="0">
-			// 	            <div class="progress-bar progress-bar-success" style="width:0%;" data-dz-uploadprogress=""></div>
-			// 	          </div>
-			// 	        </span>
-			// 	      </div>
-
-			// 	    </div>
-
-			// 		<div class="table table-striped" class="files" id="previews">
-
-			// 		  <div id="template" class="file-row">
-			// 		    <div>
-			// 		        <span class="preview"><img data-dz-thumbnail /></span>
-			// 		    </div>
-			// 		    <div>
-			// 		        <p class="name" data-dz-name></p>
-			// 		        <strong class="error text-danger" data-dz-errormessage></strong>
-			// 		    </div>
-			// 		    <div>
-			// 		        <p class="size" data-dz-size></p>
-			// 		        <div class="progress progress-striped active" role="progressbar" aria-valuemin="0" aria-valuemax="100" aria-valuenow="0">
-			// 		          <div class="progress-bar progress-bar-success" style="width:0%;" data-dz-uploadprogress></div>
-			// 		        </div>
-			// 		    </div>
-			// 		    <div>
-			// 		      <button class="btn btn-primary start">
-			// 		          <i class="glyphicon glyphicon-upload"></i>
-			// 		          <span>Start</span>
-			// 		      </button>
-			// 		      <button data-dz-remove class="btn btn-warning cancel">
-			// 		          <i class="glyphicon glyphicon-ban-circle"></i>
-			// 		          <span>Cancel</span>
-			// 		      </button>
-			// 		      <button data-dz-remove class="btn btn-danger delete">
-			// 		        <i class="glyphicon glyphicon-trash"></i>
-			// 		        <span>Delete</span>
-			// 		      </button>
-			// 		    </div>
-			// 		  </div>
-
-			// 		</div>
-			// `,
-			// 	parent 			: fragment
-			// })
-
-
-	// inputs components container
+		// inputs components container label
 		const inputs_container = ui.create_dom_element({
 			element_type	: 'div',
 			class_name		: 'inputs_container',
