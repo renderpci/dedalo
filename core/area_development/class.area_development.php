9c015755637e79102903fc62d13c77a40bdbdf0f
Paco <paco@render.es>
2023-02-12 11:31:19 +0100

    Fixed syntax errors in labels

--- core/area_development/class.area_development.php
+++ core/area_development/class.area_development.php
@@ -50,14 +50,14 @@ class area_development extends area_common {
 				$item->typo		= 'widget';
 				$item->tipo		= $this->tipo;
 				$item->parent	= $this->tipo;
-				$item->label	= label::get_label('make_backup') || 'Make backup';
+				$item->label	= label::get_label('make_backup') ?? 'Make backup';
 				$item->info		= null;
 				$file_name		= date("Y-m-d_His") .'.'. DEDALO_DATABASE_CONN .'.'. DEDALO_DB_TYPE .'_'. $_SESSION['dedalo']['auth']['user_id'] .'_forced_dbv' . implode('-', get_current_version_in_db()).'.custom.backup';
 				$item->body		= 'Force to make a full backup now like:<br><br><div>'.DEDALO_BACKUP_PATH_DB.'/<br>'.$file_name.'</div>';
 				$item->run[]	= (object)[
 					'fn'		=> 'init_form',
 					'options'	=> (object)[
-						'confirm_text' => label::get_label('sure') || 'Sure?'
+						'confirm_text' => label::get_label('sure') ?? 'Sure?'
 					]
 				];
 				$item->trigger 	= (object)[
@@ -89,7 +89,7 @@ class area_development extends area_common {
 								'mandatory'	=> true
 							]
 						],
-						'confirm_text' => label::get_label('sure') || 'Sure?'
+						'confirm_text' => label::get_label('sure') ?? 'Sure?'
 					]
 				];
 				$item->trigger 	= (object)[
@@ -161,7 +161,7 @@ class area_development extends area_common {
 				$file_path		= 'Target: '.(defined('STRUCTURE_DOWNLOAD_JSON_FILE') ? STRUCTURE_DOWNLOAD_JSON_FILE : STRUCTURE_DOWNLOAD_DIR) . '/' . $file_name;
 				// $file_url		= DEDALO_PROTOCOL . $_SERVER['HTTP_HOST'] . DEDALO_LIB_BASE_URL . '/backup/backups_structure/srt_download' . '/' . $file_name;
 				$item->body		= $file_path;
-				$confirm_text	= label::get_label('sure') || 'Sure?';
+				$confirm_text	= label::get_label('sure') ?? 'Sure?';
 				$item->run[]	= (object)[
 					'fn'		=> 'init_form',
 					'options'	=> (object)[
@@ -193,7 +193,7 @@ class area_development extends area_common {
 				$item->typo		= 'widget';
 				$item->tipo		= $this->tipo;
 				$item->parent	= $this->tipo;
-				$item->label	= label::get_label('import_json_ontology') || 'Import JSON ontology';
+				$item->label	= label::get_label('import_json_ontology') ?? 'Import JSON ontology';
 				$item->info		= null;
 
 				if (defined('ONTOLOGY_DB')) {
@@ -270,7 +270,7 @@ class area_development extends area_common {
 				$item->run[]	= (object)[
 					'fn' 	  => 'init_form',
 					'options' => (object)[
-						'confirm_text' => label::get_label('sure') || 'Sure?'
+						'confirm_text' => label::get_label('sure') ?? 'Sure?'
 					]
 				];
 				$item->trigger 	= (object)[
@@ -293,7 +293,7 @@ class area_development extends area_common {
 				$item->run[]	= (object)[
 					'fn'		=> 'init_form',
 					'options'	=> (object)[
-						'confirm_text' => label::get_label('sure') || 'Sure?'
+						'confirm_text' => label::get_label('sure') ?? 'Sure?'
 					]
 				];
 				$item->trigger 	= (object)[
@@ -316,7 +316,7 @@ class area_development extends area_common {
 				$item->run[]	= (object)[
 					'fn'		=> 'init_form',
 					'options'	=> (object)[
-						'confirm_text' => label::get_label('sure') || 'Sure?'
+						'confirm_text' => label::get_label('sure') ?? 'Sure?'
 					]
 				];
 				$item->trigger 	= (object)[
@@ -382,7 +382,7 @@ class area_development extends area_common {
 					$item->run[]	= (object)[
 						'fn' 	  => 'init_form',
 						'options' => (object)[
-							'confirm_text' => label::get_label('sure') || 'Sure?'
+							'confirm_text' => label::get_label('sure') ?? 'Sure?'
 						]
 					];
 					$item->trigger 	= (object)[
@@ -526,7 +526,7 @@ class area_development extends area_common {
 					'fn' 	  => 'init_form',
 					'options' => (object)[
 						'submit_label' => 'Create new empty test record',
-						'confirm_text' => label::get_label('sure') || 'Sure?'
+						'confirm_text' => label::get_label('sure') ?? 'Sure?'
 					]
 				];
 				$item->trigger 	= (object)[
