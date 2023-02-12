ebc5c727a108170c612cc82aec04793216637d0f
Alejandro Peña Carbonell <alex@render.es>
2023-02-12 22:42:29 +0100

    re-factorized proces_runner to be used as parallel processes in background

--- core/base/process_runner.php
+++ core/base/process_runner.php
@@ -5,6 +5,8 @@
 * This file calculates process in background.
 *
 */
+// time
+	$start_time = hrtime(true);
 
 // data
 	$data = json_decode($argv[1], true);
@@ -22,21 +24,39 @@
 		session_id($session_id);
 	}
 
-// unlock session. Only for read
-	define('PREVENT_SESSION_LOCK', true);
 
 // config. Starts a new session with forced id from command arguments
 	include (dirname(dirname(dirname(__FILE__)))) .'/config/config.php';
+// unlock session. Only for read
+	session_write_close();
 
-// actions to run
-	if (!is_callable($class_name::$method_name)) {
-		debug_log(__METHOD__." Error. Method it's not callable ".to_string(), logger::ERROR);
-		return null;
+
+// output manager
+	// class_name
+	$output_class_name		= $data['class_name'];
+	// method_name
+	$output_method_name		= $data['method_name'];
+	// params
+	$output_params			= $data['params'] ?? new stdClass();
+
+	// include class file
+	if (isset($data['file'])) {
+		include_once($data['file']);
 	}
-	$params = $data['params'] ?? null;
-	$result = isset($data['params'])
-		? $class_name::$method_name($data['params'])
-		: $class_name::$method_name();
+
+	// check callable
+	// if (!is_callable($output_class_name::$output_method_name)) {
+	// 	debug_log(__METHOD__." Error. Method it's not callable ".to_string($output_class_name .':'.$output_method_name), logger::ERROR);
+	// 	return null;
+	// }
+
+	// exec output
+	$output_params	= (object)json_decode( json_encode($output_params) );
+	$output_result	= $output_class_name::$output_method_name($output_params);
+
+
+// log write notification
+	debug_log(__METHOD__." Process runner job is done in time: ".exec_time_unit($start_time,'ms')." ms", logger::ERROR);
 
 // write result to file as text
-	echo json_encode($result);
+	echo json_encode($output_result);
