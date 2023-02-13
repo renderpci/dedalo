2506406e65d32ff7ed63133ab0071689d18ee39e
Paco <paco@render.es>
2023-02-13 11:58:42 +0100

    Added component_portal activate_autocomplete control to prevent activate when user permissions are <2

--- core/component_portal/js/render_edit_component_portal.js
+++ core/component_portal/js/render_edit_component_portal.js
@@ -887,6 +887,11 @@ export const get_buttons = (self) => {
 */
 export const activate_autocomplete = async function(self, wrapper) {
 
+	// permissions check
+		if (self.permissions<2) {
+			return
+		}
+
 	// Default source external buttons configuration,
 	// if show.interface is defined in properties used the definition, else use this default
 		if(self.context.properties.source?.mode==='external' && !self.request_config_object?.show?.interface) {
