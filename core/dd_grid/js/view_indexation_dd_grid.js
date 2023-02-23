3d3f6b77d1c8c74d09639bec043627911caa7085
Paco <paco@render.es>
2023-02-23 13:38:00 +0100

    Added component_av js static functions: open_av_player, download_av_fragment to manage dd_grid_indexation custom calls like v5

--- core/dd_grid/js/view_indexation_dd_grid.js
+++ core/dd_grid/js/view_indexation_dd_grid.js
@@ -8,11 +8,9 @@
 	// import {clone,dd_console} from '../../common/js/utils/index.js'
 	import {ui} from '../../common/js/ui.js'
 	import {
-		get_text_column,
 		get_av_column,
 		get_img_column,
 		get_label_column,
-		get_button_column,
 		get_json_column,
 		get_section_id_column,
 		get_iri_column
@@ -165,3 +163,122 @@ const get_div_container = function(current_data) {
 
 	return div_container
 }//end get_div_container
+
+
+
+/**
+* GET_BUTTON_COLUMN
+* @param object current_data
+* @return DOM node button (img)
+*/
+export const get_button_column = function(current_data) {
+
+	const value			= current_data.value[0]
+	const class_list	= value.class_list || ''
+
+	// button
+		const button = ui.create_dom_element({
+			element_type	: 'button',
+			class_name		: 'dd_grid_button',
+			title			: value.label || ''
+		})
+
+	// icon
+		ui.create_dom_element({
+			element_type	: 'span',
+			class_name		: 'icon ' + class_list, // 'icon indexation',
+			parent			: button
+		})
+
+
+	// event
+		if (value.action && value.action.event) {
+
+			// sample
+				// 'class_list'	=> 'button label',
+				// 'action'		=> (object)[
+				// 		'event'			=> 'click',
+				// 		'method'		=> 'open_tool',
+				// 		'module_path'	=> '../../../tools/tool_common/js/tool_common.js',
+				// 		'options'		=> (object)[
+				// 			'tool_name'			=> 'tool_indexation',
+				// 			'section_tipo'		=> $section_tipo,
+				// 			'section_id'		=> $section_id,
+				// 			'component_tipo'	=> $component_tipo,
+				// 			'tag_id'			=> $tag_id,
+				// 			'section_top_tipo'	=> $section_top_tipo,
+				// 			'section_top_id'	=> $section_top_id
+				// 		]
+				// ]
+
+			button.addEventListener(value.action.event, async (e)=>{
+
+				// options
+					const options			= value.action.options
+					options.button_caller	= e.target
+
+				// module
+					const module = await import(value.action.module_path)
+					console.log('module:', module);
+				// method
+					const method_name = value.action.method
+
+				// function exec. Try with fallback
+					const fn = module[method_name] // direct exported method
+							|| module.default[method_name] // fallback to default method
+							|| module.default.prototype[method_name] // fallback to default prototyped method
+
+					if (fn && typeof fn==='function') {
+						console.log('-> [button_column] Executing function:', method_name);
+						fn(options)
+					}else{
+						console.error('Unable to call method:', method_name);
+					}
+			})
+		}
+
+	return button
+}//end get_button_column
+
+
+
+/**
+* GET_TEXT_COLUMN
+* Render a span DOM node with given value
+* @param object data_item
+* @param bool use_fallback
+* @return DOM node text_node (span)
+*/
+export const get_text_column = function(data_item, use_fallback) {
+
+	const class_list = data_item.class_list || ''
+
+	const value = use_fallback===true
+		? (data_item.value && data_item.value[0]!==undefined ? data_item.value : data_item.fallback_value)
+		: data_item.value
+
+	const value_string = value
+		? value.join(' ')
+		: ''
+
+	const add_style = value_string.length>0
+		? ''
+		: ' empty'
+
+	const text_node = ui.create_dom_element({
+		element_type	: 'span',
+		class_name		: class_list + add_style,
+		inner_html		: value_string
+	})
+
+	// text_fragment case. Toggle class to display overflow hidden text
+		if (data_item.class_list==='text_fragment') {
+			console.log('data_item:', data_item);
+			text_node.addEventListener('click', function(e) {
+				text_node.classList.toggle('full')
+			})
+		}
+
+
+	return text_node
+}//end get_text_column
