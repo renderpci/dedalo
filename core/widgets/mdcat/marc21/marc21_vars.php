678b0225fce9a963a58cc17c6dae1d719878e9bb
Alejandro Peña Carbonell <alex@render.es>
2023-03-09 12:36:51 +0100

    Added calculations for mdcat

---
+++ core/widgets/mdcat/marc21/marc21_vars.php
@@ -0,0 +1,14 @@
+<?php
+/**
+* MARC21_VARS . Dédalo standar vars
+* Define correspondences from marc21 file vars to Dédalo tipos
+*/
+if (!isset($marc21_vars)) {
+	$marc21_vars = new stdClass();
+}
+	
+	$marc21_vars->custom_var1 		= 'mdcat159';
+
+
+
+?>
