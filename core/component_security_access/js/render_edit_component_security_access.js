d37828d2b265396e466d50ecb12cd2b35dd79f14
Alejandro Peña Carbonell <alex@render.es>
2023-03-12 18:33:39 +0100

    added sample to comment in security_access

--- core/component_security_access/js/render_edit_component_security_access.js
+++ core/component_security_access/js/render_edit_component_security_access.js
@@ -41,7 +41,7 @@ render_edit_component_security_access.prototype.edit = async function(options) {
 		case 'print':
 			// view print use the same view as default, except it will use read only to render content_value
 			// as different view as default it will set in the class of the wrapper
-			// sample: <div class="wrapper_component component_input_text oh14 oh1_oh14 edit view_print disabled_component">...</div>
+			// sample: <div class="component_security_access dd774 dd234_dd774 edit view_print disabled_component">...</div>
 			// take account that to change the css when the component will render in print context
 			// for print we need to use read of the contect_value and it's necessary force permissions to use read only element render
 			self.permissions = 1
