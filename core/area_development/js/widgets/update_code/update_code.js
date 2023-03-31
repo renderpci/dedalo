82404aae1670c76fca9ff38579596a6a4af6e53e
Paco <paco@render.es>
2023-03-31 20:18:06 +0200

    Added files of widget 'updated_code'

---
+++ core/area_development/js/widgets/update_code/update_code.js
@@ -0,0 +1,48 @@
+/*global get_label, page_globals, SHOW_DEBUG, DEDALO_CORE_URL, DEDALO_API_URL */
+/*eslint no-undef: "error"*/
+
+
+
+// imports
+	import {widget_common} from '../../../../widgets/widget_common/widget_common.js'
+	import {render_update_code} from './render_update_code.js'
+
+
+
+/**
+* UPDATE_CODE
+*/
+export const update_code = function() {
+
+	this.id
+
+	this.section_tipo
+	this.section_id
+	this.lang
+	this.mode
+
+	this.value
+
+	this.node
+
+	this.events_tokens	= []
+	this.ar_instances	= []
+
+	this.status
+}//end update_code
+
+
+
+/**
+* COMMON FUNCTIONS
+* extend functions from common
+*/
+// prototypes assign
+	// // lifecycle
+	update_code.prototype.init		= widget_common.prototype.init
+	update_code.prototype.build		= widget_common.prototype.build
+	update_code.prototype.render	= widget_common.prototype.render
+	update_code.prototype.destroy	= widget_common.prototype.destroy
+	// // render
+	update_code.prototype.edit		= render_update_code.prototype.list
+	update_code.prototype.list		= render_update_code.prototype.list
