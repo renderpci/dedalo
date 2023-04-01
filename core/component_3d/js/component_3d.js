d512ae30d0d765e1f73e3635cbf18c46ca5d8f37
Alejandro Peña Carbonell <alex@render.es>
2023-04-01 19:58:53 +0200

    Added to create a posterframe when file is upload to component_3d

--- core/component_3d/js/component_3d.js
+++ core/component_3d/js/component_3d.js
@@ -5,11 +5,13 @@
 
 // imports
 	import {dd_console} from '../../common/js/utils/index.js'
-	// import {data_manager} from '../../common/js/data_manager.js'
-	import {common} from '../../common/js/common.js'
+	import {data_manager} from '../../common/js/data_manager.js'
+	import {common, create_source} from '../../common/js/common.js'
 	import {component_common} from '../../component_common/js/component_common.js'
 	import {render_edit_component_3d} from '../../component_3d/js/render_edit_component_3d.js'
 	import {render_list_component_3d} from '../../component_3d/js/render_list_component_3d.js'
+
+	import {upload} from '../../services/service_upload/js/service_upload.js'
 	// import {render_mini_component_3d} from '../../component_3d/js/render_mini_component_3d.js'
 	// import {render_player_component_3d} from '../../component_3d/js/render_player_component_3d.js'
 	// import {render_viewer_component_3d} from '../../component_3d/js/render_viewer_component_3d.js'
@@ -80,3 +82,88 @@ export const component_3d = function(){
 	// component_3d.prototype.viewer			= render_viewer_component_3d.prototype.viewer
 
 	component_3d.prototype.change_mode			= component_common.prototype.change_mode
+
+
+
+/**
+* CREATE_POSTERFRAME
+* 	Creates a new posterframe file from current_view overwriting old file if exists
+* @return promise > bool
+*/
+component_3d.prototype.create_posterframe = async function( viewer ) {
+
+	const self = this
+
+	const blob = await viewer.get_image({
+		width	: 720,
+		height	: 404
+	})
+
+	blob.name = self.tipo+'_'+self.section_tipo+'_'+self.section_id+'.jpg' // added name to the tmp file
+
+	const options = {
+		id					: self.id,
+		file				: blob, // binary data as file
+		resource_type		: '3d', // target dir
+		allowed_extensions	: ['jpg'],
+		max_size_bytes		: blob.size
+	}
+	// upload file as other images to tmp directory
+	const api_response = await upload(options)
+
+	if (!api_response.result) {
+		console.error("Error on api_response:", api_response);
+		return {
+			result	: false,
+			msg		: api_response.msg || 'Error on api_response'
+		}
+	}
+
+	const file_data = api_response.file_data
+
+	// source. Note that second argument is the name of the function to manage the tool request like 'apply_value'
+	// this generates a call as my_tool_name::my_function_name(arguments)
+		const source = create_source(self)
+
+	// rqo
+		const rqo = {
+			dd_api	: 'dd_component_3d_api',
+			action	: 'move_file_to_dir',
+			source	: source,
+			options : {
+				target_dir	:'posterframe',
+				file_data	: file_data
+			}
+		}
+
+	// call to the API, fetch data and get response
+		return new Promise(function(resolve){
+
+			data_manager.request({
+				body : rqo
+			})
+			.then(function(response){
+				if(SHOW_DEVELOPER===true) {
+					dd_console("-> upload_blob API response:",'DEBUG',response);
+				}
+
+				const result = response.result // array of objects
+
+				resolve(result)
+			})
+		})
+
+
+	// save file
+	// const posterframe =  viewer.renderer.domElement.toDataURL("image/jpeg", 0.95);
+
+	// const a = document.createElement('a');
+	//	a.href = posterframe;
+	//	a.download = 'a.jpg';
+	//	document.body.appendChild(a);
+	//	a.click();
+
+
+
+
+}//end create_posterframe
