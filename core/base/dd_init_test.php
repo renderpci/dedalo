6e2e41cf8431fefd70e48ecbd7913219dea428ea
Alejandro Peña Carbonell <alex@render.es>
2023-04-06 21:10:26 +0200

    Removed DEDALO_BACKUP_PATH_USERS from check init test process

--- core/base/dd_init_test.php
+++ core/base/dd_init_test.php
@@ -97,22 +97,6 @@
 
 
 
-// BACKUP USERS DIR
-	# Target folder exists test
-	$folder_path = DEDALO_BACKUP_PATH_USERS;
-	if( !is_dir($folder_path) ) {
-		if(!mkdir($folder_path, 0700, true)) {
-
-			$init_response->msg[]	= 'Error on read or create backup users directory. Permission denied';
-			$init_response->errors	= true;
-			debug_log(__METHOD__."  ".implode(PHP_EOL, $init_response->msg), logger::ERROR);
-
-			return $init_response;
-		}
-		debug_log(__METHOD__." CREATED DIR: $folder_path  ".to_string(), logger::DEBUG);
-	}
-
-
 // BACKUP_TEMP
 	# Target folder exists test
 	if (defined('STRUCTURE_DOWNLOAD_DIR') && STRUCTURE_DOWNLOAD_DIR!==false) {
