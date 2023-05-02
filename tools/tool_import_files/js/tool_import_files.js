8bed75f08d79a9fd76e38372e0c2d98d1e72150d
Alejandro Peña Carbonell <alex@render.es>
2023-05-02 18:21:00 +0200

    Added service_dropzone to tool_import_files

--- tools/tool_import_files/js/tool_import_files.js
+++ tools/tool_import_files/js/tool_import_files.js
@@ -12,7 +12,7 @@
 	// import {ui} from '../../../core/common/js/ui.js'
 	import {tool_common} from '../../tool_common/js/tool_common.js'
 	import {render_tool_import_files} from './render_tool_import_files.js'
-	import {upload_manager_init} from './upload_manager.js'
+	import {service_dropzone} from '../../../core/services/service_dropzone/js/service_dropzone.js'
 	import {service_tmp_section} from '../../../core/services/service_tmp_section/js/service_tmp_section.js'
 
 
@@ -36,10 +36,11 @@ export const tool_import_files = function () {
 	this.langs				= null
 	this.caller				= null
 	this.key_dir			= null
-	this.active_dropzone	= null
 	this.tool_contanier		= null
 	this.files_data			= []
 
+	// services
+	this.service_dropzone		= null
 	this.service_tmp_section	= null
 	return true
 }//end page
@@ -71,8 +72,6 @@ tool_import_files.prototype.init = async function(options) {
 
 	// upload_manager_init
 		self.key_dir = self.caller.tipo + '_' + self.caller.section_tipo
-		await upload_manager_init()
-
 
 	return common_init
 }//end init
@@ -113,7 +112,23 @@ tool_import_files.prototype.build = async function(autoload=false) {
 		}//end load_target_component_context
 		self.target_component_context = await load_target_component_context()
 
-		load_target_component_context()
+		// Service DropZone
+			if(self.tool_config.file_processor){
+				self.tool_config.file_processor.map(el => {
+					el.function_name_label = self.get_tool_label(el.function_name)
+				});
+			}
+			// init service dropzone
+			self.service_dropzone = await get_instance({
+				model 				: 'service_dropzone',
+				mode 				: 'edit',
+				caller 				: self,
+				allowed_extensions	: self.allowed_extensions || [],
+				key_dir				: self.key_dir,
+				component_option	: self.tool_config.ddo_map.filter(el => el.role === 'component_option'),
+				file_processor		: self.tool_config.file_processor || null
+			})
+			await self.service_dropzone.build()
 
 		// Service tmp_section
 			// init service tmp_section
@@ -132,4 +147,5 @@ tool_import_files.prototype.build = async function(autoload=false) {
 	}
 
 
+	return common_build
 }//end build
