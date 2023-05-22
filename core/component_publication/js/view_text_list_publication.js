ee1e76ef53dd1102b4143ddd31e53880da6b6b0c
Paco <paco@render.es>
2023-05-22 16:19:52 +0200

    Fixed non translated HTML issue with component_radio_button and component_publication

--- core/component_publication/js/view_text_list_publication.js
+++ core/component_publication/js/view_text_list_publication.js
@@ -23,6 +23,8 @@ export const view_text_list_publication = function() {
 /**
 * RENDER
 * Render node to be used in current mode
+* @param object self
+* @param object options
 * @return HTMLElement text_node
 */
 view_text_list_publication.render = async function(self, options) {
@@ -33,7 +35,13 @@ view_text_list_publication.render = async function(self, options) {
 
 		const value_string = value.join(self.context.fields_separator)
 
-	const text_node = document.createTextNode(value_string)
+	// const text_node = document.createTextNode(value_string)
+	// div . Create a div instead text node to allow untranslated mark tags
+	const text_node = ui.create_dom_element({
+		element_type	: 'div',
+		inner_html		: value_string
+	})
+
 
 	return text_node
 }//end render
