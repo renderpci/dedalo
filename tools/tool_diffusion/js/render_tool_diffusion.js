6f720b410229c07a7811ce989a7317fcf396aee2
Alejandro Peña Carbonell <alex@render.es>
2023-05-25 13:42:45 +0200

    Add license to tool_diffusion js

--- tools/tool_diffusion/js/render_tool_diffusion.js
+++ tools/tool_diffusion/js/render_tool_diffusion.js
@@ -1,3 +1,4 @@
+// @license magnet:?xt=urn:btih:0b31508aeb0634b347b8270c7bee4d411b5d4109&dn=agpl-3.0.txt AGPL-3.0
 /*global get_label, page_globals, SHOW_DEBUG, DEDALO_CORE_URL, tool_diffusion */
 /*eslint no-undef: "error"*/
 
@@ -385,3 +386,7 @@ export const render_publication_items = function(self) {
 
 	return publication_items
 }//end render_publication_items
+
+
+
+// @license-end
