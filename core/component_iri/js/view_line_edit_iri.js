2a4c95955794e5f3dca2d1effaea54ee31feb928
Alejandro Peña Carbonell <alex@render.es>
2023-05-25 11:26:27 +0200

    Add license to component_iri js

--- core/component_iri/js/view_line_edit_iri.js
+++ core/component_iri/js/view_line_edit_iri.js
@@ -1,3 +1,4 @@
+// @license magnet:?xt=urn:btih:0b31508aeb0634b347b8270c7bee4d411b5d4109&dn=agpl-3.0.txt AGPL-3.0
 /* global get_label, page_globals, SHOW_DEBUG, DEDALO_CORE_URL */
 /* eslint no-undef: "error" */
 
@@ -58,3 +59,7 @@ view_line_edit_iri.render = async function(self, options) {
 
 	return wrapper
 }//end render
+
+
+
+// @license-end
