14f0d35ad767e8f2161c02e08c58a02281124c2e
Alejandro Peña Carbonell <alex@render.es>
2023-05-25 12:34:43 +0200

    Add license to section js

--- core/section/js/view_default_edit_section.js
+++ core/section/js/view_default_edit_section.js
@@ -1,3 +1,4 @@
+// @license magnet:?xt=urn:btih:0b31508aeb0634b347b8270c7bee4d411b5d4109&dn=agpl-3.0.txt AGPL-3.0
 /*global get_label, Promise, SHOW_DEVELOPER, SHOW_DEBUG */
 /*eslint no-undef: "error"*/
 
@@ -226,3 +227,7 @@ const no_records_node = () => {
 
 	return node
 }//end no_records_node
+
+
+
+// @license-end
