eee8102b01e7434134f6afcbdfc9a47858a97231
Alejandro Peña Carbonell <alex@render.es>
2023-05-25 12:09:56 +0200

    Add license to login js

--- core/login/js/render_login.js
+++ core/login/js/render_login.js
@@ -1,3 +1,4 @@
+// @license magnet:?xt=urn:btih:0b31508aeb0634b347b8270c7bee4d411b5d4109&dn=agpl-3.0.txt AGPL-3.0
 /* global get_label, page_globals, SHOW_DEBUG */
 /*eslint no-undef: "error"*/
 
@@ -508,3 +509,7 @@ export const render_files_loader = function() {
 
 	return fragment
 }//end render_files_loader
+
+
+
+// @license-end
