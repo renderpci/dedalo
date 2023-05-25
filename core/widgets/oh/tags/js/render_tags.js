114a9ca6eb214f14f1e27c538d7f40a2f2178b68
Alejandro Peña Carbonell <alex@render.es>
2023-05-25 13:26:29 +0200

    Add license to widget tags oh js

--- core/widgets/oh/tags/js/render_tags.js
+++ core/widgets/oh/tags/js/render_tags.js
@@ -1,3 +1,4 @@
+// @license magnet:?xt=urn:btih:0b31508aeb0634b347b8270c7bee4d411b5d4109&dn=agpl-3.0.txt AGPL-3.0
 /*global get_label, page_globals, SHOW_DEBUG, DEDALO_CORE_URL*/
 /*eslint no-undef: "error"*/
 
@@ -256,3 +257,7 @@ const item_value_factory = function(id, label, data) {
 
 	return wrapper
 }//end item_value_factory
+
+
+
+// @license-end
