2ce6685c79a03026831d5d0b183b843c74df8808
Alejandro Peña Carbonell <alex@render.es>
2023-05-25 11:30:49 +0200

    Add license to component_password js

--- core/component_password/js/render_edit_component_password.js
+++ core/component_password/js/render_edit_component_password.js
@@ -1,3 +1,4 @@
+// @license magnet:?xt=urn:btih:0b31508aeb0634b347b8270c7bee4d411b5d4109&dn=agpl-3.0.txt AGPL-3.0
 /* global get_label, page_globals, SHOW_DEBUG, flatpickr */
 /*eslint no-undef: "error"*/
 
@@ -51,3 +52,7 @@ render_edit_component_password.prototype.edit = async function(options) {
 			return view_default_edit_password.render(self, options)
 	}
 }//end edit
+
+
+
+// @license-end
