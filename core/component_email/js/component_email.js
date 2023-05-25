0e6dcc7435f7f11646bdbd1caf1d99e2675096ca
Alejandro Peña Carbonell <alex@render.es>
2023-05-25 11:04:47 +0200

    Add license to component_email js

--- core/component_email/js/component_email.js
+++ core/component_email/js/component_email.js
@@ -1,3 +1,4 @@
+// @license magnet:?xt=urn:btih:0b31508aeb0634b347b8270c7bee4d411b5d4109&dn=agpl-3.0.txt AGPL-3.0
 /*global get_label, page_globals, SHOW_DEBUG, DEDALO_CORE_URL*/
 /*eslint no-undef: "error"*/
 
@@ -224,3 +225,5 @@ component_email.prototype.get_ar_emails = async function() {
 }//end get_ar_emails
 
 
+
+// @license-end
