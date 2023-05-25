2c3097afdc840704b64da9f72513bacd0b76e8b0
Alejandro Peña Carbonell <alex@render.es>
2023-05-25 16:07:58 +0200

    Add license to tool_import_rdf js

--- tools/tool_import_rdf/js/tool_import_rdf.js
+++ tools/tool_import_rdf/js/tool_import_rdf.js
@@ -1,3 +1,4 @@
+// @license magnet:?xt=urn:btih:0b31508aeb0634b347b8270c7bee4d411b5d4109&dn=agpl-3.0.txt AGPL-3.0
 /*global get_label, page_globals, SHOW_DEBUG, DEDALO_CORE_URL*/
 /*eslint no-undef: "error"*/
 
@@ -144,3 +145,6 @@ tool_import_rdf.prototype.get_rdf_data = async function(ontology_tipo, ar_values
 			})
 		})
 }//end get_rdf_data
+
+
+// @license-end
