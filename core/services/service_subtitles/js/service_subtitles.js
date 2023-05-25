43ba6a5ff6220452b5cd05e05bc960f346bdeb50
Alejandro Peña Carbonell <alex@render.es>
2023-05-25 13:09:27 +0200

    Add license to service_subtitles js

--- core/services/service_subtitles/js/service_subtitles.js
+++ core/services/service_subtitles/js/service_subtitles.js
@@ -1,3 +1,4 @@
+// @license magnet:?xt=urn:btih:0b31508aeb0634b347b8270c7bee4d411b5d4109&dn=agpl-3.0.txt AGPL-3.0
 /*global get_label, SHOW_DEBUG, DEDALO_CORE_URL */
 /*eslint no-undef: "error"*/
 
@@ -80,4 +81,4 @@ service_subtitles.prototype.build = async function(autoload=false) {
 
 
 
-
+// @license-end
