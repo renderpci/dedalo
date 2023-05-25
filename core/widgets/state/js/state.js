59c128149a8626e4424fb5fc8a35ce388bb971af
Alejandro Peña Carbonell <alex@render.es>
2023-05-25 13:27:38 +0200

    Add license to widget state js

--- core/widgets/state/js/state.js
+++ core/widgets/state/js/state.js
@@ -1,3 +1,4 @@
+// @license magnet:?xt=urn:btih:0b31508aeb0634b347b8270c7bee4d411b5d4109&dn=agpl-3.0.txt AGPL-3.0
 /*global get_label, page_globals, SHOW_DEBUG, DEDALO_CORE_URL*/
 /*eslint no-undef: "error"*/
 
@@ -45,3 +46,8 @@ export const state = function(){
 	// render
 	state.prototype.edit	= render_edit_state.prototype.edit
 	state.prototype.list	= render_list_state.prototype.list
+
+
+
+
+// @license-end
