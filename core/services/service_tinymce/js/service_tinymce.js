2453976a480c94340ae3081222bc115c4199a352
Alejandro Peña Carbonell <alex@render.es>
2023-05-25 13:12:13 +0200

    Add license to service_tinymce js

--- core/services/service_tinymce/js/service_tinymce.js
+++ core/services/service_tinymce/js/service_tinymce.js
@@ -1,3 +1,4 @@
+// @license magnet:?xt=urn:btih:0b31508aeb0634b347b8270c7bee4d411b5d4109&dn=agpl-3.0.txt AGPL-3.0
 /*global get_label, page_globals, SHOW_DEBUG, tinymce */
 /*eslint no-undef: "error"*/
 
@@ -706,3 +707,8 @@ export const service_tinymce = function() {
 	return last_tag_id
 	}//end get_last_tag_id
 }//end service_tinymce
+
+
+
+// @license-end
+
