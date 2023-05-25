2c34a18948bc6f6d1be9c714d62174fd9814fd7c
Alejandro Peña Carbonell <alex@render.es>
2023-05-25 13:03:33 +0200

    Add license to section_record js

--- core/section_record/js/render_list_section_record.js
+++ core/section_record/js/render_list_section_record.js
@@ -1,3 +1,4 @@
+// @license magnet:?xt=urn:btih:0b31508aeb0634b347b8270c7bee4d411b5d4109&dn=agpl-3.0.txt AGPL-3.0
 /*global get_label, page_globals, SHOW_DEBUG*/
 /*eslint no-undef: "error"*/
 
@@ -47,3 +48,7 @@ render_list_section_record.prototype.list = async function(options={}) {
 			return view_default_list_section_record.render(self, options)
 	}
 }//end list
+
+
+
+// @license-end
--- core/section_record/js/view_mini_section_record.js
+++ core/section_record/js/view_mini_section_record.js
@@ -1,3 +1,4 @@
+// @license magnet:?xt=urn:btih:0b31508aeb0634b347b8270c7bee4d411b5d4109&dn=agpl-3.0.txt AGPL-3.0
 /*global get_label, page_globals, SHOW_DEBUG*/
 /*eslint no-undef: "error"*/
 
@@ -296,3 +297,5 @@ const build_column_node = function(column_instance, self, ar_instances){
 }// end build_column_node
 
 
+
+// @license-end
