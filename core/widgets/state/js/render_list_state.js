102b9f70bec65f972e1eacd9abc073b9b69942ea
Alejandro Peña Carbonell <alex@render.es>
2023-05-25 13:27:38 +0200

    Add license to widget state js

--- core/widgets/state/js/render_list_state.js
+++ core/widgets/state/js/render_list_state.js
@@ -1,3 +1,4 @@
+// @license magnet:?xt=urn:btih:0b31508aeb0634b347b8270c7bee4d411b5d4109&dn=agpl-3.0.txt AGPL-3.0
 /*global get_label, page_globals, SHOW_DEBUG, DEDALO_CORE_URL*/
 /*eslint no-undef: "error"*/
 
@@ -398,3 +399,7 @@ const get_value_tooltip = (output_item, data, self) => {
 
 	return fragment
 }// end get_value_tooltip
+
+
+
+// @license-end
