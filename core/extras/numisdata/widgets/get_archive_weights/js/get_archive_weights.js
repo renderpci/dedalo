ae7ed2cb4a05f35af0662effba2c2c9052ebce72
Alejandro Peña Carbonell <alex@render.es>
2023-05-25 12:06:36 +0200

    Add license to get_archive_weights numisdata js

--- core/extras/numisdata/widgets/get_archive_weights/js/get_archive_weights.js
+++ core/extras/numisdata/widgets/get_archive_weights/js/get_archive_weights.js
@@ -1,3 +1,4 @@
+// @license magnet:?xt=urn:btih:0b31508aeb0634b347b8270c7bee4d411b5d4109&dn=agpl-3.0.txt AGPL-3.0
 /*global get_label, page_globals, SHOW_DEBUG, DEDALO_CORE_URL*/
 /*eslint no-undef: "error"*/
 
@@ -85,3 +86,8 @@ get_archive_weights.prototype.render = async function(options={render_level:'ful
 
 	return node
 }//end render
+
+
+
+
+// @license-end
