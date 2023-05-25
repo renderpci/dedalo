8252a8024535e1248f6692b4206da63d9c36cc13
Alejandro Peña Carbonell <alex@render.es>
2023-05-25 13:11:00 +0200

    Add license to service_time_machine js

--- core/services/service_time_machine/js/render_service_time_machine_list.js
+++ core/services/service_time_machine/js/render_service_time_machine_list.js
@@ -1,3 +1,4 @@
+// @license magnet:?xt=urn:btih:0b31508aeb0634b347b8270c7bee4d411b5d4109&dn=agpl-3.0.txt AGPL-3.0
 /*global page_globals, SHOW_DEBUG */
 /*eslint no-undef: "error"*/
 
@@ -275,3 +276,7 @@ export const rebuild_columns_map = async function(self) {
 
 	return columns_map
 }//end rebuild_columns_map
+
+
+
+// @license-end
--- core/services/service_time_machine/js/view_default_time_machine_list.js
+++ core/services/service_time_machine/js/view_default_time_machine_list.js
@@ -1,3 +1,4 @@
+// @license magnet:?xt=urn:btih:0b31508aeb0634b347b8270c7bee4d411b5d4109&dn=agpl-3.0.txt AGPL-3.0
 /*global get_label, page_globals, SHOW_DEBUG, DEDALO_CORE_URL */
 /*eslint no-undef: "error"*/
 
@@ -75,3 +76,7 @@ view_default_time_machine_list.render = async function(self, options) {
 
 	// 	return columns_map
 	// }//end rebuild_columns_map
+
+
+
+// @license-end
