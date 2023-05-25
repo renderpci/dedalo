fefcab3f9c36a396c18a7d3a8fb65f2eac87b509
Alejandro Peña Carbonell <alex@render.es>
2023-05-25 13:59:25 +0200

    Add license to tool_import_dedalo_csv js

--- tools/tool_import_dedalo_csv/js/render_tool_import_dedalo_csv.js
+++ tools/tool_import_dedalo_csv/js/render_tool_import_dedalo_csv.js
@@ -1,3 +1,4 @@
+// @license magnet:?xt=urn:btih:0b31508aeb0634b347b8270c7bee4d411b5d4109&dn=agpl-3.0.txt AGPL-3.0
 /*global get_label, page_globals, SHOW_DEBUG, DEDALO_CORE_URL, tool_import_dedalo_csv */
 /*eslint no-undef: "error"*/
 
@@ -891,3 +892,7 @@ render_tool_import_dedalo_csv.prototype.upload_done = async function (options) {
 
 	return true
 }//end upload_done
+
+
+
+// @license-end
