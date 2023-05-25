19603be9ad7ab4da7f21ee2b611ae30b87cb3ed3
Alejandro Peña Carbonell <alex@render.es>
2023-05-25 11:57:34 +0200

    Add license to component_svg js

--- core/component_svg/js/component_svg.js
+++ core/component_svg/js/component_svg.js
@@ -1,3 +1,4 @@
+// @license magnet:?xt=urn:btih:0b31508aeb0634b347b8270c7bee4d411b5d4109&dn=agpl-3.0.txt AGPL-3.0
 /*global get_label, page_globals, SHOW_DEBUG, DEDALO_CORE_URL */
 /*eslint no-undef: "error"*/
 
@@ -63,3 +64,7 @@ export const component_svg = function(){
 	component_svg.prototype.list				= render_list_component_svg.prototype.list
 	component_svg.prototype.tm					= render_list_component_svg.prototype.list
 	component_svg.prototype.edit				= render_edit_component_svg.prototype.edit
+
+
+
+// @license-end
