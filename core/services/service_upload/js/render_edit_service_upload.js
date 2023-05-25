f67dd6e39f167110b9b4c4a6253b53e746b69b3f
Alejandro Peña Carbonell <alex@render.es>
2023-05-25 13:14:20 +0200

    Add license to service_upload js

--- core/services/service_upload/js/render_edit_service_upload.js
+++ core/services/service_upload/js/render_edit_service_upload.js
@@ -1,3 +1,4 @@
+// @license magnet:?xt=urn:btih:0b31508aeb0634b347b8270c7bee4d411b5d4109&dn=agpl-3.0.txt AGPL-3.0
 /*global get_label, DEDALO_CORE_URL */
 /*eslint no-undef: "error"*/
 
@@ -460,3 +461,8 @@ export const file_drag_hover = function(e) {
 
 	return true
 }//end file_drag_hover
+
+
+
+// @license-end
+
