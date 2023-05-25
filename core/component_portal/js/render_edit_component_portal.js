b1e3e1bdc8b6282751234bfa35683f8080b924e8
Alejandro Peña Carbonell <alex@render.es>
2023-05-25 11:36:25 +0200

    Add license to component_portal js

--- core/component_portal/js/render_edit_component_portal.js
+++ core/component_portal/js/render_edit_component_portal.js
@@ -1,3 +1,4 @@
+// @license magnet:?xt=urn:btih:0b31508aeb0634b347b8270c7bee4d411b5d4109&dn=agpl-3.0.txt AGPL-3.0
 /*global get_label, DEDALO_CORE_URL*/
 /*eslint no-undef: "error"*/
 
@@ -1029,3 +1030,8 @@ export const render_references = function(ar_references) {
 
 	return fragment
 }//end render_references
+
+
+
+// @license-end
+
--- core/component_portal/js/view_line_edit_portal.js
+++ core/component_portal/js/view_line_edit_portal.js
@@ -1,3 +1,4 @@
+// @license magnet:?xt=urn:btih:0b31508aeb0634b347b8270c7bee4d411b5d4109&dn=agpl-3.0.txt AGPL-3.0
 /*global get_label, DEDALO_CORE_URL, Promise */
 /*eslint no-undef: "error"*/
 
@@ -375,3 +376,7 @@ view_line_edit_portal.render_column_remove = function(options) {
 
 	return button_remove
 }//end render_column_remove
+
+
+
+// @license-end
