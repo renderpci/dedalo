f2bd5d31d2c23e84f4e613fd3f46ec9aa97a3a59
Alejandro Peña Carbonell <alex@render.es>
2023-05-25 16:11:00 +0200

    Add license to tool_indexation js

--- tools/tool_indexation/js/tag_note.js
+++ tools/tool_indexation/js/tag_note.js
@@ -1,3 +1,4 @@
+// @license magnet:?xt=urn:btih:0b31508aeb0634b347b8270c7bee4d411b5d4109&dn=agpl-3.0.txt AGPL-3.0
 /*global page_globals, get_label, SHOW_DEBUG */
 /* eslint no-undef: "error"*/
 
@@ -251,3 +252,5 @@ const new_tag_note = async function(tag, section_tipo) {
 	return new_section_id
 }//end new_tag_note
 
+
+// @license-end
--- tools/tool_indexation/js/tool_indexation.js
+++ tools/tool_indexation/js/tool_indexation.js
@@ -1,3 +1,4 @@
+// @license magnet:?xt=urn:btih:0b31508aeb0634b347b8270c7bee4d411b5d4109&dn=agpl-3.0.txt AGPL-3.0
 /*global get_label, page_globals, SHOW_DEBUG, DD_TIPOS */
 /*eslint no-undef: "error"*/
 
@@ -767,3 +768,6 @@ tool_indexation.prototype.delete_tag = function(tag_id) {
 		resolve(response)
 	})
 }//end delete_tag
+
+
+// @license-end
