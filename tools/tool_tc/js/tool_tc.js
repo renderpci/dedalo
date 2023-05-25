cfdf8223cd4d43a183b2cd6ee6a591febb263608
Alejandro Peña Carbonell <alex@render.es>
2023-05-25 16:24:39 +0200

    Add license to tool_tc js

--- tools/tool_tc/js/tool_tc.js
+++ tools/tool_tc/js/tool_tc.js
@@ -1,3 +1,4 @@
+// @license magnet:?xt=urn:btih:0b31508aeb0634b347b8270c7bee4d411b5d4109&dn=agpl-3.0.txt AGPL-3.0
 /*global get_label, page_globals, SHOW_DEBUG, DEDALO_CORE_URL */
 /*eslint no-undef: "error"*/
 
@@ -205,3 +206,7 @@ tool_tc.prototype.change_all_time_codes = function(offset_seconds) {
 
 	// 	return new_tag
 	// }//end add_time_code_offset
+
+
+
+// @license-end
