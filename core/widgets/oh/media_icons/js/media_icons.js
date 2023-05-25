daad7959703d583f88645e30dff721d750021a60
Alejandro Peña Carbonell <alex@render.es>
2023-05-25 13:25:47 +0200

    Add license to widget media_icons oh js

--- core/widgets/oh/media_icons/js/media_icons.js
+++ core/widgets/oh/media_icons/js/media_icons.js
@@ -1,3 +1,4 @@
+// @license magnet:?xt=urn:btih:0b31508aeb0634b347b8270c7bee4d411b5d4109&dn=agpl-3.0.txt AGPL-3.0
 /*global get_label, page_globals, SHOW_DEBUG, DEDALO_CORE_URL*/
 /*eslint no-undef: "error"*/
 
@@ -48,3 +49,8 @@ export const media_icons = function(){
 	// render
 	media_icons.prototype.edit		= render_media_icons.prototype.edit
 	media_icons.prototype.list		= render_media_icons.prototype.list
+
+
+
+
+// @license-end
