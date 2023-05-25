6021208db0af0ffb87730af6b1a1d4c45f9322ac
Alejandro Peña Carbonell <alex@render.es>
2023-05-25 10:57:44 +0200

    Add license to area_development js

--- core/area_development/js/widgets/publication_api/publication_api.js
+++ core/area_development/js/widgets/publication_api/publication_api.js
@@ -1,3 +1,4 @@
+// @license magnet:?xt=urn:btih:0b31508aeb0634b347b8270c7bee4d411b5d4109&dn=agpl-3.0.txt AGPL-3.0
 /*global get_label, page_globals, SHOW_DEBUG, DEDALO_CORE_URL, DEDALO_API_URL */
 /*eslint no-undef: "error"*/
 
@@ -46,3 +47,7 @@ export const publication_api = function() {
 	// // render
 	publication_api.prototype.edit		= render_publication_api.prototype.list
 	publication_api.prototype.list		= render_publication_api.prototype.list
+
+
+
+// @license-end
