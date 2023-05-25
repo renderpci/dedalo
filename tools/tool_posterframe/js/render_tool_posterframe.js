5e53228a47aae4d9b5b212e927a10bb9deee0fc5
Alejandro Peña Carbonell <alex@render.es>
2023-05-25 16:20:50 +0200

    Add license to tool_posterframe js

--- tools/tool_posterframe/js/render_tool_posterframe.js
+++ tools/tool_posterframe/js/render_tool_posterframe.js
@@ -1,3 +1,4 @@
+// @license magnet:?xt=urn:btih:0b31508aeb0634b347b8270c7bee4d411b5d4109&dn=agpl-3.0.txt AGPL-3.0
 /*global get_label, page_globals, SHOW_DEBUG, DEDALO_CORE_URL, tool_posterframe */
 /*eslint no-undef: "error"*/
 
@@ -266,3 +267,4 @@ const get_buttons = function(self) {
 
 
 
+// @license-end
