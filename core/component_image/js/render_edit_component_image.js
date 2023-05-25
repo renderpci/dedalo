92f6a24e340a769c66323d29764f03f97cd2c5aa
Alejandro Peña Carbonell <alex@render.es>
2023-05-25 11:15:32 +0200

    Add license to component_image js

--- core/component_image/js/render_edit_component_image.js
+++ core/component_image/js/render_edit_component_image.js
@@ -1,3 +1,4 @@
+// @license magnet:?xt=urn:btih:0b31508aeb0634b347b8270c7bee4d411b5d4109&dn=agpl-3.0.txt AGPL-3.0
 /* global get_label, page_globals, SHOW_DEBUG, flatpickr */
 /*eslint no-undef: "error"*/
 
@@ -105,3 +106,8 @@ export const get_quality_selector = (self) => {
 
 	return quality_selector
 }//end get_quality_selector
+
+
+
+// @license-end
+
