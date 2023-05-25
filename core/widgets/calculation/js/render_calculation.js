f75155fdc73156d0a80c78a51c3930aa185b65c5
Alejandro Peña Carbonell <alex@render.es>
2023-05-25 13:21:25 +0200

    Add license to widget calculation js

--- core/widgets/calculation/js/render_calculation.js
+++ core/widgets/calculation/js/render_calculation.js
@@ -1,3 +1,4 @@
+// @license magnet:?xt=urn:btih:0b31508aeb0634b347b8270c7bee4d411b5d4109&dn=agpl-3.0.txt AGPL-3.0
 /*global get_label, page_globals, SHOW_DEBUG, DEDALO_CORE_URL*/
 /*eslint no-undef: "error"*/
 
@@ -198,3 +199,7 @@ const get_value_element = (i, data, inputs_container, self) => {
 
 	return li
 }//end input_element
+
+
+
+// @license-end
