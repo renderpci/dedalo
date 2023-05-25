7ad765a33cc8b6cc88279cdedd828c715b5807de
Alejandro Peña Carbonell <alex@render.es>
2023-05-25 11:20:38 +0200

    Add license to component_input_text js

--- core/component_input_text/js/render_edit_component_input_text.js
+++ core/component_input_text/js/render_edit_component_input_text.js
@@ -1,3 +1,4 @@
+// @license magnet:?xt=urn:btih:0b31508aeb0634b347b8270c7bee4d411b5d4109&dn=agpl-3.0.txt AGPL-3.0
 /*global */
 /*eslint no-undef: "error"*/
 
@@ -141,3 +142,7 @@ export const remove_handler = function(input, key, self) {
 
 	return response
 }//end remove_handler
+
+
+
+// @license-end
