26c34f45b88ccc497707d04bfebc2ebd5ac130b8
Alejandro Peña Carbonell <alex@render.es>
2023-05-25 13:34:03 +0200

    Add license to tool_cataloging js

--- tools/tool_cataloging/js/render_tool_cataloging.js
+++ tools/tool_cataloging/js/render_tool_cataloging.js
@@ -1,3 +1,4 @@
+// @license magnet:?xt=urn:btih:0b31508aeb0634b347b8270c7bee4d411b5d4109&dn=agpl-3.0.txt AGPL-3.0
 /*global get_label, page_globals, SHOW_DEBUG, DEDALO_CORE_URL*/
 /*eslint no-undef: "error"*/
 
@@ -290,3 +291,7 @@ const drop = function (options) {
 				},false)
 	}// end for (let i = drop_zones_len - 1; i >= 0; i--)
 }// end drop
+
+
+
+// @license-end
