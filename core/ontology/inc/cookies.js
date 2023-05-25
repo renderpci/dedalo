bcc756f83d87ffbcef9ec9a7fdf894a7486c0117
Alejandro Peña Carbonell <alex@render.es>
2023-05-25 12:23:37 +0200

    Add license to ontology external js

--- core/ontology/inc/cookies.js
+++ core/ontology/inc/cookies.js
@@ -1,3 +1,6 @@
+// @license magnet:?xt=urn:btih:0b31508aeb0634b347b8270c7bee4d411b5d4109&dn=agpl-3.0.txt AGPL-3.0
+
+
 'use strict';
 /**
 * Cockies javascript 
@@ -134,3 +137,5 @@ function clear_some_local_storage(startsWith) {
 }
 
 
+
+// @license-end
