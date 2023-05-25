a6265983038afcb290b73075ee6b7b046b89b8d6
Alejandro Peña Carbonell <alex@render.es>
2023-05-25 13:14:20 +0200

    Add license to service_upload js

--- core/services/service_upload/js/service_upload.js
+++ core/services/service_upload/js/service_upload.js
@@ -1,3 +1,4 @@
+// @license magnet:?xt=urn:btih:0b31508aeb0634b347b8270c7bee4d411b5d4109&dn=agpl-3.0.txt AGPL-3.0
 /*global get_label, SHOW_DEBUG, DEDALO_CORE_URL, DEDALO_API_URL */
 /*eslint no-undef: "error"*/
 
@@ -620,3 +621,7 @@ service_upload.prototype.join_chunked_files = async function(options) {
 			})
 		})
 }//end get_system_info
+
+
+
+// @license-end
