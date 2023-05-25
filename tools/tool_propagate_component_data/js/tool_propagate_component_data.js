13fd280714e12aafb922c4b8644c2d536be87976
Alejandro Peña Carbonell <alex@render.es>
2023-05-25 16:22:05 +0200

    Add license to tool_propagate_component_data js

--- tools/tool_propagate_component_data/js/tool_propagate_component_data.js
+++ tools/tool_propagate_component_data/js/tool_propagate_component_data.js
@@ -1,3 +1,4 @@
+// @license magnet:?xt=urn:btih:0b31508aeb0634b347b8270c7bee4d411b5d4109&dn=agpl-3.0.txt AGPL-3.0
 /*global get_label, page_globals, SHOW_DEBUG, DEDALO_CORE_URL */
 /*eslint no-undef: "error"*/
 
@@ -231,3 +232,6 @@ tool_propagate_component_data.prototype.on_close_actions = async function(open_a
 
 	return true
 }//end on_close_actions
+
+
+// @license-end
