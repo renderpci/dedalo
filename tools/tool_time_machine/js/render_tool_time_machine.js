2ee22ce9c716102b1aa49920780c9bc498e51bda
Alejandro Peña Carbonell <alex@render.es>
2023-05-25 16:25:39 +0200

    Add license to tool_time_machine js

--- tools/tool_time_machine/js/render_tool_time_machine.js
+++ tools/tool_time_machine/js/render_tool_time_machine.js
@@ -1,3 +1,4 @@
+// @license magnet:?xt=urn:btih:0b31508aeb0634b347b8270c7bee4d411b5d4109&dn=agpl-3.0.txt AGPL-3.0
 /*global get_label, page_globals, SHOW_DEBUG, DEDALO_CORE_URL */
 /*eslint no-undef: "error"*/
 
@@ -232,3 +233,7 @@ export const add_component = async (self, component_container, lang_value, label
 
 	return node
 }//end add_component
+
+
+
+// @license-end
