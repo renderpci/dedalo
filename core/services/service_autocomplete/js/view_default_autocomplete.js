f3ca265136b272999a403c9d3d53745a67b1c60d
Alejandro Peña Carbonell <alex@render.es>
2023-05-25 13:07:04 +0200

    Add license to service_autocomplete js

--- core/services/service_autocomplete/js/view_default_autocomplete.js
+++ core/services/service_autocomplete/js/view_default_autocomplete.js
@@ -1,3 +1,4 @@
+// @license magnet:?xt=urn:btih:0b31508aeb0634b347b8270c7bee4d411b5d4109&dn=agpl-3.0.txt AGPL-3.0
 /*global get_label, DEDALO_CORE_URL */
 /*eslint no-undef: "error"*/
 
@@ -1419,3 +1420,7 @@ const get_grid_choose_data = async function(self, selected_instance, params) {
 
 	return grid_choose_data
 }//end get_grid_choose_data
+
+
+
+// @license-end
