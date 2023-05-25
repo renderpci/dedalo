64aec750cc4d6a0829ca519c347676f516e57a59
Alejandro Peña Carbonell <alex@render.es>
2023-05-25 13:08:57 +0200

    Add license to service_dropzone js

--- core/services/service_dropzone/js/service_dropzone.js
+++ core/services/service_dropzone/js/service_dropzone.js
@@ -1,3 +1,4 @@
+// @license magnet:?xt=urn:btih:0b31508aeb0634b347b8270c7bee4d411b5d4109&dn=agpl-3.0.txt AGPL-3.0
 /*global get_label, SHOW_DEBUG, DEDALO_CORE_URL, DEDALO_API_URL */
 /*eslint no-undef: "error"*/
 
@@ -143,3 +144,7 @@ const get_system_info = async function() {
 			})
 		})
 }//end get_system_info
+
+
+
+// @license-end
