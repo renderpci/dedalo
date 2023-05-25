a18684434fbf6d7af42ca34370f3918172ebe2ba
Alejandro Peña Carbonell <alex@render.es>
2023-05-25 12:00:19 +0200

    Add license to component_text_area js

--- core/component_text_area/js/component_text_area.js
+++ core/component_text_area/js/component_text_area.js
@@ -1,3 +1,4 @@
+// @license magnet:?xt=urn:btih:0b31508aeb0634b347b8270c7bee4d411b5d4109&dn=agpl-3.0.txt AGPL-3.0
 /*global get_label, page_globals, SHOW_DEBUG, DEDALO_CORE_URL*/
 /*eslint no-undef: "error"*/
 
@@ -1223,3 +1224,7 @@ component_text_area.prototype.add_component_history_note = async function(option
 
 		return note_section_id;
 	}//end create_reference
+
+
+
+// @license-end
