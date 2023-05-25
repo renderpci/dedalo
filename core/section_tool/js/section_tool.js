c96b3f92992cef6aa18ef40f924a4600edbcef4d
Alejandro Peña Carbonell <alex@render.es>
2023-05-25 13:04:58 +0200

    Add license to section_tool js

--- core/section_tool/js/section_tool.js
+++ core/section_tool/js/section_tool.js
@@ -1,3 +1,4 @@
+// @license magnet:?xt=urn:btih:0b31508aeb0634b347b8270c7bee4d411b5d4109&dn=agpl-3.0.txt AGPL-3.0
 /* global */
 /*eslint no-undef: "error"*/
 
@@ -16,3 +17,7 @@
 * the db and the instances need to be connected to these section_tipo
 */
 export const section_tool = section
+
+
+
+// @license-end
