c0b4aa449e7423140618e21f1b7770431fd9d42d
Paco <paco@render.es>
2023-06-02 12:08:48 +0200

    Added component_geolocation load JS lib license preview

--- core/component_geolocation/js/component_geolocation.js
+++ core/component_geolocation/js/component_geolocation.js
@@ -123,25 +123,60 @@ component_geolocation.prototype.init = async function(options) {
 		// load dependencies js/css
 			const load_promises = []
 
+			const license = null
+				// `
+				// /**
+				//  *
+				//  * @source: http://www.lduros.net/some-javascript-source.js
+				//  *
+				//  * @licstart  The following is the entire license notice for the
+				//  *  JavaScript code in this page.
+				//  *
+				//  * Copyright (C) 2014  Loic J. Duros
+				//  *
+				//  *
+				//  * The JavaScript code in this page is free software: you can
+				//  * redistribute it and/or modify it under the terms of the GNU
+				//  * General Public License (GNU GPL) as published by the Free Software
+				//  * Foundation, either version 3 of the License, or (at your option)
+				//  * any later version.  The code is distributed WITHOUT ANY WARRANTY;
+				//  * without even the implied warranty of MERCHANTABILITY or FITNESS
+				//  * FOR A PARTICULAR PURPOSE.  See the GNU GPL for more details.
+				//  *
+				//  * As additional permission under GNU GPL version 3 section 7, you
+				//  * may distribute non-source (e.g., minimized or compacted) forms of
+				//  * that code without the copy of the GNU GPL normally required by
+				//  * section 4, provided you include this license notice and a URL
+				//  * through which recipients can access the Corresponding Source.
+				//  *
+				//  * @licend  The above is the entire license notice
+				//  * for the JavaScript code in this page.
+				//  *
+				//  */
+				//  `
+
 			// leaflet. (!) It's necessary to be loaded fully before 'geoman'
 				const lib_js_file = DEDALO_ROOT_WEB + '/lib/leaflet/dist/leaflet.js'
-				await common.prototype.load_script(lib_js_file)
+				await common.prototype.load_script(
+					lib_js_file,
+					license
+				)
 
 			// another loads in parallel
 				const lib_css_file = DEDALO_ROOT_WEB + '/lib/leaflet/dist/leaflet.css'
 				load_promises.push( common.prototype.load_style(lib_css_file) )
 
 				const geo_editor_lib_js_file = DEDALO_ROOT_WEB + '/lib/leaflet/dist/leaflet-geoman/leaflet-geoman.min.js'
-				load_promises.push( common.prototype.load_script(geo_editor_lib_js_file) )
+				load_promises.push( common.prototype.load_script(geo_editor_lib_js_file, license) )
 
 				const geo_editor_lib_css_file = DEDALO_ROOT_WEB + '/lib/leaflet/dist/leaflet-geoman/leaflet-geoman.css'
 				load_promises.push( common.prototype.load_style(geo_editor_lib_css_file) )
 
 				const geo_messure_lib_js_file = DEDALO_ROOT_WEB + '/lib/leaflet/dist/turf/turf.min.js'
-				load_promises.push( common.prototype.load_script(geo_messure_lib_js_file) )
+				load_promises.push( common.prototype.load_script(geo_messure_lib_js_file, license) )
 
 				const color_picker_lib_js_file = DEDALO_ROOT_WEB + '/lib/iro/dist/iro.min.js'
-				load_promises.push( common.prototype.load_script(color_picker_lib_js_file) )
+				load_promises.push( common.prototype.load_script(color_picker_lib_js_file, license) )
 
 			// load and set JSON langs file
 				load_promises.push(
