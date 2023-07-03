e5ab27ffc1869c34ef4c936dc8e766ba59cfde79
Alejandro Peña Carbonell <alex@render.es>
2023-07-03 21:21:04 +0200

    Added conform_import_data to component_imput_text

--- core/component_input_text/class.component_input_text.php
+++ core/component_input_text/class.component_input_text.php
@@ -602,4 +602,60 @@ class component_input_text extends component_common {
 
 
 
+	/**
+	* CONFORM_IMPORT_DATA
+	* @param string $import_value
+	* @param string $column_name
+	* @return object $response
+	*/
+	public function conform_import_data(string $import_value, string $column_name) : object {
+
+		// Response
+		$response = new stdClass();
+			$response->result	= null;
+			$response->errors 	= [];
+			$response->msg		= 'Error. Request failed';
+
+		// Check if is a JSON string. Is yes, decode
+			if(json_handler::is_json($import_value)){
+				// try to JSON decode (null on not decode)
+				$dato_from_json = json_handler::decode($import_value); // , false, 512, JSON_INVALID_UTF8_SUBSTITUTE
+				$value = $dato_from_json;
+			}else{
+
+				// check the begin and end of the value string, if it has a [] or other combination that seems array
+				// sometimes the value text could be [Ac], as numismatic legends, it's admit, but if the text has [" or "] it's not admitted.
+				$begins_one	= substr($import_value, 0, 1);
+				$ends_one	= substr($import_value, -1);
+				$begins_two	= substr($import_value, 0, 2);
+				$ends_two	= substr($import_value, -2);
+
+				if (($begins_two !== '["' && $ends_two !== '"]') ||
+					($begins_two !== '["' && $ends_one !== ']') ||
+					($begins_one !== '[' && $ends_two !== '"]')
+					){
+					$value = empty($import_value)
+						? null
+						: [$import_value];
+				}else{
+					// log JSON conversion error
+					debug_log(__METHOD__." json_last_error: ".json_last_error(), logger::ERROR);
+
+					$failed = new stdClass();
+						$failed->section_id		= $this->section_id;
+						$failed->data			= stripslashes( $import_value );
+						$failed->component_tipo	= $this->get_tipo();
+						$failed->msg			= 'IGNORED: malformed data '. to_string($import_value);
+					$response->errors[] = $failed;
+
+					return $response;
+				}
+			}
+
+		$response->result	= $value;
+		$response->msg		= 'ok.';
+
+		return $response;
+	}//end conform_import_data
+
 }//end class component_input_text
