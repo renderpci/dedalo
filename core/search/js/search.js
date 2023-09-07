ac0796e5b1a8bf21eb3b3e5acf78a33becf88d98
Alejandro Peña Carbonell <alex@render.es>
2023-09-07 21:48:30 +0200

    Refactored the is_filter_empty function with recursive check of multiple operators in search

--- core/search/js/search.js
+++ core/search/js/search.js
@@ -1088,19 +1088,62 @@ search.prototype.get_panels_status = async function() {
 
 
 /**
-* FILTER_IS_EMPTY
+* IS_FILTER_EMPTY
 * Check if filter is empty
 * @param object filter_obj
 * @return bool is_empty
 */
-search.prototype.filter_is_empty = function(filter_obj) {
+export const is_filter_empty = function(filter_obj) {
 
-	const first_property	= filter_obj[Object.keys(filter_obj)[0]]
-	const is_empty			= (first_property.length<1) ? true : false
+	// Recursive function to get every filter state
+	 const check_deep_filter = (filter_obj) => {
+	 	// store if q filters are empty (true) or not (false)
+	 	const ar_empty = []
+	 	// get the operator key ($and, $or)
+	 	const ar_operators = Object.keys(filter_obj)
+	 	const operators_length = ar_operators.length
+	 	for (let i = operators_length - 1; i >= 0; i--) {
+	 		// get current filter with the operator
+	 		const current_operator 	= ar_operators[i]
+	 		const filter			= filter_obj[current_operator]
 
+	 		// check if the filter is empty
+	 		const is_empty_current_filter = (filter.length<1) ? true : false
+			if(is_empty_current_filter === false){
 
-	return is_empty
-}//end filter_is_empty
+				// check if the current filter has q
+				// of the filter has q, check if is empty
+				// else the filter has an operator ($and, $or) and set null q
+				const is_empty_q = filter.q
+					? filter.q.length<1
+					: null
+
+				// if the filter has an operator, recursion to get next level
+				if(is_empty_q === null){
+					const result = check_deep_filter(filter)
+
+					// store the result of the recursion
+					ar_empty.push(...result)
+
+				}else{
+					// store the state of the q
+					ar_empty.push(is_empty_q)
+				}
+			}
+		}
+		// return the states
+		return ar_empty
+	}
+
+	// check if the filter has any q with data
+	const result_check = check_deep_filter(filter_obj)
+	// check if any q is not empty (false)
+	const find_if_any_has_filter = result_check.find(el => el === false)
+	// if any q has a value to search return false in any other case return true
+	const empty = find_if_any_has_filter===false ? false : true
+
+	return empty
+}//end is_filter_empty
 
 
 
