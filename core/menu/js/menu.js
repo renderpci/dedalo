d06a84e6d4dd465acf5cd1418a25afc542317958
Alejandro Peña Carbonell <alex@render.es>
2023-10-05 17:51:35 +0200

    Fixed comment in menu.js

--- core/menu/js/menu.js
+++ core/menu/js/menu.js
@@ -155,7 +155,7 @@ menu.prototype.build = async function(autoload=true) {
 			console.log(`__Time to build ${self.model} [autoload:${autoload}] ms:`, performance.now()-t0);
 		}
 
-	// fix menu instance as global to be available
+	// set menu instance as global to be available
 		window.menu = self
 
 	// status update
