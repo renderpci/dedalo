d84093e3bffb7a985fb82d41fbc990de8cbbbaad
Alejandro Peña Carbonell <alex@render.es>
2023-11-05 10:58:27 +0100

    chore: remove old code

--- tools/tool_propagate_component_data/class.tool_propagate_component_data.php
+++ tools/tool_propagate_component_data/class.tool_propagate_component_data.php
@@ -12,53 +12,26 @@ class tool_propagate_component_data extends tool_common {
 	* PROPAGATE_COMPONENT_DATA
 	* Exec a custom action called from client
 	* Note that tool config is stored in the tool section data (tools_register)
-	* @param object $request_options
+	* @param object $options
 	* @return object $response
 	*/
-	public static function propagate_component_data(object $request_options) : object {
+	public static function propagate_component_data( object $options ) : object {
 
 		$response = new stdClass();
 			$response->result	= false;
 			$response->msg		= 'Error. Request failed ['.__FUNCTION__.']';
 
 		// options
-			$options = new stdClass();
-				$options->section_tipo			= null;
-				$options->section_id			= null;
-				$options->component_tipo		= null;
-				$options->action				= null; // add | delete
-				$options->lang					= null;
-				$options->propagate_data_value	= null;
-				foreach ($request_options as $key => $value) {if (property_exists($options, $key)) $options->$key = $value;}
-
-		// short vars
 			$section_tipo			= $options->section_tipo;
 			$section_id				= $options->section_id;
 			$component_tipo			= $options->component_tipo;
 			$action					= $options->action;
 			$lang					= $options->lang;
 			$propagate_data_value	= $options->propagate_data_value;
+
+		// short vars
 			$model			= RecordObj_dd::get_modelo_name_by_tipo($component_tipo,true);
 			$with_relations	= in_array($model, component_relation_common::get_components_with_relations());
-
-
-		// source component
-			// $source_component = component_common::get_instance(
-			// 	$model,
-			// 	$component_tipo,
-			// 	$section_id,
-			// 	'list',
-			// 	$lang,
-			// 	$section_tipo
-			// );
-			// $source_dato = $source_component->get_dato();
-			// if (empty($propagate_data_value)) {
-			// 	$response->msg .= ' Empty dato! Ignored action '.$action;
-			// 	return $response;
-			// }
-		// Disable logging activity and time machine # !IMPORTANT
-			// logger_backend_activity::$enable_log = false;
-			// RecordObj_time_machine::$save_time_machine_version = false;
 
 		// RECORDS. Use actual list search options as base to build current search
 			$sqo_id	= implode('_', ['section', $section_tipo]); // cache key sqo_id
@@ -80,10 +53,6 @@ class tool_propagate_component_data extends tool_common {
 
 				// section_id
 					$current_section_id	= $row->section_id;
-					// if ($current_section_id==$section_id) {
-					// 	// skip self component
-					// 	continue;
-					// }
 
 				// current component
 					$current_component	= component_common::get_instance(
@@ -105,7 +74,6 @@ class tool_propagate_component_data extends tool_common {
 
 							$final_dato = $propagate_data_value;
 							$save = true;
-
 							break;
 
 						case 'delete':
@@ -137,7 +105,6 @@ class tool_propagate_component_data extends tool_common {
 							$save = ($final_dato!==$current_dato)
 								? true
 								: false;
-
 							break;
 					}
 
@@ -148,12 +115,7 @@ class tool_propagate_component_data extends tool_common {
 
 						debug_log(__METHOD__." Updated dato of $section_tipo - $current_section_id - $component_tipo ".to_string(), logger::DEBUG);
 					}
-
 			}//end foreach ($records_data->result as $key => $ar_value)
-
-		// Enable logging activity and time machine # !IMPORTANT
-			// logger_backend_activity::$enable_log = true;
-			// RecordObj_time_machine::$save_time_machine_version = true;
 
 		// response
 			$response->result			= true;
@@ -161,7 +123,6 @@ class tool_propagate_component_data extends tool_common {
 			$response->action			= $action;
 			$response->section_label	= RecordObj_dd::get_termino_by_tipo($section_tipo);
 			$response->count			= count($rows_data->ar_records);
-
 
 		return $response;
 	}//end propagate_component_data
