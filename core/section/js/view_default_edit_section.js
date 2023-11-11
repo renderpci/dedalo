e6268ed999df6d43e8d3f9d19dc38ed05ed3758c
Paco <paco@render.es>
2023-11-11 18:12:03 +0100

    fix: add section wrapper 'mousedown' listener to allow component de-section on click in modal windows

--- core/section/js/view_default_edit_section.js
+++ core/section/js/view_default_edit_section.js
@@ -7,6 +7,7 @@
 // imports
 	import {get_section_records} from '../../section/js/section.js'
 	import {set_element_css} from '../../page/js/css.js'
+	import {page} from '../../page/js/page.js'
 	import {ui} from '../../common/js/ui.js'
 
 
@@ -133,6 +134,9 @@ view_default_edit_section.render = async function(self, options) {
 					}
 				}
 		}
+
+	// mousedown
+		wrapper.addEventListener('mousedown', page.prototype.deactivate_components)
 
 
 	return wrapper
