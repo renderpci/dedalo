51f24f03e6d965f2ae9e9babe97abdfabec8d763
Paco <paco@render.es>
2023-11-24 10:58:46 +0100

    chore: code clean up

--- core/component_common/js/component_common.js
+++ core/component_common/js/component_common.js
@@ -17,6 +17,7 @@
 	import {set_element_css} from '../../page/js/css.js'
 
 
+
 export const component_common = function(){
 
 	return true
@@ -439,7 +440,7 @@ component_common.prototype.save = async function(new_changed_data) {
 			}
 		}
 
-	// remove previous success/error CSS class if exists
+	// remove the previous success/error CSS class if it exists
 		if (self.node) {
 			if (self.node.classList.contains('error')) {
 				self.node.classList.remove('error')
@@ -790,7 +791,7 @@ component_common.prototype.update_datum = async function(new_datum) {
 
 
 		// check data
-			if (typeof self.data==="undefined") {
+			if (typeof self.data==='undefined') {
 				if(SHOW_DEBUG===true) {
 					console.trace();
 					console.warn("++++++++++++++++++++ self.datum:",self.datum);
