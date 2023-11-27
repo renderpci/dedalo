b41a71059d1f80c54fa0895870066d1af0292933
Paco <paco@render.es>
2023-11-27 11:25:06 +0100

    fix: fix the problem of formulas when summarizing given empty values (widget calculation)

--- core/widgets/calculation/formulas.php
+++ core/widgets/calculation/formulas.php
@@ -1,17 +1,26 @@
 <?php
 /**
-* Formulas used by Calculation
+* Formulas used by the Calculation widget
 */
+
+
 
 /**
 * SUMMARIZE
+* Collect data given (array of values) and summarize values
+* in the number format defined as type
 * @param object $request_options
 * 	{
 * 		data: object with var and value as { "number": 23.5}
 * 		options: object with some other params
 * 	}
+* @return array $result
+* {
+* 	id : total,
+* 	value: 65
+* }
 */
-function summarize($request_options) : array {
+function summarize( string|object $request_options) : array {
 
 	$options = is_string($request_options)
 		? json_decode($request_options)
@@ -20,8 +29,20 @@ function summarize($request_options) : array {
 	$data   = $options->data;
 	$opt    = $options->options;
 
+	// sample data:
+		// {
+		//     "au": "",
+		//     "ag": "",
+		//     "cu": "",
+		//     "pb": "",
+		//     "sn": ""
+		// }
+
 	$ar_values = [];
 	foreach ($data as $key => $value) {
+		if (empty($value)) {
+			continue;
+		}
 		$ar_values[] = $value;
 	}
 	$total = array_sum($ar_values);
@@ -42,9 +63,10 @@ function summarize($request_options) : array {
 	}
 
 	$result[] = (object)[
-		'id' => 'total',
-		'value' => $total,
+		'id'	=> 'total',
+		'value'	=> $total
 	];
 
+
 	return $result;
-}
+}//end summarize
