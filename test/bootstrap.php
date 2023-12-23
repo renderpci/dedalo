d5d59d736345b9416a016783ce554d30bb54c7e2
Paco <paco@render.es>
2023-12-23 20:27:22 +0100

    chore: add PHPUnitUtil class to call private methods in test unit

--- test/bootstrap.php
+++ test/bootstrap.php
@@ -25,3 +25,15 @@ define('TEST_USER_ID', 1); // DEDALO_SUPERUSER
 	require_once 'components/data.php';
 	require_once 'components/elements.php';
 	require_once dirname(__FILE__) . '/login/login_Test.php';
+
+
+// PHPUnitUtil reflection
+	class PHPUnitUtil
+	{
+		public static function callMethod($obj, $name, array $args) {
+			$class = new \ReflectionClass($obj);
+			$method = $class->getMethod($name);
+			// $method->setAccessible(true); // Use this if you are running PHP older than 8.1.0
+			return $method->invokeArgs($obj, $args);
+		}
+	}
