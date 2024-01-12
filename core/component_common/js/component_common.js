9601fcdd91019079f02838315ab4c7ade08bea2d
Paco <paco@render.es>
2024-01-12 21:54:25 +0100

    fix: fix component_common.update_data_value issue where empty self.data.value fires an error
    detected in component number (hierarchy48)

--- core/component_common/js/component_common.js
+++ core/component_common/js/component_common.js
@@ -862,7 +862,7 @@ component_common.prototype.update_data_value = function(changed_data_item) {
 				// delete all values
 				self.data.value = []
 		}else{
-			if (changed_value===null) {
+			if (changed_value===null && self.data.value) {
 				// delete current value key from array
 				self.data.value.splice(data_key,1)
 			}else{
