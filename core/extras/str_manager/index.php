3c5653ec7d7489e5de89616da711535dc4807927
Paco <paco@render.es>
2024-02-27 16:07:22 +0100

    chore: add version path control to str_manager (used by master to get requested files)

--- core/extras/str_manager/index.php
+++ core/extras/str_manager/index.php
@@ -12,9 +12,16 @@ session_write_close();
 // DATA . The only one var received is a JSON encoded var called "data"
 	$data = json_decode($_REQUEST['data']);
 
+	// debug
+		error_log('Update Ontology request data: ' . PHP_EOL . to_string($data));
+
 // CODE auth. Check valid code match, received with config defined STRUCTURE_SERVER_CODE
 // If not is the same, return error code 401 and exit
-	if (!$data || $data->code!==STRUCTURE_SERVER_CODE) {
+	$valid_codes = defined('STRUCTURE_SERVER_CODE_OTHERS')
+		? STRUCTURE_SERVER_CODE_OTHERS
+		: [];
+	$valid_codes[] = STRUCTURE_SERVER_CODE;	 // add main code
+	if (!$data || !in_array($data->code, $valid_codes)) {
 		http_response_code(401); // Unauthorized
 		exit();
 	}
@@ -35,16 +42,46 @@ session_write_close();
 		}
 	}
 	if (is_null($selected_obj)) {
+		trigger_error('Invalid selected_obj');
 		http_response_code(400); // Bad request
 		exit();
 	}
 
+// version path
+	$dedalo_version_string	= $data->dedalo_version ?? '';
+	$dedalo_version_array	= explode('.', $dedalo_version_string);
+
+	if (
+		(isset($dedalo_version_array[0]) && (int)$dedalo_version_array[0]=='6' &&
+		 isset($dedalo_version_array[1]) && (int)$dedalo_version_array[1]=='0')
+		) {
+
+		// 6.0 case. Use legacy freeze version copy of Ontology
+		$version_path = '/6.0';
+
+	}else{
+
+		// Others case. Use default active version path
+		$version_path = '';
+	}
+	error_log('Update Ontology version_path: ' . PHP_EOL . to_string($version_path));
+
 // file info
 	$file_name 	= $selected_obj->name;
-	$file_path 	= $selected_obj->path .'/'. $selected_obj->name;
+	$file_path 	= str_replace('/str_data', $version_path . '/str_data', $selected_obj->path) .'/'. $selected_obj->name;
+	// debug
+		error_log('file_path: ' . $file_path);
+
+// check file
+	$file_found = file_exists($file_path);
+	if (!$file_found) {
+		debug_log(__METHOD__." Trying to get structure from a non-existing file: ".to_string($file_path), logger::ERROR);
+	}
 
 // file size in bytes
-	$fsize = filesize($file_path);
+	$fsize = ($file_found)
+		? filesize($file_path)
+		: 0;
 
 // set headers
 	header("Pragma: public");
