78aee77f8d41a9b17e612d9606a5e70a897b7a91
Paco <paco@render.es>
2024-03-10 20:10:01 +0100

    chore: sample config : add session_start_manager->save_path = '/tmp' as default value to avoid de-synchronization between php-fpm and CLI session path
    Tested in RockyLinux and Ubuntu

--- config/sample.config.php
+++ config/sample.config.php
@@ -162,7 +162,7 @@
 		session_start_manager([
 			'save_handler'				=> 'files',
 			'timeout_seconds'			=> $timeout_seconds,
-			// 'save_path'				=> '/tmp', string optional
+			'save_path'					=> '/tmp', // string optional
 			// 'additional_save_path'	=> false, bool optional
 			'prevent_session_lock'		=> defined('PREVENT_SESSION_LOCK') ? PREVENT_SESSION_LOCK : false,
 			'session_name'				=> 'dedalo_'.DEDALO_ENTITY,
