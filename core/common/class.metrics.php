c7fec7e972b64aff7280d8911bd5f2fba47e7d49
Paco <paco@render.es>
2024-03-21 15:57:27 +0100

    feat: add debug metrics class to centralize performance info like permissions calculation, etc.

---
+++ core/common/class.metrics.php
@@ -0,0 +1,19 @@
+<?php
+declare(strict_types=1);
+/**
+* METRICS CLASS
+* Class to centralize performance info like permissions calculation, etc.
+*/
+final class metrics {
+
+
+	// permissions. Time to calculate user permissions
+		static $security_permissions_table_time = 0;
+		static $security_permissions_table_count = 0;
+		static $security_permissions_total_time = 0;
+		static $security_permissions_total_calls = 0;
+
+
+
+
+}//end class metrics
