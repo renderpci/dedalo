e5a0446a498082e00990c8e8adadc5ccefc5b2d2
Paco <paco@render.es>
2024-04-01 21:27:28 +0200

    refactor: re-organizes the SSE stream rendering flow for ease of use

--- core/area_maintenance/js/widgets/make_backup/render_make_backup.js
+++ core/area_maintenance/js/widgets/make_backup/render_make_backup.js
@@ -7,6 +7,7 @@
 // imports
 	import {ui} from '../../../../common/js/ui.js'
 	import {data_manager} from '../../../../common/js/data_manager.js'
+	import {render_stream} from '../../../../common/js/render_common.js'
 	// import {object_to_url_vars} from '../../../../common/js/utils/index.js'
 
 
@@ -108,44 +109,53 @@ const get_content_data = async function(self) {
 
 	// update_process_status
 		const update_process_status = (pid, pfile) => {
+
+			// locks the button submit
 			button_submit.classList.add('loading')
-			ui.render_process_status({
-				id				: 'process_make_backup', // used to store DB local data status
-				pid				: pid,
-				pfile			: pfile,
-				container		: body_response,
-				callback_done	: () => {
-					button_submit.classList.remove('loading')
-				},
-				callback_read : () => {
-					// get files list updated
-					data_manager.request({
-						use_worker	: true,
-						body		: {
-							dd_api	: 'dd_area_maintenance_api',
-							action	: 'class_request',
-							source	: {
-								action : 'get_dedalo_backup_files'
-							},
-							options	: {
-								max_files			: 1,
-								psql_backup_files	: true,
-								mysql_backup_files	: false
-							}
-						}
-					})
-					.then(function(response){
-						// display_json_box.innerHTML += '\n' + JSON.stringify(response.result, null, 2)
-						const backup_files_info = document.getElementById('backup_files_info') || ui.create_dom_element({
-							element_type	: 'pre',
-							id				: 'backup_files_info',
-							class_name		: 'backup_files_info',
-							parent			: body_response
-						})
-						const msg = response?.result?.psql_backup_files[0]
-						backup_files_info.innerHTML = JSON.stringify(msg, null, 2)
-					})
+
+			// get_process_status from API and returns a SEE stream
+			data_manager.request_stream({
+				body : {
+					dd_api		: 'dd_utils_api',
+					action		: 'get_process_status',
+					update_rate	: 1000, // int milliseconds
+					options		: {
+						pid		: pid,
+						pfile	: pfile
+					}
 				}
+			})
+			.then(function(stream){
+
+				// render base nodes and set functions to manage
+				// the stream reader events
+				const render_response = render_stream({
+					container		: body_response,
+					id				: 'process_make_backup',
+					pid				: pid,
+					pfile			: pfile
+				})
+
+				// on_read event (called on every chunk from stream reader)
+				const on_read = (sse_response) => {
+					// fire update_stream on every reader read chunk
+					render_response.update_stream(sse_response)
+					// get files list updated with last file only
+					update_last_file_info()
+				}
+
+				// on_done event (called once at finish or cancel the stream read)
+				const on_done = () => {
+					// is triggered at the reader's closing
+					render_response.done()
+					// unlocks the button submit
+					button_submit.classList.remove('loading')
+				}
+
+				// read stream. Creates ReadableStream that fire
+				// 'on_read' function on each stream chunk at update_rate
+				// (1 second default) until stream is done (PID is no longer running)
+				data_manager.read_stream(stream, on_read, on_done)
 			})
 		}
 
@@ -166,6 +176,35 @@ const get_content_data = async function(self) {
 		}
 		check_process_data()
 
+	// update_last_file_info. CAll API to get updated info about the last updated file
+		const update_last_file_info = () => {
+			data_manager.request({
+				use_worker	: true,
+				body		: {
+					dd_api	: 'dd_area_maintenance_api',
+					action	: 'class_request',
+					source	: {
+						action : 'get_dedalo_backup_files'
+					},
+					options	: {
+						max_files			: 1,
+						psql_backup_files	: true,
+						mysql_backup_files	: false
+					}
+				}
+			})
+			.then(function(response){
+				// backup_files_info node created once
+				const backup_files_info = document.getElementById('backup_files_info') || ui.create_dom_element({
+					element_type	: 'pre',
+					id				: 'backup_files_info',
+					class_name		: 'backup_files_info',
+					parent			: body_response
+				})
+				const msg = response?.result?.psql_backup_files[0]
+				backup_files_info.innerHTML = JSON.stringify(msg, null, 2)
+			})
+		}
 
 	// fn_submit
 		const fn_submit = async (e) => {
