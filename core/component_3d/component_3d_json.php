96e9242f2523ca3c8087b430c4d49977f1b2366d
Alejandro Peña Carbonell <alex@render.es>
2024-04-14 10:36:52 +0200

    change: component_3d return the correct posterframe, calling to the correct method, in his controller

--- core/component_3d/component_3d_json.php
+++ core/component_3d/component_3d_json.php
@@ -65,14 +65,9 @@
 			$item = $this->get_data_item($value);
 
 			// posterframe_url
-			// @todo: include posterframe_url as thumb quality info value
-				$item->posterframe_url = $this->get_thumb_url();
-
-			// model_url. Default quality video URL (usually from 404)
-				// $default_quality = $this->get_default_quality();
-				// $item->model_url = $this->quality_file_exist($default_quality)
-				// 	? $this->get_url(false)
-				// 	: null;
+				$item->posterframe_url = empty($value)
+					? null
+					: $this->get_posterframe_url();
 
 		// player mode case. Send the media header when the component are working as player
 			if($mode==='edit') {
