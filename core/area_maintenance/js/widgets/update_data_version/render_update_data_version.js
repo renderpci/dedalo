75b856d5d7d241cd98a005c953eb6a173b44205a
Paco <paco@render.es>
2024-04-16 10:26:19 +0200

    change: move area_maintenence function update_data_version from API to class to allow fire it as process

--- core/area_maintenance/js/widgets/update_data_version/render_update_data_version.js
+++ core/area_maintenance/js/widgets/update_data_version/render_update_data_version.js
@@ -6,7 +6,8 @@
 
 // imports
 	import {ui} from '../../../../common/js/ui.js'
-	// import {object_to_url_vars} from '../../../../common/js/utils/index.js'
+	import {render_stream} from '../../../../common/js/render_common.js'
+	import {data_manager} from '../../../../common/js/data_manager.js'
 
 
 
@@ -67,11 +68,15 @@ render_update_data_version.prototype.list = async function(options) {
 const get_content_data = async function(self) {
 
 	// short vars
-		const value					= self.value || {}
-		const update_version		= value.update_version
-		const current_version_in_db	= value.current_version_in_db
-		const dedalo_version		= value.dedalo_version
-		const updates				= value.updates
+		const value						= self.value || {}
+		const update_version			= value.update_version
+		const current_version_in_db		= value.current_version_in_db
+		const dedalo_version			= value.dedalo_version
+		const updates					= value.updates
+		const process_id				= 'process_update_data_version'
+
+	// maintenance_mode from environment
+		const maintenance_mode = page_globals.maintenance_mode
 
 	// content_data
 		const content_data = ui.create_dom_element({
@@ -169,17 +174,130 @@ const get_content_data = async function(self) {
 		})
 
 	// form init
-		self.caller.init_form({
-			submit_label	: self.name,
-			confirm_text	: get_label.sure || 'Sure?',
-			body_info		: content_data,
-			body_response	: body_response,
-			trigger : {
-				dd_api	: 'dd_area_maintenance_api',
-				action	: 'update_data_version',
-				options	: null
-			}
-		})
+		switch (true) {
+			case (page_globals.is_root!==true):
+				// message not allowed
+				ui.create_dom_element({
+					element_type	: 'div',
+					class_name		: 'warning',
+					inner_html		: 'Only root user can do this action',
+					parent			: body_response
+				})
+				break;
+			case (maintenance_mode===true):
+				// message not allowed
+				ui.create_dom_element({
+					element_type	: 'div',
+					class_name		: 'warning',
+					inner_html		: 'Update data is not allowed if Dédalo is not in maintenance_mode',
+					parent			: body_response
+				})
+				break;
+			default:
+				// create the submit button
+				self.caller.init_form({
+					submit_label	: self.name,
+					confirm_text	: get_label.sure || 'Sure?',
+					body_info		: content_data,
+					body_response	: body_response,
+					on_submit	: () => {
+						// update_data_version
+						update_data_version()
+						.then(function(response){
+							update_process_status(
+								response.pid,
+								response.pfile,
+								body_response
+							)
+						})
+					}
+				})
+				break;
+		}
+
+	// update_data_version
+		const update_data_version = async () => {
+
+			// update_data_version process fire
+			const response = await data_manager.request({
+				body		: {
+					dd_api	: 'dd_area_maintenance_api',
+					action	: 'class_request',
+					source	: {
+						action	: 'update_data_version',
+					},
+					options : {
+						background_running	: true // set run in background CLI
+					}
+				}
+			})
+
+			return response
+		}//end update_data_version
+
+	// update_process_status
+		const update_process_status = function(pid, pfile, container) {
+
+			// get_process_status from API and returns a SEE stream
+				data_manager.request_stream({
+					body : {
+						dd_api		: 'dd_utils_api',
+						action		: 'get_process_status',
+						update_rate	: 1000, // int milliseconds
+						options		: {
+							pid		: pid,
+							pfile	: pfile
+						}
+					}
+				})
+				.then(function(stream){
+
+					// render base nodes and set functions to manage
+					// the stream reader events
+					const render_stream_response = render_stream({
+						container		: container,
+						id				: process_id,
+						pid				: pid,
+						pfile			: pfile,
+						display_json	: true
+					})
+
+					// on_read event (called on every chunk from stream reader)
+					const on_read = (sse_response) => {
+						// fire update_info_node on every reader read chunk
+						render_stream_response.update_info_node(sse_response)
+					}
+
+					// on_done event (called once at finish or cancel the stream read)
+					const on_done = () => {
+						// is triggered at the reader's closing
+						render_stream_response.done()
+					}
+
+					// read stream. Creates ReadableStream that fire
+					// 'on_read' function on each stream chunk at update_rate
+					// (1 second default) until stream is done (PID is no longer running)
+					data_manager.read_stream(stream, on_read, on_done)
+				})
+		}//end update_process_status
+
+		// check process status always
+		const check_process_data = () => {
+			data_manager.get_local_db_data(
+				process_id,
+				'status'
+			)
+			.then(function(local_data){
+				if (local_data && local_data.value) {
+					update_process_status(
+						local_data.value.pid,
+						local_data.value.pfile,
+						body_response
+					)
+				}
+			})
+		}
+		check_process_data()
 
 	// add at end body_response
 		content_data.appendChild(body_response)
