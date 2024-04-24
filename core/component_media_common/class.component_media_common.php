ce7be951219a3c5ec7573ad00757a97cb9d57af3
Alejandro Peña Carbonell <alex@render.es>
2024-04-24 18:07:43 +0200

    fix: check the original_upload_date data when the original_normalized_name can be recreated with database data.

--- core/component_media_common/class.component_media_common.php
+++ core/component_media_common/class.component_media_common.php
@@ -2029,6 +2029,18 @@ class component_media_common extends component_common {
 						if ( !isset($dato[0]->original_normalized_name) ) {
 							$dato[0]->original_normalized_name = $this->get_id() .'.'. get_file_extension($filename_dato[0]);
 						}
+
+						// original_upload_date
+						if (!isset($dato[0]->original_upload_date)) {
+
+							$file_path = $this->get_media_path_dir( $this->get_original_quality() ) .'/'. $dato[0]->original_normalized_name;
+							if (file_exists($file_path)) {
+								$modification_time				= filectime($file_path);
+								$dato[0]->original_upload_date	= !empty($modification_time)
+									? dd_date::get_dd_date_from_unix_timestamp($modification_time)
+									: null;
+							}
+						}
 					}
 
 					// replace existing dato
@@ -2052,7 +2064,6 @@ class component_media_common extends component_common {
 					if (!isset($dato[0]->original_upload_date)) {
 
 						$file_path = $this->get_media_path_dir($original_quality) .'/'. $original_normalized_name;
-
 						if (file_exists($file_path)) {
 							$modification_time				= filectime($file_path);
 							$dato[0]->original_upload_date	= !empty($modification_time)
