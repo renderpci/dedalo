8342e41ff7f0305d05be3bfb51417d537407644d
Paco <paco@render.es>
2024-04-26 17:06:29 +0200

    fix: fix cache issue with tool_image_rotation image

--- tools/tool_image_rotation/js/render_tool_image_rotation.js
+++ tools/tool_image_rotation/js/render_tool_image_rotation.js
@@ -63,10 +63,7 @@ const get_content_data = async function(self) {
 
 	const default_file_info = self.main_element.get_default_file_info()
 
-
 	const fragment = new DocumentFragment()
-
-
 
 	// main_element_container
 		const main_element_container = ui.create_dom_element({
@@ -75,27 +72,27 @@ const get_content_data = async function(self) {
 			parent			: fragment
 		})
 
-			// image_container
+		// image_container
 			const image_container = ui.create_dom_element({
 				element_type	: 'div',
 				class_name		: 'image_container',
 				parent			: main_element_container
 			})
-					self.image_container = image_container
+			self.image_container = image_container
 
-				// temporal image to show while main_element is rebuilt and rendered
-				const main_element_image = ui.create_dom_element({
-					element_type	: 'img',
-					src				: DEDALO_MEDIA_URL + default_file_info.file_path,
-					parent			: image_container
-				})
-				self.main_element_image = main_element_image
+			// temporal image to show while main_element is rebuilt and rendered
+			const main_element_image = ui.create_dom_element({
+				element_type	: 'img',
+				src				: DEDALO_MEDIA_URL + default_file_info.file_path + '?t=' + (new Date()).getTime(),
+				parent			: image_container
+			})
+			self.main_element_image = main_element_image
 
-				main_element_image.onload = () => {
-					const image_size	= main_element_image.getBoundingClientRect()
-					image_container.style.width	= image_size.width +'px'
-					image_container.style.height	= image_size.height +'px'
-				}
+			main_element_image.onload = () => {
+				const image_size	= main_element_image.getBoundingClientRect()
+				image_container.style.width	= image_size.width +'px'
+				image_container.style.height	= image_size.height +'px'
+			}
 
 		const axis_container = ui.create_dom_element({
 			element_type	: 'div',
