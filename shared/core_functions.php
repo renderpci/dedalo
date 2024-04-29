e5b26ef08ef92f16f2c1b06913a7f725f73fbe85
Paco <paco@render.es>
2024-04-29 09:21:05 +0200

    change: improve core function 'is_serialized' check

--- shared/core_functions.php
+++ shared/core_functions.php
@@ -919,12 +919,67 @@ function dedalo_decrypt_openssl(string $string_value, string $key=DEDALO_INFORMA
 
 /**
 * IS_SERIALIZED
-* Check if given string is serialized
-* @return bool
-*/
-function is_serialized(string $str) : bool {
-
-	return ($str == serialize(false) || @unserialize($str)!==false);
+* Check value to find if it was serialized.
+* @param string $data   Value to check to see if was serialized.
+* @param bool $strict Optional. Whether to be strict about the end of the string. Default true.
+* @return bool False if not serialized and true if it was.
+ */
+function is_serialized( $data, $strict = true ) {
+	// If it isn't a string, it isn't serialized.
+	if ( ! is_string( $data ) ) {
+		return false;
+	}
+	$data = trim( $data );
+	if ( 'N;' === $data ) {
+		return true;
+	}
+	if ( strlen( $data ) < 4 ) {
+		return false;
+	}
+	if ( ':' !== $data[1] ) {
+		return false;
+	}
+	if ( $strict ) {
+		$lastc = substr( $data, -1 );
+		if ( ';' !== $lastc && '}' !== $lastc ) {
+			return false;
+		}
+	} else {
+		$semicolon = strpos( $data, ';' );
+		$brace     = strpos( $data, '}' );
+		// Either ; or } must exist.
+		if ( false === $semicolon && false === $brace ) {
+			return false;
+		}
+		// But neither must be in the first X characters.
+		if ( false !== $semicolon && $semicolon < 3 ) {
+			return false;
+		}
+		if ( false !== $brace && $brace < 4 ) {
+			return false;
+		}
+	}
+	$token = $data[0];
+	switch ( $token ) {
+		case 's':
+			if ( $strict ) {
+				if ( '"' !== substr( $data, -2, 1 ) ) {
+					return false;
+				}
+			} elseif ( false === strpos( $data, '"' ) ) {
+				return false;
+			}
+			// Or else fall through.
+		case 'a':
+		case 'O':
+			return (bool) preg_match( "/^{$token}:[0-9]+:/s", $data );
+		case 'b':
+		case 'i':
+		case 'd':
+			$end = $strict ? '$' : '';
+			return (bool) preg_match( "/^{$token}:[0-9.E+-]+;$end/", $data );
+	}
+	return false;
 }//end is_serialized
 
 
