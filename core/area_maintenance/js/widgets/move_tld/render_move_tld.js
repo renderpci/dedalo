5404410ae3c5a1392f984ae15c4e21abe9161bfc
Paco <paco@render.es>
2024-05-23 12:32:27 +0200

    feat: add area_maintenance widget 'move_tld'

---
+++ core/area_maintenance/js/widgets/move_tld/render_move_tld.js
@@ -0,0 +1,289 @@
+/*global get_label, page_globals, SHOW_DEBUG, DEDALO_CORE_URL, DEDALO_API_URL */
+/*eslint no-undef: "error"*/
+
+
+
+// imports
+	import {ui} from '../../../../common/js/ui.js'
+	import {render_stream} from '../../../../common/js/render_common.js'
+	import {data_manager} from '../../../../common/js/data_manager.js'
+
+
+
+/**
+* RENDER_MOVE_TLD
+* Manages the component's logic and appearance in client side
+*/
+export const render_move_tld = function() {
+
+	return true
+}//end render_move_tld
+
+
+
+/**
+* LIST
+* Creates the nodes of current widget.
+* The created wrapper will be append to the widget body in area_maintenance
+* @param object options
+* 	Sample:
+* 	{
+*		render_level : "full"
+		render_mode : "list"
+*   }
+* @return HTMLElement wrapper
+* 	To append to the widget body node (area_maintenance)
+*/
+render_move_tld.prototype.list = async function(options) {
+
+	const self = this
+
+	const render_level = options.render_level || 'full'
+
+	// content_data
+		const content_data = await get_content_data_edit(self)
+		if (render_level==='content') {
+			return content_data
+		}
+
+	// wrapper. ui build_edit returns widget wrapper
+		const wrapper = ui.widget.build_wrapper_edit(self, {
+			content_data : content_data
+		})
+		// set pointers
+		wrapper.content_data = content_data
+
+
+	return wrapper
+}//end list
+
+
+
+/**
+* GET_CONTENT_DATA_EDIT
+* @param object self
+* @return HTMLElement content_data
+*/
+const get_content_data_edit = async function(self) {
+
+	// short vars
+		const value			= self.value || {}
+		const body			= value.body
+		const files			= value.files || []
+		const process_id	= 'process_move_tld'
+
+	// content_data
+		const content_data = ui.create_dom_element({
+			element_type : 'div'
+		})
+
+	// info
+		const info = ui.create_dom_element({
+			element_type	: 'div',
+			class_name		: 'info_text',
+			inner_html		: body,
+			parent			: content_data
+		})
+
+	// files_list
+		const files_list = ui.create_dom_element({
+			element_type	: 'div',
+			class_name		: 'files_list',
+			parent			: content_data
+		})
+		let file_selected = null
+		const files_length = files.length
+		for (let i = 0; i < files_length; i++) {
+
+			const item = files[i]
+
+			// file_container
+			const file_container = ui.create_dom_element({
+				element_type	: 'div',
+				class_name		: 'file_container',
+				parent			: files_list
+			})
+
+			// label
+			const input_label = ui.create_dom_element({
+				element_type	: 'label',
+				class_name		: 'label',
+				inner_html		: item.file_name,
+				parent			: file_container
+			})
+
+			// input radio button
+			const input = ui.create_dom_element({
+				element_type	: 'input',
+				type			: 'radio',
+				value			: item.file_name,
+				name			: 'files_list'
+			})
+			input_label.prepend(input)
+			input.addEventListener('change', function(e) {
+				// reset selected style
+				[...files_list.querySelectorAll('.label')].map(el => {
+					el.classList.remove('selected')
+				})
+				// set as selected
+				if (input.checked) {
+					file_selected = item.file_name
+					input_label.classList.add('selected')
+				}
+			})
+
+			// show_file_content (arrow)
+			const show_file_content = ui.create_dom_element({
+				element_type	: 'div',
+				class_name		: 'show_file_content icon_arrow',
+				inner_html		: 'File contents',
+				parent			: file_container
+			})
+			// file_content_container
+			const file_content_container = ui.create_dom_element({
+				element_type	: 'pre',
+				class_name		: 'file_content_container hide',
+				inner_html		: JSON.stringify(item.content, null, 2),
+				parent			: file_container
+			})
+			// collapse file_content
+			ui.collapse_toggle_track({
+				toggler				: show_file_content,
+				container			: file_content_container,
+				collapsed_id		: 'collapsed_move_tld_file_'+item.file_name,
+				collapse_callback	: () => {
+					show_file_content.classList.remove('up')
+				},
+				expose_callback		: () => {
+					show_file_content.classList.add('up')
+				},
+				default_state : 'closed'
+			})
+		}
+
+
+	// body_response
+		const body_response = ui.create_dom_element({
+			element_type	: 'div',
+			class_name		: 'body_response'
+		})
+
+	// form init
+		self.caller.init_form({
+			submit_label	: 'Move TLD terms',
+			// confirm_text	: confirm_text,
+			body_info		: content_data,
+			body_response	: body_response,
+			on_submit	: (e, values) => {
+
+				if (!file_selected) {
+					alert("Error: no file is selected");
+					return
+				}
+
+				// move_tld
+				move_tld(file_selected)
+				.then(function(response){
+					update_process_status(
+						process_id,
+						response.pid,
+						response.pfile,
+						body_response
+					)
+				})
+			}
+		})
+
+	// move_tld
+		const move_tld = async (file_selected) => {
+
+			// move_tld process fire
+			const response = await data_manager.request({
+				body		: {
+					dd_api	: 'dd_area_maintenance_api',
+					action	: 'class_request',
+					source	: {
+						action	: 'move_tld',
+					},
+					options : {
+						background_running	: true, // set run in background CLI
+						file_selected		: file_selected // string e.g. 'finds_numisdata279_to_tchi1.json'
+					}
+				}
+			})
+
+			return response
+		}//end move_tld
+
+	// update_process_status
+		const update_process_status = function(id, pid, pfile, container) {
+
+			// get_process_status from API and returns a SEE stream
+				data_manager.request_stream({
+					body : {
+						dd_api		: 'dd_utils_api',
+						action		: 'get_process_status',
+						update_rate	: 1000, // int milliseconds
+						options		: {
+							pid		: pid,
+							pfile	: pfile
+						}
+					}
+				})
+				.then(function(stream){
+
+					// render base nodes and set functions to manage
+					// the stream reader events
+					const render_stream_response = render_stream({
+						container		: container,
+						id				: id,
+						pid				: pid,
+						pfile			: pfile,
+						display_json	: true
+					})
+
+					// on_read event (called on every chunk from stream reader)
+					const on_read = (sse_response) => {
+						// fire update_info_node on every reader read chunk
+						render_stream_response.update_info_node(sse_response)
+					}
+
+					// on_done event (called once at finish or cancel the stream read)
+					const on_done = () => {
+						// is triggered at the reader's closing
+						render_stream_response.done()
+					}
+
+					// read stream. Creates ReadableStream that fire
+					// 'on_read' function on each stream chunk at update_rate
+					// (1 second default) until stream is done (PID is no longer running)
+					data_manager.read_stream(stream, on_read, on_done)
+				})
+		}//end update_process_status
+
+		// check process status always
+		const check_process_data = () => {
+			data_manager.get_local_db_data(
+				process_id,
+				'status'
+			)
+			.then(function(local_data){
+				if (local_data && local_data.value) {
+					update_process_status(
+						process_id,
+						local_data.value.pid,
+						local_data.value.pfile,
+						body_response
+					)
+				}
+			})
+		}
+		check_process_data()
+
+
+	// add at end body_response
+		content_data.appendChild(body_response)
+
+
+	return content_data
+}//end get_content_data_edit
