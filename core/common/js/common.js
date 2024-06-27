9fa1849fb4d0b50a95d2c7d141f0508fbd3caeae
Alejandro Peña Carbonell <alex@render.es>
2024-06-27 17:23:55 +0200

    feat: add show_section_id to show_interface properties, by default is true, when false remove the section_id label (not the functionality of edit button)

--- core/common/js/common.js
+++ core/common/js/common.js
@@ -194,7 +194,8 @@ export const set_context_vars = function(self) {
 				button_tree						: false, // bool false
 				button_fullscreen				: true, // bool false
 				button_save						: true, // bool true (used by component_geolocation, text_area...)
-				show_autocomplete				: true // bool true
+				show_autocomplete				: true, // bool true
+				show_section_id					: true // bool true
 			}
 			// set the instance show_interface
 			self.show_interface = (!self.context.properties?.show_interface && !self.request_config_object?.show?.interface)
