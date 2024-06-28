f2c4855e03f34962c230835730ef754a205000ee
Alejandro Peña Carbonell <alex@render.es>
2024-06-28 09:29:03 +0200

    refactor: change the sum_dates mdcat widget to v6 format

--- core/widgets/mdcat/sum_dates/js/sum_dates.js
+++ core/widgets/mdcat/sum_dates/js/sum_dates.js
@@ -1,52 +1,50 @@
 // @license magnet:?xt=urn:btih:0b31508aeb0634b347b8270c7bee4d411b5d4109&dn=agpl-3.0.txt AGPL-3.0
+/*global get_label, page_globals, SHOW_DEBUG, DEDALO_CORE_URL*/
+/*eslint no-undef: "error"*/
+
+
+
+// imports
+	import {widget_common} from '../../../widget_common/js/widget_common.js'
+	import {render_sum_dates} from '../js/render_sum_dates.js'
+
+
+
+export const sum_dates = function(){
+
+	this.id
+
+	this.section_tipo
+	this.section_id
+	this.lang
+	this.mode
+
+	this.value
+
+	this.node
+
+	this.events_tokens = []
+
+	this.status
+
+	return true
+}//end sum_dates
 
 
 
 /**
-* WIDGET_SUM_DATES
+* COMMON FUNCTIONS
+* extend functions from common
 */
-var widget_sum_dates = new function() {
+// prototypes assign
+	// lifecycle
+	sum_dates.prototype.init	= widget_common.prototype.init
+	sum_dates.prototype.build	= widget_common.prototype.build
+	sum_dates.prototype.render	= widget_common.prototype.render
+	sum_dates.prototype.destroy	= widget_common.prototype.destroy
+	// render
+	sum_dates.prototype.edit	= render_sum_dates.prototype.edit
 
-
-	/**
-	* ACTIVATE_LISTENER_VALUE
-	*/
-	this.activate_listener_value = function(component_target_tipo) {
-
-		$(function() {
-			var reference_input	= document.querySelector('.css_wrap_portal[data-tipo="' + component_target_tipo + '"] > .content_data') // > .content_data .table_portal_rows_list
-			//  console.log(reference_input);
-			if (reference_input) {
-
-				// Remove listener to avoid duplicates
-				reference_input.removeEventListener('change', widget_sum_dates.update_state)
-
-				// Add listner change
-				reference_input.addEventListener("change", widget_sum_dates.update_state, false);
-			}
-		});
-	}//end activate_listener_value
-
-
-
-	/**
-	* UPDATE_STATE
-	*/
-	this.update_state = function(e) {
-
-		// Select current widget element
-		var widget_sum_dates = document.getElementById('widget_sum_dates')
-
-		// Update info (reload component info)
-		var js_promise = component_info.update_info(widget_sum_dates)
-			//js_promise.then(function() { })
-	}//end update_state
-
-
-
-
-
-}//end widget_sum_dates
 
 
 // @license-end
