6f14ac42bff45f8dd61fbddab0de35d45cd37cc3
Paco <paco@render.es>
2024-07-08 16:24:45 +0200

    chore: update login SAML files and config to allow to use in v6

--- core/login/saml/index.php
+++ core/login/saml/index.php
@@ -1,4 +1,7 @@
 <?php
+// config include file
+	include dirname(dirname(dirname(dirname(__FILE__)))) . '/config/config_saml.php';
+
 /**
  * SAML Authorization. Initiate a SAML Authorization request
  *
@@ -8,16 +11,9 @@
  * details.
  */
 
-// Require files
-	require_once( dirname(__FILE__) . '/saml_config.php' );
-	require_once( SAML_SETTINGS_PATH );
-	require_once( TOOLKIT_PATH . '_toolkit_loader.php' );
-
 // Login into idp
 	// v3.0
 		$auth = new OneLogin\Saml2\Auth($saml_settings); // Constructor of the SP, loads settings.php and advanced_settings.php
-	// v2.14
-		#$auth = new OneLogin_Saml2_Auth($saml_settings); // Constructor of the SP, loads settings.php and advanced_settings.php
 
 	// login method. Like $auth->login(null, array(), false, false, true);
 		// $returnTo - url to change the workflow and redirect the user to the other PHP file
@@ -27,5 +23,3 @@
 		// $strict - True if we want to stay (returns the url string) False to redirect
 		// $setNameIdPolicy - When true the AuthNRequest will set a nameIdPolicy element.
 		$auth->login();   // Method that sent the AuthNRequest
-
-
