2b5c5e03dc4aef57200f047f639c69006ba2d5ff
Alejandro Peña Carbonell <alex@render.es>
2024-08-16 14:31:29 +0200

    ci: remove the text view of the component_portal view_indexation

--- core/component_portal/js/view_indexation_edit_portal.js
+++ core/component_portal/js/view_indexation_edit_portal.js
@@ -72,8 +72,7 @@ view_indexation_edit_portal.render = async function(self, options) {
 			mode		: 'list',
 			columns_map	: self.columns_map,
 			value		: value_combined,
-			id_variant	: id_variant,
-			view 		: 'text'
+			id_variant	: id_variant
 		})
 		// store to allow destroy later
 		self.ar_instances.push(...ar_section_record)
