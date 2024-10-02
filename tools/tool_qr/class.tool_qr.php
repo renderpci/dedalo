99c16b26c95eac11162b89699caa26c4f11cef7c
Paco <paco@render.es>
2024-10-02 12:38:40 +0200

    feature!: add tool_qr (revamped from v5)

---
+++ tools/tool_qr/class.tool_qr.php
@@ -0,0 +1,12 @@
+<?php
+declare(strict_types=1);
+/**
+* CLASS TOOL_qr
+* This tool is intended to be used as a base build for new tools. Do not use as a production tool.
+*
+*/
+class tool_qr extends tool_common {
+
+
+
+}//end class tool_qr
