30f6a59f0ebf8c6b484aad946ea6dded185d2122
Alejandro Peña Carbonell <alex@render.es>
2024-10-12 09:48:26 +0200

    feat: create new area ontology, it will use to manage ontolgy in the same way that other hierarchies

---
+++ core/area_ontology/class.area_ontology.php
@@ -0,0 +1,28 @@
+<?php
+/**
+* AREA_ONTOLOGY
+* Manage whole ontology hierarchy
+*
+*/
+class area_ontology extends area_thesaurus {
+
+
+
+	/**
+	* CLASS VARS
+	* @var
+	*/
+
+	/**
+	* GET_hierarchy_section_tipo
+	* @return string $section_tipo
+	*/
+	public function get_hierarchy_section_tipo() : string {
+
+		$hierarchy_section_tipo = 'ontology35';
+
+		return $hierarchy_section_tipo;
+	}//end get_hierarchy_section_tipo
+
+
+}//end area_ontology
