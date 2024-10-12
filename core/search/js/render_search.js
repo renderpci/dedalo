007c252f31c649d3e7e8dba9a4ca5d792a267d0e
Alejandro Peña Carbonell <alex@render.es>
2024-10-12 09:50:39 +0200

    refactor: integrate area_ontology with the hierarchy process. Now the hierarchy take account the are_ontology to search, and build the tree.

--- core/search/js/render_search.js
+++ core/search/js/render_search.js
@@ -126,7 +126,7 @@ render_search.prototype.render_base = function() {
 		self.search_global_container = search_global_container
 
 	// thesaurus add on
-		if (self.caller.model==='area_thesaurus') {
+		if (self.caller.model==='area_thesaurus' || self.caller.model === 'area_ontology') {
 			const thesaurus_options_node = render_sections_selector(self)
 			search_global_container.appendChild(thesaurus_options_node)
 		}
