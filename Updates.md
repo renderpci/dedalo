a7f41fb320a42be84e1f88eb65230844a5fff9e1
Alejandro Peña Carbonell <alex@render.es>
2024-10-13 18:24:39 +0200

    deprecate: last version of ontology editor of v5

--- Updates.md
+++
@@ -1,3 +0,0 @@
-**UPDATES AND CHANGES**
-
-
--- tools/tool_update_cache/js/index.js
+++
@@ -1,6 +0,0 @@
-// @license magnet:?xt=urn:btih:0b31508aeb0634b347b8270c7bee4d411b5d4109&dn=agpl-3.0.txt AGPL-3.0
-
-export * from './tool_update_cache.js'
-
-
-// @license-end
