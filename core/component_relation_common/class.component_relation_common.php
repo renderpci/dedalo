f6851d3e54f2ab7d14707d9f051ca79c4ba87b05
Paco <paco@render.es>
2024-10-14 10:12:06 +0200

    fix: fix `hierarchy::$main_table` calls with old var name

--- core/component_relation_common/class.component_relation_common.php
+++ core/component_relation_common/class.component_relation_common.php
@@ -1217,7 +1217,7 @@ class component_relation_common extends component_common {
 
 		// short vars
 			$section_table		= common::get_matrix_table_from_tipo($section_tipo); // Normally 'matrix_hierarchy'
-			$hierarchy_table	= hierarchy::$table;	// Normally 'hierarchy'. Look too in 'matrix_hierarchy_main' table for references
+			$hierarchy_table	= hierarchy::$main_table;	// Normally 'hierarchy'. Look too in 'matrix_hierarchy_main' table for references
 			$ar_tables			= [$section_table, $hierarchy_table];
 			$parents			= component_relation_parent::get_parents(
 				$section_id,
