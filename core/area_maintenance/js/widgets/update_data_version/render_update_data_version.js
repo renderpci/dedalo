be0c0e7b2c9c237c629fb5c1f5edd5d339e3aa68
Paco <paco@render.es>
2024-11-05 13:22:21 +0100

    change: change widget `update_data_version` update rate from 41 to 1000 to avoid unnecessary large process log

--- core/area_maintenance/js/widgets/update_data_version/render_update_data_version.js
+++ core/area_maintenance/js/widgets/update_data_version/render_update_data_version.js
@@ -319,7 +319,7 @@ const get_content_data = async function(self) {
 					body : {
 						dd_api		: 'dd_utils_api',
 						action		: 'get_process_status',
-						update_rate	: 41, // int milliseconds
+						update_rate	: 1000, // int milliseconds
 						options		: {
 							pid		: pid,
 							pfile	: pfile
