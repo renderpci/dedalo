a4440ffb64141cb603df0a8ac5adbf2a6e4c0b9e
Paco <paco@render.es>
2024-11-07 20:32:52 +0100

    feat: add `area_maintenence` widget `system_info` to display system information about CPU, RAM, HD, etc.

--- core/area_maintenance/class.area_maintenance_widgets.php
+++ core/area_maintenance/class.area_maintenance_widgets.php
@@ -71,4 +71,102 @@ class area_maintenance_widgets extends area_common {
 
 
 
+	/**
+	* SYSTEM_INFO
+	* Returns updated widget value
+	* It is used to update widget data dynamically
+	* @return object $response
+	*/
+	public static function system_info() : object {
+
+		$datalist = [];
+
+		// @use linfo
+		// linfo lib installed via composer
+		// @see https://github.com/jrgp/linfo
+		include_once DEDALO_LIB_PATH . '/vendor/autoload.php';
+
+		$linfo = new \Linfo\Linfo;
+
+		$datalist[] = (object)[
+			'name'	=> 'cpu',
+			'value'	=> $linfo->getOS()
+		];
+
+		$datalist[] = (object)[
+			'name'	=> 'hostname',
+			'value'	=> $linfo->getHostname()
+		];
+
+		$datalist[] = (object)[
+			'name'	=> 'virtualization',
+			'value'	=> $linfo->getVirtualization()
+		];
+
+		$datalist[] = (object)[
+			'name'	=> 'load',
+			'value'	=> $linfo->getLoad()
+		];
+
+		$datalist[] = (object)[
+			'name'	=> 'cpu',
+			'value'	=> $linfo->getCPU()
+		];
+
+		$datalist[] = (object)[
+			'name'	=> 'ram',
+			'value'	=> $linfo->getRam()
+		];
+
+		$datalist[] = (object)[
+			'name'	=> 'model',
+			'value'	=> $linfo->getModel()
+		];
+
+		$datalist[] = (object)[
+			'name'	=> 'hd',
+			'value'	=> $linfo->getHD()
+		];
+
+		$datalist[] = (object)[
+			'name'	=> 'mounts',
+			'value'	=> $linfo->getMounts()
+		];
+
+		$datalist[] = (object)[
+			'name'	=> 'net',
+			'value'	=> $linfo->getNet()
+		];
+
+		$datalist[] = (object)[
+			'name'	=> 'uptime',
+			'value'	=> $linfo->getUpTime()
+		];
+
+		$datalist[] = (object)[
+			'name'	=> 'process_status',
+			'value'	=> $linfo->getProcessStats()
+		];
+
+		$result = (object)[
+			'datalist'	=> $datalist,
+			'errors'	=> null
+		];
+
+		// matrix_tools field 'Developer' check
+		if (empty(RecordObj_dd::get_modelo_name_by_tipo('dd1644',true))) {
+			$result->errors = ['Your Ontology is outdated. Term \'dd1644\' (Developer) do not exists'];
+		}
+
+		$response = new stdClass();
+			$response->result	= $result;
+			$response->msg		= 'OK. Request done successfully';
+			$response->errors	= [];
+
+
+		return $response;
+	}//end system_info
+
+
+
 }//end area_maintenance_widgets
