8626e9f0cce619e4f6ef71fc3d495c2a146a635a
Paco <paco@render.es>
2024-11-07 14:14:36 +0100

    refactor: refactor `area_maintence` widget `register_tools` datalist render to improve control and sync with updated data

--- core/area_maintenance/js/widgets/register_tools/render_register_tools.js
+++ core/area_maintenance/js/widgets/register_tools/render_register_tools.js
@@ -67,33 +67,12 @@ const render_content_data = async function(self) {
 
 	// short vars
 		const value		= self.value || {}
-		const datalist	= value.datalist || []
 		const errors	= value.errors || []
-
-	// check versions
-		const outdated = datalist.reduce((carry, value) => {
-			if (value.version !== value.installed_version) {
-				carry.push(value)
-			}
-			return carry
-		}, [])
 
 	// content_data
 		const content_data = ui.create_dom_element({
 			element_type : 'div'
 		})
-
-	// set widget container label color style
-		if (errors.length || outdated.length) {
-			const when_in_dom_handler = () => {
-				const wrapper = self.node
-				const widget_container = wrapper.parentNode.parentNode
-				if (widget_container) {
-					widget_container.classList.add('danger')
-				}
-			}
-			when_in_dom(content_data, when_in_dom_handler)
-		}
 
 	// datalist
 		// header
@@ -142,80 +121,17 @@ const render_content_data = async function(self) {
 				parent			: tool_item
 			})
 
-		// list
-			const datalist_length = datalist.length
-			for (let i = 0; i < datalist_length; i++) {
-
-				const item = datalist[i]
-
-				const name				= item.name
-				const version			= item.version
-				const developer			= item.developer
-				const installed_version	= item.installed_version
-				const ar_warning		= item.warning
-					? [item.warning]
-					: []
-
-				// tool_item
-				const tool_item = ui.create_dom_element({
-					element_type	: 'div',
-					class_name		: 'tool_item',
-					parent			: content_data
-				})
-
-				// name
-				ui.create_dom_element({
-					element_type	: 'div',
-					class_name		: 'tool_name',
-					inner_html		: name,
-					parent			: tool_item
-				})
-
-				// developer
-				ui.create_dom_element({
-					element_type	: 'div',
-					class_name		: 'developer',
-					inner_html		: developer,
-					parent			: tool_item
-				})
-
-				// version
-				ui.create_dom_element({
-					element_type	: 'div',
-					class_name		: 'tool_version',
-					inner_html		: version,
-					parent			: tool_item
-				})
-
-				// installed_version
-				const installed_version_node = ui.create_dom_element({
-					element_type	: 'div',
-					class_name		: 'tool_installed_version',
-					inner_html		: installed_version,
-					parent			: tool_item
-				})
-				if (installed_version!==version) {
-					installed_version_node.classList.add('warning')
-				}
-
-				// warning
-				if (!version) {
-					ar_warning.push('Tool version not defined')
-				}
-				if (!installed_version) {
-					ar_warning.push('Installed version not defined')
-				}
-				const warning_node = ui.create_dom_element({
-					element_type	: 'div',
-					class_name		: 'tool_warning',
-					inner_html		: ar_warning.join('<br>'),
-					parent			: tool_item
-				})
-			}
+		// datalist_container
+			const datalist_container = ui.create_dom_element({
+				element_type	: 'div',
+				class_name		: 'datalist_container',
+				parent			: content_data
+			})
+			render_datalist(self, datalist_container)
 
 	// info errors
-		if (value.errors) {
-			const text = `Errors found. Fix this errors before continue: <br>` + value.errors.join('<br>')
+		if (errors.length) {
+			const text = `Errors found. Fix this errors before continue: <br>` + errors.join('<br>')
 			ui.create_dom_element({
 				element_type	: 'div',
 				inner_html		: text,
@@ -244,6 +160,14 @@ const render_content_data = async function(self) {
 						action : 'register_tools'
 					},
 					options	: {}
+				},
+				on_done : async () => {
+
+					// get and update value
+					self.value = await self.get_widget_value()
+
+					// render datalist again
+					render_datalist(self, datalist_container)
 				}
 			})
 		}
@@ -254,6 +178,120 @@ const render_content_data = async function(self) {
 
 	return content_data
 }//end render_content_data
+
+
+
+/**
+* RENDER_DATALIST
+* Create the datalist nodes and add nodes to datalist_container
+* @param object self
+* @return bool
+*/
+const render_datalist = (self, datalist_container) => {
+
+	// short vars
+		const value		= self.value || {}
+		const datalist	= value.datalist || []
+		const errors	= value.errors || []
+
+	// check versions
+		const outdated = datalist.reduce((carry, value) => {
+			if (value.version !== value.installed_version) {
+				carry.push(value)
+			}
+			return carry
+		}, [])
+
+	// set widget container label color style
+		if (errors.length || outdated.length) {
+			set_widget_label_style(self, 'danger', 'add', datalist_container)
+		}else{
+			set_widget_label_style(self, 'danger', 'remove', datalist_container)
+		}
+
+	const fragment = new DocumentFragment()
+
+	const datalist_length = datalist.length
+	for (let i = 0; i < datalist_length; i++) {
+
+		const item = datalist[i]
+
+		const name				= item.name
+		const version			= item.version
+		const developer			= item.developer
+		const installed_version	= item.installed_version
+		const ar_warning		= item.warning
+			? [item.warning]
+			: []
+
+		// tool_item
+		const tool_item = ui.create_dom_element({
+			element_type	: 'div',
+			class_name		: 'tool_item',
+			parent			: fragment
+		})
+
+		// name
+		ui.create_dom_element({
+			element_type	: 'div',
+			class_name		: 'tool_name',
+			inner_html		: name,
+			parent			: tool_item
+		})
+
+		// developer
+		ui.create_dom_element({
+			element_type	: 'div',
+			class_name		: 'developer',
+			inner_html		: developer,
+			parent			: tool_item
+		})
+
+		// version
+		ui.create_dom_element({
+			element_type	: 'div',
+			class_name		: 'tool_version',
+			inner_html		: version,
+			parent			: tool_item
+		})
+
+		// installed_version
+		const installed_version_node = ui.create_dom_element({
+			element_type	: 'div',
+			class_name		: 'tool_installed_version',
+			inner_html		: installed_version,
+			parent			: tool_item
+		})
+		if (installed_version!==version) {
+			installed_version_node.classList.add('warning')
+		}
+
+		// warning
+		if (!version) {
+			ar_warning.push('Tool version not defined')
+		}
+		if (!installed_version) {
+			ar_warning.push('Installed version not defined')
+		}
+		const warning_node = ui.create_dom_element({
+			element_type	: 'div',
+			class_name		: 'tool_warning',
+			inner_html		: ar_warning.join('<br>'),
+			parent			: tool_item
+		})
+	}
+
+	// clean node
+	while (datalist_container.firstChild) {
+		datalist_container.removeChild(datalist_container.firstChild);
+	}
+
+	// append
+	datalist_container.appendChild(fragment)
+
+
+	return true
+}//end render_datalist
 
 
 
