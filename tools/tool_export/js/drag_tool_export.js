af27fd1d4af3eae23b6a63d37a0e819174c283e3
Paco <paco@render.es>
2024-12-03 12:35:20 +0100

    fix: fix `tool_export` error when the same component in two different sections stops drag and drop of the second one

--- tools/tool_export/js/drag_tool_export.js
+++ tools/tool_export/js/drag_tool_export.js
@@ -129,10 +129,11 @@ export const on_drop = function(container, event) {
 	// short vars
 		const path	= parsed_data.path
 		const ddo	= parsed_data.ddo
+		const id	= self.compose_id(ddo, path)
 
 	// rebuild ddo
 		const new_ddo = {
-			id				: ddo.section_tipo +'_'+ ddo.tipo +'_list_'+ ddo.lang,
+			id				: id,
 			tipo			: ddo.tipo,
 			section_tipo	: ddo.section_tipo,
 			model			: ddo.model,
