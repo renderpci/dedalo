8db7d28096dff6ac39c9c35b1f272d258a1b940a
Paco <paco@render.es>
2024-12-16 17:28:05 +0100

    change: add `to_hilite` to thesaurus search result for easy access to highlight terms at parse

--- core/area_thesaurus/js/render_area_thesaurus.js
+++ core/area_thesaurus/js/render_area_thesaurus.js
@@ -70,6 +70,7 @@ render_area_thesaurus.prototype.list = async function(options) {
 						() => {
 							self.ts_object.parse_search_result(
 								data.ts_search.result, // object data
+								data.ts_search.to_hilite,
 								null, // HTMLElement main_div
 								false // bool is_recursion
 							)
@@ -117,7 +118,12 @@ render_area_thesaurus.prototype.list = async function(options) {
 	// ts_search case
 		if (data.ts_search) {
 			const render_handler = () => {
-				self.ts_object.parse_search_result(data.ts_search.result, null, false)
+				self.ts_object.parse_search_result(
+					data.ts_search.result,
+					data.ts_search.to_hilite,
+					null,
+					false
+				)
 			}
 			self.events_tokens.push(
 				event_manager.subscribe('render_'+self.filter.id, render_handler)
