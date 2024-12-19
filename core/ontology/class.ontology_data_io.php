0dc11521187e80a2f47d67c4da363b285752a328
Alejandro Peña Carbonell <alex@render.es>
2024-12-19 18:57:53 +0100

    feat: add new `ontology_data_io` class. It will used to handle input/output import/export data from ontology

---
+++ core/ontology/class.ontology_data_io.php
@@ -0,0 +1,69 @@
+<?php declare(strict_types=1);
+/**
+* ONTOLOGY_DATA_IO
+* Manages the ontology in/out, the class export and import files to sync nodes between installations
+* Only shared Ontologies will be processed
+* 
+* Note: Local Ontologies are managed by every installation and they are not used here.
+*/
+class ontology_data_io {
+
+
+
+	public static $dd_tables = ['matrix_dd',"matrix_counter_dd","matrix_layout_dd"];
+
+
+
+	/**
+	* EXPORT_ONTOLOGY_INFO
+	*
+	* @return object $response
+	*/
+	public static function export_ontology_info() : object {
+
+		$response = new stdClass();
+			$response->result	= false;
+			$response->msg		= 'Error. Request failed';
+			$response->errors	= [];
+
+		// properties component (dd1)
+			$section_tipo	= 'ontology40';
+			$section_id		= '1';
+			$tipo			= 'ontology18';
+			$model			= RecordObj_dd::get_modelo_name_by_tipo( $tipo );
+			$properties_component = component_common::get_instance(
+				$model, // string model
+				$tipo, // string tipo
+				$section_id, // string section_id
+				'list', // string mode
+				DEDALO_DATA_NOLAN, // string lang
+				$section_tipo // string section_tipo
+			);
+
+			$properties_data	= $properties_component->get_dato();
+			$data				= $properties_data[0] ?? null;
+			$data_string		= json_encode($data, JSON_PRETTY_PRINT|JSON_UNESCAPED_UNICODE|JSON_UNESCAPED_SLASHES);
+
+		// path to save the file
+			$ontology_io_path = ontology_data_io::set_ontology_io_path();
+			if ( $ontology_io_path === false ) {
+				$response->msg		= 'Error. Invalid directory: '.$ontology_io_path;
+				$response->errors[]	= 'Unable to create directory: '.$ontology_io_path;
+				return $response;
+			}
+			$path_file = "{$ontology_io_path}/ontology.json";
+
+		// set data into ontology file
+			$saved = file_put_contents( $path_file, $data_string );
+			if($saved === false){
+				$response->msg		= 'Error. Impossible to save data in ontology.json file';
+				$response->errors[]	= 'Impossible to save data in ontology.json file';
+				return $response;
+			}
+
+		$response->result	= true;
+		$response->msg		= 'OK. Request done';
+
+
+		return $response;
+	}//end export_ontology_info
