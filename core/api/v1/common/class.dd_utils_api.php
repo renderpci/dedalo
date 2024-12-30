d274805f91cd62a599a4775d3c82b5af0004b069
Alejandro Peña Carbonell <alex@render.es>
2024-12-30 16:59:46 +0100

    feat: add `get_code_update_info()` method in `dd_utils_api`, used to collect code files compatible with client request.

--- core/api/v1/common/class.dd_utils_api.php
+++ core/api/v1/common/class.dd_utils_api.php
@@ -1678,6 +1678,95 @@ final class dd_utils_api {
 
 
 
+
+	/**
+	* GET_CODE_UPDATE_INFO
+	* Ontology server provide information about the ontology that it can provide.
+	* Client needs to provide his version and the code for this server.
+	* @param object $rqo
+	* @return object $response
+	*/
+	public static function get_code_update_info( object $rqo ) : object {
+
+		// session unlock
+			session_write_close();
+
+		// response
+		$response = new stdClass();
+			$response->result	= false;
+			$response->msg		= 'Error. Request failed';
+			$response->errors	= [];
+
+		// check if the server is ontology server, if not stop the process
+		// Only ontology servers can provide his ontology files.
+			if ( !defined('IS_A_CODE_SERVER') ||  IS_A_CODE_SERVER === false ) {
+				$response->result	= false;
+				$response->msg		= 'Error. Server is not an code server';
+				return $response;
+			}
+
+		// RQO options
+		// client will send his version and the code that able to get the ontology information
+			$options = $rqo->options;
+
+		// check configuration of the ontology constants
+			if ( !defined('DEDALO_CODE_FILES_DIR') ) {
+				$response->msg		= 'Error. Dédalo is miss configured. Define DEDALO_CODE_FILES_DIR as sample.config.php defines';
+				$response->errors[]	= 'Error. Bad DEDALO_CODE_FILES_DIR';
+				return $response;
+			}
+
+		// Version
+		// client needs to provide his own version of Dédalo
+		// only compatible code files with the caller version will be provided
+		// if the client doesn't send a valid version will be refuse the call.
+			$string_version	= $options->version;
+			$ar_version 	= explode( '.', $string_version );
+
+			foreach($ar_version as $key => $version_number){
+				if($key > 2){
+					break;
+				}
+				$check = is_numeric( $version_number );
+				if (!$check) {
+					$response->msg		= 'Error. Invalid version number';
+					$response->errors[]	= 'Invalid version number';
+					return $response;
+				}
+			}
+
+		// code
+		// client needs to provide a valid code.
+		// valid code is defined in config.php constant of CODE_SERVERS
+			$code = $options->code;
+
+			$ontology_servers = defined( 'CODE_SERVERS' )
+				? CODE_SERVERS
+				: [];
+
+			$valid_code = false;
+			foreach ( $ontology_servers as $current_server_info ) {
+				if( $current_server_info['code'] === $code ){
+					$valid_code = true;
+				}
+			}
+
+			if( $valid_code === false ){
+				$response->msg		= 'Error. Invalid code';
+				$response->errors[]	= 'Invalid code';
+				return $response;
+			}
+
+		// Client made a valid request.
+		// get the information to be provided to client
+			$response = update_code::get_code_update_info( $ar_version );
+
+
+		return $response;
+	}//end get_ontology_update_info
+
+
+
 	// private methods ///////////////////////////////////
 
 
