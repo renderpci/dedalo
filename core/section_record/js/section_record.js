2bee3567a6ff552fb32bef78a51263d966218bd3
Paco <paco@render.es>
2025-01-16 13:07:01 +0100

    change!: add random number to section_record `id_variant` top prevent issues like `dd0` children cases where some instances are nor properly rendered because the section_record instance id is the same in more than one instance.

--- core/section_record/js/section_record.js
+++ core/section_record/js/section_record.js
@@ -192,7 +192,7 @@ const build_instance = async (self, context, section_id, current_data, column_id
 			}
 
 		// id_variant . Propagate a custom instance id to children
-			const section_record_id_variant = `${self.tipo}_${section_id}_${self.caller.section_tipo}_${self.caller.section_id}`
+			const section_record_id_variant = `${self.tipo}_${section_id}_${self.caller.section_tipo}_${self.caller.section_id}` +'_'+ Math.random()
 			instance_options.id_variant = self.id_variant
 				? self.id_variant + '_' + section_record_id_variant
 				: section_record_id_variant
