eeba78f88212f33efa1f5bf053e033b87dc766a5
Alejandro Peña Carbonell <alex@render.es>
2025-02-15 13:45:27 +0100

    ci: add update to v6.4.2

--- core/base/update/updates.php
+++ core/base/update/updates.php
@@ -35,6 +35,24 @@ global $updates;
 $updates = new stdClass();
 
 
+$v=642; #####################################################################################
+$updates->$v = new stdClass();
+
+	# UPDATE TO
+	$updates->$v->version_major			= 6;
+	$updates->$v->version_medium		= 4;
+	$updates->$v->version_minor			= 2;
+
+	# MINIMUM UPDATE FROM
+	$updates->$v->update_from_major		= 6;
+	$updates->$v->update_from_medium	= 4;
+	$updates->$v->update_from_minor		= 1;
+
+	// Re-index and vacuum tables
+		$updates->$v->SQL_update[] = PHP_EOL.sanitize_query('
+			VACUUM FULL VERBOSE ANALYZE public.jer_dd;
+		');
+
 
 $v=641; #####################################################################################
 $updates->$v = new stdClass();
