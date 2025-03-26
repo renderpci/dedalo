efb3aadd9b0c5ac19b4173e001f87abbed5f7c35
Paco <paco@render.es>
2025-03-26 14:10:32 +0100

    refactor: refactor Ontology version info in `Login` and `Area maintenance` to display more useful information.

--- core/login/class.login.php
+++ core/login/class.login.php
@@ -1383,10 +1383,12 @@ class login extends common {
 				'value'	=> implode('.', get_current_version_in_db())
 			];
 		// ontology version
+			$RecordObj_dd		= new RecordObj_dd('dd1');
+			$dd1_properties		= $RecordObj_dd->get_properties();
 			$properties->info[] = [
 				'type'	=> 'version',
 				'label'	=> 'Ontology version',
-				'value'	=> RecordObj_dd::get_termino_by_tipo(DEDALO_ROOT_TIPO, DEDALO_STRUCTURE_LANG, true, true)
+				'value'	=> [$dd1_properties->version, $dd1_properties->date]
 			];
 
 		// development server only
