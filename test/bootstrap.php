ca1c49a9bb86193a74527d335b4589186abd9db9
Paco <paco@render.es>
2025-03-26 11:29:21 +0100

    style: clean code

--- test/bootstrap.php
+++ test/bootstrap.php
@@ -4,7 +4,7 @@
 // require __DIR__ . '/autoload.php';
 
 // SHOW_DEBUG. Overwrite config SHOW_DEBUG
-	define('SHOW_DEBUG', false);
+	define('SHOW_DEBUG', true);
 
 // TEST_USER_ID: [
 	// 	-1, // root development user
