4008908b2fcad97b1eb30b4df40571b5b829c50b
Alejandro Peña Carbonell <alex@render.es>
2025-06-08 10:42:29 +0200

    style: update version number to 6.6.2

--- core/base/update/updates.php
+++ core/base/update/updates.php
@@ -35,6 +35,25 @@ global $updates;
 $updates = new stdClass();
 
 
+$v=662; #####################################################################################
+$updates->$v = new stdClass();
+
+	# UPDATE TO
+	$updates->$v->version_major			= 6;
+	$updates->$v->version_medium		= 6;
+	$updates->$v->version_minor			= 2;
+
+	# MINIMUM UPDATE FROM
+	$updates->$v->update_from_major		= 6;
+	$updates->$v->update_from_medium	= 6;
+	$updates->$v->update_from_minor		= 1;
+
+	// Re-index and vacuum tables
+		$updates->$v->SQL_update[] = PHP_EOL.sanitize_query('
+			VACUUM FULL VERBOSE ANALYZE public.jer_dd;
+		');
+
+
 $v=661; #####################################################################################
 $updates->$v = new stdClass();
 
