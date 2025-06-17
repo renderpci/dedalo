0b40259eabfe5cd24bdbce84f7c759314a99a7ea
Alejandro Peña Carbonell <alex@render.es>
2025-06-17 08:55:32 +0200

    refactor: changes the `diffusion_class` to be used in a common diffusion data flow. Now the components has a common way to return its data.

--- core/diffusion/class.diffusion_data.php
+++ core/diffusion/class.diffusion_data.php
@@ -8,9 +8,11 @@ class diffusion_data {
 
 	/**
 	* GET_DDO_MAP
-	* @return
+	* @param string $diffusion_node_tipo
+	* @param string $section_tipo
+	* @return array $ddo_map
 	*/
-	public static function get_ddo_map( string $diffusion_node_tipo, string $section_tipo) {
+	public static function get_ddo_map( string $diffusion_node_tipo, string $section_tipo) : array {
 
 		// ddo_map create or get from properties
 		$ddo_map			= [];
@@ -45,13 +47,15 @@ class diffusion_data {
 	/**
 	* GET_DDO_MAP_VALUE
 	* resolve the ddo_map components
-	* @param array $ddo_map
-	* @param string $parent
-	* @param string $section_tipo
-	* @param string|int $section_id
+	* @param object $options
 	* @return array $ar_values
 	*/
-	public static function get_ddo_map_value(array $ddo_map, string $parent, $section_tipo, $section_id) : array {
+	public static function get_ddo_map_value( object $options ) : array {
+
+		$ddo_map		=  $options->ddo_map;
+		$parent			=  $options->section_tipo;
+		$section_tipo	=  $options->section_tipo;
+		$section_id		=  $options->section_id;
 
 		$ar_values = [];
 
@@ -114,115 +118,23 @@ class diffusion_data {
 
 		if(empty($children)) {
 
-			$id			= $ddo->id ?? null;
-			$value_fn	= $ddo->value_fn ?? 'get_diffusion_value';
+			$diffusion_data	= $element->get_diffusion_data( $ddo );
+			$ar_values		= array_merge($ar_values, $diffusion_data);
 
-			if($model_name==='relation_list') {
-
-				// diffusion_properties
-				$diffusion_properties = $ddo->diffusion_properties;
-				$element->set_diffusion_properties($diffusion_properties);
-
-				// value (dato)
-				$value = $element->get_diffusion_dato();
-
-				// $config_properties = $ddo->config ?? null;
-				// if($config_properties){
-				// 	$config = $this->resolve_configuration($config_properties);
-				// 	$result = $this->{$config_properties->process_fn}((object)[
-				// 		'config_properties'	=> $config_properties,
-				// 		'config'			=> $config,
-				// 		'value'				=> $value
-				// 	]);
-					$ar_values = $result;
-				}else{
-					foreach ($value as $value) {
-						$ddo_value = new stdClass();
-							$ddo_value->tipo	= $ddo->tipo;
-							$ddo_value->lang	= null;
-							$ddo_value->value	= $value->section_id;
-							$ddo_value->id		= $id;
-
-						$ar_values[] = $ddo_value;
-					}
-				}
-			}elseif($model_name==='component_section_id') {
-
-				$ddo_value = new stdClass();
-					$ddo_value->tipo	= $ddo->tipo;
-					$ddo_value->lang	= null;
-					$ddo_value->value	= $section_id;
-					$ddo_value->id		= $id;
-
-				$ar_values[] = $ddo_value;
-			}elseif($model_name === 'component_image') {
-
-				$quality		= $ddo->fn_params->quality ?? '1.5MB';
-				$test_file		= $ddo->fn_params->test_file ?? false;
-				$absolute		= $ddo->fn_params->absolute ?? false;
-				$default_add	= $ddo->fn_params->default_add ?? false;
-
-				// value
-				$value = $element->get_image_url($quality, $test_file, $absolute, $default_add);
-
-				$ddo_value = new stdClass();
-					$ddo_value->tipo	= $ddo->tipo;
-					$ddo_value->lang	= null;
-					$ddo_value->value	= $value;
-					$ddo_value->id		= $id;
-
-				$ar_values[] = $ddo_value;
-
-			}elseif(in_array($model_name, component_relation_common::get_components_with_relations())) {
-
-				// value
-				$value = $element->{$value_fn}();
-
-				$ddo_value = new stdClass();
-					$ddo_value->tipo	= $ddo->tipo;
-					$ddo_value->lang	= null;
-					$ddo_value->value	= $value;
-					$ddo_value->id		= $id;
-
-				$ar_values[] = $ddo_value;
-			}else{
-
-				$dato_full = $element->get_dato_full();
-				if(!empty($dato_full)) {
-					foreach ($dato_full as $current_lang => $value) {
-						if(!empty($value)) {
-
-							$element->set_lang($current_lang);
-							$lang_alpha2 = $current_lang===DEDALO_DATA_NOLAN
-								? lang::get_alpha2_from_code(DEDALO_DATA_LANG_DEFAULT)
-								: lang::get_alpha2_from_code($current_lang);
-
-							$value	= $element->{$value_fn}($current_lang);
-
-							$ddo_value = new stdClass();
-								$ddo_value->tipo	= $ddo->tipo;
-								$ddo_value->lang	= $lang_alpha2;
-								$ddo_value->value	= $value;
-								$ddo_value->id		= $id;
-
-							$ar_values[] = $ddo_value;
-						}
-					}
-				}
-			}
 		}else{
 
 			// no empty($children) case
-
 			$ar_locators = $element->get_dato();
 			foreach ($ar_locators as $current_locator) {
-				$result	= $this->get_ddo_map_value(
-					$ddo_map,
-					$parent,
-					$current_locator->section_tipo,
-					$current_locator->section_id
-				);
-				$ar_values = array_merge($ar_values, $result);
+
+				$resolve_options = new stdClass();
+					$resolve_options->ddo_map		= $ddo_map;
+					$resolve_options->parent		= $parent;
+					$resolve_options->section_tipo	= $current_locator->section_tipo;
+					$resolve_options->section_id	= $current_locator->section_id;
+
+				$diffusion_data	= diffusion_data::get_ddo_map_value( $resolve_options );
+				$ar_values = array_merge($ar_values, $diffusion_data);
 			}
 		}//end if(empty($children))
 
