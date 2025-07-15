3aa9713ea0b1ce4dcbe6e49d3905d7c7a8660b59
Paco <paco@render.es>
2025-07-15 10:13:05 +0200

    refactor: Unify `add_hierarchy` widget with new model for `area_maintenance` widgets.

---
+++ core/area_maintenance/widgets/add_hierarchy/class.add_hierarchy.php
@@ -0,0 +1,38 @@
+<?php declare(strict_types=1);
+/**
+* ADD_HIERARCHY
+* Widget to manage Dédalo database tasks
+*/
+class add_hierarchy {
+
+
+
+	/**
+	* GET_VALUE
+	* Returns updated widget value
+	* It is used to update widget data dynamically
+	* @return object $response
+	*/
+	public static function get_value() : object {
+
+		$install_config = install::get_config();
+
+		$result = (object)[
+			'hierarchies'				=> install::get_available_hierarchy_files()->result,
+			'active_hierarchies'		=> hierarchy::get_active_elements(),
+			'hierarchy_files_dir_path'	=> $install_config->hierarchy_files_dir_path,
+			'hierarchy_typologies'		=> $install_config->hierarchy_typologies
+		];
+
+		$response = new stdClass();
+			$response->result	= $result;
+			$response->msg		= 'OK. Request done successfully';
+			$response->errors	= [];
+
+
+		return $response;
+	}//end get_value
+
+
+
+}//end add_hierarchy
--- core/area_maintenance/widgets/add_hierarchy/js/add_hierarchy.js
+++ core/area_maintenance/widgets/add_hierarchy/js/add_hierarchy.js
@@ -5,6 +5,7 @@
 
 
 // imports
+	import {data_manager} from '../../../../common/js/data_manager.js'
 	import {widget_common} from '../../../../widgets/widget_common/js/widget_common.js'
 	import {render_add_hierarchy} from './render_add_hierarchy.js'
 
@@ -35,14 +36,78 @@ export const add_hierarchy = function() {
 */
 // prototypes assign
 	// lifecycle
-	add_hierarchy.prototype.init	= widget_common.prototype.init
-	add_hierarchy.prototype.build	= widget_common.prototype.build
-	add_hierarchy.prototype.render	= widget_common.prototype.render
-	add_hierarchy.prototype.destroy	= widget_common.prototype.destroy
-	add_hierarchy.prototype.refresh	= widget_common.prototype.refresh
+	add_hierarchy.prototype.init		= widget_common.prototype.init
+	// add_hierarchy.prototype.build	= widget_common.prototype.build
+	add_hierarchy.prototype.render		= widget_common.prototype.render
+	add_hierarchy.prototype.destroy		= widget_common.prototype.destroy
+	add_hierarchy.prototype.refresh		= widget_common.prototype.refresh
 	// render
-	add_hierarchy.prototype.edit	= render_add_hierarchy.prototype.list
-	add_hierarchy.prototype.list	= render_add_hierarchy.prototype.list
+	add_hierarchy.prototype.edit		= render_add_hierarchy.prototype.list
+	add_hierarchy.prototype.list		= render_add_hierarchy.prototype.list
+
+
+
+/**
+* BUILD
+* Custom build overwrites common widget method
+* @param bool autoload = false
+* @return bool
+*/
+add_hierarchy.prototype.build = async function(autoload=false) {
+
+	const self = this
+
+	// call generic common tool build
+		const common_build = await widget_common.prototype.build.call(this, autoload);
+
+	try {
+
+		// specific actions.. like fix main_element for convenience
+		self.value = await self.get_widget_value()
+
+	} catch (error) {
+		self.error = error
+		console.error(error)
+	}
+
+
+	return common_build
+}//end build_custom
+
+
+
+/**
+* GET_WIDGET_VALUE
+* Get widget value from class maintenance
+* The options 'name' property is the class method name
+* @return object result
+*/
+add_hierarchy.prototype.get_widget_value = async () => {
+
+	// get value from API
+	const api_response = await data_manager.request({
+		use_worker	: true,
+		body		: {
+			dd_api			: 'dd_area_maintenance_api',
+			action			: 'get_widget_value',
+			prevent_lock	: true,
+			source			: {
+				type	: 'widget',
+				model	: 'add_hierarchy'
+			}
+		},
+		retries : 1, // one try only
+		timeout : 3600 * 1000 // 1 hour waiting response
+	})
+	if(SHOW_DEBUG===true) {
+		console.log('))) get_widget_value add_hierarchy api_response:', api_response);;
+	}
+
+	const result = api_response.result
+
+
+	return result
+}//end get_widget_value
 
 
 
