a8c55eec3434df74a7772d2947be3d466cee1bde
Paco <paco@render.es>
2025-07-15 11:30:49 +0200

    style: clean code

--- tools/tool_numisdata_order_coins/js/render_tool_numisdata_order_coins.js
+++ tools/tool_numisdata_order_coins/js/render_tool_numisdata_order_coins.js
@@ -12,7 +12,7 @@
 
 
 /**
-* RENDER_tool_numisdata_order_coins
+* RENDER_TOOL_NUMISDATA_ORDER_COINS
 * Manages the component's logic and appearance in client side
 */
 export const render_tool_numisdata_order_coins = function() {
@@ -24,7 +24,8 @@ export const render_tool_numisdata_order_coins = function() {
 
 /**
 * EDIT
-* Render node
+* Render main tool DOM node for current mode
+* @param object options = {render_level:'full'}
 * @return HTMLElement wrapper
 */
 render_tool_numisdata_order_coins.prototype.edit = async function(options={render_level:'full'}) {
@@ -49,7 +50,6 @@ render_tool_numisdata_order_coins.prototype.edit = async function(options={rende
 		const header_options_node = await render_header_options(self, content_data)
 		wrapper.tool_buttons_container.appendChild(header_options_node)
 
-
 	// render_activity_info are the information of the activity as "Save"
 		const activity_info = render_activity_info(self)
 		wrapper.activity_info_container.appendChild(activity_info)
@@ -67,6 +67,9 @@ render_tool_numisdata_order_coins.prototype.edit = async function(options={rende
 
 /**
 * GET_CONTENT_DATA_EDIT
+* Render content data DOM nodes
+* @param object self
+* 	Tool instance
 * @return HTMLElement content_data
 */
 const get_content_data_edit = async function(self) {
@@ -81,7 +84,6 @@ const get_content_data_edit = async function(self) {
 		})
 
 	// component_epigraphy. render another node of component caller and append to container
-
 		self.coins.render_views.push(
 			{
 				view	: 'coins_mosaic',
@@ -119,7 +121,10 @@ const get_content_data_edit = async function(self) {
 /**
 * RENDER_HEADER_OPTIONS
 * This is used to build a optional buttons inside the header
-* @return HTMLElement fragment
+* @param object self
+* 	Tool instance
+* @param HTMLElement content_data
+* @return DocumentFragment fragment
 */
 const render_header_options = async function(self, content_data) {
 
@@ -214,11 +219,13 @@ const render_header_options = async function(self, content_data) {
 			for (let i = 0; i < weight_data_len; i++) {
 				const section_id	= weight_data[i].section_id
 				const value_order	= data.value.find(el=>el.section_id === section_id)
-				order_data_value.push(value_order)
+				if (value_order) {
+					order_data_value.push(value_order)
+				}
 			}
 		}else{
 			// order by original section_id
-			const order_data	= data.value.sort(function(a, b) {
+			const order_data = data.value.sort(function(a, b) {
 				return a.section_id - b.section_id;
 			});
 
@@ -230,40 +237,32 @@ const render_header_options = async function(self, content_data) {
 		self.coins.render('content')
 	}
 
+	// set_original_button
 	const set_original_button = ui.create_dom_element({
 		element_type	: 'button',
 		class_name		: 'tool_button set_original light',
 		text_content	: self.get_tool_label('original_copy') || 'Set Original / Copy',
 		parent			: fragment
 	})
-	set_original_button.addEventListener('mouseup', async ()=>{
+	//  mouseup event
+	const set_original_button_mouseup_handler = (e) => {
+		e.stopPropagation()
+
 		const left_container = self.node.content_data.left_container
 
 		const input_original_nodes	= left_container.querySelectorAll('input.input_original')
 		const input_copy_nodes		= left_container.querySelectorAll('input.input_copy')
 
-		const ar_original = []
-		const input_original_nodes_len = input_original_nodes.length
+		const ar_original = Array.from(input_original_nodes).filter(input => input.checked);
+		const ar_copies   = Array.from(input_copy_nodes).filter(input => input.checked);
 
-		for (let i = input_original_nodes_len - 1; i >= 0; i--) {
-			if(input_original_nodes[i].checked){
-				ar_original.push(input_original_nodes[i])
-			}
-		}
-
-		const ar_copies	= []
-		const input_copy_nodes_len = input_copy_nodes.length
-		for (let i = input_copy_nodes_len - 1; i >= 0; i--) {
-			if(input_copy_nodes[i].checked){
-				ar_copies.push(input_copy_nodes[i])
-			}
-		}
 		self.set_original_copy({
 			ar_original	: ar_original,
 			ar_copies	: ar_copies
 		})
+	}
+	set_original_button.addEventListener('mouseup', set_original_button_mouseup_handler)
 
-	})
 
 	return fragment
 }//end render_header_options
@@ -291,7 +290,7 @@ const render_activity_info = function(self) {
 		)
 		function fn_saved(options) {
 
-			// recived options contains an object with instance and api_response
+			// received options contains an object with instance and api_response
 			const node_info_options = Object.assign(options,{
 				container : activity_info_body
 			})
@@ -354,7 +353,6 @@ const get_ordered_coins = async function(self){
 	// 	})
 	// }
 
-
 	render_tool_numisdata_order_coins.prototype.drop({
 		self : self
 	})
@@ -375,7 +373,6 @@ render_tool_numisdata_order_coins.prototype.drop = function (options) {
 	const ar_drop_nodes = self.ordered_coins.node.querySelectorAll('.column_numisdata9')
 
 	const drop_zones_len = ar_drop_nodes.length
-
 	for (let i = drop_zones_len - 1; i >= 0; i--) {
 
 		const current_node = ar_drop_nodes[i]
