4983d51098e94be6afdf01011addd9833e7fae18
Paco <paco@render.es>
2025-07-17 10:01:31 +0200

    chore: Add `test_diffusion` draft test to JS unit test.

--- core/unit_test/js/list.js
+++ core/unit_test/js/list.js
@@ -21,7 +21,7 @@ import {elements} from './elements.js'
 		'test_no_logged_error',
 		'test_unknown_error',
 		'test_page',
-		'test_component_text_area',
+		'test_diffusion',
 		// 'test_component_portal_pagination'
 	]
 
