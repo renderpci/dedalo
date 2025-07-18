7e0e2556da51c3a81b0249b6de24cf18b07c3dcb
Alejandro Peña Carbonell <alex@render.es>
2025-07-18 16:51:04 +0200

    change: Use new raw data API function to get list of the component data instead use read. It allow to include all components in a set founded.

--- core/component_portal/js/buttons.js
+++ core/component_portal/js/buttons.js
@@ -3,12 +3,13 @@
 
 
 // imports
-	import {data_manager} from '../../common/js/data_manager.js'
-	import {get_instance} from '../../common/js/instances.js'
-	import {ui} from '../../common/js/ui.js'
-	import {object_to_url_vars, open_window, open_records_in_window, clone} from '../../common/js/utils/index.js'
-
-
+	import { data_manager } from '../../common/js/data_manager.js'
+	import { get_instance } from '../../common/js/instances.js'
+	import { create_source } from '../../common/js/common.js'
+	import { ui } from '../../common/js/ui.js'
+	import { object_to_url_vars, open_window, open_records_in_window, clone } from '../../common/js/utils/index.js'
+	import { is_filter_empty } from '../../../core/search/js/search.js'
+	import { printf } from '../../common/js/utils/index.js'
 
 /**
 * BUTTONS
@@ -371,9 +372,218 @@ buttons.render_list_from_component_data_button = (self) => {
 		class_name		: 'button list',
 		title			: get_label.list_from_component_data || 'List from component data'
 	})
+
 	// event mousedown. Add listener to the button
 	const mousedown_handler = async (e) => {
 		e.stopPropagation()
+
+		const sqo = clone(self.caller.caller.rqo?.sqo) || {};
+
+		if (!sqo) {
+			console.error('Missing SQO in section in its request config object');
+			return;
+		}
+
+		// open data selection, by default = current.
+		// options: current | found
+		const data_selection = {
+			selected_value : 'current'
+		};
+
+		// perform the search and open data in windows
+		const perform_open_related_data = async function(){
+
+			// check if the filter is empty
+			// when the user don't find anything, the result can be huge
+			// test the sqo filter
+			const filter_empty = sqo.filter
+				? is_filter_empty(sqo.filter)
+				: true
+
+			// close modal
+			modal.close()
+
+			// when the user select 'found', the API will get data from the selected component in all records found
+			if( data_selection.selected_value === 'found' ){
+				// warning user before execute when no filter is used
+				// in this case it could be huge!
+				if(filter_empty === true){
+
+					const raw_label = get_label.will_open_all_related_records || 'This will open all related data of {0} of all records in a new window without filtering. Are you sure?'
+					const label = printf(raw_label, self.label)
+					const warning_label =  get_label.warning.toUpperCase() || 'WARNING'
+					const msg = warning_label
+					+ '\n' + label
+					if (!confirm(msg)){
+						return false
+					}
+				}
+
+				//remove the limit and offset when the found data is selected
+				// Note: open windows will be paginated, but here is necessary all data of the component.
+				sqo.limit = 0
+				sqo.offset = 0
+			}
+
+			// read_raw from Dédalo API
+			const rqo = {
+				action			: 'read_raw',
+				source			: create_source(self),
+				sqo				: sqo
+			}
+
+			// perform the search and get the result
+			const api_response = await data_manager.request({
+				body : rqo
+			})
+
+			if (!api_response?.result) {
+				console.error('Failed api response:', api_response);
+				return []
+			}
+			//value is the result as an array of locators
+			const value = api_response.result
+
+			// section tipo
+			// get the target section tipos in the data
+				const ar_section_tipo			= value.map(el => el?.section_tipo).filter(tipo => tipo !== undefined)
+				const unique_ar_section_tipo	= [...new Set(ar_section_tipo)];
+
+			// open every target section in different windows
+				const target_sections_len = unique_ar_section_tipo.length
+				for (let i = target_sections_len - 1; i >= 0; i--) {
+					const current_section_tipo = unique_ar_section_tipo[i]
+
+					// section_id list
+					const ar_section_id = value
+						.filter(el => el.section_tipo === current_section_tipo && el.section_id)
+						.map(el => el.section_id)
+
+					const window_options = {
+						caller			: self,
+						section_tipo	: current_section_tipo,
+						ar_section_id	: ar_section_id,
+						target_window	: current_section_tipo,
+						left			: i*30,
+						top				: (i*30)+25 // MacOs menu has 25 pixels
+					}
+
+					// open_records_in_window
+					open_records_in_window( window_options );
+					// open_records_in_window(self, current_section_tipo, ar_section_id, current_section_tipo);
+				}
+		}
+
+
+		// Modal
+		// Ask user if want this record or all records
+		// by default only current record will used.
+		// header
+			const header = ui.create_dom_element({
+				element_type	: 'div',
+				class_name		: 'header'
+			})
+
+			ui.create_dom_element({
+				element_type	: 'span',
+				class_name		: 'label',
+				text_node		: get_label.open_relationships || 'Open relationships',
+				parent			: header
+			})
+
+		// body
+			const body = ui.create_dom_element({
+				element_type	: 'div',
+				class_name		: 'content body'
+			})
+
+			const raw_body_label = get_label.open_relationships_of_field || 'Open relationships of field {0} from the current record (1) or from all found records ({1})?'
+			const body_label = printf(raw_body_label, self.label, self.caller.caller.total)
+
+			ui.create_dom_element({
+				element_type	: 'div',
+				class_name		: 'label',
+				text_node		: body_label,
+				parent			: body
+			})
+
+			const change_mode_handler = (e)=> {
+				data_selection.selected_value = e.target.value
+			}
+
+			const radio_button_content = ui.create_dom_element({
+				element_type	: 'div',
+				class_name		: 'radio_button_content',
+				parent			: body
+			})
+
+			// option current_record
+				const current_record_label = ui.create_dom_element({
+					element_type	: 'label',
+					class_name		: 'current_record_label unselectable',
+					text_node		: get_label.current_record || 'Current record',
+					parent			: radio_button_content
+				})
+
+				const current_record_radio = ui.create_dom_element({
+					element_type	: 'input',
+					type			: 'radio',
+					name			: 'data_selection',
+					value 			: 'current'
+				})
+				current_record_radio.checked = true
+				current_record_radio.addEventListener('change', change_mode_handler)
+				current_record_label.prepend(current_record_radio)
+
+			// option found_records
+				const found_records_label = ui.create_dom_element({
+					element_type	: 'label',
+					class_name		: 'found_records_label unselectable',
+					text_node		: get_label.found_records || 'All found records',
+					parent			: radio_button_content
+				})
+
+				const found_records_radio = ui.create_dom_element({
+					element_type	: 'input',
+					type			: 'radio',
+					name			: 'data_selection',
+					value 			: 'found'
+				})
+				found_records_radio.addEventListener('change', change_mode_handler)
+				found_records_label.prepend(found_records_radio)
+
+
+			// button_open
+				const button_open_container = ui.create_dom_element({
+					element_type	: 'div',
+					class_name		: 'button_open_container',
+					parent			: body
+				})
+
+				const button_open = ui.create_dom_element({
+					element_type	: 'button',
+					class_name		: 'success',
+					inner_html		: get_label.open || 'Open',
+					parent			: button_open_container
+				})
+
+				button_open.addEventListener('mouseup', perform_open_related_data)
+		// modal
+			const modal = ui.attach_to_modal({
+				header		: header,
+				body		: body,
+				size		: 'small', // string size big|normal|small
+				minimizable	: false,
+				callback	: (dd_modal) => {
+					dd_modal.classList.add('open_relations_modal')
+				}
+			})
+
+
+	return
+
+
+
 
 		if (!self.request_config_object?.sqo) {
 			console.error('Missing request config object');
