e99ee2927452931f871f1e2706d6736ccc2d661e
Paco <paco@render.es>
2025-07-19 15:23:40 +0200

    refactor!: Refactor `tool_hierarchy` to allow to create new target sections in 'General term' and 'General term model' on generate.

--- tools/tool_hierarchy/class.tool_hierarchy.php
+++ tools/tool_hierarchy/class.tool_hierarchy.php
@@ -22,9 +22,15 @@ class tool_hierarchy extends tool_common {
 			$response->errors	= [];
 
 		// options
-			$section_id			= $options->section_id;
-			$section_tipo		= $options->section_tipo;
-			$force_to_create	= $options->force_to_create;
+			$section_id			= $options->section_id ?? null;
+			$section_tipo		= $options->section_tipo ?? null;
+			$force_to_create	= $options->force_to_create ?? false;
+
+		// validate vars
+			if (empty($section_id) || empty($section_tipo)) {
+				$response->errors[] = 'Missing section_id or section_tipo.';
+				return $response;
+			}
 
 		// check if is necessary to delete the previous ontology terms before added new ones
 			if($force_to_create===true){
@@ -37,7 +43,7 @@ class tool_hierarchy extends tool_common {
 				}
 			}
 
-		// create a new virtual section from real
+		// create a new virtual section from real. This build the new hierarchy elements needed to run (ontology, jer_dd, etc.)
 			$hierarchy_response = hierarchy::generate_virtual_section((object)[
 				'section_id'	=> $section_id,
 				'section_tipo'	=> $section_tipo
@@ -46,9 +52,23 @@ class tool_hierarchy extends tool_common {
 				$response->errors = array_merge($response->errors, $hierarchy_response->errors);
 			}
 
+		// Create the target section if not already exists.
+		// If is not created, the thesaurus view do not display nothing (no hierarchy root nodes are visualized now)
+		// 'hierarchy45' for General term
+			$created_general_term = hierarchy::create_thesaurus_general_term( $section_tipo, $section_id, 'hierarchy45' );
+
+		// Same for model
+		// 'hierarchy59' for General term model
+			$created_general_term_model = hierarchy::create_thesaurus_general_term( $section_tipo, $section_id, 'hierarchy59' );
+
+		// delete previous cache files (forces update menu)
+			dd_cache::delete_cache_files();
+
 		// response
-			$response->result	= $hierarchy_response->result;
-			$response->msg		= $hierarchy_response->msg ;
+			$response->result						= $hierarchy_response->result;
+			$response->created_general_term			= $created_general_term;
+			$response->created_general_term_model	= $created_general_term_model;
+			$response->msg							= $hierarchy_response->msg ;
 
 
 		return $response;
