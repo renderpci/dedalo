a1e47d5b4b68f642fa3ff092b4b74e561acfabcc
Paco <paco@render.es>
2025-07-19 22:32:21 +0200

    style: clean code

--- core/common/js/common.js
+++ core/common/js/common.js
@@ -777,9 +777,9 @@ const do_delete_self = async function (self) {
 				const item = self.caller.ar_instances[i]
 				if (item.id===self.id) {
 					self.caller.ar_instances.splice(i, 1)
-					if(SHOW_DEBUG===true) {
-						// console.log('))))) deleted caller instance reference:', self.caller.model, self.id);
-					}
+					// if(SHOW_DEBUG===true) {
+					// 	console.log('))))) deleted caller instance reference:', self.caller.model, self.id);
+					// }
 					break;
 				}
 			}
