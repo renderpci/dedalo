9c8a84e8a02ff0fc65bf547233ff839523226226
Paco <paco@render.es>
2025-08-08 19:44:37 +0200

    chore: Improve `v6_to_v7` class.

--- core/base/upgrade/class.v6_to_v7.php
+++ core/base/upgrade/class.v6_to_v7.php
@@ -1,8 +1,5 @@
 <?php
-// declare(strict_types=1);
-
-use SebastianBergmann\Type\Type;
-
+declare(strict_types=1);
 /**
 * CLASS v6_to_v7
 *
@@ -26,20 +23,15 @@ class v6_to_v7 {
 
 
 
-
-
 	/**
-	* reformat_matrix_data
-	* Check all main component data to review if its own dataframe require add section_tipo_key
-	* dataframe in version >=6.4.3 define the section_tipo_key to bind the dataframe data to main data
-	* used in multiple target_section components as Collection (numisdata159) that call People (rsc197) and Entities (rsc106)
+	* REFORMAT_MATRIX_DATA
+	* Converts v6 data to v7 data format
 	* @param array $ar_tables
 	* @return bool
 	*/
 	public static function reformat_matrix_data( array $ar_tables ) : bool {
 
 		// ALTER TABLE "matrix" ADD "data" jsonb NULL;
-
 
 		debug_log(__METHOD__ . PHP_EOL
 			. " ))))))))))))))))))))))))))))))))))))))))))))))))))))))) " . PHP_EOL
@@ -101,105 +93,123 @@ class v6_to_v7 {
 						switch ($datos_key) {
 							case 'relations_search':
 							case 'relations':
-								// add relations
-									$new_matrix_data->{$datos_key} = new stdClass();
+								// add relations property
+								$new_matrix_data->{$datos_key} = new stdClass();
 
 								// update relations array
-									$relations = $datos_value ?? [];
+								$relations = $datos_value ?? [];
 
 								foreach ($relations as $locator) {
 									if(!isset($new_matrix_data->{$datos_key}->{$locator->from_component_tipo})){
 										$new_matrix_data->{$datos_key}->{$locator->from_component_tipo} = [];
 									}
-
 									$new_matrix_data->{$datos_key}->{$locator->from_component_tipo}[] = $locator;
-
 								}
 								break;
 
 							case 'components':
-
-								// add relations
-									$new_matrix_data->literals = new stdClass();
+								// add literals property
+								$new_matrix_data->literals = new stdClass();
 
 								// update components object
-									$literal_components = $datos_value ?? [];
-
+								$literal_components = $datos_value ?? [];
 
 								foreach ($literal_components as $literal_tipo => $literal_value) {
 
-									if( !isset($literal_value->dato) ){
-										debug_log(__METHOD__
-											. " **-------- Literal without information --------** " . PHP_EOL
-											. " component tipo: ". $literal_tipo . PHP_EOL
-											. " section tipo: ". $section_tipo . PHP_EOL
-											. " section id: ". $section_id . PHP_EOL
-											, logger::ERROR
-										); die();
-									}
 									$model = RecordObj_dd::get_modelo_name_by_tipo($literal_tipo);
-									if( $model==='component_filter' ){
+									if( in_array($model, ['component_filter','component_section_id']) ){
 										continue;
 									}
 
+									if( !isset($literal_value->dato) ){
+										debug_log(__METHOD__
+											. " **-------- STOP Literal without information --------** " . PHP_EOL
+											. " model: " . $model. PHP_EOL
+											. " component tipo: ". $literal_tipo . PHP_EOL
+											. " section tipo: ". $section_tipo . PHP_EOL
+											. " section id: ". $section_id . PHP_EOL
+											. " literal_value: ". json_encode( $literal_value ). PHP_EOL
+											. " literal_value type: " . gettype( $literal_value )
+											, logger::ERROR
+										);
+										die();
+									}
+
 									$old_data = $literal_value->dato;
-
-									$new_matrix_data->literals->{$literal_tipo} = [];
-
-
 									foreach ($old_data as $lang => $ar_value) {
+
 										if( !isset($ar_value) || empty($ar_value) ){
 											debug_log(__METHOD__
-												. " **-------- Data without information --------** " . PHP_EOL
+												. " **-------- IGNORED Data without information --------** " . PHP_EOL
+												. " model: " . $model. PHP_EOL
 												. " component tipo: ". $literal_tipo . PHP_EOL
 												. " section tipo: ". $section_tipo . PHP_EOL
 												. " section id: ". $section_id . PHP_EOL
-												. " value: ". to_string( $ar_value ). PHP_EOL
-												. " type: " . gettype( $ar_value )
-												, logger::ERROR
+												. " value: ". json_encode( $ar_value ). PHP_EOL
+												. " value type: " . gettype( $ar_value )
+												, logger::WARNING
 											);
 											continue;
 										}
+
+										// Not array cases
 										if( !is_array($ar_value) ){
+											// ignore old recycled component order data (hierarchy42)
 											if($literal_tipo === 'hierarchy42' && $lang==='lg-nolan'){
 												continue;
 											}
+											// ignore TinyMCE empty data values
 											if($ar_value==='<br data-mce-bogus="1">'){
 												continue;
 											}
+											// ignore not used anymore component_layout dd23
 											if($literal_tipo === 'dd23'){
 												continue;
 											}
 											debug_log(__METHOD__
-												. " <<-------- Data with wrong format is not array -------->> " . PHP_EOL
+												. " <<-------- CHANGED Data with wrong format is not array -------->> " . PHP_EOL
+												. " model: " . $model. PHP_EOL
 												. " component tipo: ". $literal_tipo . PHP_EOL
 												. " section tipo: ". $section_tipo . PHP_EOL
 												. " section id: ". $section_id . PHP_EOL
-												. " value: ". to_string( $ar_value ). PHP_EOL
-												. " type: " . gettype( $ar_value )
-												, logger::ERROR
+												. " value: ". json_encode( $ar_value ). PHP_EOL
+												. " value type: " . gettype( $ar_value )
+												, logger::WARNING
 											);
 											$ar_value = [$ar_value];
 										}
 
 										$new_literal_obj = new stdClass();
 										foreach ($ar_value as $key => $value) {
-											if( !isset($ar_value) ){
+
+											if( !isset($value) ){
 												debug_log(__METHOD__
-													. " <<-------- Data with wrong format -------->> " . PHP_EOL
+													. " <<-------- IGNORED Data with wrong format. -------->> " . PHP_EOL
+													. " model: " . $model. PHP_EOL
 													. " component tipo: ". $literal_tipo . PHP_EOL
 													. " section tipo: ". $section_tipo . PHP_EOL
 													. " section id: ". $section_id . PHP_EOL
-													. " value: ". to_string( $ar_value ). PHP_EOL
-													. " key: ". $key . PHP_EOL
-													, logger::ERROR
+													. " value: ". json_encode( $value ) . PHP_EOL
+													. " value type: " . gettype( $value ) . PHP_EOL
+													. " ar_value: ". json_encode( $ar_value ) . PHP_EOL
+													. " key: ". $key
+													, logger::WARNING
 												);
-												die();
+												continue;
+											}
+
+											if (empty($value)) {
+												continue;
 											}
 
 											$new_literal_obj->key	= $key+1; // add 1 to the array key
 											$new_literal_obj->lang	= $lang;
 											$new_literal_obj->value	= $value;
+
+											// set first time if not already set
+											if (!property_exists($new_matrix_data->literals, $literal_tipo)) {
+												$new_matrix_data->literals->{$literal_tipo} = [];
+											}
 
 											$new_matrix_data->literals->{$literal_tipo}[] = $new_literal_obj;
 										}
@@ -210,7 +220,6 @@ class v6_to_v7 {
 							default:
 								// update other properties like section_tipo, section_real_tipo, etc.
 								$new_matrix_data->{$datos_key} = $datos_value;
-
 								break;
 						}
 					}//end foreach ($datos as $datos_key => $datos_value)
@@ -233,6 +242,7 @@ class v6_to_v7 {
 
 		return true;
 	}//end reformat_matrix_data
+
 
 
 }//end class v6_to_v7
