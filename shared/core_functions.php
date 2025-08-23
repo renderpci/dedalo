d7b00692f2f3bbf939f3c64ce5cc86afc868d3aa
Paco <paco@render.es>
2025-08-23 21:10:27 +0200

    change: Increase debug_backtrace level in `get_backtrace_sequence` core function from 10 to 20 for convenience.

--- shared/core_functions.php
+++ shared/core_functions.php
@@ -2549,7 +2549,7 @@ function get_backtrace_sequence() : array  {
 
 	$bt = debug_backtrace();
 	$seq = [];
-	foreach ([1,2,3,4,5,6,7,8,9,10] as $key) {
+	foreach ([1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20] as $key) {
 
 		$name_function	= $bt[$key]['function'] ?? null;
 		$name_class		= $bt[$key]['class'] ?? null;
