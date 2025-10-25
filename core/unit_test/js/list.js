8e13dda3a8ccaf193b28e8a880baf5daf4cba6e1
devpacdd <paco@render.es>
2025-10-25 11:56:15 +0200

    feat: Add `unit_test` JS `ts_object` test.

--- core/unit_test/js/list.js
+++ core/unit_test/js/list.js
@@ -5,83 +5,82 @@ import {ui} from '../../common/js/ui.js'
 import {elements} from './elements.js'
 
 
-// list
-	export const list_of_test = [
-		'test_key_instances',
-		'test_get_instance',
-		'test_delete_instance',
-		'test_components_lifecycle',
-		'test_others_lifecycle',
-		'test_instances_lifecycle',
-		'test_event_manager',
-		'test_components_data_changes',
-		'test_components_activate',
-		'test_components_render',
-		'test_component_text_area',
-		'test_no_logged_error',
-		'test_unknown_error',
-		'test_page',
-		'test_diffusion',
-		// 'test_component_portal_pagination'
-	]
+// content: (!) Note that content value is automatically set by mocha selecting page HTMLElment #content
+if (typeof content!=='undefined') {
 
-	// content: (!) Note that content value is automatically set by mocha selecting page #content
-	// const content = document.getElementById('content')
-	if (typeof content!=='undefined') {
+	// container
+		const container	= ui.create_dom_element({
+			element_type	: 'div',
+			class_name		: 'list_of_test_container',
+			parent			: content
+		})
 
-		// container
-			const container	= ui.create_dom_element({
-				element_type	: 'div',
-				class_name		: 'list_of_test_container',
-				parent			: content
-			})
-
-		// title Test list
-			ui.create_dom_element({
-				element_type	: 'h1',
-				class_name		: '',
-				inner_html		: 'Test generic list',
-				parent			: container
-			})
-
+	// list : Test generic list (list_of_test)
+		// list. Definition of test to do. Every test has a file as 'test_key_instances' + '.js'
+		 const list_of_test = [
+			'test_key_instances',
+			'test_get_instance',
+			'test_delete_instance',
+			'test_components_lifecycle',
+			'test_others_lifecycle',
+			'test_instances_lifecycle',
+			'test_event_manager',
+			'test_components_data_changes',
+			'test_components_activate',
+			'test_components_render',
+			'test_component_text_area',
+			'test_no_logged_error',
+			'test_unknown_error',
+			'test_page',
+			'test_diffusion',
+			'test_ts_object'
+			// 'test_component_portal_pagination'
+		]
+		ui.create_dom_element({
+			element_type	: 'h1',
+			class_name		: '',
+			inner_html		: 'Test generic list',
+			parent			: container
+		})
 		// links list
-			const list_of_test_length = list_of_test.length
-			for (let i = 0; i < list_of_test_length; i++) {
+		const list_of_test_length = list_of_test.length
+		for (let i = 0; i < list_of_test_length; i++) {
 
-				const test_name = list_of_test[i]
+			const test_name = list_of_test[i]
 
-				// link
-				ui.create_dom_element({
-					element_type	: 'a',
-					href			: `./?area=${test_name}`,
-					class_name		: 'list_of_test_item link',
-					inner_html		: test_name,
-					parent			: container
-				})
-			}
-
-		// title Test list
+			// link
 			ui.create_dom_element({
-				element_type	: 'h1',
-				class_name		: '',
-				inner_html		: 'Test full component list',
+				element_type	: 'a',
+				href			: `./?area=${test_name}`,
+				class_name		: 'list_of_test_item link',
+				inner_html		: test_name,
 				parent			: container
 			})
-			const elements_length = elements.length
-			for (let i = 0; i < elements_length; i++) {
+		}
 
-				const element = elements[i]
+	// list : Test full component list (elements)
+		ui.create_dom_element({
+			element_type	: 'h1',
+			class_name		: '',
+			inner_html		: 'Test full component list',
+			parent			: container
+		})
+		// links list
+		const elements_length = elements.length
+		for (let i = 0; i < elements_length; i++) {
 
-				// link
-				ui.create_dom_element({
-					element_type	: 'a',
-					href			: `./?area=test_component_full&model=${element.model}`,
-					class_name		: 'list_of_test_element link',
-					inner_html		: element.model + ` [${element.tipo}]`,
-					parent			: container
-				})
-			}
-	}//end if (content)
+			const element = elements[i]
+
+			// link
+			ui.create_dom_element({
+				element_type	: 'a',
+				href			: `./?area=test_component_full&model=${element.model}`,
+				class_name		: 'list_of_test_element link',
+				inner_html		: element.model + ` [${element.tipo}]`,
+				parent			: container
+			})
+		}
+}//end if (content)
 
 
 
