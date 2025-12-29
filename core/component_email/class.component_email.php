74aab2fb3daaa0ca96c2b9faa77379a6e7daaa6d
devpacdd <paco@render.es>
2025-12-29 22:19:11 +0100

    refactor: Refactor `component_email` for version 7. Add samples of data, api_data, context.

--- core/component_email/class.component_email.php
+++ core/component_email/class.component_email.php
@@ -1,7 +1,7 @@
 <?php declare(strict_types=1);
 /**
 * CLASS COMPONENT EMAIL
-*
+* Manages e-mail addresses data.
 *
 */
 class component_email extends component_string_common {
@@ -22,67 +22,84 @@ class component_email extends component_string_common {
 
 
 	/**
-	* SET_DATO
-	* @param array|null $dato
+	* SET_DATA
+	* @param array|null $data
 	* @return bool
 	*/
-	public function set_dato($dato) : bool {
+	public function set_data( ?array $data ) : bool {
 
-		if (empty($dato)) {
+		if (empty($data)) {
 
 			// null case
 
-			$dato = null;
+			$data = null;
 
 		}else{
 
 			// array case
 
-			$safe_dato = [];
-			foreach ((array)$dato as $value) {
-				$safe_dato[] = empty($value)
-					? null
-					: component_email::clean_email($value);
+			$safe_data = [];
+			foreach ($data as $data_item) {
+
+				if( is_object($data_item) ) {
+					$data_item->value = component_email::clean_email($data_item->value);
+				}else{
+					if( empty($data_item) ) {
+						$data_item = null;
+					}else{
+						// wrong data item format
+						debug_log(__METHOD__
+							. " Error. Wrong data item format "
+							. ' data_item:' . to_string($data_item)
+							, logger::ERROR
+						);
+						continue;
+					}
+				}
+				$safe_data[] = $data_item;
 			}
-			$dato = $safe_dato;
+			// Replace data with safe data
+			$data = $safe_data;
 		}
 
 
-		return parent::set_dato( $dato );
-	}//end set_dato
+		return parent::set_data( $data );
+	}//end set_data
 
 
 
 	/**
 	* SAVE OVERRIDE
-	* Overwrite component_common method to set always lang to config:DEDALO_DATA_NOLAN before save
+	* Overwrite component_common method to check data before save:
+	* E-mail format validation.
 	* @return bool
 	*/
 	public function save() : bool {
 
 		// Optionally, the data could be validated here... although it has already been done in javascript
-			$email = $this->get_dato();
-			foreach ((array)$email as $value) {
-				if (!empty($value) && false===component_email::is_valid_email($value)) {
+		$data = $this->get_data();
+		if (!empty($data)) {
+			foreach ($data as $data_item) {
+				if (!empty($data_item->value) && !component_email::is_valid_email($data_item->value)) {
 					debug_log(__METHOD__
-						. " No data is saved. Invalid email "
-						. ' value:' . to_string($value)
+						. " Data is NOT saved. Invalid email !"
+						. ' value:' . to_string($data_item->value)
 						, logger::ERROR
 					);
 					return false;
 				}
 			}
+		}
 
-		// from here, we save as standard
-			$result = parent::save();
-
-		return $result;
+		return parent::save();
 	}//end save
 
 
 
 	/**
 	* IS_VALID_EMAIL
+	* Validate email format
+	* @param string $email
 	* @return bool
 	*/
 	public static function is_valid_email( string $email ) : bool {
@@ -95,9 +112,11 @@ class component_email extends component_string_common {
 
 	/**
 	* CLEAN_EMAIL
-	* @return string|null $email
+	* Clean email from special characters
+	* @param ?string $email
+	* @return ?string $email
 	*/
-	public static function clean_email(string $email) : string {
+	public static function clean_email(?string $email) : ?string {
 
 		if (!empty($email)) {
 			$email = trim(
@@ -107,24 +126,6 @@ class component_email extends component_string_common {
 
 		return $email;
 	}//end clean_email
-
-
-
-	/**
-	* GET_DIFFUSION_VALUE
-	* Overwrite component_common method
-	* @param string|null $lang = null
-	* @param object|null $option_obj = null
-	* @return string|null $diffusion_value
-	*
-	* @see class.diffusion_mysql.php
-	*/
-	public function get_diffusion_value(?string $lang=null, ?object $option_obj=null) : ?string {
-
-		return $this->get_value(
-			($lang ?? DEDALO_DATA_LANG)
-		);
-	}//end get_diffusion_value
 
 
 
@@ -151,9 +152,9 @@ class component_email extends component_string_common {
 			if(json_handler::is_json($import_value)){
 
 				// try to JSON decode (null on not decode)
-				$dato_from_json	= json_handler::decode($import_value); // , false, 512, JSON_INVALID_UTF8_SUBSTITUTE
+				$data_from_json	= json_handler::decode($import_value); // , false, 512, JSON_INVALID_UTF8_SUBSTITUTE
 
-				$response->result	= $dato_from_json;
+				$response->result	= $data_from_json;
 				$response->msg		= 'OK';
 
 				return $response;
