6b7d542d7624f40186f7edcee5780b3fd497f7e1
Alejandro Peña Carbonell <alex@render.es>
2025-12-29 19:13:45 +0100

    remove: delete unused diffusion methods from component_select
    Removed the get_diffusion_value and get_diffusion_dato methods that were overriding parent class implementations and are no longer needed. These methods were redundant and duplicated functionality from the parent component_relation_common class. The component now relies on the inherited diffusion handling while maintaining its sortable functionality.

--- core/component_select/class.component_select.php
+++ core/component_select/class.component_select.php
@@ -15,59 +15,6 @@ class component_select extends component_relation_common {
 	public $test_equal_properties = ['section_tipo','section_id','type','from_component_tipo'];
 
 
-
-	/**
-	* GET_DIFFUSION_VALUE
-	* Overwrite component common method
-	* Calculate current component diffusion value for target field (usually a MySQL field)
-	* Used for diffusion_mysql to unify components diffusion value call
-	* @see class.diffusion_mysql.php
-	* @param string|null $lang = null
-	* @param object|null $option_obj = null
-	* @return string|null $diffusion_value
-	*/
-	public function get_diffusion_value( ?string $lang=null, ?object $option_obj=null ) : ?string {
-
-		$diffusion_lang = $lang ?? DEDALO_DATA_LANG;
-		$diffusion_value = $this->get_valor(
-			$diffusion_lang
-		);
-
-		$diffusion_value = !empty($diffusion_value)
-			? strip_tags($diffusion_value)
-			: null;
-
-		return $diffusion_value;
-	}//end get_diffusion_value
-
-
-
-	/**
-	* GET_DIFFUSION_DATO
-	* @return string|null $diffusion_value
-	*/
-	public function get_diffusion_dato() : ?string {
-
-		$dato = $this->get_dato();
-
-		if (is_array($dato)) {
-
-			$final_dato = [];
-			foreach ($dato as $current_locator) {
-				$final_dato[] = $current_locator->section_id;
-			}
-			$diffusion_value = json_encode($final_dato);
-
-		}else{
-
-			$diffusion_value = null;
-		}
-
-		return $diffusion_value;
-	}//end get_diffusion_dato
-
-
-
 	/**
 	* GET_SORTABLE
 	* @return bool true
