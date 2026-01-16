0b36b0c22335945873050b1f1d51a2a1aed3bd09
devpacdd <paco@render.es>
2026-01-16 10:27:22 +0100

    style: clean code

--- test/server/common/event_manager_class_Test.php
+++ test/server/common/event_manager_class_Test.php
@@ -1,12 +1,10 @@
 <?php declare(strict_types=1);
-// PHPUnit classes
-use PHPUnit\Framework\TestCase;
 // bootstrap
 require_once dirname(dirname(__FILE__)) . '/bootstrap.php';
 
 
 
-final class event_manager_class_Test extends TestCase {
+final class event_manager_class_Test extends BaseTestCase {
 
 
 
