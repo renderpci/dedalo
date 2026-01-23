0bf5c27fa2f8a85133d1f0817886759e679b71ec
devpacdd <paco@render.es>
2026-01-23 12:00:22 +0100

    feat: Added search capability to `component_geolocation` for convenience.

--- core/component_geolocation/js/render_search_component_geolocation.js
+++ core/component_geolocation/js/render_search_component_geolocation.js
@@ -8,10 +8,7 @@
 
 // imports
 	import {ui} from '../../common/js/ui.js'
-	import {
-		get_content_data
-	}
-	from './view_default_edit_geolocation.js'
+	// import {get_content_data} from './view_default_edit_geolocation.js'
 
 
 
@@ -54,6 +51,80 @@ render_search_component_geolocation.prototype.search = async function(options) {
 
 	return wrapper
 }//end search
+
+
+
+/**
+* GET_CONTENT_DATA
+* @return HTMLElement content_data
+*/
+const get_content_data = function(self) {
+
+	const value	= self.data.value
+
+	// content_data
+		const content_data = ui.component.build_content_data(self)
+
+	// values (inputs)
+		const inputs_value	= value || []
+		const value_length	= inputs_value.length || 1
+		for (let i = 0; i < value_length; i++) {
+			const content_value = get_content_value(i, inputs_value[i], self)
+			content_data.appendChild(content_value)
+			// set pointers
+			content_data[i] = content_value
+			break; // only one is used for the time being
+		}
+
+
+	return content_data
+}//end get_content_data
+
+
+
+/**
+* GET_CONTENT_VALUE
+* @return DOM element input
+*/
+const get_content_value = (i, current_value, self) => {
+
+	// content_value
+		const content_value = ui.create_dom_element({
+			element_type	: 'div',
+			class_name		: 'content_value'
+		})
+
+	// input field
+		const input = ui.create_dom_element({
+			element_type	: 'input',
+			type			: 'text',
+			class_name		: 'input_value',
+			value			: current_value,
+			parent			: content_value
+		})
+		input.addEventListener('change', function() {
+
+			// safe_value
+				const safe_value = (this.value.length>0) ? this.value : null
+
+			// changed_data
+				const changed_data_item = Object.freeze({
+					action	: 'update',
+					key		: i,
+					value	: safe_value
+				})
+
+			// update the instance data (previous to save)
+				self.update_data_value(changed_data_item)
+			// set data.changed_data. The change_data to the instance
+				// self.data.changed_data = changed_data
+			// publish search. Event to update the dom elements of the instance
+				event_manager.publish('change_search_element', self)
+		})//end event change
+
+
+	return content_value
+}//end get_content_value
 
 
 
