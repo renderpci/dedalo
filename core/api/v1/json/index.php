333868a174b9daa9add2fc6dbc118ef2bc42ee7f
devpacdd <paco@render.es>
2026-01-26 13:59:12 +0100

    fix: Fix incorrect API entry pint debug `total_time`.

--- core/api/v1/json/index.php
+++ core/api/v1/json/index.php
@@ -337,7 +337,7 @@ if (SHOW_DEBUG && defined('SHOW_DEBUG_PROFILER') && SHOW_DEBUG_PROFILER) {
 
 	// log real execution time
 	$id	= $rqo->id ?? $rqo->source->tipo ?? '';
-	$text = 'API REQUEST ' . $rqo->action . ' (' . $id . ') END IN ' . $total_time_api_exec .' - ' . dd_memory_usage();
+	$text = 'API REQUEST ' . $rqo->action . ' (' . $id . ') END IN ' . $total_time .' - ' . dd_memory_usage();
 	$line = 'API END POINT FINISHED: ' . PHP_EOL . $text . PHP_EOL;
 	debug_log($line, logger::DEBUG);
 }
