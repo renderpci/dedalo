95e8f3af102f5924cb303d78a71fba0af8752e65
devpacdd <paco@render.es>
2026-02-18 09:18:29 +0100

    refactor(tools): optimize `hydrate_tools_info` and enable metadata caching

--- core/component_check_box/class.component_check_box.php
+++ core/component_check_box/class.component_check_box.php
@@ -31,49 +31,13 @@ class component_check_box extends component_relation_common {
 		// Execute get_ar_list_of_values
 		$ar_list_of_values_response = component_common::get_ar_list_of_values($lang, false);
 
-		// Add tool information when the component is component_security_tools (dd1353)
-		// the component_security_tools is built as component_check_box and rendered as 'view_tools'
-		// this information is required to get specific tool information
+		$datalist = $ar_list_of_values_response->result ?? [];
+
+		// Add tool information when the component is component_security_tools (dd1353).
+		// The component_security_tools is built as component_check_box and rendered with view 'view_tools'.
+		// This information is required to get specific tool information.
 		if($this->tipo===DEDALO_COMPONENT_SECURITY_TOOLS_PROFILES_TIPO) {
-
-			$tipo_tool_name = tools_register::$tipo_tool_name;
-			$model_tool_name = ontology_node::get_model_by_tipo($tipo_tool_name, true);
-
-			$tipo_always_active = tools_register::$tipo_always_active;
-			$model_always_active = ontology_node::get_model_by_tipo($tipo_always_active, true);
-
-			$datalist = [];
-			$list = $ar_list_of_values_response->result ?? [];
-			foreach ($list as $key => $item) {
-
-				// name
-				$component_tool_name  = component_common::get_instance(
-					$model_tool_name,
-					$tipo_tool_name,
-					$item->value->section_id,
-					'list',
-					$lang,
-					$item->value->section_tipo
-				);
-				$item->tool_name = $component_tool_name->get_data()[0]->value ?? '';
-
-				// always_active
-				$component_always_active  = component_common::get_instance(
-					$model_always_active,
-					$tipo_always_active,
-					$item->value->section_id,
-					'list',
-					$lang,
-					$item->value->section_tipo
-				);
-				$item->always_active = $component_always_active->get_data()[0]->section_id ?? false;
-
-				$datalist[] = $item;
-			}
-
-		}else{
-
-			$datalist = $ar_list_of_values_response->result;
+			$datalist = tool_common::hydrate_tools_info($datalist, $lang);
 		}
 
 
