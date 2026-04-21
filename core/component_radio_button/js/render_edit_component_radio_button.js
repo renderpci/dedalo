f0cfb927115a1db61ace3e4b69363dca18ab463f
devpacdd <paco@render.es>
2026-04-21 09:52:12 +0200

    fix(component_radio_button): prevent undefined access to entries[0].id

--- core/component_radio_button/js/render_edit_component_radio_button.js
+++ core/component_radio_button/js/render_edit_component_radio_button.js
@@ -166,7 +166,7 @@ const get_content_value = (i, datalist_item, self) => {
 			// therefore, a new id will be generated on save.
 			const changed_data = [Object.freeze({
 				action	: 'update',
-				id		: entries[0].id,
+				id		: entries[0] ? entries[0].id : null,
 				value	: datalist_value
 			})]
 			self.change_value({
