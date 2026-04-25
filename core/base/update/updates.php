f9e8526f72391436b9aa13fa8140d6da0c8ac694
Alejandro Peña Carbonell <alex@render.es>
2026-04-25 13:01:10 +0200

    ci: update to version 6.8.9

--- core/base/update/updates.php
+++ core/base/update/updates.php
@@ -33,6 +33,25 @@
 global $updates;
 $updates = new stdClass();
 
+$v=68; #####################################################################################
+$updates->$v = new stdClass();
+
+	# UPDATE TO
+	$updates->$v->version_major			= 6;
+	$updates->$v->version_medium		= 8;
+	$updates->$v->version_minor			= 9;
+
+	# MINIMUM UPDATE FROM
+	$updates->$v->update_from_major		= 6;
+	$updates->$v->update_from_medium	= 8;
+	$updates->$v->update_from_minor		= 8;
+
+	// Re-index and vacuum tables
+		$updates->$v->SQL_update[] = PHP_EOL.sanitize_query('
+			VACUUM FULL VERBOSE ANALYZE public.matrix_ontology;
+		');
+		
+
 $v=688; #####################################################################################
 $updates->$v = new stdClass();
 
