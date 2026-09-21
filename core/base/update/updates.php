d50ec90c60bceaf6773911ddd8345705242ae14d
Alejandro Peña Carbonell <alex@dedalo.dev>
2026-09-21 10:56:05 +0200

    chore: update to version 6.9.6 with database vacuum optimization
    Updated Dédalo version from 6.9.5 to 6.9.6 and added database maintenance SQL statement to vacuum and analyze the jer_dd table during update process. Also updated build timestamp to reflect current deployment time.

--- core/base/update/updates.php
+++ core/base/update/updates.php
@@ -33,6 +33,23 @@
 global $updates;
 $updates = new stdClass();
 
+$v=696; #####################################################################################
+$updates->$v = new stdClass();
+
+	# UPDATE TO
+	$updates->$v->version_major			= 6;
+	$updates->$v->version_medium		= 9;
+	$updates->$v->version_minor			= 6;
+
+	# MINIMUM UPDATE FROM
+	$updates->$v->update_from_major		= 6;
+	$updates->$v->update_from_medium	= 9;
+	$updates->$v->update_from_minor		= 5;
+
+	// Re-index and vacuum tables
+		$updates->$v->SQL_update[] = PHP_EOL.sanitize_query('
+			VACUUM FULL VERBOSE ANALYZE public.jer_dd;
+		');
 
 $v=695; #####################################################################################
 $updates->$v = new stdClass();
